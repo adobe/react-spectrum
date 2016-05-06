0356a44bb0ae502d303f3bf52a6f36be6d2b9132
Aaron Hardy <aahardy@adobe.com>
2016-05-06 17:03:35 -0600

    Added accordion without tests.

---
+++ components/utils/createID.js
@@ -0,0 +1,8 @@
+export default () => {
+  // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/2117523#2117523
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    var r = Math.random() * 16 | 0;
+    var v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
