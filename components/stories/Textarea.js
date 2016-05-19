e3c4a6d1e0ed5d68992bf766b7def6ef416da989
Nate Ross <nross@adobe.com>
2016-05-19 17:08:17 -0600

    Adding Textarea component, stories, and tests.

---
+++ components/stories/Textarea.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+import Textarea from '../Textarea';
+
+storiesOf('Textarea', module)
+  .add('Default', () => render() )
+  .add('quiet: true', () => render({ quiet: true }) )
+  .add('disabled: true', () => render({ disabled: true }) )
+  .add('invalid: true', () => render({ invalid: true }) )
+  .add('readOnly: true', () => render({ readOnly: true }) )
+  .add('required: true', () => render({ required: true }) )
+
+function render(props = {}) {
+  return (
+    <Textarea
+      placeholder="React"
+      onChange={ action('change') }
+      onFocus={ action('focus') }
+      onBlur={ action('blur') }
+      { ...props }
+    />
+  );
+}
