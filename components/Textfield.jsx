8bf43b627452836b7d1723380957853beb9b4160
Nate Ross <nross@adobe.com>
2016-05-19 16:28:24 -0600

    Adding Textfield component, stories, and tests.

---
+++ components/Textfield.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default ({
+  className,
+  quiet,
+  disabled = false,
+  required = false,
+  invalid = false,
+  readOnly = false,
+  ...rest
+}) => {
+  return (
+    <input
+      className={
+        classNames(
+          'coral-Textfield',
+          {
+            'is-invalid': invalid,
+            'coral-Textfield--quiet': quiet
+          },
+          className
+        )
+      }
+      aria-disabled={ disabled }
+      aria-required={ required }
+      aria-invalid={ invalid }
+      aria-readonly={ readOnly }
+      disabled={ disabled }
+      required={ required }
+      invalid={ invalid }
+      readonly={ readOnly }
+      { ...rest }
+    />
+  );
+}
