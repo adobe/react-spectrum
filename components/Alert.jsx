ef28d4ab9b00b754a1af0cffe2bd0537a22f0560
Nate Ross <nross@adobe.com>
2016-05-20 11:25:02 -0600

    Adding Alert component, stories, and tests.

---
+++ components/Alert.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import Icon from './Icon';
+import Button from './Button';
+import { getVariantIcon } from '../helpers/icon-variant';
+
+import './Alert.styl';
+
+export default ({
+  header,
+  children,
+  closable = false,
+  variant = 'info', // info, help, success, error, warning
+  large = false,
+  className,
+  onClose = () => {},
+  ...otherProps
+}) => (
+  <div
+    className={
+      classNames(
+        'coral3-Alert',
+        `coral3-Alert--${ variant }`,
+        `coral3-Alert--${ large ? 'large' : 'small' }`,
+        className
+      )
+    }
+    { ...otherProps }
+  >
+    <Icon
+      className="coral3-Alert-typeIcon"
+      icon={ getVariantIcon(variant) }
+      size="XS"
+    />
+    {
+      closable &&
+      <Button
+      className="coral3-Alert-closeButton u-coral-pullRight"
+      variant="minimal"
+      square
+      size="M"
+      icon="close"
+      iconSize="XS"
+      onClick={ onClose }
+      />
+    }
+    <div className="coral3-Alert-header">{ header }</div>
+    <div className="coral3-Alert-content">{ children }</div>
+  </div>
+)
