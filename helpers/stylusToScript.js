6e662aa34fb0bbdf91e639cd073ec75c3be47374
Jeff Hicken <hicken@adobe.com>
2016-06-16 16:51:57 -0600

    fixing styles in build

---
+++ helpers/stylusToScript.js
@@ -0,0 +1,42 @@
+/*eslint-disable*/
+
+'use strict';
+
+var stylus = require('stylus')
+var path = require('path');
+var dirname = path.dirname;
+var isAbsolute = path.isAbsolute;
+var resolve = path.resolve;
+
+var fs = require('fs');
+var readFileSync = fs.readFileSync;
+
+
+module.exports = function (babel) {
+  var t = babel.types;
+  function resolveModulePath(filename) {
+    var dir = dirname(filename);
+    if (isAbsolute(dir)) {
+      return dir;
+    }
+    if (process.env.PWD) {
+      return resolve(process.env.PWD, dir);
+    }
+    return resolve(dir);
+  }
+  return {
+    visitor: {
+      ImportDeclaration: function(path, options) {
+        var fileName = path.node.source.value;
+        var filePath = resolve(resolveModulePath(options.file.opts.filename), fileName);
+        if (fileName.match(/^.*\.(styl)$/)) {
+          var content = fs.readFileSync(filePath, 'utf8'),
+            css = stylus(content, {compress: true}).render()
+            path.replaceWithSourceString(
+              'require(\'./buildGlobalScript.js\').default("' + encodeURI(css) + '")'
+            );
+        }
+      }
+    }
+  };
+}
