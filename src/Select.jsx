767cdbf164287f8e08269df0898fe4edf21e4918
Serban Stancu <stancu@adobe.com>
2016-06-27 14:04:06 -0600

    Take into consideration multi flag.

--- src/Select.jsx
+++ src/Select.jsx
@@ -81,7 +81,9 @@ export default class Select extends Component {
           'Select-option': 'coral3-SelectList-item',
           'Select-values': 'coral-TagList coral-Autocomplete-tagList'
         } }
-        valueComponent={ this.props.multiple ? this.valuesComponent : this.valueComponent }
+        valueComponent={
+          (this.props.multiple || this.props.multi) ? this.valuesComponent : this.valueComponent
+        }
         clearable={ false }
         autosize={ false }
         searchable={ false }
