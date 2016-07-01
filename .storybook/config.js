de37ce3331b02cbe88dcd3a6232c7a22de96e143
Tony Thomson <thomson@me.com>
2016-07-01 14:28:43 -0700

    Add 'coral--light' class to storybook iframe body

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,10 +3,13 @@ import './storybook.styl';
 
 import '../css/coral.css';
 
+let iframeBodyClass = 'coral--light';
+
 function loadStories() {
   var storiesContext = require.context('../src/stories', true, /^(.*\.(js|jsx))$/);
   storiesContext.keys().forEach(storiesContext);
   //require('../src/stories/');
 }
 
+document.getElementsByTagName('body')[0].classList.add(iframeBodyClass);
 configure(loadStories, module);
