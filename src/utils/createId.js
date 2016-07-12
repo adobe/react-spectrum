28a35f79bb9144eff5b40856ccd95151c8c86a53
Aaron Hardy <aahardy@adobe.com>
2016-07-12 12:43:39 -0600

    Simplifying createId.

--- src/utils/createId.js
+++ src/utils/createId.js
@@ -1,8 +1,4 @@
-// http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/2117523#2117523
-export default () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-};
+const PREFIX = 'react-coral-';
+var incrementor = 0;
+
+export default () => PREFIX + ++incrementor;
