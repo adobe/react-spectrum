bb3acba051d337db28676dcc070c0fcf422201c6
Aaron Hardy <aahardy@adobe.com>
2016-08-11 16:21:11 -0600

    Fixes #137. Making selectedIndex work with string value and zero.

--- test/accordion-test.js
+++ test/accordion-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import expect from 'expect';
 import Accordion from '../src/Accordion';
+import AccordionItem from '../src/AccordionItem';
 import { shallow } from 'enzyme';
 
 describe('Accordion', () => {
@@ -23,5 +24,59 @@ describe('Accordion', () => {
     const wrapper = tree.find('.coral3-Accordion');
 
     expect(wrapper.hasClass('coral3-Accordion--large')).toBe(true);
+  });
+
+  describe('selectedKey', () => {
+    const renderAccordionWithSelectedIndex = index => shallow(
+      <Accordion selectedIndex={ index }>
+        <AccordionItem header="One" className="one">
+          One content.
+        </AccordionItem>
+        <AccordionItem header="Two" className="two">
+          Two content.
+        </AccordionItem>
+      </Accordion>
+    );
+
+    const assertChildWithClassNameSelected = (tree, className) => {
+      const child = tree.find('[selected=true]');
+      expect(child.length).toBe(1);
+      expect(child.node.props.className).toBe(className);
+    };
+
+    it('supports string index', () => {
+      const tree = renderAccordionWithSelectedIndex('1');
+      assertChildWithClassNameSelected(tree, 'two');
+    });
+
+    it('supports integer index', () => {
+      const tree = renderAccordionWithSelectedIndex(1);
+      assertChildWithClassNameSelected(tree, 'two');
+    });
+
+    // Issue #137
+    it('supports integer index of 0', () => {
+      const tree = renderAccordionWithSelectedIndex(0);
+      assertChildWithClassNameSelected(tree, 'one');
+    });
+  });
+
+
+  it('supports defaultSelectedIndex', () => {
+    const tree = shallow(
+      <Accordion defaultSelectedIndex={ 1 }>
+        <AccordionItem header="One" className="one">
+          One content.
+        </AccordionItem>
+        <AccordionItem header="Two" className="two">
+          Two content.
+        </AccordionItem>
+      </Accordion>
+    );
+
+    const child = tree.find('[selected=true]');
+
+    expect(child.length).toBe(1);
+    expect(child.node.props.className).toBe('two');
   });
 });
