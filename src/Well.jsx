bb7ad5e0cd23fbb6a55e89feb85378149255cf26
mohicken <mhicken@adobe.com>
2016-08-24 14:09:42 -0600

    Added Well component, story, and test

---
+++ src/Well.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function Well({
+  children,
+  className,
+  ...otherProps
+}) {
+  return (
+    <div
+      className={
+        classNames(
+          'coral-Well',
+          className
+        )
+      }
+      { ...otherProps }
+    >
+      { children }
+    </div>
+  );
+}
