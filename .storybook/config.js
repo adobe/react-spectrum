e58fc6b63782af37d8e7b23447094e792a162d1a
Devon Govett <devongovett@gmail.com>
2017-04-20 10:30:58 -0700

    Add page js to setup typekit and coral theme defaults

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,17 +4,13 @@ import infoAddon from '@kadira/react-storybook-addon-info';
 import configureTypekit from '../src/utils/configureTypekit';
 
 import './storybook.styl';
-import '../src/page/index.styl';
+import '../src/page';
 
 setAddon(infoAddon);
-configureTypekit('ruf7eed');
-
-let iframeBodyClass = 'coral--light';
 
 function loadStories() {
   var storiesContext = require.context('../stories', true, /^(.*\.(js|jsx))$/);
   storiesContext.keys().forEach(storiesContext);
 }
 
-document.getElementsByTagName('body')[0].classList.add(iframeBodyClass);
 configure(loadStories, module);
