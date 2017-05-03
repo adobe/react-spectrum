5369be8a0caeba30c8a1eea6d6e86ef2141b0f88
Devon Govett <devongovett@gmail.com>
2017-05-03 13:47:22 -0700

    Add Rating component
    Copied from react-components and spectrumified.

---
+++ stories/Rating.js
@@ -0,0 +1,39 @@
+import {action, storiesOf} from '@kadira/storybook';
+import React from 'react';
+import Rating from '../src/Rating';
+import {VerticalCenter} from '../.storybook/layout';
+
+storiesOf('Rating', module)
+  .addDecorator(story => (
+    <VerticalCenter style={ {textAlign: 'left', margin: '0 100px 50px', position: 'static', transform: 'none'} }>
+      { story() }
+    </VerticalCenter>
+  ))
+  .addWithInfo(
+    'Default',
+    () => (
+      <Rating onChange={action('change')} />
+    ),
+    {inline: true}
+  )
+  .addWithInfo(
+    'Disabled',
+    () => (
+      <Rating disabled value={3} onChange={action('change')} />
+    ),
+    {inline: true}
+  )
+  .addWithInfo(
+    'Read Only',
+    () => (
+      <Rating readOnly value={3} onChange={action('change')} />
+    ),
+    {inline: true}
+  )
+  .addWithInfo(
+    'Controlled',
+    () => (
+      <Rating value={3} onChange={action('change')} />
+    ),
+    {inline: true}
+  );
