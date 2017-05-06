7d7a8f2af3b5ceb52f7d67e16c86165516adb0bd
dannify <drobinson@livefyre.com>
2017-05-05 17:32:15 -0700

    overlaytrigger stories

--- stories/OverlayTrigger.js
+++ stories/OverlayTrigger.js
@@ -1,28 +1,56 @@
 import Button from '../src/Button';
 import OverlayTrigger from '../src/OverlayTrigger';
-// import Popover from '../src/Popover';
+import Popover from '../src/Popover';
 import React from 'react';
 import {storiesOf} from '@kadira/storybook';
 import Tooltip from '../src/Tooltip';
+import {VerticalCenter} from '../.storybook/layout';
 
 
 storiesOf('OverlayTrigger', module)
+  .addDecorator(story => (
+    <VerticalCenter style={ {textAlign: 'left', margin: '0 100px 50px', position: 'static', transform: 'none'} }>
+      {story()}
+    </VerticalCenter>
+  ))
   .addWithInfo(
-    'with OverlayTrigger: hover',
-    () => render('This is a tooltip.', {trigger: 'click'}),
+    'with trigger: hover',
+    () => render('popover', {trigger: 'hover', placement: 'right'}),
     {inline: true}
   )
   .addWithInfo(
-    'with OverlayTrigger: hover',
-    () => render('This is a tooltip.', {trigger: 'click'}),
+    'with trigger: click',
+    () => render('popover',{trigger: 'click', placement: 'right'}),
+    {inline: true}
+  )
+  .addWithInfo(
+    'with tooltip',
+    () => render('tooltip', {trigger: 'click', placement: 'right'}),
+    {inline: true}
+  )
+  .addWithInfo(
+    'with overlay',
+    () => render('popover', {trigger: 'click', placement: 'bottom', variant: 'warning'}),
     {inline: true}
   );
 
-const render = (props = {}) => (
-  <div style={{paddingLeft: '200px'}}> 
-    <OverlayTrigger trigger="click" placement="bottom">
+function render(type, props = {}) {
+  if (type === 'popover') {
+    return (
+      <OverlayTrigger {...props}>
+        <Button label="Click Me" variant="primary" />
+        <Popover
+          open
+          title="Popover title">
+            Popover content goes here...
+        </Popover>
+      </OverlayTrigger>
+    );
+  }
+  return (
+    <OverlayTrigger {...props}>
       <Button label="Click Me" variant="primary" />
       <Tooltip open>Notes from a tooltip</Tooltip>
     </OverlayTrigger>
-  </div>
-);
+  );
+};
