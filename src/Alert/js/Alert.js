3aa36852e08865b112aa955c273db38e8d2e388d
Devon Govett <devongovett@gmail.com>
2017-07-23 18:26:42 -0700

    Convert Alert to spectrum-css
    Removes closable and large since spectrum doesn’t seem to support those.

--- src/Alert/js/Alert.js
+++ src/Alert/js/Alert.js
@@ -1,16 +1,12 @@
 import Button from '../../Button';
 import classNames from 'classnames';
-import {getVariantIcon} from '../../utils/icon-variant';
-import Icon from '../../Icon';
 import React from 'react';
 import '../style/index.styl';
 
 export default function Alert({
   header,
   children,
-  closable = false,
   variant = 'info', // info, help, success, error, warning
-  large = false,
   className,
   onClose = function () {},
   ...otherProps
@@ -19,33 +15,16 @@ export default function Alert({
     <div
       className={
         classNames(
-          'coral3-Alert',
-          `coral3-Alert--${variant}`,
-          `coral3-Alert--${large ? 'large' : 'small' }`,
+          'spectrum-Alert',
+          `spectrum-Alert--${variant}`,
           className
         )
       }
       {...otherProps}
     >
-      <Icon
-        className="coral3-Alert-typeIcon"
-        icon={getVariantIcon(variant)}
-        size="XS"
-      />
-      {
-        closable &&
-          <Button
-            className="coral3-Alert-closeButton u-coral-pullRight"
-            variant="minimal"
-            square
-            size="M"
-            icon="close"
-            iconSize="XS"
-            onClick={onClose}
-          />
-      }
-      <div className="coral3-Alert-header">{header}</div>
-      <div className="coral3-Alert-content">{children}</div>
+      <div className="spectrum-Alert-typeIcon" role="img" aria-label="info" />
+      <div className="spectrum-Alert-header">{header}</div>
+      <div className="spectrum-Alert-content">{children}</div>
     </div>
   );
 }
