5b0d60fedf3077547558429b64f4616f6585d0fb
Devon Govett <devongovett@gmail.com>
2017-07-24 15:45:14 -0700

    Convert Breadcrumb to spectrum-css

--- src/Breadcrumbs/js/Breadcrumbs.js
+++ src/Breadcrumbs/js/Breadcrumbs.js
@@ -1,40 +1,28 @@
-import classNames from 'classnames';
 import Icon from '../../Icon';
 import React from 'react';
 import '../style/index.styl';
-
-const ICON_SIZE = {
-  S: 'S',
-  M: 'S',
-  L: 'M'
-};
 
 export default class Breadcrumbs extends React.Component {
   static defaultProps = {
     items: [],
     icon: null,
-    disabled: false,
-    size: 'S',
     onBreadcrumbClick: function () {}
   };
 
   render() {
-    const {items, icon, disabled, onBreadcrumbClick, size} = this.props;
+    const {items, icon, onBreadcrumbClick} = this.props;
 
     return (
-      <div className={classNames('coral-Breadcrumbs', 'coral-Breadcrumbs--' + size, {'is-disabled': disabled})}>
+      <div className="spectrum-Breadcrumbs">
         {icon &&
-          <Icon icon={icon} size={ICON_SIZE[size]} />
+          <Icon icon={icon} size="S" />
         }
 
         {items.map((item, i) =>
           <span
             key={i}
-            className="coral-Breadcrumb"
-            onClick={!disabled && items.length > 1 && i < items.length - 1 && onBreadcrumbClick.bind(null, item, items.length - i - 1)}>
-              {i > 0 &&
-                <Icon icon="chevronRight" size="XS" />
-              }
+            className="spectrum-Breadcrumb"
+            onClick={items.length > 1 && i < items.length - 1 && onBreadcrumbClick.bind(null, item, items.length - i - 1)}>
               {item.label}
           </span>
         )}
