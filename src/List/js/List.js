9bbf6ba0e1b2c7580f21cfb836ac7e9a8dfe04d5
Devon Govett <govett@adobe.com>
2017-09-11 11:30:56 -0700

    Revert "Make it so List has its onSelect function called when a list item is selected" (#50)
    * Revert "Added type="button" to Tag when closable (#49)"
    
    This reverts commit 62bfabee5088d76a4689519d56bf432dfcc1890f.
    
    * Revert "1.4.0"
    
    This reverts commit dcc3f5035735b6541a2428eb98b246c7671f8e51.
    
    * Revert "Fix styling of tagField input (#46)"
    
    This reverts commit 4c14b53d626771ce3407a4e94fcfb93cffb5a3ae.
    
    * Revert "Make it so List has its onSelect function called when a list item is selected (#45)"
    
    This reverts commit 85a3146793754b4c4f15732342847adec046f66f.

--- src/List/js/List.js
+++ src/List/js/List.js
@@ -6,10 +6,6 @@ const LIST_ITEM_SELECTOR = '.coral-BasicList-item:not(.is-disabled)';
 const SELECTED_LIST_ITEM_SELECTOR = LIST_ITEM_SELECTOR + '.is-selected';
 
 export default class List extends Component {
-  static defaultProps = {
-    onSelect: () => {},
-  };
-
   getItems() {
     return Array.from(this.list.querySelectorAll(LIST_ITEM_SELECTOR));
   }
@@ -56,7 +52,6 @@ export default class List extends Component {
     const {
       className,
       children,
-      onSelect,
       role = 'listbox',
       ...otherProps
     } = this.props;
@@ -74,7 +69,6 @@ export default class List extends Component {
         {...otherProps}>
         {React.Children.map(children, child => (
           React.cloneElement(child, {
-            onSelect,
             onFocusNext: this.handleFocusNext,
             onFocusPrevious: this.handleFocusPrevious,
             onFocusFirst: this.handleFocusFirst,
