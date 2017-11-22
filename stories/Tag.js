6629dda0018344dae2bb77356947480739da7183
Devon Govett <devongovett@gmail.com>
2017-11-21 19:59:38 -0500

    Remove tag multiline story

--- stories/Tag.js
+++ stories/Tag.js
@@ -28,14 +28,6 @@ storiesOf('Tag', module)
     'closable: true',
     () => render({closable: true}),
     {inline: true}
-  )
-  .addWithInfo(
-    'multiline: true',
-    () => render({
-      children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel vestibulum neque, eu sollicitudin arcu. Etiam sed dolor egestas, rutrum ante quis, aliquam urna. Cras maximus quis ligula a vulputate. Ut non est sagittis, sodales est ac, ullamcorper libero. Aenean quis elementum velit. Nullam eu nulla lectus. Donec sed est nec mi cursus sodales. Aenean imperdiet tristique suscipit. Aenean varius pellentesque mauris. Nunc scelerisque nibh facilisis quam hendrerit eleifend. Phasellus a dolor enim. Etiam ullamcorper euismod nisl quis accumsan. Pellentesque bibendum vulputate interdum. Duis ut mi sapien. Ut vehicula feugiat erat, et posuere nulla facilisis in.',
-      multiline: true
-    }),
-    {inline: true}
   );
 
 function render(props = {}, children = 'Cool Tag') {
