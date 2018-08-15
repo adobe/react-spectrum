1baf94e79ab521c963edf12297cb2047f75c316d
Michael Jordan <mijordan@adobe.com>
2018-08-15 18:15:45 -0400

    RSP-425 [Accessibility] Incorporate React-axe to help identify accessibility errors in Storybook (#286)
    Add React-axe to Storybook layout to log accessibility errors
    discovered while testing components in Storybook to the console.
    
    Fix page-level accessibility issues in layout itself to reduce the
    number of false positives.

--- .storybook/layout.js
+++ .storybook/layout.js
@@ -1,6 +1,9 @@
 import FieldLabel from '../src/FieldLabel';
+import {getLocale} from '../src/utils/intl';
 import Provider from '../src/Provider';
 import React from 'react';
+import reactAxe from 'react-axe';
+import ReactDOM from 'react-dom';
 import Select from '../src/Select';
 
 export function VerticalCenter({children, className, style}) {
@@ -48,17 +51,28 @@ export class StoryWrapper extends React.Component {
     theme: 'light',
     scale: 'medium'
   }
+
+  componentWillMount() {
+    // add lang attribute to html element
+    document.documentElement.lang = getLocale();
+
+    // load react-axe to log Accessibility issues to console
+    reactAxe(React, ReactDOM, 500);
+  }
+
   render() {
     return <Provider theme={this.state.theme} scale={this.state.scale}>
-      <div style={{position: 'absolute', right: 100}}>
+      <aside style={{position: 'absolute', right: 100}}>
         <FieldLabel label="Theme" position="left">
           <Select onChange={theme => this.setState({theme})} options={[{label: "Light", value: "light"}, {label: "Lightest", value: "lightest"}, {label: "Dark", value: "dark"}, {label: "Darkest", value: "darkest"}]} />
         </FieldLabel>
         <FieldLabel label="Scale" position="left">
           <Select onChange={scale => this.setState({scale})} options={[{label: "Medium", value: "medium"}, {label: "Large", value: "large"}]} />
         </FieldLabel>
-      </div>
-      {this.props.children}
+      </aside>
+      <main>
+        {this.props.children}
+      </main>
     </Provider>
   }
 }
