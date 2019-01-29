991211fa073bf4c3b4ea8a80a386065e53020ee1
Devon Govett <devongovett@gmail.com>
2019-01-28 22:34:12 -0800

    Testaroos

--- src/TreeView/js/TreeViewDataSource.js
+++ src/TreeView/js/TreeViewDataSource.js
@@ -193,7 +193,7 @@ export default class TreeViewDataSource extends ArrayDataSource {
   async updateItemStates(props) {
     this.props = props;
 
-    if (props.selectedItems !== this.props.selectedItems) {
+    if (props.selectedItems) {
       this.setSelectedItems(props.selectedItems);
     }
 
