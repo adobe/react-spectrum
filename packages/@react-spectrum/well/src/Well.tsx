4b7156ff91b00596bd18153dddebf6bee849b49c
Kyle Taborski <ktabors@yahoo.com>
2019-06-17 17:49:37 -0700

    migrating well to v3 (#333)

---
+++ packages/@react-spectrum/well/src/Well.tsx
@@ -0,0 +1,26 @@
+import {classNames} from '@react-spectrum/utils/src/classNames';
+import filterDOMProps from '@react-spectrum/utils/src/filterDOMProps';
+import React, {ReactNode} from 'react';
+import styles from '@adobe/spectrum-css-temp/components/well/vars.css';
+
+interface WellProps extends React.AllHTMLAttributes<HTMLElement> {
+  children: ReactNode
+}
+
+export function Well({
+  children,
+  className,
+  ...otherProps}: WellProps) {
+
+  return (
+    <div
+      {...filterDOMProps(otherProps)}
+      className={classNames(
+        styles,
+        'spectrum-Well',
+        className
+      )}>
+      {children}
+    </div>
+  );
+}
