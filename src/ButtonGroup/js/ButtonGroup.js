4cce49a7a27fdd5a0b371d1865e445fbb726dc54
Michael Jordan <mijordan@adobe.com>
2019-09-10 21:15:47 -0400

    Fix #504 [Accessibility] ButtonGroup should support manageTabIndex={false} (#505)
    Depending on the use case, developers should be able to have all buttons in ButtonGroup remain in the tab navigation order for the page.

--- src/ButtonGroup/js/ButtonGroup.js
+++ src/ButtonGroup/js/ButtonGroup.js
@@ -43,6 +43,11 @@ export default class ButtonGroup extends Component {
     invalid: PropTypes.bool,
 
     /**
+     * Whether to use roving tabIndex so that only one element within the group can receive focus with tab key at a time.
+     */
+    manageTabIndex: PropTypes.bool,
+
+    /**
      * Allows multi select
      */
     multiple: PropTypes.bool,
@@ -85,6 +90,7 @@ export default class ButtonGroup extends Component {
   static defaultProps = {
     disabled: false,
     invalid: false,
+    manageTabIndex: true,
     multiple: false,
     readOnly: false,
     required: false,
@@ -220,6 +226,7 @@ export default class ButtonGroup extends Component {
       invalid,
       required,
       orientation,
+      manageTabIndex,
       ...otherProps
     } = this.props;
 
@@ -237,7 +244,11 @@ export default class ButtonGroup extends Component {
     }
 
     return (
-      <FocusManager itemSelector={BUTTONGROUP_ITEM_SELECTOR} selectedItemSelector={BUTTONGROUP_SELECTED_ITEM_SELECTOR} orientation={otherProps.role === 'toolbar' ? orientation : 'both'}>
+      <FocusManager
+        itemSelector={BUTTONGROUP_ITEM_SELECTOR}
+        selectedItemSelector={BUTTONGROUP_SELECTED_ITEM_SELECTOR}
+        orientation={otherProps.role === 'toolbar' ? orientation : 'both'}
+        manageTabIndex={manageTabIndex}>
         <div
           aria-invalid={invalid || null}
           aria-required={required || null}
