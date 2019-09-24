44d072805e7f1c01a75aeb0d2ed1f519707554cb
Daniel <dlu@livefyre.com>
2019-09-24 15:07:15 -0700

    more debug logging

--- .github/actions/comment/index.js
+++ .github/actions/comment/index.js
@@ -8,9 +8,9 @@ async function run() {
   try {
     console.log('github.context', github.context);
     console.log('github.context.repository', github.context.payload.repository.owner);
-
+    console.log('github.context.repo', ...github.context.repo);
     await octokit.issues.createComment({
-      ...github.context.payload.repository.owner,
+      ...github.context.repo,
       issue_number: github.context.payload.number,
       body: `Build successful! [View the storybook](https://reactspectrum.blob.core.windows.net/reactspectrum/${github.context.sha}/index.html)`
     });
