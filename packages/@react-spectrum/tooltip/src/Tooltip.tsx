bc8b2647a30d6f9655aa0c405c4e4adc0b07749d
Jean Leconte II <lecontei@adobe.com>
2019-10-14 19:42:59 -0700

    duplicate key error

--- packages/@react-spectrum/tooltip/src/Tooltip.tsx
+++ packages/@react-spectrum/tooltip/src/Tooltip.tsx
@@ -1,39 +1,26 @@
 import {classNames, filterDOMProps} from '@react-spectrum/utils';
-/*
 import {TooltipContext} from './context';
-import {DOMProps} from '@react-types/shared';
-import {FocusScope} from '@react-aria/focus';
 import {mergeProps} from '@react-aria/utils';
-*/
-// import {HTMLElement} from 'react-dom';
 import React, {ReactNode, RefObject, useContext, useRef} from 'react';
 import styles from '@adobe/spectrum-css-temp/components/tooltip/vars.css';
-// import {useTooltipState} from '@react-stately/tooltip';
-// import {useProviderProps} from '@react-spectrum/provider';
 import {useTooltip} from '@react-aria/tooltip';
 
-
-export interface TooltipProps {
+interface TooltipProps {
   children: ReactNode,
   variant?: 'neutral' | 'positive' | 'negative' | 'info',
   showIcon?: boolean
 }
 
-export const Tooltip = React.forwardRef((props: TooltipProps, ref: RefObject<HTMLDivElement>) => { //HTMLDivElement instead of HTMLElement
-  // let defaults = {};
-  // let completeProps = Object.assign({}, defaults, useProviderProps(props));
-  // let state = useTooltipState(completeProps);
-  // let ariaProps = useTooltip(completeProps, state);
+export const Tooltip = React.forwardRef((props: TooltipProps, ref: RefObject<HTMLDivElement>) => {
   let {
     type = 'top',
     ...contextProps
   } = useContext(TooltipContext) || {};
   if (type === 'top') {
-    return <BaseDialog {...mergeProps(contextProps, props)} ref={ref} />;
+    return <BaseTooltip {...mergeProps(contextProps, props)} ref={ref} />;
   } else {
     return <p> future iterations coming soon </p>
   }
-
 });
 
 const BaseTooltip = React.forwardRef(({children, className, ...otherProps}: TooltipProps, ref: RefObject<HTMLDivElement>) => {
