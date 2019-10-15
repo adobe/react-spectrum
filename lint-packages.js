cecfa2ae55721588d4e047d7b93af021be1c1106
Devon Govett <devongovett@gmail.com>
2019-10-15 09:54:07 -0700

    Fix package linter

--- lint-packages.js
+++ lint-packages.js
@@ -13,7 +13,7 @@ for (let pkg of packages) {
   assert.deepEqual(json.files, ['dist'], `${pkg} did not match "files"`);
   assert.equal(json.sideEffects, false, `${pkg} is missing sideEffects: false`);
   assert(!json.dependencies || !json.dependencies['@adobe/spectrum-css-temp'], `${pkg} has @adobe/spectrum-css-temp in dependencies instead of devDependencies`);
-  assert(!json.dependencies || !json.dependencies['@babel/runtime'], `${pkg} is missing a dependency on @babel/runtime`);
+  assert(json.dependencies && json.dependencies['@babel/runtime'], `${pkg} is missing a dependency on @babel/runtime`);
 
   if (json.name.startsWith('@react-spectrum') && json.devDependencies && json.devDependencies['@adobe/spectrum-css-temp']) {
     assert.deepEqual(json.targets, {
