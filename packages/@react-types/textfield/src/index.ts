bab2a0178e1fe2f2b1939f4af984ee56069ff7ea
Daniel Lu <dlu@livefyre.com>
2019-10-18 13:46:09 -0700

    bumping spectrum css to 2.14.0 (#641)

--- packages/@react-types/textfield/src/index.ts
+++ packages/@react-types/textfield/src/index.ts
@@ -1,3 +1,3 @@
 import {DOMProps, InputBase, TextInputBase, ValueBase} from '@react-types/shared';
 
-export interface TextFieldProps extends DOMProps, InputBase, TextInputBase, ValueBase<string | string[]> {}
+export interface TextFieldProps extends DOMProps, InputBase, TextInputBase, ValueBase<string | number | string[]> {}
