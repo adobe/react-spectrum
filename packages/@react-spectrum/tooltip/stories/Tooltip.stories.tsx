0d15b76e1374fd6f0f76bfaaeec67fd39c96c0bf
Jean Leconte II <lecontei@adobe.com>
2019-10-24 12:17:25 -0700

    resolved import issues

--- packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
+++ packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {Tooltip} from '../src';
+import {Tooltip, TooltipTrigger} from '../src';
 
 storiesOf('Tooltip', module)
   .add(
     'default',
+    () => render()
+  )
+  .add(
+    'placement: left',
+    () => render()
+  )
+  .add(
+    'placement: top',
+    () => render()
+  )
+  .add(
+    'placement: bottom',
+    () => render()
+  )
+  .add(
+    'variant: neutral',
+    () => render()
+  )
+  .add(
+    'variant: positive',
+    () => render()
+  )
+  .add(
+    'variant: negative',
+    () => render()
+  )
+  .add(
+    'variant: info',
+    () => render()
+  )
+  .add(
+    'long content',
+    () => render()
+  )
+  .add(
+    'triggered using click',
+    () => render()
+  )
+  .add(
+    'triggered using hover',
     () => render()
   );
 
@@ -17,3 +57,13 @@ function render() {
     </div>
   );
 }
+
+const longMarkup = (
+  <div>
+  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor
+  quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
+  ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+  Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt
+  condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+  </div>
+);
