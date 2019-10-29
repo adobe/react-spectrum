a51d4a636e535811f87fe8299c44d27fbf9f0dd2
pst67662 <pst67662@adobe.com>
2019-10-29 14:48:27 +0100

    RSP-1134: removeClass name from interface

--- packages/@react-types/breadcrumbs/src/index.d.ts
+++ packages/@react-types/breadcrumbs/src/index.d.ts
@@ -5,11 +5,9 @@ import {ReactElement, ReactNode} from 'react';
 export interface BreadcrumbItemProps extends DOMProps {
   isCurrent?: boolean,
   onPress?: (e: PressEvent) => void,
-  className?: string,
   children: ReactNode
 }
 
 export interface BreadcrumbsProps extends DOMProps {
-  className? : string
   children: ReactElement<BreadcrumbItemProps> | ReactElement<BreadcrumbItemProps>[]
 }
