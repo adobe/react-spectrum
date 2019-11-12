d021b0156c84633a9898fd6e6780af0ccd32f628
Daniel <dlu@livefyre.com>
2019-11-12 12:12:23 -0800

    Fixing test

--- packages/@react-spectrum/menu-trigger/test/MenuTrigger.test.js
+++ packages/@react-spectrum/menu-trigger/test/MenuTrigger.test.js
@@ -5,7 +5,7 @@ import {Provider} from '@react-spectrum/provider';
 import React from 'react';
 import scaleMedium from '@adobe/spectrum-css-temp/vars/spectrum-medium-unique.css';
 import themeLight from '@adobe/spectrum-css-temp/vars/spectrum-light-unique.css';
-import {triggerPress} from '@react-spectrum/button/test/utils'; // TODO: Move this util funct to test-utils folder (https://github.com/adobe/react-spectrum-v3/pull/32)
+import {triggerPress} from '@react-spectrum/test-utils';
 import V2Button from '@react/react-spectrum/Button';
 import V2Dropdown from '@react/react-spectrum/Dropdown';
 import {Menu as V2Menu, MenuItem as V2MenuItem} from '@react/react-spectrum/Menu';
