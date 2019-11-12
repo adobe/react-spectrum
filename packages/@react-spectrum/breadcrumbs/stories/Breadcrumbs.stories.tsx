b98a30c59b399f1352dd2a7293cc1acd95de8764
Aleksandra Ljubenovic <pst67662@adobe.com>
2019-11-13 00:50:32 +0100

    RSP-1365: Breadcrumbs v3 improvements (#45)

--- packages/@react-spectrum/breadcrumbs/stories/Breadcrumbs.stories.tsx
+++ packages/@react-spectrum/breadcrumbs/stories/Breadcrumbs.stories.tsx
@@ -3,7 +3,13 @@ import {BreadcrumbItem, Breadcrumbs} from '../';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
+let styles = {
+  width: '100%'
+};
+const CenterDecorator = storyFn => <div style={styles}><div>{storyFn()}</div></div>;
+
 storiesOf('Breadcrumbs', module)
+  .addDecorator(CenterDecorator)
   .add(
     'Default',
     () => render({})
@@ -19,6 +25,30 @@ storiesOf('Breadcrumbs', module)
   .add(
     'onPress',
     () => renderPress({})
+  )
+  .add(
+    'maxVisibleItems: 4',
+    () => renderPress({})
+  )
+  .add(
+    'maxVisibleItems: 4, showRoot: true',
+    () => renderPress({showRoot: true})
+  )
+  .add(
+    'maxVisibleItems: auto',
+    () => renderPress({maxVisibleItems: 'auto'})
+  )
+  .add(
+    'maxVisibleItems: auto, size: L',
+    () => renderPress({maxVisibleItems: 'auto', size: 'L'})
+  )
+  .add(
+    'isDisabled: true',
+    () => render({isDisabled: true})
+  )
+  .add(
+    'isDisabled: true, size: L',
+    () => render({isDisabled: true, size: 'L'})
   );
 
 function render(props = {}) {
@@ -37,6 +67,10 @@ function renderPress(props = {}) {
       <BreadcrumbItem onPress={action('press Folder 1')}>Folder 1</BreadcrumbItem>
       <BreadcrumbItem onPress={action('press Folder 2')}>Folder 2</BreadcrumbItem>
       <BreadcrumbItem onPress={action('press Folder 3')}>Folder 3</BreadcrumbItem>
+      <BreadcrumbItem onPress={action('press Folder 4')}>Folder 4</BreadcrumbItem>
+      <BreadcrumbItem onPress={action('press Folder 5')}>Folder 5</BreadcrumbItem>
+      <BreadcrumbItem onPress={action('press Folder 6')}>Folder 6</BreadcrumbItem>
+      <BreadcrumbItem onPress={action('press Folder 7')}>Folder 7</BreadcrumbItem>
     </Breadcrumbs>
   );
 }
