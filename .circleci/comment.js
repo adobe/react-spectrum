bd059102b1a28e830c464e06c9eff39a30eefede
Devon Govett <devongovett@gmail.com>
2019-11-13 19:08:24 -0800

    Circle is stupid

--- .circleci/comment.js
+++ .circleci/comment.js
@@ -7,14 +7,15 @@ const octokit = new Octokit({
 run();
 
 async function run() {
-  if (!process.env.CIRCLE_PR_NUMBER) {
+  if (!process.env.CIRCLE_PULL_REQUEST) {
     return;
   }
 
+  let pr = process.env.CIRCLE_PULL_REQUEST.split('/').pop();
   await octokit.issues.createComment({
     owner: 'adobe',
     repo: 'react-spectrum-v3',
-    issue_number: process.env.CIRCLE_PR_NUMBER,
+    issue_number: pr,
     body: `Build successful! [View the storybook](https://reactspectrum.blob.core.windows.net/reactspectrum/${process.env.CIRCLE_SHA1}/index.html)`
   });
 }
