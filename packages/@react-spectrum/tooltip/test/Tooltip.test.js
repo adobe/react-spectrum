5df54f607456e9babd5e6b2e3c7558d29a544656
Jean Leconte II <lecontei@adobe.com>
2019-11-15 11:44:21 -0800

    tooltip tests

--- packages/@react-spectrum/tooltip/test/Tooltip.test.js
+++ packages/@react-spectrum/tooltip/test/Tooltip.test.js
@@ -1,37 +1,55 @@
-/*
-
-Comes from v2
-
-import assert from 'assert';
+import {cleanup, render} from '@testing-library/react';
 import React from 'react';
-import {shallow} from 'enzyme';
-import Tooltip from '../../src/Tooltip';
+import {Tooltip} from '../';
+import {triggerPress} from '@react-spectrum/test-utils';
+import {Tooltip as V2Tooltip} from '@react/react-spectrum/Tooltip';
 
-describe('Tooltip', () => {
-  it('supports different placements', () => {
-    const tree = shallow(<Tooltip placement="top" />);
-    assert(tree.hasClass('spectrum-Tooltip--top'));
+let testId = 'test-id';
+
+describe('Tooltip', function () {
+
+  afterEach(() => {
+    cleanup();
   });
 
-  it('supports different variants', () => {
-    const tree = shallow(<Tooltip variant="info" />);
-    assert(tree.hasClass('spectrum-Tooltip--info'));
+  it.each`
+    Name           | Component    | props
+    ${'Tooltip'}     | ${Tooltip}     | ${{className: 'foo'}}
+    ${'V2Tooltip'}   | ${V2Tooltip}   | ${{className: 'foo'}}
+  `('$Name supports additional classNames', function ({Component, props}) {
+    let {getByTestId} = render(<Component {...props}>My Tooltip</Component>);
+    let className = getByTestId(testId).className;
+    expect(className.includes('spectrum-Tooltip')).toBeTruthy();
+    expect(className.includes('foo')).toBeTruthy();
   });
 
-  it('supports additional classNames', () => {
-    const tree = shallow(<Tooltip className="foo" />);
-    assert(tree.hasClass('foo'));
+  it.each`
+    Name      | Component
+    ${'Tooltip'}   | ${Tooltip}
+    ${'Tooltip'}   | ${V2Tooltip}
+  `('$Name supports children', ({Component}) => {
+    let {getByText} = render(<Component>I am a Tooltip</Component>);
+    expect(getByText('I am a Tooltip')).toBeTruthy();
   });
 
-  it('supports children', () => {
-    const tree = shallow(<Tooltip>oh hey</Tooltip>);
-    assert.equal(tree.find('.spectrum-Tooltip-label').prop('children'), 'oh hey');
+  it.each`
+    Name           | Component    | props
+    ${'Tooltip'}     | ${Tooltip}     | ${{placement: 'top'}}
+    ${'V2Tooltip'}   | ${V2Tooltip}   | ${{placement: 'top'}}
+  `('$Name supports different placements', function ({Component, props}) {
+    let {getByTestId} = render(<Component {...props}>My Tooltip</Component>);
+    let className = getByTestId(testId).className;
+    expect(className.includes('spectrum-Tooltip--top')).toBeTruthy();
   });
 
-  it('has WAI-ARIA role="tooltip"', () => {
-    const tree = shallow(<Tooltip>oh hey</Tooltip>);
-    assert.equal(tree.prop('role'), 'tooltip');
+  it.each`
+    Name           | Component    | props
+    ${'Tooltip'}     | ${Tooltip}     | ${{variant: 'info'}}
+    ${'V2Tooltip'}   | ${V2Tooltip}   | ${{variant: 'info'}}
+  `('$Name supports different variants', function ({Component, props}) {
+    let {getByTestId} = render(<Component {...props}>My Tooltip</Component>);
+    let className = getByTestId(testId).className;
+    expect(className.includes('spectrum-Tooltip--info')).toBeTruthy();
   });
+
 });
-
-*/
