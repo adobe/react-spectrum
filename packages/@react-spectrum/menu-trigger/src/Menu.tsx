dde324a884a8ff8ef8a221fb737860072718f5bc
Daniel <dlu@livefyre.com>
2019-11-15 09:49:35 -0800

    more questions for design review, more stories, tentative components

--- packages/@react-spectrum/menu-trigger/src/Menu.tsx
+++ packages/@react-spectrum/menu-trigger/src/Menu.tsx
@@ -2,16 +2,22 @@ import {classNames} from '@react-spectrum/utils';
 import {DOMProps} from '@react-types/shared';
 import {MenuContext} from './context';
 import {mergeProps} from '@react-aria/utils';
-import React, {ReactElement, useContext} from 'react';
+import React, {Fragment, ReactElement, useContext, useMemo} from 'react';
 import styles from '@adobe/spectrum-css-temp/components/menu/vars.css';
 
 
 import {CollectionBase, Expandable, MultipleSelection} from '@react-types/shared';
 import {Popover} from '@react-spectrum/overlays';
 import {FocusRing, FocusScope} from '@react-aria/focus';
+import {useTreeState} from '@react-stately/tree';
+import {Item, ListLayout, Section} from '@react-stately/collections';
+import {CollectionView} from '@react-aria/collections';
+
+
+export {Item, Section};
 
 interface MenuProps extends DOMProps{
-  children?: ReactElement[]
+  children?: ReactElement | ReactElement[]
 }
 
 // This is a filler Menu component, used to illustrate how the MenuContext might be consumed. 
@@ -55,6 +61,10 @@ export function Menu(props: MenuProps) {
 
 // For now, export a v3 version of Menu just so I can gradually replace the mock Menu component above
 export function V3Menu<T>(props: CollectionBase<T> & Expandable & MultipleSelection) {
+  // Do we want it to look like v2 Menu or spectrum-css example? v2 Menu has the sectionHeading class on the <li>
+  // wheras spectrum-css has it on the <span>
+  
+  
   // grab context from MenuTrigger its got things like id, aria stuff etc, spread it on the top
 
   // Is Menu going to be an internal component? If not, do I need to move Popover back into here and adjust the MenuTrigger context obj to contain the popover props as well
@@ -66,44 +76,119 @@ export function V3Menu<T>(props: CollectionBase<T> & Expandable & MultipleSelect
   // Figure out how to propagate the onSelect event (prop just placed on the top level menu and passed to useTreeState)
 
   // Figure out how to get the Focus working
-  // Add a FocusRing wrapping the whole Menu and put a FocusScope around it as well?
-  // FocusScope might go around each Heading/Section instead
+  // Where to put Focus scope? FocusRing can go around the MenuItem
   // see if that gives me the ability to use arrow keys to shift focus, otherwise look at v2 dropdown to see
   // Actually I'm pretty sure this is handled in devon's selection and focus management pull
 
-  // Planning for what the stuff will look like
-  // if (type === 'section'), then return a Heading with trailing divider
-  // else just return the MenuItem
-  
-  return (
-    // <Popover isOpen hideArrow> 
-      <ul
-        role="menu"
-        className={classNames(
-          styles,
-          'spectrum-Menu')}>
-        {props.children}
-      </ul>
-    // </Popover>
-  )
+  // Will we be supporting horizontal orientation of menu?
 
+  let contextProps = useContext(MenuContext) || {};
+  let completeProps = mergeProps(contextProps, props);
+
+  let {
+    tree,
+    onSelectToggle
+  } = useTreeState(completeProps);
+
+  let {
+    id,
+    role = 'menu',
+    'aria-labelledby': labelledBy
+  } = completeProps
+
+  // TODO: put all of the above and the aria-orientation below into an react-aria hook (useMenu)
+  // id should be generated if not provided
+  // should accept a user defined aria-label
+
+  let layout = useMemo(() => 
+    new ListLayout({
+      rowHeight: 32 // This makes the heading wrapping div 32, but the heading should only take up 20
+    })
+  , []);
+
+  return (
+    <Popover isOpen hideArrow> 
+      <FocusScope autoFocus>
+        <CollectionView
+          id={id}
+          role={role}
+          aria-labelledby={labelledBy}
+          aria-orientation="vertical"
+          className={classNames(styles, 'spectrum-Menu')} // I had to add a static height and width to menu css to use this style, doesn't seem right. This is because the wrapping divs do some kind of calc and hard set a width
+          layout={layout}
+          collection={tree}>
+          {(type, item) => {
+            if (type === 'section') {
+              return (
+                <Fragment>
+                  <MenuHeading item={item} />
+                  <MenuDivider />
+                </Fragment>
+              )
+            }
+            console.log('item', item);
+            return (
+              <MenuItem 
+                item={item}
+                onSelectToggle={() => onSelectToggle(item)} />
+            );
+          }}
+        </CollectionView>
+      </FocusScope>
+    </Popover>
+  );
 }
 
 interface MenuItemProps extends DOMProps {
-  children?: ReactElement[];
+
 }
 
 // For now export just to see what it looks like, remove after
 // Placeholder for now, Rob's pull will make the real menuItem
-export function MenuItem(props: MenuItemProps) {
-  
+// How would we get MenuItem user specified props in?
+export function MenuItem({item, onSelectToggle}) {
+  let {
+    rendered,
+    isSelected
+  } = item;
+
+  // Missing checkmark selection icon at the moment, I'll put it in later
+  // Missing aria-disabled, need something to tell me that it is disabled first
   return (
     <FocusRing 
       focusClass={classNames(styles, 'is-focused')}
       focusRingClass={classNames(styles, 'focus-ring')}>
       <li
         role="menuitem"
-        tabindex="0"
+        tabIndex="0" // will probably need disabled logic
+        onMouseDown={() => onSelectToggle(item)} // Looks like if you click too fast it stops being responsive and falls out of sync
+        className={classNames(
+          styles,
+          'spectrum-Menu-item',
+          {
+            'is-disabled': false, // no disabled info attached to item? Should I add it in useTreeState?
+            'is-selected': isSelected
+          }
+        )}>
+        <span
+          className={classNames(
+            styles,
+            'spectrum-Menu-itemLabel')}>
+          {rendered}
+        </span>
+      </li>
+   </FocusRing>
+  )
+}
+
+export function V2MenuItem(props) {
+  return (
+    <FocusRing 
+      focusClass={classNames(styles, 'is-focused')}
+      focusRingClass={classNames(styles, 'focus-ring')}>
+      <li
+        role="menuitem"
+        tabIndex="0"
         className={classNames(
           styles,
           'spectrum-Menu-item')}>
@@ -114,7 +199,7 @@ export function MenuItem(props: MenuItemProps) {
           {props.children}
         </span>
       </li>
-    </FocusRing>
+   </FocusRing>
   )
 }
 
@@ -123,7 +208,7 @@ interface MenuDividerProps {
 }
 
 // For now export just to see what it looks like, remove after
-export function MenuDivider(props: MenuDividerProps) {
+export function MenuDivider() {
   return (
     <li 
       className={classNames(
@@ -134,12 +219,40 @@ export function MenuDivider(props: MenuDividerProps) {
   )
 }
 
+// For now export just to see what it looks like, remove after
+export function V2MenuDivider() {
+  return (
+    <li 
+      aria-orientation="horizontal"
+      className={classNames(
+        styles,
+        'spectrum-Menu-divider'
+      )}
+      role="separator" />
+  )
+}
+
 interface MenuHeadingProps {
-  children?: ReactElement[];
+  children?: ReactElement | ReactElement[];
 }
 
 // For now export just to see what it looks like, remove after
-export function MenuHeading(props: MenuHeadingProps) {
+export function MenuHeading({item}) {
+  return (
+    <li role="presentation">
+      <span 
+        role="heading" 
+        aria-hidden="true"
+        className={classNames(
+          styles,
+          'spectrum-Menu-sectionHeading')}>
+        {item.rendered}
+      </span>
+    </li>
+  )
+}
+
+export function V2MenuHeading(props) {
   return (
     <li role="presentation">
       <span 
--- packages/@react-spectrum/menu-trigger/stories/Menu.stories.tsx
+++ packages/@react-spectrum/menu-trigger/stories/Menu.stories.tsx
@@ -1,60 +1,173 @@
 import {action} from '@storybook/addon-actions';
 // import {Button} from '@react-spectrum/button';
-import {Menu, MenuDivider, MenuHeading, MenuItem, V3Menu} from '../';
+import {Item, Menu, MenuDivider, MenuHeading, MenuItem, Section, V3Menu, V2MenuDivider, V2MenuHeading, V2MenuItem} from '../';
 // import {MenuTrigger} from '../';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
+let flatMenu = [
+  {name: 'Aardvark'},
+  {name: 'Kangaroo'},
+  {name: 'Snake'}
+]
+
+let withSection = [
+  {name: 'Animals', children: [
+    {name: 'Aardvark'},
+    {name: 'Kangaroo'},
+    {name: 'Snake'}
+  ]},
+  {name: 'People', children: [
+    {name: 'Danni'},
+    {name: 'Devon'},
+    {name: 'Ross'}
+  ]}
+];
+
 storiesOf('Menu', module)
   .add(
-    'default',
+    'using old menu, delete after, test story',
     () => (
-      <V3Menu>
-        <MenuHeading>Heading 1</MenuHeading>
-        <MenuItem>blah</MenuItem>
-        <MenuItem>blah1</MenuItem>
-        <MenuItem>blah2</MenuItem>
-        <MenuDivider />
-        <MenuHeading>Heading 2</MenuHeading>
-        <MenuItem>heh</MenuItem>
-        <MenuItem>heh1</MenuItem>
-        <MenuItem>heh2</MenuItem>
-        <MenuDivider />
-      </V3Menu>   
+      <Menu>
+        <V2MenuHeading>Heading 1</V2MenuHeading>
+        <V2MenuItem>blah</V2MenuItem>
+        <V2MenuItem>blah1</V2MenuItem>
+        <V2MenuItem>blah2</V2MenuItem>
+        <V2MenuDivider />
+        <V2MenuHeading>Heading 2</V2MenuHeading>
+        <V2MenuItem>heh</V2MenuItem>
+        <V2MenuItem>heh1</V2MenuItem>
+        <V2MenuItem>heh2</V2MenuItem>
+        <V2MenuDivider />
+      </Menu>
     )
   )
   .add(
-    'align="end"',
-    () => render({align: 'end'})
+    'Default Menu',
+    () => (
+      <V3Menu items={flatMenu} itemKey="name">
+        {item => <Item childItems={item.children}>{item.name}</Item>}
+      </V3Menu>
+    )
   )
   .add(
-    'direction="top"',
-    () => render({direction: 'top'})
+    'Menu w/ sections',
+    () => (
+      <V3Menu items={withSection} itemKey="name">
+        {item => (
+          <Section items={item.children} title={item.name}>
+            {item => <Item childItems={item.children}>{item.name}</Item>}
+          </Section>
+        )}
+      </V3Menu>
+    )
   )
   .add(
-    'shouldFlip',
-    () => render({shouldFlip: true})
+    'Static',
+    () => (
+      <V3Menu>
+        <Item>One</Item>
+        <Item>Two</Item>
+        <Item>Three</Item>
+      </V3Menu>
+    )
   )
   .add(
-    'isOpen',
-    () => render({isOpen: true})
+    'Static with sections',
+    () => (
+      <V3Menu>
+        <Section title="Section 1">
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+        <Section title="Section 2">
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+      </V3Menu>
+    )
   )
   .add(
-    'defaultOpen',
-    () => render({defaultOpen: true})
+    'with default selected menu items',
+    () => (
+      <V3Menu items={withSection} itemKey="name" defaultSelectedKeys={['Kangaroo']}>
+        {item => (
+          <Section items={item.children} title={item.name}>
+            {item => <Item childItems={item.children}>{item.name}</Item>}
+          </Section>
+        )}
+      </V3Menu>
+    )
   )
   .add(
-    'trigger="longPress" TODO out of scope',
-    () => render()
+    'static with default selected menu items',
+    () => (
+      <V3Menu>
+        <Section title="Section 1">
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+        <Section title="Section 2">
+          <Item isSelected>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+      </V3Menu>
+    )
   )
   .add(
-    'menu+submenus TODO out of scope',
-    () => render()
+    'with selected menu items (controlled)',
+    () => (
+      <V3Menu items={withSection} itemKey="name" selectedKeys={['Kangaroo']}>
+        {item => (
+          <Section items={item.children} title={item.name}>
+            {item => <Item childItems={item.children}>{item.name}</Item>}
+          </Section>
+        )}
+      </V3Menu>
+    )
   )
   .add(
-    'popup with role=listbox',
-    () => render({}, {role: 'listbox'})
+    'static with selected menu items (controlled)',
+    () => (
+      <V3Menu>
+        <Section title="Section 1">
+          <Item>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+        <Section title="Section 2">
+          <Item isSelected>One</Item>
+          <Item>Two</Item>
+          <Item>Three</Item>
+        </Section>
+      </V3Menu>
+    )
   );
+  // .add(
+  // Not available in useTreeState?
+  //   'with disabled menu items',
+  //   () => (
+
+  //   )
+  // )
+  // .add(
+  //   'static with disabled menu items',
+  //   () => (
+      
+  //   )
+  // )
+  // .add(
+  //   'with submenus, OUT OF SCOPE',
+  //   () => ()
+  // )
+  // .add(
+  //   'static with submenus, OUT OF SCOPE',
+  //   () => ()
+  // );
   
 function render(props = {}, menuProps = {}) {
   return (
