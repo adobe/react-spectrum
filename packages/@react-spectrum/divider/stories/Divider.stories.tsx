9997f735ca86c2af3b642f8004d6746edc0d9514
Jean Leconte II <lecontei@adobe.com>
2019-12-04 07:29:23 -0800

    reading ticket descriptions fully is important

--- packages/@react-spectrum/divider/stories/Divider.stories.tsx
+++ packages/@react-spectrum/divider/stories/Divider.stories.tsx
@@ -33,40 +33,22 @@ storiesOf('Divider', module)
       </section>
     )
   )
-  .add('Vertical, Large (Default), Buttons',
-    () => renderVerticalDividerBetweenButtons(),
+  .add('Vertical, Large (Default)',
+    () => renderVertical(),
   )
-  .add('Vertical, Medium, Buttons',
-    () => renderVerticalDividerBetweenButtons({size: 'M'})
+  .add('Vertical, Medium',
+    () => renderVertical({size: 'M'})
   )
-  .add('Vertical, Small, Buttons',
-    () => renderVerticalDividerBetweenButtons({size: 'S'})
-  ).add('Vertical, Large (Default), Text',
-    () => renderVerticalDividerBetweenText(),
-  )
-  .add('Vertical, Medium, Text',
-    () => renderVerticalDividerBetweenText({size: 'M'})
-  )
-  .add('Vertical, Small, Text',
-    () => renderVerticalDividerBetweenText({size: 'S'})
+  .add('Vertical, Small',
+    () => renderVertical({size: 'S'})
   );
 
-function renderVerticalDividerBetweenButtons(props = {}) {
+function renderVertical(props = {}) {
   return (
     <section style={{display: 'flex', flexDirection: 'row', height: '32px'}}>
-      <ActionButton icon={<Properties />} isQuiet />
+      <ActionButton icon={<Properties />} aria-label="Properties" isQuiet />
       <Divider orientation="vertical" {...props} />
-      <ActionButton icon={<Select />} isQuiet />
-    </section>
-  );
-}
-
-function renderVerticalDividerBetweenText(props = {}) {
-  return (
-    <section style={{display: 'flex', flexDirection: 'row', height: '62px'}}>
-      <h1 style={{padding: 5}}>Sample Heading</h1>
-      <Divider orientation="vertical" {...props} />
-      <p style={{padding: 5}}>Sample Text</p>
+      <ActionButton icon={<Select />} aria-label="Select" isQuiet />
     </section>
   );
 }
