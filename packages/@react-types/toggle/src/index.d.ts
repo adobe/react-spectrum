f57b369660bb3633710c8bac9477cf6313d04d5c
Danni <drobinson@livefyre.com>
2019-12-06 16:16:54 -0800

    remove event

--- packages/@react-types/toggle/src/index.d.ts
+++ packages/@react-types/toggle/src/index.d.ts
@@ -2,5 +2,5 @@ import {ChangeEvent} from 'react';
 
 export interface ToggleState {
   checked: boolean,
-  setChecked: (value:boolean, e:ChangeEvent<HTMLInputElement>) => void
+  setChecked: (value:boolean) => void
 }
