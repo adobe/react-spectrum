41e17a2cad4263e1a5648984f799f92e8f9e03d8
Devon Govett <devongovett@gmail.com>
2019-12-09 10:27:22 -0800

    Some API + code cleanup

--- packages/@react-spectrum/theme-default/src/index.ts
+++ packages/@react-spectrum/theme-default/src/index.ts
@@ -1,12 +1,12 @@
-import global from '@adobe/spectrum-css-temp/vars/spectrum-global.css';
 import dark from '@adobe/spectrum-css-temp/vars/spectrum-dark-unique.css';
+import global from '@adobe/spectrum-css-temp/vars/spectrum-global.css';
 import large from '@adobe/spectrum-css-temp/vars/spectrum-large-unique.css';
 import light from '@adobe/spectrum-css-temp/vars/spectrum-light-unique.css';
 import medium from '@adobe/spectrum-css-temp/vars/spectrum-medium-unique.css';
 import {Theme} from '@react-types/shared';
 
 export let defaultTheme: Theme = {
-	global,
+  global,
   light,
   dark,
   medium,
