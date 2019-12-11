64cf194cf1b1d36220f7b7edc0e3778a35e35992
Devon Govett <devongovett@gmail.com>
2019-12-10 18:57:41 -0800

    Separate Meter from ProgressBar (#87)
    * WIP: separate Meter from ProgressBar
    
    * Audit ProgressBar/ProgressCircle/Meter API
    
    * Use actual label element
    
    * Add missing dep
    
    * Update package.json
    
    * Update package.json

---
+++ packages/@react-spectrum/meter/src/Meter.tsx
@@ -0,0 +1,38 @@
+import {classNames, DOMRef} from '@react-spectrum/utils';
+import {ProgressBarBase, SpectrumProgressBarBaseProps} from '@react-spectrum/progress';
+import React from 'react';
+import styles from '@adobe/spectrum-css-temp/components/barloader/vars.css';
+import {useMeter} from '@react-aria/meter';
+
+export interface SpectrumMeterProps extends SpectrumProgressBarBaseProps {
+  variant?: 'positive' | 'warning' | 'critical'
+}
+
+function Meter(props: SpectrumMeterProps, ref: DOMRef<HTMLDivElement>) {
+  let {variant = 'positive', ...otherProps} = props;
+  const {
+    meterProps,
+    labelProps
+  } = useMeter({...otherProps, textValue: otherProps.valueLabel as any});
+
+  return (
+    <ProgressBarBase
+      {...otherProps}
+      ref={ref}
+      barProps={meterProps}
+      labelProps={labelProps}
+      barClassName={
+        classNames(
+          styles,
+          {
+            'is-positive': variant === 'positive',
+            'is-warning': variant === 'warning',
+            'is-critical': variant === 'critical'
+          }
+        )
+      } />
+  );
+}
+
+let _Meter = React.forwardRef(Meter);
+export {_Meter as Meter};
