37fa960ead512bf9de1e20f3877a3dee90998ab4
Daniel <dlu@livefyre.com>
2019-12-11 15:54:23 -0800

    fixing lint

--- packages/@react-spectrum/textfield/src/TextField.tsx
+++ packages/@react-spectrum/textfield/src/TextField.tsx
@@ -2,11 +2,10 @@ import React, {forwardRef, RefObject} from 'react';
 import {SpectrumTextFieldProps, TextFieldRef} from './types';
 import {TextFieldBase} from './TextFieldBase';
 
-export const TextField = forwardRef((props: SpectrumTextFieldProps, ref: RefObject<TextFieldRef>) => {
-
-  return (
+export const TextField = forwardRef((props: SpectrumTextFieldProps, ref: RefObject<TextFieldRef>) =>
+  (
     <TextFieldBase
       {...props}
       ref={ref} />
-  );
-});
+  )
+);
