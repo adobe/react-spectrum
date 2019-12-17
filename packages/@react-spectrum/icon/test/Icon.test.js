c7d9dbb67b7bab1fc49cab077e7f093101879655
Michael Jordan <mijordan@adobe.com>
2019-12-17 11:45:55 -0500

    RSP-1360 [Accessibility] v3 Icon should support role override
    RSP-1360 Use `toBeTruthy` rather than `not.toBeNull`

--- packages/@react-spectrum/icon/test/Icon.test.js
+++ packages/@react-spectrum/icon/test/Icon.test.js
@@ -26,7 +26,7 @@ describe('Icon', function () {
     rerender(<Component><FakeIcon /></Component>);
     icon = getByRole('img');
     expect(icon).not.toHaveAttribute('aria-label');
-    expect(icon).toHaveAttribute('aria-hidden');
+    expect(icon).toHaveAttribute('aria-hidden', 'true');
   });
 
   it.each`
@@ -36,5 +36,52 @@ describe('Icon', function () {
     let tree = render(<Component size="XL"><FakeIcon /></Component>);
     let icon = tree.getByRole('img');
     expect(icon).toHaveAttribute('class', expect.stringContaining('XL'));
+  });
+
+  it.each`
+    Name      | Component
+    ${'Icon'} | ${Icon}
+  `('$Name supports role prop override', function ({Component}) {
+    let {getByRole, rerender} = render(<Component role="presentation"><FakeIcon /></Component>);
+
+    let icon = getByRole('presentation');
+    expect(icon).toHaveAttribute('focusable', 'false');
+    expect(icon).toHaveAttribute('aria-hidden', 'true');
+
+    rerender(<Component role="alert" alt="alt"><FakeIcon /></Component>);
+    icon = getByRole('alert');
+    expect(icon).toHaveAttribute('aria-label', 'alt');
+    expect(icon).not.toHaveAttribute('aria-hidden');
+
+    rerender(<Component role="alert" aria-label="aria-label"><FakeIcon /></Component>);
+    icon = getByRole('alert');
+    expect(icon).toHaveAttribute('aria-label', 'aria-label');
+    expect(icon).not.toHaveAttribute('aria-hidden');
+
+    rerender(<Component role="alert"><FakeIcon /></Component>);
+    icon = getByRole('alert');
+    expect(icon).not.toHaveAttribute('aria-label');
+    expect(icon).toHaveAttribute('aria-hidden', 'true');
+
+    rerender(<Component><FakeIcon /></Component>);
+    icon = getByRole('img');
+    expect(icon).not.toHaveAttribute('aria-label');
+    expect(icon).toHaveAttribute('aria-hidden', 'true');
+  });
+
+  it.each`
+    Name      | Component
+    ${'Icon'} | ${Icon}
+  `('$Name supports aria-hidden prop', function ({Component}) {
+    let {getByRole, rerender} = render(<Component alt="explicitly hidden alt" aria-hidden="true"><FakeIcon /></Component>);
+
+    let icon = getByRole('img');
+    expect(icon).toHaveAttribute('aria-label', 'explicitly hidden alt');
+    expect(icon).toHaveAttribute('aria-hidden', 'true');
+
+    rerender(<Component aria-label="explicitly not hidden aria-label" aria-hidden="false"><FakeIcon /></Component>);
+    icon = getByRole('img');
+    expect(icon).toHaveAttribute('aria-label', 'explicitly not hidden aria-label');
+    expect(icon).not.toHaveAttribute('aria-hidden');
   });
 });
