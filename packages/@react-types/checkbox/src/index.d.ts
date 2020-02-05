7fc30951a4819aaa4bf2fc7ab2d01dae2ef701ed
Jean Leconte II <lecontei@adobe.com>
2020-02-05 13:27:04 -0800

    js docs rough draft

--- packages/@react-types/checkbox/src/index.d.ts
+++ packages/@react-types/checkbox/src/index.d.ts
@@ -3,7 +3,6 @@ import {ReactNode} from 'react';
 
 export interface CheckboxBase extends InputBase {
   children?: ReactNode, // pass in children to render label
-
   defaultSelected?: boolean,
   isSelected?: boolean,
   onChange?: (isSelected: boolean) => void,
@@ -12,9 +11,18 @@ export interface CheckboxBase extends InputBase {
 }
 
 export interface CheckboxProps extends CheckboxBase {
+  /**
+   * Indeterminism is presentational, when a checkbox is indeterminate, it overrides the selection state.
+   * Any subsequent click or tap will select the checkbox, and update all values as checked or unchecked.
+   * The indeterminate visual representation remains, masking the checkbox value.
+   */
   isIndeterminate?: boolean
 }
 
 export interface SpectrumCheckboxProps extends CheckboxProps, DOMProps, StyleProps {
+  /**
+   * By default, checkboxes are not emphasized (gray).
+   * The emphasized (blue) version provides visual prominence.
+   */
   isEmphasized?: boolean
 }
