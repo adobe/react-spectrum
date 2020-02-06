aed3cae1b54c3b4456f555b660572e869d0d3378
Daniel <dlu@livefyre.com>
2020-02-05 16:40:47 -0800

    Adding jsdocs to inputs and labelable types

--- packages/@react-types/shared/src/inputs.d.ts
+++ packages/@react-types/shared/src/inputs.d.ts
@@ -2,29 +2,48 @@ import {FocusableProps} from './events';
 
 export type ValidationState = 'valid' | 'invalid';
 export interface InputBase extends FocusableProps {
+  /** Whether the input is disabled */
   isDisabled?: boolean,
+  /** Whether user input is required on the input before form submission */
   isRequired?: boolean,
+  /** 
+   * Whether the input should display its "invalid" visual styling. 
+   * @default "valid"
+   */
   validationState?: ValidationState,
+  /** Whether the input can be selected but not changed by the user */
   isReadOnly?: boolean
 }
 
 export interface ValueBase<T> {
+  /** The value of a input, sets input behavior to "controlled" */
   value?: T,
+  /** The value of a input, sets input behavior to "uncontrolled" */
   defaultValue?: T,
+  /** 
+   * A user defined callback function triggered upon any change to the input's value.
+   * Passes the new value as an input to the callback.
+   */
   onChange?: (value: T) => void,
 }
 
 export interface TextInputBase {
+  /** Temporary text that occupies the text input when it is empty */
   placeholder?: string
 }
 
 export interface RangeValue<T> {
+  /** The value of start position of the range */
   start: T,
+  /** The value of end position of the range */
   end: T
 }
 
 export interface RangeInputBase<T> {
+  /** The smallest value allowed for the input */
   minValue?: T,
+  /** The largest value allowed for the input */
   maxValue?: T,
+  /** The amount that the input value changes with each increment or decrement "tick" */
   step?: T // ??
 }
--- packages/@react-types/shared/src/labelable.d.ts
+++ packages/@react-types/shared/src/labelable.d.ts
@@ -5,12 +5,27 @@ export type Alignment = 'start' | 'end';
 export type NecessityIndicator = 'icon' | 'label';
 
 export interface LabelableProps {
+  /** The content to display as the label */
   label?: ReactNode,
+  /** Whether the label is labeling a required field or group */
   isRequired?: boolean
 }
 
 export interface SpectrumLabelableProps extends LabelableProps {
+  /** 
+   * The label's overall position relative to the element it is labeling.
+   * @default "top" 
+   */
   labelPosition?: LabelPosition,
+  /** 
+   * The label's horizontal alignment relative to the element it is labeling.
+   * @default "start" 
+   */
   labelAlign?: Alignment,
+  /** 
+   * Controls if the label displays its field's or group's required status via 
+   * an asterisk icon or a “(required)” label.
+   * @default "icon" 
+   */
   necessityIndicator?: NecessityIndicator
 }
