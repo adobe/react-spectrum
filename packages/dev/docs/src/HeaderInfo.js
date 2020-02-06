cc296176dc676436a11d1619112c96d0453cf829
Danni <drobins@adobe.com>
2020-02-05 20:10:17 -0800

    cleaning heading table

--- packages/dev/docs/src/HeaderInfo.js
+++ packages/dev/docs/src/HeaderInfo.js
@@ -11,7 +11,7 @@ Lowlight.registerLanguage('js', js);
 
 export function HeaderInfo(props) {
   let {
-    headerData,
+    packageData,
     componentNames,
     sourceData
   } = props;
@@ -22,16 +22,16 @@ export function HeaderInfo(props) {
         <tbody>
           <tr>
             <th className={bodyStyles['spectrum-Body--secondary']}>install</th>
-            <td className={bodyStyles['spectrum-Body4']}><code className={typographyStyles['spectrum-Code4']}>yarn add {headerData.name}</code></td>
+            <td className={bodyStyles['spectrum-Body4']}><code className={typographyStyles['spectrum-Code4']}>yarn add {packageData.name}</code></td>
           </tr>
           <tr>
             <th className={bodyStyles['spectrum-Body--secondary']}>version</th>
-            <td className={bodyStyles['spectrum-Body4']}>{headerData.version}</td>
+            <td className={bodyStyles['spectrum-Body4']}>{packageData.version}</td>
           </tr>
           <tr>
             <th className={bodyStyles['spectrum-Body--secondary']}>usage</th>
             <td className={bodyStyles['spectrum-Body4']}>
-              <Lowlight language="js" value={`import {${componentNames.join(',')}} from '${headerData.name}'`} inline className={typographyStyles['spectrum-Code4']} />
+              <Lowlight language="js" value={`import {${componentNames.join(',')}} from '${packageData.name}'`} inline className={typographyStyles['spectrum-Code4']} />
             </td>
           </tr>
         </tbody>
@@ -40,6 +40,8 @@ export function HeaderInfo(props) {
         {sourceData.map((source) => (
           <ResourceCard type={source.type} url={source.url} />
         ))}
+        <ResourceCard type="GitHub" url={`https://github.com/adobe-private/react-spectrum-v3/tree/master/packages/${encodeURI(packageData.name)}`} />
+        <ResourceCard type="NPM" url={`https://www.npmjs.com/package/${encodeURI(packageData.name)}`} />
       </div>
     </>
   );
