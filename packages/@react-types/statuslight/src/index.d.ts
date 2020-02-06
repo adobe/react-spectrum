bd105e810ea715796fad4b7f2b9bd75c251b074f
pst67662 <pst67662@adobe.com>
2020-02-06 13:49:36 +0100

    RSP-1263: Documentation for StatusLight

--- packages/@react-types/statuslight/src/index.d.ts
+++ packages/@react-types/statuslight/src/index.d.ts
@@ -2,7 +2,16 @@ import {DOMProps, StyleProps} from '@react-types/shared';
 import {ReactNode} from 'react';
 
 export interface SpectrumStatusLightProps extends DOMProps, StyleProps {
+  /** The content to display as the label */
   children: ReactNode,
+  /**
+   * The variant changes the color of the status light
+   * When status lights have a semantic meaning, they should use the variant for semantic colors.
+   */
   variant: 'positive' | 'negative' | 'notice' | 'info' | 'neutral' | 'celery' | 'chartreuse' | 'yellow' | 'magenta' | 'fuchsia' | 'purple' | 'indigo' | 'seafoam',
+  /**
+   * Whether the status light is disabled or not.
+   * Shows that an element exists, but is not available in that circumstance.
+   */
   isDisabled?: boolean
 }
