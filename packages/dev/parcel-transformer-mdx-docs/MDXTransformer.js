129389c3f2005aa3fb3524b9a2b7cfb3d1bd217b
Rob Snow <rsnow@adobe.com>
2020-02-07 13:31:46 -0800

    Fix mdx processing so we don't need Fragment

--- packages/dev/parcel-transformer-mdx-docs/MDXTransformer.js
+++ packages/dev/parcel-transformer-mdx-docs/MDXTransformer.js
@@ -30,7 +30,6 @@ module.exports = new Transformer({
             }
 
             exampleCode.push(code);
-            node.meta = null;
 
             return [
               node,
@@ -46,8 +45,115 @@ module.exports = new Transformer({
       })
     );
 
+    const openingTag = '<>\n';
+    const closingTag = '\n</>';
+    const fragmentWrap = () => (tree, file) => (
+      flatMap(tree, node => {
+        if (node.type === 'code') {
+          if (node.meta === 'example') {
+            let code = node.value;
+            if (!code.startsWith('function ')) {
+              node.value = `${openingTag}${code}${closingTag}`;
+            }
+
+            return [
+              node
+            ];
+          }
+        }
+
+        return [node];
+      })
+    );
+
+    const get = p => o =>
+      p.reduce((xs, x) =>
+        (xs && xs[x]) ? xs[x] : null, o);
+
+    /**
+     * Example of what the nodes look like
+{
+  type: 'code',
+  lang: 'tsx',
+  meta: 'example',
+  value: '<>\n<Button variant="cta">Test</Button>\n</>',
+  position: Position {
+    start: { line: 28, column: 1, offset: 646 },
+    end: { line: 30, column: 4, offset: 700 },
+    indent: [ 1, 1 ]
+  },
+  data: {
+    hProperties: { className: [ 'tree-sitter', 'language-tsx' ] },
+    hChildren: [
+      {
+        type: 'element',
+        tagName: 'span',
+        properties: { className: [ 'source', 'ts' ] },
+        children: [
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: [ 'keyword', 'operator', 'js' ] },
+            children: [ { type: 'text', value: '<' } ]
+          },
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: [ 'keyword', 'operator', 'js' ] },
+            children: [ { type: 'text', value: '>' } ]
+          },
+          { type: 'text', value: '\n' },
+          ...
+          { type: 'text', value: '\n' },
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: [ 'keyword', 'operator', 'js' ] },
+            children: [ { type: 'text', value: '<' } ]
+          },
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: [ 'keyword', 'operator', 'js' ] },
+            children: [ { type: 'text', value: '/' } ]
+          },
+          {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: [ 'keyword', 'operator', 'js' ] },
+            children: [ { type: 'text', value: '>' } ]
+          }
+     */
+
+    const fragmentUnWrap = () => (tree, file) => (
+      flatMap(tree, node => {
+        if (node.type === 'code') {
+          if (node.meta === 'example' && node.data.hChildren) {
+            if (get(['data', 'hChildren', 0, 'children', 1, 'children', 0, 'value'])(node) === '>') {
+              // unshift the children that make up `<>\n`
+              node.data.hChildren[0].children.shift();
+              node.data.hChildren[0].children.shift();
+              node.data.hChildren[0].children.shift();
+
+              // remove the last children that make up `\n</>`
+              node.data.hChildren[0].children.length = node.data.hChildren[0].children.length - 4;
+
+              // fix the 'value' field to reflect what we've done getting rid of the wrapping <></>
+              node.value = node.value.slice(3, node.value.length - 4);
+            }
+
+            return [
+              node
+            ];
+          }
+        }
+
+        return [node];
+      })
+    );
+
     const compiled = await mdx(await asset.getCode(), {
-      remarkPlugins: [extractExamples, [treeSitter, {grammarPackages: ['@atom-languages/language-typescript']}]]
+      remarkPlugins: [extractExamples, fragmentWrap, [treeSitter, {grammarPackages: ['@atom-languages/language-typescript']}], fragmentUnWrap]
     });
 
     let exampleBundle = exampleCode.length === 0
