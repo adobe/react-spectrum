6d3f42c34e932a3cd8b52f8e1012e9c2b1917caf
Danni <drobins@adobe.com>
2020-02-13 10:49:27 -0800

    update stories

--- packages/@react-spectrum/button/stories/ActionButton.stories.tsx
+++ packages/@react-spectrum/button/stories/ActionButton.stories.tsx
@@ -3,7 +3,7 @@
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
@@ -16,6 +16,7 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 storiesOf('Button/ActionButton', module)
+  .addParameters({providerSwitcher: {status: "positive"}})
   .add(
     'default',
     () => render()
