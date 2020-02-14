7829e6a1d2477e030fbe87b29593749d6b08c52e
Devon Govett <devongovett@gmail.com>
2020-02-14 14:38:17 -0800

    Remove eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,8 @@ const ERROR = 2;
 let rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = './bin';
 
-const HEADER = `
- * Copyright ${new Date().getFullYear()} Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- `;
-
 module.exports = {
-  plugins: ['react', 'rulesdir', 'jsx-a11y', 'react-hooks', 'jest', 'import', 'monorepo', 'header'],
+  plugins: ['react', 'rulesdir', 'jsx-a11y', 'react-hooks', 'jest', 'import', 'monorepo'],
   extends: ['eslint:recommended'],
   parser: 'babel-eslint',
   parserOptions: {
@@ -108,7 +96,6 @@ module.exports = {
     'no-nested-ternary': ERROR,
     'no-multiple-empty-lines': ERROR,
     'no-unneeded-ternary': ERROR,
-    'header/header': [ERROR, 'block', HEADER],
 
     // Below are rules that are needed for linter functionality when using React
     'react/display-name': OFF,
