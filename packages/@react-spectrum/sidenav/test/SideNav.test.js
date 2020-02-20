d03f5003afcf5e532733e2433dd4b7b950d4cd61
pst67662 <pst67662@adobe.com>
2020-02-20 15:46:51 +0100

    [WIP]RSP-1516: More SideNav tests

--- packages/@react-spectrum/sidenav/test/SideNav.test.js
+++ packages/@react-spectrum/sidenav/test/SideNav.test.js
@@ -1,45 +1,76 @@
 import {cleanup, render, waitForDomChange} from '@testing-library/react';
-import {Item} from '@react-spectrum/tree';
-import {Provider} from '@react-spectrum/provider';
+import {Item, Section} from '@react-spectrum/tree';
 import React from 'react';
-import scaleMedium from '@adobe/spectrum-css-temp/vars/spectrum-medium-unique.css';
 import {SideNav} from '../src';
-import themeLight from '@adobe/spectrum-css-temp/vars/spectrum-light-unique.css';
+import {triggerPress} from '@react-spectrum/test-utils';
 import {SideNav as V2SideNav, SideNavItem as V2SideNavItem} from '@react/react-spectrum/SideNav';
 
-let theme = {
-  light: themeLight,
-  medium: scaleMedium
-};
+let flatItems = [
+  {name: 'Foo'},
+  {name: 'Bar'},
+  {name: 'Bob'},
+  {name: 'Alice'}
+];
 
-function renderComponent(Component, props) {
-  if (Component === V2SideNav) {
-    return render(
-      <V2SideNav>
-        <V2SideNavItem>Foo</V2SideNavItem >
-        <V2SideNavItem>Bar</V2SideNavItem>
-        <V2SideNavItem>Bob</V2SideNavItem>
-        <V2SideNavItem>Alice</V2SideNavItem>
-      </V2SideNav>
-    );
-  } else {
-    return render(
-      <Provider theme={theme}>
-        <SideNav >
-          <Item>Foo</Item>
-          <Item>Bar</Item>
-          <Item>Bob</Item>
-          <Item>Alice</Item>
-        </SideNav>
-      </Provider>
-    );
+let withSection = [
+  {name: 'Section 1', children: [
+    {name: 'Foo'},
+    {name: 'Bar'}
+  ]},
+  {name: 'Section 2', children: [
+    {name: 'Bob'},
+    {name: 'Alice'}
+  ]}
+];
+
+function renderComponent(Name, Component, ComponentSection, ComponentItem, props = {}) {
+  switch (Name) {
+    case 'SideNav':
+      return render(
+        <Component {...props} items={flatItems} itemKey="name">
+          {item => <ComponentItem>{item.name}</ComponentItem>}
+        </Component>
+      );
+    case 'V2SideNav':
+    case 'SideNavStatic':
+      return render(
+        <Component {...props} >
+          <ComponentItem>Foo</ComponentItem>
+          <ComponentItem>Bar</ComponentItem>
+          <ComponentItem>Bob</ComponentItem>
+          <ComponentItem>Alice</ComponentItem>
+        </Component>
+      );
+    case 'SideNavWithSections':
+      return render(
+        <Component {...props} items={withSection} itemKey="name">
+          {item => (
+            <ComponentSection items={item.children} title={item.name}>
+              {item => <ComponentItem>{item.name}</ComponentItem>}
+            </ComponentSection>
+          )}
+        </Component>
+      );
+    case 'SideNavStaticWithSections':
+      return render(
+        <Component {...props} >
+          <ComponentSection title="Section 1">
+            <ComponentItem>Foo</ComponentItem>
+            <ComponentItem>Bar</ComponentItem>
+          </ComponentSection>
+          <ComponentSection title="Section 2">
+            <ComponentItem>Bob</ComponentItem>
+            <ComponentItem>Alice</ComponentItem>
+          </ComponentSection>
+        </Component>
+      );
+    default:
+      return null;
   }
 }
 
 describe('SideNav', function () {
-  afterEach(() => {
-    cleanup();
-  });
+  afterEach(cleanup);
 
   let stub1, stub2;
   beforeAll(function () {
@@ -52,11 +83,14 @@ describe('SideNav', function () {
   });
 
   it.each`
-    Name           | Component    | props
-    ${'SideNav'}   | ${SideNav}   | ${{}}
-    ${'V2SideNav'} | ${V2SideNav} | ${{}}
-  `('$Name has default behavior', async function ({Component}) {
-    let {getByRole, getAllByRole, getByText} = renderComponent(Component);
+    Name                           | Component    | ComponentSection | ComponentItem
+    ${'SideNav'}                   | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavStatic'}             | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavWithSections'}       | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavStaticWithSections'} | ${SideNav}   | ${Section}       | ${Item}
+    ${'V2SideNav'}                 | ${V2SideNav} | ${V2SideNavItem} | ${V2SideNavItem}
+  `('$Name has default behavior', async function ({Name, Component, ComponentSection, ComponentItem}) {
+    let {getByRole, getAllByRole, getByText} = renderComponent(Name, Component, ComponentSection, ComponentItem);
 
     if (Component === SideNav) {
       // wait for v3 components
@@ -89,5 +123,91 @@ describe('SideNav', function () {
     let sideNavListItemLinks = getAllByRole('link');
     expect(sideNavListItemLinks.length).toBe(4);
     expect(sideNavListItemLinks[0].getAttribute('target')).toBe('_self');
+  });
+
+  it.each`
+    Name           | Component    | ComponentSection       | ComponentItem | props
+    ${'SideNav'}   | ${SideNav}   | ${Section}             | ${Item}       | ${{UNSAFE_className: 'test-class'}}
+    ${'V2SideNav'} | ${V2SideNav} | ${V2SideNavItem} | ${V2SideNavItem}}   | ${{className: 'test-class'}}
+  `('$Name can pass className', async function ({Name, Component, ComponentSection, ComponentItem, props}) {
+    let {getByRole} = renderComponent(Name, Component, ComponentSection, ComponentItem, props);
+
+    if (Component === SideNav) {
+      // wait for v3 components
+      await waitForDomChange();
+    }
+
+    let sideNav = getByRole('navigation');
+    expect(sideNav).toHaveAttribute('class', expect.stringContaining('test-class'));
+  });
+
+  // omitting v2 component
+  it.each`
+    Name                           | Component    | ComponentSection | ComponentItem
+    ${'SideNav'}                   | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavStatic'}             | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavWithSections'}       | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavStaticWithSections'} | ${SideNav}   | ${Section}       | ${Item}
+  `('$Name can select an item', async function ({Name, Component, ComponentSection, ComponentItem}) {
+    let spy = jest.fn();
+    let {getByText} = renderComponent(Name, Component, ComponentSection, ComponentItem, {onSelectionChange: spy});
+
+    await waitForDomChange();
+
+    let [foo, bar, bob, alice] = [
+      getByText('Foo'),
+      getByText('Bar'),
+      getByText('Bob'),
+      getByText('Alice')
+    ];
+
+    bar.focus();
+    triggerPress(bar);
+  
+    expect(foo).not.toHaveAttribute('aria-current');
+    expect(foo).toHaveAttribute('tabindex', '-1');
+    expect(bar).toHaveAttribute('aria-current', 'page');
+    expect(bar).toHaveAttribute('tabindex', '0');
+    expect(bob).not.toHaveAttribute('aria-current');
+    expect(bob).toHaveAttribute('tabindex', '-1');
+    expect(alice).not.toHaveAttribute('aria-current');
+    expect(alice).toHaveAttribute('tabindex', '-1');
+
+    expect(spy).toBeCalledTimes(1);
+
+    alice.focus();
+    triggerPress(alice);
+
+    expect(foo).not.toHaveAttribute('aria-current');
+    expect(foo).toHaveAttribute('tabindex', '-1');
+    expect(bar).not.toHaveAttribute('aria-current');
+    expect(bar).toHaveAttribute('tabindex', '-1');
+    expect(bob).not.toHaveAttribute('aria-current');
+    expect(bob).toHaveAttribute('tabindex', '-1');
+    expect(alice).toHaveAttribute('aria-current', 'page');
+    expect(alice).toHaveAttribute('tabindex', '0');
+
+    expect(spy).toBeCalledTimes(2);
+  });
+
+  it.each`
+    Name                           | Component    | ComponentSection | ComponentItem
+    ${'SideNavWithSections'}       | ${SideNav}   | ${Section}       | ${Item}
+    ${'SideNavStaticWithSections'} | ${SideNav}   | ${Section}       | ${Item}
+  `('$Name can render sections', async function ({Name, Component, ComponentSection, ComponentItem}) {
+    let {getByText, getAllByRole} = renderComponent(Name, Component, ComponentSection, ComponentItem);
+
+    await waitForDomChange();
+
+    let [section1, section2] = [
+      getByText('Section 1'),
+      getByText('Section 2')
+    ];
+
+    expect(section1).toBeTruthy();
+    expect(section2).toBeTruthy();
+
+    expect(getAllByRole('link').length).toBe(4);
+    expect(getAllByRole('presentation').length).toBe(6);
   });
 });
