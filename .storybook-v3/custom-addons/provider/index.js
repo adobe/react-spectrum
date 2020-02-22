b01b2fec7e6e56abcdadcb568ffd871c695fc22d
Danni <drobins@adobe.com>
2020-02-21 16:34:08 -0800

    fix storybook warnings

--- .storybook-v3/custom-addons/provider/index.js
+++ .storybook-v3/custom-addons/provider/index.js
@@ -47,7 +47,7 @@ function ProviderUpdater(props) {
 
   return (
     <Provider theme={theme} colorScheme={colorScheme} scale={scaleValue} locale={localeValue} toastPlacement={toastPositionValue} typekitId="pbi5ojv">
-      <div style={{'padding-top': '20px', 'padding-left': '20px', 'padding-right': '20px'}}><div style={{'font-size': '18px', 'padding-left': '10px', 'padding-right': '10px'}}><strong>Status</strong></div><StatusLight variant={props.options.status || 'negative'}>{statusMap[props.options.status || 'negative']}</StatusLight></div>
+      <div style={{'paddingTop': '20px', 'paddingLeft': '20px', 'paddingRight': '20px'}}><div style={{'fontSize': '18px', 'paddingLeft': '10px', 'paddingRight': '10px'}}><strong>Status</strong></div><StatusLight variant={props.options.status || 'negative'}>{statusMap[props.options.status || 'negative']}</StatusLight></div>
       {storyReady && props.children}
     </Provider>
   );
