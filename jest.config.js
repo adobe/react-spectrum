d830175cef610a564b688420a15f146b3485a06a
Rob Snow <rsnow@adobe.com>
2020-02-26 14:35:44 -0800

    try submitting to codecov

--- jest.config.js
+++ jest.config.js
@@ -67,7 +67,14 @@ module.exports = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: null,
+  // coverageThreshold: {
+  //       "global": {
+  //         "branches": 80,
+  //         "functions": 80,
+  //         "lines": 80,
+  //         "statements": -10
+  //       }
+  //     },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: null,
