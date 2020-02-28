92758c1b3bee19ada0b773fa8c2acf17a05d632e
Jean Leconte II <lecontei@adobe.com>
2020-02-28 14:03:49 -0800

    delete failed files

--- packages/@react-aria/interactions/src/DOMPropsResponder.tsx
+++ packages/@react-aria/interactions/src/DOMPropsResponder.tsx
@@ -24,7 +24,6 @@ export const DOMPropsResponder = React.forwardRef(({children, ...props}: DOMProp
     ...props,
     ref,
     register() {
-      console.log('trying to register');
       isRegistered.current = true;
     }
   };
@@ -32,7 +31,6 @@ export const DOMPropsResponder = React.forwardRef(({children, ...props}: DOMProp
   // TODO: Think of a more generic message when this replaces the PressResponder as well
   useEffect(() => {
     if (!isRegistered.current) {
-      console.log("oposite of isRegistered.current", !isRegistered.current);
       console.warn(
         'A DOMPropsResponder was ultilized without a hoverable DOM node.'
       );
