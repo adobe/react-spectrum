6d98f997c231d6d595d786dd8d0ed4d764bd9b3a
Rob Snow <rsnow@adobe.com>
2020-03-11 09:07:39 -0700

    missed committing a file

---
+++ packages/@react-spectrum/test-utils/src/slots.js
@@ -0,0 +1,24 @@
+import {Grid} from '@react-spectrum/layout';
+import {Provider} from '@react-spectrum/provider';
+import React from 'react';
+import {render} from '@testing-library/react';
+import scaleMedium from '@adobe/spectrum-css-temp/vars/spectrum-medium-unique.css';
+import themeLight from '@adobe/spectrum-css-temp/vars/spectrum-light-unique.css';
+
+let theme = {
+  light: themeLight,
+  medium: scaleMedium
+};
+
+export function testSlotsAPI(Component) {
+  let {getByTestId, debug} = render(
+    <Provider theme={theme}>
+      <Grid data-testid="grid" slots={{dummySlot: {UNSAFE_className: 'slotClassName'}}}>
+        <Component slot="dummySlot" />
+      </Grid>
+    </Provider>
+  );
+  let root = getByTestId('grid');
+  debug();
+  expect(root.firstChild).toHaveClass('slotClassName');
+}
