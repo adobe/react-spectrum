fe5a53b9b10a9fa8d6667c273c88808d2a96d68e
Kyle Taborski <ktabors@yahoo.com>
2020-03-10 19:58:47 -0700

    adding JSDocs to meter, progress, switch and link typescript interfaces

--- packages/@react-types/progress/src/index.d.ts
+++ packages/@react-types/progress/src/index.d.ts
@@ -14,37 +14,75 @@ import {DOMProps, LabelPosition, StyleProps} from '@react-types/shared';
 import {ReactNode} from 'react';
 
 interface ProgressBaseProps {
+  /**
+   * The current value (controlled).
+   * @default "0"
+   */
   value?: number,
+  /**
+   * The smallest value allowed for the input.
+   * @default "0"
+   */
   minValue?: number,
+  /**
+   * The largest value allowed for the input.
+   * @default "100"
+   */
   maxValue?: number
 }
 
 export interface ProgressBarBaseProps extends ProgressBaseProps {
+  /** The content to display as the label. */
   label?: ReactNode,
+  /** Whether the value's label is displayed */
   showValueLabel?: boolean, // true by default if label, false by default if not
+  /** The display format of the value label. */
   formatOptions?: Intl.NumberFormatOptions, // defaults to formatting as a percentage.
+  /** The content to display as the value's label. */
   valueLabel?: ReactNode // custom value label (e.g. 1 of 4)
 }
 
 export interface ProgressBarProps extends ProgressBarBaseProps {
+  /**
+   * Whether presentation is indeterminate when progress isn't known.
+   * @default "false"
+   */
   isIndeterminate?: boolean
 }
 
 export interface ProgressCircleProps extends ProgressBaseProps {
+  /**
+   * Whether presentation is indeterminate when progress isn't known.
+   * @default "false"
+   */
   isIndeterminate?: boolean
 }
 
 export interface SpectrumProgressCircleProps extends ProgressCircleProps, DOMProps, StyleProps {
+  /**
+   * What the ProgressCircle's diameter should be.
+   * @default "M"
+   */
   size?: 'S' | 'M' | 'L',
+  /** The [visual style](https://spectrum.adobe.com/page/circle-loader/#Over-background-variant) of the ProgressCircle. */
   variant?: 'overBackground',
   isCentered?: boolean
 }
 
 export interface SpectrumProgressBarBaseProps extends ProgressBarBaseProps, DOMProps, StyleProps {
+  /**
+   * How thick the ProgressBar should be.
+   * @default "L"
+   */
   size?: 'S' | 'L',
+  /**
+   * The label's overall position relative to the element it is labeling.
+   * @default "top"
+   */
   labelPosition?: LabelPosition
 }
 
 export interface SpectrumProgressBarProps extends SpectrumProgressBarBaseProps, ProgressBarProps {
+  /** The [visual style](https://spectrum.adobe.com/page/bar-loader/#Over-background) of the ProgressBar. */
   variant?: 'overBackground'
 }
