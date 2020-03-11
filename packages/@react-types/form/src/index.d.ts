df70e7c825d89f54551794118d06c6fc509a48b7
Daniel <dlu@livefyre.com>
2020-03-11 12:37:14 -0700

    Form docs

--- packages/@react-types/form/src/index.d.ts
+++ packages/@react-types/form/src/index.d.ts
@@ -14,11 +14,21 @@ import {DOMProps, SpectrumLabelableProps, StyleProps, ValidationState} from '@re
 import {ReactElement} from 'react';
 
 export interface SpectrumFormProps extends DOMProps, StyleProps, SpectrumLabelableProps {
+  /** The contents of the form. */
   children: ReactElement<SpectrumLabelableProps> | ReactElement<SpectrumLabelableProps>[],
+  /** Whether the form elements are displayed with their quiet style. */
   isQuiet?: boolean,
+  /** Whether the form elements are rendered with their emphasized style (blue). */
   isEmphasized?: boolean,
+  /** Whether the form elements are disabled. */
   isDisabled?: boolean,
+  /** Whether user input is required on each of the form elements before form submission. */
   isRequired?: boolean,
+  /** Whether the form elements can be selected but not changed by the user. */
   isReadOnly?: boolean,
+  /** 
+   * Whether the form elements should display their "valid" or "invalid" visual styling. 
+   * @default "valid"
+   */
   validationState?: ValidationState
 }
