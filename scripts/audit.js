b40242528cce296fa9c4471de4d18b6709ee6aa8
Rob Snow <rsnow@adobe.com>
2020-03-18 14:43:04 -0700

    Start work to automate auditing changes needed based on the top level components we're going to release

---
+++ scripts/audit.js
@@ -0,0 +1,52 @@
+
+import glob from 'fast-glob';
+import fs from 'fs';
+import path from 'path';
+
+let startingPackages = glob.sync(path.dirname(__dirname) + '/packages/@react-spectrum/*/package.json');
+let allPackages = glob.sync(path.dirname(__dirname) + '/packages/**/package.json', {ignore: ['**/node_modules/**', '**/dist/**', '**/.parcel-cache/**']});
+
+function isOurs(dep, ourPackages) {
+  return ourPackages.some(ourDep => ourDep.includes(dep));
+}
+
+function traceDeps(dep, ourPackages) {
+  let nextPackage = ourPackages.find(ourDep => ourDep.includes(dep));
+  let contents = fs.readFileSync(nextPackage, 'utf8');
+  let parsed = JSON.parse(contents);
+  let name = parsed.name;
+  let isPrivate = parsed.private;
+  ourPackagesInUse.set(name, {...ourPackagesInUse.get(name), isPrivate});
+  let deps = parsed.dependencies;
+  deps && Object.entries(deps).filter(([key]) => {
+    return isOurs(key, ourPackages);
+  }).forEach(([key, value]) => {
+    if (ourPackagesInUse.has(key)) {
+      let versions = ourPackagesInUse.get(key).versions;
+      ourPackagesInUse.set(key, {...ourPackagesInUse.get(key), versions: [...versions, value]});
+    }
+    ourPackagesInUse.set(key, {versions: [value]});
+    traceDeps(key, ourPackages);
+  });
+}
+
+let ourPackagesInUse = new Map();
+
+for (let file of startingPackages) {
+  let contents = fs.readFileSync(file, 'utf8');
+  let parsed = JSON.parse(contents);
+  if (!parsed.private) {
+    let deps = parsed.dependencies;
+    deps && Object.entries(deps).filter(([key]) => {
+      return isOurs(key, allPackages);
+    }).forEach(([key, value]) => {
+      if (ourPackagesInUse.has(key)) {
+        ourPackagesInUse.set(key, {...ourPackagesInUse.get(key), versions: [...ourPackagesInUse.get(key).versions, value]});
+      }
+      ourPackagesInUse.set(key, {versions: [value]});
+      traceDeps(key, allPackages);
+    });
+  }
+}
+
+console.log(ourPackagesInUse);
