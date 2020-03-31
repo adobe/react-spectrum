3db8eb3c38699ffe62c3379b206f2a0ecd673096
Devon Govett <devongovett@gmail.com>
2020-03-31 16:29:15 -0700

    Update typography types

--- packages/@react-types/typography/src/keyboard.d.ts
+++ packages/@react-types/typography/src/keyboard.d.ts
@@ -11,8 +11,8 @@
  */
 
 import {DOMProps, StyleProps} from '@react-types/shared';
-import {ReactElement, ReactNode} from 'react';
+import {ReactNode} from 'react';
 
 export interface KeyboardProps extends DOMProps, StyleProps {
-  children: ReactElement | ReactNode
+  children: ReactNode
 }
