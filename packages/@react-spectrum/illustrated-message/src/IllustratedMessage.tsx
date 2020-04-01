effb3d2d7d63488dececb5a253803d9be8552388
Kyle Taborski <ktabors@yahoo.com>
2020-03-31 20:26:03 -0700

    IllustratedMessage using slots (#311)

--- packages/@react-spectrum/illustrated-message/src/IllustratedMessage.tsx
+++ packages/@react-spectrum/illustrated-message/src/IllustratedMessage.tsx
@@ -12,56 +12,52 @@
 
 import {classNames, filterDOMProps, useDOMRef, useSlotProps, useStyleProps} from '@react-spectrum/utils';
 import {DOMRef} from '@react-types/shared';
+import {Flex} from '@react-spectrum/layout';
 import React, {forwardRef} from 'react';
 import {SpectrumIllustratedMessageProps} from '@react-types/illustrated-message';
 import styles from '@adobe/spectrum-css-temp/components/illustratedmessage/vars.css';
 import typographyStyles from '@adobe/spectrum-css-temp/components/typography/vars.css';
-import {useIllustratedMessage} from '@react-aria/illustrated-message';
 
 function IllustratedMessage(props: SpectrumIllustratedMessageProps, ref: DOMRef<HTMLDivElement>) {
   props = useSlotProps(props);
   let {
-    illustration,
-    heading,
-    description,
+    children,
+    slots,
     ...otherProps
   } = props;
   let domRef = useDOMRef(ref);
   let {styleProps} = useStyleProps(otherProps);
-  let {
-    illustrationProps,
-    headingProps
-  } = useIllustratedMessage(props);
+  let headingClassName = classNames(
+    {},
+    classNames(typographyStyles, 'spectrum-Heading', 'spectrum-Heading--pageTitle'),
+    classNames(styles, 'spectrum-IllustratedMessage-heading')
+  );
+  let contentClassName = classNames(
+    {},
+    classNames(typographyStyles, 'spectrum-Body--secondary'),
+    classNames(styles, 'spectrum-IllustratedMessage-description')
+  );
 
-  // todo replace h2 with rsp heading when it exists
+  if (!slots) {
+    slots = {
+      heading: {UNSAFE_className: headingClassName},
+      content: {UNSAFE_className: contentClassName}
+    };
+  }
+
   return (
-    <div
+    <Flex
       {...filterDOMProps(otherProps)}
       {...styleProps}
-      ref={domRef}
-      className={classNames(styles, 'spectrum-IllustratedMessage', styleProps.className)}>
-      {illustration &&
-        React.cloneElement(illustration, {
-          ...illustrationProps,
-          className: classNames(styles, illustration.props.className, 'spectrum-IllustratedMessage-illustration')
-        })
-      }
-      {heading &&
-        <h2
-          {...headingProps}
-          className={
-            classNames(
-              {},
-              classNames(typographyStyles, 'spectrum-Heading', 'spectrum-Heading--pageTitle'),
-              classNames(styles, 'spectrum-IllustratedMessage-heading')
-            )}>
-          {heading}
-        </h2>
-      }
-      {description &&
-        <p className={classNames({}, classNames(typographyStyles, 'spectrum-Body--secondary'), classNames(styles, 'spectrum-IllustratedMessage-description'))}>{description}</p>
-      }
-    </div>
+      UNSAFE_className={classNames(
+        styles,
+        'spectrum-IllustratedMessage',
+        styleProps.className
+      )}
+      slots={slots}
+      ref={domRef}>
+      {children}
+    </Flex>
   );
 }
 
