315d6c2fb4f29ebc3256dcad7e8b162ab13e7e20
Daniel <dlu@livefyre.com>
2020-05-01 11:39:16 -0700

    removing extra key focus logic frm combobox arrowup/arrow down

--- packages/@react-aria/combobox/src/useComboBox.ts
+++ packages/@react-aria/combobox/src/useComboBox.ts
@@ -105,20 +105,12 @@ export function useComboBox<T>(props: AriaComboBoxProps<T>, state: ComboBoxState
       case 'ArrowDown':
         if (!state.isOpen) {
           state.toggle('first');
-        } else if (!focusedItem) { 
-          // If there isn't a focused item (allowsCustomValue=true) but the menu is open , set a item to focus when the user
-          // presses the arrow keys
-          let firstKey = state.collection.getFirstKey();
-          state.selectionManager.setFocusedKey(firstKey);
         }
 
         break;
       case 'ArrowUp':
         if (!state.isOpen) {
           state.toggle('last');
-        } else if (!focusedItem) { 
-          let lastKey = state.collection.getLastKey();
-          state.selectionManager.setFocusedKey(lastKey);
         }
 
         break;
