9111f2a8dad7e5b7986cd0f05faef9d3e21f3e7a
Daniel <dlu@livefyre.com>
2020-05-12 16:53:36 -0700

    using isolation:isolate to fix nested dialogs and moving it to Overlay

--- packages/@react-spectrum/provider/src/Provider.tsx
+++ packages/@react-spectrum/provider/src/Provider.tsx
@@ -149,8 +149,7 @@ const ProviderWrapper = React.forwardRef(function ProviderWrapper(props: Provide
     ...styleProps.style,
     // This ensures that browser native UI like scrollbars are rendered in the right color scheme.
     // See https://web.dev/color-scheme/.
-    colorScheme: props.colorScheme ?? Object.keys(theme).filter(k => k === 'light' || k === 'dark').join(' '),
-    opacity: .99
+    colorScheme: props.colorScheme ?? Object.keys(theme).filter(k => k === 'light' || k === 'dark').join(' ')
   };
 
   return (
