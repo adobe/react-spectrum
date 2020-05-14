e058b93c4a853333cc492a76eae52bb3e9c41742
Ross pfahler <pfahler@adobe.com>
2020-05-14 10:10:07 -0700

    Prop descriptions

--- packages/@react-types/actiongroup/src/index.d.ts
+++ packages/@react-types/actiongroup/src/index.d.ts
@@ -20,15 +20,20 @@ export interface ActionGroupProps<T> extends DOMProps, StyleProps, MultipleSelec
    * @default 'horizontal'
    */
   orientation?: Orientation,
+  /** An list of `Item` elements or an `ItemRenderer`. If the latter, a list of items must be provided as an separate prop. */
   children: ItemElement<T> | ItemElement<T>[] | ItemRenderer<T>,
+  /** A list of items to iterate through and display as children. Must be used with an `ItemRenderer` as the sole child. */
   items?: Iterable<T>,
+  /** A field used as the `uniqueKey` if providing a list of items as a prop. */
   itemKey?: string,
+  /** A list of `uniqueKeys` to disable. */
   disabledKeys?: Iterable<Key>,
   /**
    * Whether the ActionGroup is disabled.
    * Shows that a selection exists, but is not available in that circumstance.
    */
   isDisabled?: boolean,
+  /** Invoked when an action is taken on a child. The sole argument `key` is the uniqueKey for the item. */
   onAction?: (key: Key) => void
 }
 
