4485baa8fbe05a184c6183d4264505666ca33a77
Ross Pfahler <pfahler@adobe.com>
2020-05-20 09:37:05 -0700

    Inline font-face declarations from external css into docs.css (#476)
    This avoids two extra http calls on page load, one of which is to a different domain (p.typekit.net).

--- packages/dev/docs/src/Layout.js
+++ packages/dev/docs/src/Layout.js
@@ -113,7 +113,6 @@ function Page({children, title, styles, scripts}) {
             window.addEventListener('storage', update);
           })();
         `.replace(/\n|\s{2,}/g, '')}} />
-        <link rel="stylesheet" href="https://use.typekit.net/uma8ayv.css" />
         <link rel="preload" as="font" href="https://use.typekit.net/af/eaf09c/000000000000000000017703/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3" crossOrigin="" />
         <link rel="preload" as="font" href="https://use.typekit.net/af/cb695f/000000000000000000017701/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3" crossOrigin="" />
         <link rel="preload" as="font" href="https://use.typekit.net/af/505d17/00000000000000003b9aee44/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n9&v=3" crossOrigin="" />
