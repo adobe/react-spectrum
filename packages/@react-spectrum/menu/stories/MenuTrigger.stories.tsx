f8754e128d06feebca910ae99df40e5c0abe7e01
Devon Govett <devongovett@gmail.com>
2020-05-29 22:09:25 -0700

    Fix menu item heights (#547)

--- packages/@react-spectrum/menu/stories/MenuTrigger.stories.tsx
+++ packages/@react-spectrum/menu/stories/MenuTrigger.stories.tsx
@@ -110,34 +110,34 @@ storiesOf('MenuTrigger', module)
           </ActionButton>
           <Menu onAction={action('action')}>
             <Section title="Section 1">
-              <Item>
+              <Item textValue="Copy">
                 <Copy size="S" />
                 <Text>Copy</Text>
                 <Keyboard>⌘C</Keyboard>
               </Item>
-              <Item>
+              <Item textValue="Cut">
                 <Cut size="S" />
                 <Text>Cut</Text>
                 <Keyboard>⌘X</Keyboard>
               </Item>
-              <Item>
+              <Item textValue="Paste">
                 <Paste size="S" />
                 <Text>Paste</Text>
                 <Keyboard>⌘V</Keyboard>
               </Item>
             </Section>
             <Section title="Section 2">
-              <Item>
+              <Item textValue="Puppy">
                 <AlignLeft size="S" />
                 <Text>Puppy</Text>
                 <Text slot="description">Puppy description super long as well geez</Text>
               </Item>
-              <Item>
+              <Item textValue="Doggo with really really really long long long text">
                 <AlignCenter size="S" />
                 <Text>Doggo with really really really long long long text</Text>
                 <Text slot="end">Value</Text>
               </Item>
-              <Item>
+              <Item textValue="Floof">
                 <AlignRight size="S" />
                 <Text>Floof</Text>
               </Item>
