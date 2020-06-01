a5e835354367c0fc2f31c7b68695a5e9eb3138f2
Devon Govett <devongovett@gmail.com>
2020-06-01 13:53:36 -0700

    Remove typekitId prop from Provider

--- packages/@react-spectrum/provider/src/Provider.tsx
+++ packages/@react-spectrum/provider/src/Provider.tsx
@@ -11,7 +11,6 @@
  */
 
 import classNames from 'classnames';
-import configureTypekit from './configureTypekit';
 import {DOMRef} from '@react-types/shared';
 import {filterDOMProps} from '@react-aria/utils';
 import {Provider as I18nProvider, useLocale} from '@react-aria/i18n';
@@ -49,7 +48,6 @@ function Provider(props: ProviderProps, ref: DOMRef<HTMLDivElement>) {
   let {
     colorScheme = usePrevColorScheme ? prevColorScheme : autoColorScheme,
     scale = prevContext ? prevContext.scale : autoScale,
-    typekitId,
     locale = prevContext ? prevLocale : null,
     children,
     isQuiet,
@@ -79,10 +77,6 @@ function Provider(props: ProviderProps, ref: DOMRef<HTMLDivElement>) {
 
   // Merge options with parent provider
   let context = Object.assign({}, prevContext, filteredProps);
-
-  useEffect(() => {
-    configureTypekit(typekitId);
-  }, [typekitId]);
 
   // Only wrap in a DOM node if the theme, colorScheme, or scale changed
   let contents = children;
