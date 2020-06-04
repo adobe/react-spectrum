c4f69264b28d1c389e5d77f0084d2e055c7ad73e
Daniel <dlu@livefyre.com>
2020-06-04 16:37:28 -0700

    changing heading so it only gets aria-current from slots

--- packages/@react-spectrum/text/src/Heading.tsx
+++ packages/@react-spectrum/text/src/Heading.tsx
@@ -11,13 +11,22 @@
  */
 
 import {DOMRef} from '@react-types/shared';
-import {filterDOMProps} from '@react-aria/utils';
+import {filterDOMProps, mergeProps} from '@react-aria/utils';
 import {HeadingProps} from '@react-types/text';
 import React, {ElementType, forwardRef} from 'react';
 import {useDOMRef, useSlotProps, useStyleProps} from '@react-spectrum/utils';
 
+const slotDOMProps = new Set(['aria-current']);
+
 function Heading(props: HeadingProps, ref: DOMRef<HTMLHeadingElement>) {
-  props = useSlotProps(props, 'heading');
+  let slotProps = useSlotProps({}, 'heading');
+  props = mergeProps(slotProps, props);
+
+  let domProps = mergeProps(
+    filterDOMProps(props),
+    filterDOMProps(slotProps, {propNames: slotDOMProps})
+  );
+
   let {
     children,
     level = 3,
@@ -28,7 +37,7 @@ function Heading(props: HeadingProps, ref: DOMRef<HTMLHeadingElement>) {
   let HeadingTag = `h${level}` as ElementType;
 
   return (
-    <HeadingTag {...filterDOMProps(otherProps, {propNames: new Set(['aria-current'])})} {...styleProps} ref={domRef}>
+    <HeadingTag {...domProps} {...styleProps} ref={domRef}>
       {children}
     </HeadingTag>
   );
