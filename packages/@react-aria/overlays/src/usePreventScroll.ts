8d40e66b889b89521ab53cb29c717b4889b507b6
Danni <darobins@adobe.com>
2020-06-08 20:51:04 -0700

    try this

--- packages/@react-aria/overlays/src/usePreventScroll.ts
+++ packages/@react-aria/overlays/src/usePreventScroll.ts
@@ -19,14 +19,16 @@ import {useEffect} from 'react';
  */
 export function usePreventScroll(): void {
   useEffect(() => {
-    let overflow = document.body.style.overflow;
+    let overflowX = document.body.style.overflow;
     let paddingRight = document.body.style.paddingRight;
+    let position = document.body.style.position;
     document.body.style.paddingRight = window.innerWidth - document.documentElement.clientWidth + 'px';
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflowX = 'hidden';
     document.body.style.position = 'relative';
     return () => {
-      document.body.style.overflow = overflow;
+      document.body.style.overflowX = overflow;
       document.body.style.paddingRight = paddingRight;
+      document.body.style.position = position;
     };
   }, []);
 }
