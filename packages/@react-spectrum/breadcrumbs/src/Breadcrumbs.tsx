8fa3f30e7c480ab487ca6c4a9893aca53d71ea40
Devon Govett <devongovett@gmail.com>
2020-06-10 17:58:50 -0700

    Bugfix and some comments

--- packages/@react-spectrum/breadcrumbs/src/Breadcrumbs.tsx
+++ packages/@react-spectrum/breadcrumbs/src/Breadcrumbs.tsx
@@ -221,36 +221,48 @@ function Breadcrumbs<T>(props: SpectrumBreadcrumbsProps<T>, ref: DOMRef) {
   );
 }
 
+// This hook works like `useState`, but when setting the value, you pass a generator function
+// that can yield multiple values. Each yielded value updates the state and waits for the next
+// layout effect, then continues the generator. This allows sequential updates to state to be
+// written linearly.
 function useValueEffect(defaultValue) {
   let [value, setValue] = useState(defaultValue);
   let effect = useRef(null);
 
-  let next = useCallback(() => {
+  // Store the function in a ref so we can always access the current version
+  // which has the proper `value` in scope.
+  let nextRef = useRef(null);
+  nextRef.current = () => {
+    // Run the generator to the next yield.
     let newValue = effect.current.next();
+
+    // If the generator is done, reset the effect.
     if (newValue.done) {
       effect.current = null;
       return;
     }
 
-    setValue(value => {
-      if (value === newValue.value) {
-        next();
-      }
-
-      return newValue.value;
-    });
-  }, [effect, setValue]);
+    // If the value is the same as the current value,
+    // then continue to the next yield. Otherwise,
+    // set the value in state and wait for the next layout effect.
+    if (value === newValue.value) {
+      nextRef.current();
+    } else {
+      setValue(newValue.value);
+    }
+  };
 
   useLayoutEffect(() => {
+    // If there is an effect currently running, continue to the next yield.
     if (effect.current) {
-      next();
+      nextRef.current();
     }
   });
 
   let queue = useCallback(fn => {
     effect.current = fn();
-    next();
-  }, [effect, next]);
+    nextRef.current();
+  }, [effect, nextRef]);
 
   return [value, queue];
 }
