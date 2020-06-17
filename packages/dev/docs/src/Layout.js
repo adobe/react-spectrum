6ed9875766067adc6cd9319ed082d041118bff06
Devon Govett <devongovett@gmail.com>
2020-06-17 10:26:45 -0700

    Random perf improvements for the docs (#650)
    * Use font-display: swap to ensure text is visible while typekit is loading
    
    * Defer analytics script to reduce CPU usage during load and improve TTI

--- packages/dev/docs/src/Layout.js
+++ packages/dev/docs/src/Layout.js
@@ -139,7 +139,7 @@ function Page({children, currentPage, publicUrl, styles, scripts}) {
         <link rel="preload" as="font" href="https://use.typekit.net/af/74ffb1/000000000000000000017702/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=i4&v=3" crossOrigin="" />
         {styles.map(s => <link rel="stylesheet" href={s.url} />)}
         {scripts.map(s => <script type={s.type} src={s.url} defer />)}
-        <script src="https://assets.adobedtm.com/a7d65461e54e/01d650a3ee55/launch-4d5498348926.min.js" async />
+        <script src="https://assets.adobedtm.com/a7d65461e54e/01d650a3ee55/launch-4d5498348926.min.js" defer />
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
         <meta property="og:title" content={currentPage.title} />
