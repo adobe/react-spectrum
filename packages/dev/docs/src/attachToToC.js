f2b5a47fda6c772d0af69b3f14d2fc5d83ea9978
Ross pfahler <pfahler@adobe.com>
2020-06-17 12:27:30 -0700

    Wrap toc calculation in a raf to avoid reflow

--- packages/dev/docs/src/attachToToC.js
+++ packages/dev/docs/src/attachToToC.js
@@ -22,23 +22,23 @@ export function attachToToC() {
   }
 
   function updateToc() {
-    // this needs to be improved a little but the math hurts my head right now
-    // right now it's impossible to select the last section if the last two heights combined are smaller than the viewport height
-    headers.some((header, i) => {
-      if ((header.header.offsetTop + header.header.getBoundingClientRect().height) > document.documentElement.scrollTop) {
-        let currentSelection = document.querySelectorAll(`#toc .${sideNavStyles['is-selected']}`);
-        if (currentSelection) {
-          currentSelection.forEach(node => node.classList.remove(sideNavStyles['is-selected']));
+    requestAnimationFrame(() => {
+      // this needs to be improved a little but the math hurts my head right now
+      // right now it's impossible to select the last section if the last two heights combined are smaller than the viewport height
+      headers.some((header, i) => {
+        if ((header.header.offsetTop + header.header.getBoundingClientRect().height) > document.documentElement.scrollTop) {
+          let currentSelection = document.querySelectorAll(`#toc .${sideNavStyles['is-selected']}`);
+          if (currentSelection) {
+            currentSelection.forEach(node => node.classList.remove(sideNavStyles['is-selected']));
+          }
+          header.link.parentElement.classList.add(sideNavStyles['is-selected']);
+          return true;
         }
-        header.link.parentElement.classList.add(sideNavStyles['is-selected']);
-        return true;
-      }
+      });
     });
   }
 
   updateToc();
 
-  document.addEventListener('scroll', () => {
-    updateToc();
-  });
+  document.addEventListener('scroll', updateToc);
 }
