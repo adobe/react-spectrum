05c14e96db73bb7bab8f0db207e68ab59e1d9ba1
Michael Jordan <mijordan@adobe.com>
2020-06-18 21:24:32 -0400

    RSP-1834 [Accessibility][Docs] Popup links are not keyboard accessible in Firefox
    Popup links have a valid href, so usePress is not interpreting them as links that that React-Spectrum should handle with the Enter key, even though the default behavior has been prevented with event.preventDefault in and onClick handler.
    
    See: https://github.com/adobe-private/react-spectrum-v3/blob/master/packages/%40react-aria/interactions/src/usePress.ts#L573-L577
    
    One solution is to remove href and set role="link" and tabIndex="0". We also have to make sure that links within the Popup still work, but this may be another use case we need to account for in usePress.

--- packages/dev/docs/src/docs.js
+++ packages/dev/docs/src/docs.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /*
  * Copyright 2020 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
@@ -19,7 +21,7 @@ import docsStyle from './docs.css';
 import {Heading} from '@react-spectrum/text';
 import highlightCss from './syntax-highlight.css';
 import {Pressable} from '@react-aria/interactions';
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {ThemeProvider} from './ThemeSwitcher';
 
@@ -31,36 +33,50 @@ for (let link of links) {
     <ThemeProvider UNSAFE_className={docsStyle.inlineProvider}>
       <DialogTrigger type="popover">
         <Pressable>
-          <a href={link.href} data-link={link.dataset.link} className={link.className} onClick={e => e.preventDefault()}>{link.textContent}</a>
+          <a role="link" tabIndex={0} aria-haspopup="dialog" data-href={link.href} data-link={link.dataset.link} className={link.className}>{link.textContent}</a>
         </Pressable>
         <LinkPopover id={link.dataset.link} />
       </DialogTrigger>
     </ThemeProvider>
   , container);
 
-  link.parentNode.replaceChild(container, link);
+  link.parentNode.replaceChild(container.firstElementChild, link);
 }
 
 function LinkPopover({id}) {
   let ref = useRef();
   let [breadcrumbs, setBreadcrumbs] = useState([document.getElementById(id)]);
 
+  const onBlurHeading = useCallback((event) => {
+    event.target.removeEventListener('blur', onBlurHeading);
+    event.target.tabIndex = -1;
+  },
+  []);
+
   useEffect(() => {
-    let links = ref.current.querySelectorAll('[data-link]');
+    let links = ref.current.querySelectorAll(`.${docsStyle.popover} [data-link]`);
     for (let link of links) {
+      link.href = link.dataset.href;
+      link.removeAttribute('aria-haspopup');
       link.addEventListener('click', (e) => {
         e.preventDefault();
         setBreadcrumbs([...breadcrumbs, document.getElementById(link.dataset.link)]);
       });
     }
-  }, [breadcrumbs]);
+    const h3 = ref.current.closest(`.${highlightCss.spectrum}.${docsStyle.popover}`).querySelector('h3[aria-current]');
+    if (h3) {
+      h3.tabIndex = 0;
+      h3.addEventListener('blur', onBlurHeading);
+      requestAnimationFrame(() => h3.focus());
+    }
+  }, [breadcrumbs, onBlurHeading]);
 
   return (
     <Dialog UNSAFE_className={`${highlightCss.spectrum} ${docsStyle.popover}`} size="L">
       <View slot="heading">
         <Breadcrumbs onAction={(key) => setBreadcrumbs(breadcrumbs.slice(0, key))}>
           {breadcrumbs.map((b, i) => (
-            <Item key={i + 1}>
+            <Item key={i + 1} textValue={b.dataset.title}>
               {i < (breadcrumbs.length - 1) ? b.dataset.title : <Heading level={3}>{b.dataset.title}</Heading>}
             </Item>
           ))}
