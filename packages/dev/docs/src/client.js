c14971c209e3b52d557a56d8d863bf605c4b89d9
Devon Govett <devongovett@gmail.com>
2020-07-12 18:13:58 -0700

    Restore sidebar scroll position when navigating to a new page

--- packages/dev/docs/src/client.js
+++ packages/dev/docs/src/client.js
@@ -112,3 +112,18 @@ document.addEventListener('blur', (e) => {
   }
 }, true);
 
+let sidebar = document.querySelector('.' + docsStyle.nav);
+let lastSelectedItem = sessionStorage.getItem('sidebarSelectedItem');
+let lastScrollPosition = sessionStorage.getItem('sidebarScrollPosition');
+
+// If we have a recorded scroll position, and the last selected item is in the sidebar
+// (e.g. we're in the same category), then restore the scroll position.
+if (lastSelectedItem && lastScrollPosition && sidebar.querySelector(`a[href="${lastSelectedItem}"]`)) {
+  sidebar.scrollTop = parseInt(lastScrollPosition, 10);
+}
+
+// Save scroll position of the sidebar when we're about to navigate
+window.addEventListener('beforeunload', () => {
+  sessionStorage.setItem('sidebarSelectedItem', location.pathname);
+  sessionStorage.setItem('sidebarScrollPosition', sidebar.scrollTop);
+});
