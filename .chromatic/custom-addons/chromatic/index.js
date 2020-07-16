087c54164af5226a2e7dd29927efd538f5b10f2a
Rob Snow <rsnow@adobe.com>
2020-07-15 17:15:18 -0700

    working on the rest of buttons

--- .chromatic/custom-addons/chromatic/index.js
+++ .chromatic/custom-addons/chromatic/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import {Flex} from '@react-spectrum/layout';
+import {locales, scales, themes} from '../../constants';
 import {makeDecorator} from '@storybook/addons';
 import {Provider} from '@react-spectrum/provider';
-import {themes, scales, locales} from '../../constants';
+import React from 'react';
+import {defaultTheme} from '@adobe/react-spectrum';
 
 export const withChromaticProvider = makeDecorator({
   name: 'withChromaticProvider',
@@ -12,18 +14,20 @@ export const withChromaticProvider = makeDecorator({
     let height = options.height;
 
     return (
-      <div style={{height}}>
-        {(options.colorSchemes || Object.keys(themes)).map(colorScheme =>
-          (options.scales || Object.keys(scales)).map(scale =>
-            (colorScheme === 'light' ? selectedLocales : ['en-US']).map(locale =>
-              <Provider key={`${colorScheme}_${scale}_${locale}`} theme={themes[colorScheme]} colorScheme={colorScheme.replace(/est$/, '')} scale={scale} locale={locale} typekitId="pbi5ojv">
-                <h1 style={{margin: 0, padding: '10px'}}>{`${colorScheme}, ${scale}, ${locale}`}</h1>
-                {getStory(context)}
-              </Provider>
+      <Provider theme={defaultTheme}>
+        <Flex style={{height}} direction="column" gap="size-100">
+          {(options.colorSchemes || Object.keys(themes)).map(colorScheme =>
+            (options.scales || Object.keys(scales)).map(scale =>
+              (colorScheme === 'light' ? selectedLocales : ['en-US']).map(locale =>
+                <Provider key={`${colorScheme}_${scale}_${locale}`} theme={themes[colorScheme]} colorScheme={colorScheme.replace(/est$/, '')} scale={scale} locale={locale} typekitId="pbi5ojv">
+                  <h1 style={{margin: 0, padding: '10px'}}>{`${colorScheme}, ${scale}, ${locale}`}</h1>
+                  {getStory(context)}
+                </Provider>
+              )
             )
-          )
-        )}
-      </div>
+          )}
+        </Flex>
+      </Provider>
     )
   }
 });
