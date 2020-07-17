f83e0260def741dd8c84ad6bc25f48257ccbabd5
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-07-17 08:51:56 -0500

    Allow mergeProps to take arbitrary number of args

--- packages/@react-aria/utils/src/mergeProps.ts
+++ packages/@react-aria/utils/src/mergeProps.ts
@@ -18,42 +18,67 @@ interface Props {
   [key: string]: any
 }
 
+// taken from: https://stackoverflow.com/questions/51603250/typescript-3-parameter-list-intersection-type/51604379#51604379
+type TupleTypes<T> = { [P in keyof T]: T[P] } extends { [key: number]: infer V }
+  ? V
+  : never;
+type UnionToIntersection<U> = (U extends any
+  ? (k: U) => void
+  : never) extends ((k: infer I) => void)
+  ? I
+  : never;
+
 /**
  * Merges multiple props objects together. Event handlers are chained,
  * classNames are combined, and ids are deduplicated. For all other props,
- * the second props object overrides the first.
+ * the last prop object overrides all previous ones.
  * @param a - The first set of props to merge.
- * @param b - The second set of props to merge.
+ * @param rest - The remaining sets of props to merge.
  */
-export function mergeProps<T extends Props, U extends Props>(a: T, b: U): T & U {
-  let res: Props = {};
-  for (let key in a) {
-    // Chain events
-    if (/^on[A-Z]/.test(key) && typeof a[key] === 'function' && typeof b[key] === 'function') {
-      res[key] = chain(a[key], b[key]);
+export function mergeProps<T extends Props, U extends Props[]>(
+  a: T,
+  ...rest: U
+): T & UnionToIntersection<TupleTypes<U>> {
+  let result: Props = { ...a };
+  for (let b of rest) {
+    for (let key in result) {
+      // Chain events
+      if (
+        /^on[A-Z]/.test(key) &&
+        typeof result[key] === "function" &&
+        typeof b[key] === "function"
+      ) {
+        result[key] = chain(result[key], b[key]);
 
-    // Merge classnames, sometimes classNames are empty string which eval to false, so we just need to do a type check
-    } else if (key === 'className' && typeof a.className === 'string' && typeof b.className === 'string') {
-      res[key] = classNames(a.className, b.className);
+        // Merge classnames, sometimes classNames are empty string which eval to false, so we just need to do a type check
+      } else if (
+        key === "className" &&
+        typeof result.className === "string" &&
+        typeof b.className === "string"
+      ) {
+        result[key] = classNames(result.className, b.className);
+      } else if (
+        key === "UNSAFE_className" &&
+        typeof result.UNSAFE_className === "string" &&
+        typeof b.UNSAFE_className === "string"
+      ) {
+        result[key] = classNames(result.UNSAFE_className, b.UNSAFE_className);
+      } else if (key === "id" && result.id && b.id) {
+        result.id = mergeIds(result.id, b.id);
 
-    } else if (key === 'UNSAFE_className' && typeof a.UNSAFE_className === 'string' && typeof b.UNSAFE_className === 'string') {
-      res[key] = classNames(a.UNSAFE_className, b.UNSAFE_className);
+        // Override others
+      } else {
+        result[key] = b[key] !== undefined ? b[key] : result[key];
+      }
+    }
 
-    } else if (key === 'id' && a.id && b.id) {
-      res.id = mergeIds(a.id, b.id);
-
-    // Override others
-    } else {
-      res[key] = b[key] !== undefined ? b[key] : a[key];
+    // Add props from b that are not in a
+    for (let key in b) {
+      if (result[key] === undefined) {
+        result[key] = b[key];
+      }
     }
   }
 
-  // Add props from b that are not in a
-  for (let key in b) {
-    if (a[key] === undefined) {
-      res[key] = b[key];
-    }
-  }
-
-  return res as T & U;
+  return result as T & UnionToIntersection<TupleTypes<U>>;
 }
