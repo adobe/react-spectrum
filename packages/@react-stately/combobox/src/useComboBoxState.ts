113ad2ff4a00cfb1b8113fccbcea219f0d9a99a5
Daniel <dlu@livefyre.com>
2020-07-16 17:36:35 -0700

    cleaning some comments

--- packages/@react-stately/combobox/src/useComboBoxState.ts
+++ packages/@react-stately/combobox/src/useComboBoxState.ts
@@ -117,6 +117,7 @@ export function useComboBoxState<T extends object>(props: ComboBoxStateProps<T>)
     // think about the below conditionals below
     // If I don't have the extra itemText check, then setting props.selectedKey to undef or just deleting one letter of the text
     // so it doesn't match a key will then clear the textfield entirely (in controlled selected key case)
+    // Problem with this is that a clear button w/ setSelectedKey = '' won't actually clear the textfield because of this itemText check (also controlled selected key only case, input value uncontrolled).
     itemText && setInputValue(itemText);
 
     // If itemText happens to be the same as the current input text but the keys don't match
@@ -127,15 +128,6 @@ export function useComboBoxState<T extends object>(props: ComboBoxStateProps<T>)
       }
     }
 
-    // Only close the menu if the key is being set to something and not to undefined? (this is so when user backspaces the menu stays open)
-    // Or should this even be here? If we remove and put .close on Enter in useComboBox then we can
-    // have consitent behavior of menu staying open when the user types in something matching a combobox option for controlled and uncontrolled
-    // and still have Enter/Click close the menu
-    // This question is mainly around the behavior of if the menu should close when the user
-    // types something matching a option or if it should stay open
-    // If we want to keep the behavior of closing the menu when the user types in a valid combobox value
-    // then I think we'll have to add someting to onInputChange where it calls .close after onSelectionCHange (actually doesn't work cuz onChange in useCOmbobox makes it open again)
-    // key && triggerState.setOpen(false);
   }, [collection, setInputValue, inputValue, onSelectionChange, selectedKey]);
 
 
@@ -147,8 +139,6 @@ export function useComboBoxState<T extends object>(props: ComboBoxStateProps<T>)
       setSelectedKey(props.selectedKey);
     }
     lastSelectedKeyProp.current = props.selectedKey;
-    // as asked about, should the the triggerstate.setOpen be put here?
-    // props.selectedKey && triggerState.setOpen(false);
   }, [props.selectedKey, setSelectedKey]);
 
   // If props.inputValue changes, call onSelectionChange (it doesn't get called since onInputChange doesn't trigger on prop.inputValue changes)
