0e7fb6d408c360f1eb9ddd97531e1aec565b2627
nishan <newslaundry@Newslaundrys-MacBook-Pro.local>
2020-07-20 01:08:46 +0530

    Copyright text in useLongPress - MenuTrigger longPress #780

--- packages/@react-aria/interactions/src/useLongPress.ts
+++ packages/@react-aria/interactions/src/useLongPress.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+// Portions of the code in this file are based on code from react.
+// Original licensing for the following can be found in the
+// NOTICE file in the root directory of this source tree.
+// See https://github.com/facebook/react/tree/cc7c1aece46a6b69b41958d731e0fd27c94bfc6c/packages/react-interactions
+
 import { useRef } from "react";
 import { usePress } from "./usePress";
 import { PressEvent } from "@react-types/shared";
