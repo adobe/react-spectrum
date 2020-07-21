7fb45aa71f1a90eec675bd7d46247f3e7ce2f57e
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-07-21 02:33:57 +0200

    Fix typescript build errors/warnings (#713)

--- packages/@react-spectrum/tabs/src/index.ts
+++ packages/@react-spectrum/tabs/src/index.ts
@@ -10,6 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
+/// <reference types="css-module-types" />
+
 export * from './TabList';
 export * from './Tab';
 export * from './Tabs';
