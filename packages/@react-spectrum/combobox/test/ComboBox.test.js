252985494e353aac5c2ee3766ed1c5bc555af4d6
Daniel <dlu@livefyre.com>
2020-08-05 11:03:26 -0700

    Adding check on thrown error message for combobox

--- packages/@react-spectrum/combobox/test/ComboBox.test.js
+++ packages/@react-spectrum/combobox/test/ComboBox.test.js
@@ -1201,7 +1201,7 @@ describe('ComboBox', function () {
     it('supports selectedKey and inputValue (not matching should error)', function () {
       const originalError = console.error;
       console.error = jest.fn();
-      expect(() => renderComboBox({selectedKey: '2', inputValue: 'Three'})).toThrow();
+      expect(() => renderComboBox({selectedKey: '2', inputValue: 'Three'})).toThrow('Mismatch between selected item and inputValue!');
       console.error = originalError;
     });
 
