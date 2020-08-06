d7359b69a7ed9fdca3e16ad6089ed21cebdfd608
Robert Snow <rsnow@adobe.com>
2020-08-06 14:24:12 -0700

    Tooltips (#843)

--- packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
+++ packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
@@ -48,10 +48,26 @@ storiesOf('Tooltip', module)
     () => render('This is a tooltip.', {variant: 'info'})
   )
   .add(
+    'variant: neutral, icon',
+    () => render('This is a tooltip.', {variant: 'neutral', showIcon: true})
+  )
+  .add(
+    'variant: positive, icon',
+    () => render('This is a tooltip.', {variant: 'positive', showIcon: true})
+  )
+  .add(
+    'variant: negative, icon',
+    () => render('This is a tooltip.', {variant: 'negative', showIcon: true})
+  )
+  .add(
+    'variant: info, icon',
+    () => render('This is a tooltip.', {variant: 'info', showIcon: true})
+  )
+  .add(
     'long content',
     () => render(longMarkup)
   );
- 
+
 const longMarkup = (
   <div>
   Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor
