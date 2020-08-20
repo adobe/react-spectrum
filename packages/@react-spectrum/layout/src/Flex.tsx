43bb0d811888c176d992c58b02eb28e3d1086f31
Devon Govett <devongovett@gmail.com>
2020-08-20 13:13:53 -0700

    Fix weird issue with flex in SSR tests in case-insensitive file systems

--- packages/@react-spectrum/layout/src/Flex.tsx
+++ packages/@react-spectrum/layout/src/Flex.tsx
@@ -15,7 +15,7 @@ import {DOMRef} from '@react-types/shared';
 import {filterDOMProps} from '@react-aria/utils';
 import {FlexProps} from '@react-types/layout';
 import React, {forwardRef} from 'react';
-import styles from './flex.css';
+import styles from './flex-gap.css';
 
 const flexStyleProps: StyleHandlers = {
   direction: ['flexDirection', passthroughStyle],
