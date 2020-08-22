c94aaf1f82eaa2ad52e58eaf0222c165cd668f95
Chang Wang <garnwraly@gmail.com>
2020-08-21 21:55:16 -0400

    make separate story for popover with function child

--- packages/@react-spectrum/dialog/stories/DialogTrigger.stories.tsx
+++ packages/@react-spectrum/dialog/stories/DialogTrigger.stories.tsx
@@ -278,6 +278,27 @@ storiesOf('DialogTrigger', module)
     () => renderPopover({type: 'popover', placement: 'bottom', width: 'calc(100vh - 100px)', containerPadding: 20})
   )
   .add(
+    'contents of function render',
+    () => (
+      <div style={{display: 'flex', margin: '100px 0'}}>
+        <DialogTrigger type="popover" onOpenChange={action('open change')}>
+          <ActionButton>Trigger</ActionButton>
+          {(close) => (
+            <Dialog>
+              <Heading>The Heading</Heading>
+              <Header>The Header</Header>
+              <Divider />
+              <Content><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet tristique risus. In sit amet suscipit lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In condimentum imperdiet metus non condimentum. Duis eu velit et quam accumsan tempus at id velit. Duis elementum elementum purus, id tempus mauris posuere a. Nunc vestibulum sapien pellentesque lectus commodo ornare.</Text></Content>
+              <ButtonGroup>
+                <Button variant="secondary" onPress={chain(close, action('cancel'))}>Cancel</Button>
+              </ButtonGroup>
+            </Dialog>
+          )}
+        </DialogTrigger>
+      </div>
+    )
+  )
+  .add(
     'targetRef',
     () => (<TriggerWithRef type="popover" />)
   )
@@ -314,17 +335,12 @@ function renderPopover({width = 'auto', ...props}) {
     <div style={{display: 'flex', width, margin: '100px 0'}}>
       <DialogTrigger {...props} onOpenChange={action('open change')}>
         <ActionButton>Trigger</ActionButton>
-        {(close) => (
-          <Dialog>
-            <Heading>The Heading</Heading>
-            <Header>The Header</Header>
-            <Divider />
-            <Content><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet tristique risus. In sit amet suscipit lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In condimentum imperdiet metus non condimentum. Duis eu velit et quam accumsan tempus at id velit. Duis elementum elementum purus, id tempus mauris posuere a. Nunc vestibulum sapien pellentesque lectus commodo ornare.</Text></Content>
-            <ButtonGroup>
-              <Button variant="secondary" onPress={chain(close, action('cancel'))}>Cancel</Button>
-            </ButtonGroup>
-          </Dialog>
-        )}
+        <Dialog>
+          <Heading>The Heading</Heading>
+          <Header>The Header</Header>
+          <Divider />
+          <Content><Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet tristique risus. In sit amet suscipit lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In condimentum imperdiet metus non condimentum. Duis eu velit et quam accumsan tempus at id velit. Duis elementum elementum purus, id tempus mauris posuere a. Nunc vestibulum sapien pellentesque lectus commodo ornare.</Text></Content>
+        </Dialog>
       </DialogTrigger>
     </div>
   );
