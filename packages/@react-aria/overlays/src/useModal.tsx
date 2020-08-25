78ac497400786ba2572d3ee076a158038e4a409e
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-08-25 13:56:15 +0200

    fix: useModal: wrap context value in useMemo

--- packages/@react-aria/overlays/src/useModal.tsx
+++ packages/@react-aria/overlays/src/useModal.tsx
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import React, {AriaAttributes, HTMLAttributes, ReactNode, useContext, useEffect, useState} from 'react';
+import React, {AriaAttributes, HTMLAttributes, ReactNode, useContext, useEffect, useMemo, useState} from 'react';
 import ReactDOM from 'react-dom';
 
 interface ModalProviderProps extends HTMLAttributes<HTMLElement> {
@@ -38,7 +38,7 @@ export function ModalProvider(props: ModalProviderProps) {
   let {children} = props;
   let parent = useContext(Context);
   let [modalCount, setModalCount] = useState(parent ? parent.modalCount : 0);
-  let context = {
+  let context = useMemo(() => ({
     parent,
     modalCount,
     addModal() {
@@ -53,7 +53,7 @@ export function ModalProvider(props: ModalProviderProps) {
         parent.removeModal();
       }
     }
-  };
+  }), [parent, modalCount]);
 
   return (
     <Context.Provider value={context}>
