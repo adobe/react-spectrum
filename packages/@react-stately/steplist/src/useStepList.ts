efff97c9ce45a560d7dab736221574d3b02b8569
Vijay Jain <mnnit.vijay@gmail.com>
2020-09-05 01:24:25 +0530

    Simplify state hook

--- packages/@react-stately/steplist/src/useStepList.ts
+++ packages/@react-stately/steplist/src/useStepList.ts
@@ -1,8 +1,7 @@
-import {Key, useEffect, useState} from 'react';
+import {Key, useEffect, useMemo} from 'react';
 import {StepListProps} from '@react-types/steplist';
 import {useControlledState} from '@react-stately/utils';
 import {SingleSelectListState, useSingleSelectListState} from '@react-stately/list';
-import {Collection, Node} from '@react-types/shared';
 
 export interface StepListState<T> extends SingleSelectListState<T> {
   /** Last completed step in the list */
@@ -16,19 +15,6 @@ export interface StepListState<T> extends SingleSelectListState<T> {
 }
 
 /**
- * Get all keys which appears before given key in collection
- */
-const getKeysBefore = (collection: Collection<Node<unknown>>, key: Key) => {
-  const keys: Key[] = [];
-  let currentKey = collection.getKeyBefore(key);
-  while(currentKey != null) {
-    keys.push(currentKey);
-    currentKey = collection.getKeyBefore(currentKey);
-  }
-  return keys.reverse();
-};
-
-/**
  * Provides state management for steplist componet with functionality of single selection among
  * children Items and keeps track of completed steps.
  */
@@ -40,34 +26,30 @@ export function useStepListState<T extends object>(props: StepListProps<T>): Ste
     props.defaultCompletedStep,
     () => {}
   );
-  const [completedSteps, setCompletedSteps] = useState([
-    ...getKeysBefore(collection, props.defaultCompletedStep),
-    props.defaultCompletedStep
-  ]);
 
-  // Update completedSteps when lastCompletedStep changes
-  useEffect(() => {
-    if (props.lastCompletedStep == null) return;
-    const keys = getKeysBefore(collection, props.lastCompletedStep);
-    keys.push(props.lastCompletedStep);
-    setCompletedSteps(keys);
-  }, [collection, props.lastCompletedStep]);
+  const stepOrderMap =  useMemo(() => {
+    const stepOrderMap = {};
+    let order = 1;
+    for(let key of collection.getKeys()) {
+      stepOrderMap[key] = order++;
+    }
+    return stepOrderMap;
+  }, [collection]);
 
-  // Update completedSteps when selectedKey changes
+  // Update lastCompletedStep when selectedKey changes
   useEffect(() => {
-    // If no selectedKey or controlled with lastCompletedStep
+    // Do nothing if no selectedKey or controlled with lastCompletedStep
     if(selectedKey == null || props.lastCompletedStep !== undefined) {
       return;
     }
-    const keys = getKeysBefore(collection, selectedKey);
-    if(keys.length && (lastCompletedStep == null || keys.includes(lastCompletedStep))) {
-      setCompletedSteps(keys);
-      setLastCompletedStep(keys[keys.length - 1]);
+    // Update if lastCompletedStep is null or before selectedKey
+    if(lastCompletedStep == null || stepOrderMap[selectedKey] > stepOrderMap[lastCompletedStep]) {
+      setLastCompletedStep(collection.getKeyBefore(selectedKey));
     }
-  }, [collection, selectedKey, props.lastCompletedStep, lastCompletedStep]);
+  }, [collection, selectedKey, props.lastCompletedStep, lastCompletedStep, stepOrderMap]);
 
   return {
-    isCompleted: key => completedSteps.includes(key),
+    isCompleted: key => stepOrderMap[key] <= stepOrderMap[lastCompletedStep],
     lastCompletedStep,
     setLastCompletedStep,
     ...selectionState,
