3385b6dc622f189307c6ccde53fa73d09dd1396c
Devon Govett <devongovett@gmail.com>
2020-09-28 18:03:55 -0700

    Add DialogContainer component to support controlled dialogs (#1082)
    * Add DialogContainer component for controlled modals
    
    * Add docs for DialogContainer
    
    Co-authored-by: Danni <drobinson@livefyre.com>

--- packages/@react-types/dialog/src/index.d.ts
+++ packages/@react-types/dialog/src/index.d.ts
@@ -38,6 +38,22 @@ export interface SpectrumDialogTriggerProps extends OverlayTriggerProps, Positio
   isKeyboardDismissDisabled?: boolean
 }
 
+export interface SpectrumDialogContainerProps {
+  /** The Dialog to display, if any. */
+  children: ReactNode,
+  /** Handler that is called when the 'x' button of a dismissable Dialog is clicked. */
+  onDismiss: () => void,
+  /**
+   * The type of Dialog that should be rendered. See the visual options below for examples of each.
+   * @default 'modal'
+   */
+  type?: 'modal' | 'fullscreen' | 'fullscreenTakeover',
+  /** Whether the Dialog is dismissable. See the [Dialog docs](Dialog.html#dismissable-dialogs) for more details. */
+  isDismissable?: boolean,
+  /** Whether pressing the escape key to close the dialog should be disabled. */
+  isKeyboardDismissDisabled?: boolean
+}
+
 export interface AriaDialogProps extends DOMProps, AriaLabelingProps {
   /**
    * The accessibility role for the dialog.
