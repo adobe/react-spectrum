23e675ee465588d819884e44dc329402edf5c671
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-09-29 11:50:42 +0200

    Fix prop types for useListBox

--- packages/@react-aria/listbox/src/useListBox.ts
+++ packages/@react-aria/listbox/src/useListBox.ts
@@ -49,7 +49,7 @@ interface AriaListBoxOptions<T> extends AriaListBoxProps<T> {
  * @param props - Props for the listbox.
  * @param state - State for the listbox, as returned by `useListState`.
  */
-export function useListBox<T>(props: AriaListBoxOptions<T>, state: ListState<T>, ref: RefObject<HTMLElement>): ListBoxAria {
+export function useListBox<T>(props: Omit<AriaListBoxOptions<T>, 'children'>, state: ListState<T>, ref: RefObject<HTMLElement>): ListBoxAria {
   let domProps = filterDOMProps(props, {labelable: true});
   let {listProps} = useSelectableList({
     ...props,
@@ -72,9 +72,10 @@ export function useListBox<T>(props: AriaListBoxOptions<T>, state: ListState<T>,
 
   return {
     labelProps,
-    listBoxProps: mergeProps(domProps, {
+    listBoxProps: mergeProps(domProps, state.selectionManager.selectionMode === 'multiple' ? {
+      'aria-multiselectable': 'true'
+    } : {}, {
       role: 'listbox',
-      'aria-multiselectable': state.selectionManager.selectionMode === 'multiple' ? 'true' : undefined,
       ...mergeProps(fieldProps, listProps)
     })
   };
