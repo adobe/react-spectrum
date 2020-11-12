ce3d0ffaf28ca9fe8e2b1ca676fa92e2152dc7df
Tom Schindl <tom.schindl@bestsolution.at>
2020-11-12 23:40:10 +0100

    remove onFocusChange

--- packages/@react-aria/select/src/useSelect.ts
+++ packages/@react-aria/select/src/useSelect.ts
@@ -134,7 +134,6 @@ export function useSelect<T>(props: AriaSelectOptions<T>, state: SelectState<T>,
 
         state.setFocused(false);
       },
-      onFocusChange: props.onFocusChange,
       onKeyDown: onKeyDown,
       onKeyUp: props.onKeyUp
     }),
