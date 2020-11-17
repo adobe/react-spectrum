473fa630b848f21cdee8d8c76699ad3e5b9c743a
samiur98 <37990997+samiur98@users.noreply.github.com>
2020-11-16 16:13:59 -0800

    Replaced the use of React.children.toArray in TooltipTrigger to fix mutation issue (#1242)

--- packages/@react-spectrum/tooltip/src/TooltipTrigger.tsx
+++ packages/@react-spectrum/tooltip/src/TooltipTrigger.tsx
@@ -73,7 +73,14 @@ function TooltipTrigger(props: SpectrumTooltipTriggerProps) {
 
 // Support TooltipTrigger inside components using CollectionBuilder.
 TooltipTrigger.getCollectionNode = function* (props: SpectrumTooltipTriggerProps) {
-  let [trigger, tooltip] = React.Children.toArray(props.children) as [ReactElement, ReactElement];
+  // Replaced the use of React.Childern.toArray because it mutates the key prop.
+  let childArray: ReactElement[] = [];
+  React.Children.forEach(props.children, child => {
+    if (React.isValidElement(child)) {
+      childArray.push(child);
+    }
+  });
+  let [trigger, tooltip] = childArray;
   yield {
     element: trigger,
     wrapper: (element) => (
