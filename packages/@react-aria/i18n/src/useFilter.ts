de04d613d174eb11b36b83ab4d2dfadef79c6af5
Devon Govett <devongovett@gmail.com>
2020-11-25 15:18:28 -0500

    Add docs for useFilter hook

--- packages/@react-aria/i18n/src/useFilter.ts
+++ packages/@react-aria/i18n/src/useFilter.ts
@@ -13,11 +13,18 @@
 import {useCollator} from './useCollator';
 
 interface Filter {
+  /** Returns whether a string starts with a given substring. */
   startsWith(string: string, substring: string): boolean,
+  /** Returns whether a string ends with a given substring. */
   endsWith(string: string, substring: string): boolean,
+  /** Returns whether a string contains a given substring. */
   contains(string: string, substring: string): boolean
 }
 
+/**
+ * Provides localized string search functionality that is useful for filtering or matching
+ * items in a list. Options can be provided to adjust the sensitivity to case, diacritics, etc.
+ */
 export function useFilter(options?: Intl.CollatorOptions): Filter {
   let collator = useCollator({
     usage: 'search',
