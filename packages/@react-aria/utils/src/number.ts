331933a31d2a0f0b0fa1742ae66400f3079dde87
Devon Govett <devongovett@gmail.com>
2020-12-17 14:50:21 -0500

    Fix floating point precision errors when rounding slider value to step

--- packages/@react-aria/utils/src/number.ts
+++ packages/@react-aria/utils/src/number.ts
@@ -16,3 +16,28 @@
 export function clamp(value: number, min: number = -Infinity, max: number = Infinity): number {
   return Math.min(Math.max(value, min), max);
 }
+
+export function snapValueToStep(value: number, min: number, max: number, step: number): number {
+  let remainder = ((value - min) % step);
+  let snappedValue = Math.abs(remainder) * 2 >= step
+    ? (value - Math.abs(remainder)) + step
+    : value - remainder;
+
+  if (snappedValue < min) {
+    snappedValue = min;
+  } else if (snappedValue > max) {
+    snappedValue = min + Math.floor((max - min) / step) * step;
+  }
+
+  // correct floating point behavior by rounding to step precision
+  let string = step.toString();
+  let index = string.indexOf('.');
+  let precision = index >= 0 ? string.length - index : 0;
+
+  if (precision > 0) {
+    let pow = Math.pow(10, precision);
+    snappedValue = Math.round(snappedValue * pow) / pow;
+  }
+
+  return snappedValue;
+}
