b3df455af794e7ed32e835c0616c584db10e5fde
Devon Govett <devongovett@gmail.com>
2020-12-21 18:31:13 -0500

    Restore overrides for docs

--- packages/@react-types/slider/src/index.d.ts
+++ packages/@react-types/slider/src/index.d.ts
@@ -23,7 +23,23 @@ export interface SliderProps<T = number[]> extends RangeInputBase<number>, Value
   /** Whether the whole Slider is disabled. */
   isDisabled?: boolean,
   /** Fired when the slider stops moving, due to being let go. */
-  onChangeEnd?: (value: T) => void
+  onChangeEnd?: (value: T) => void,
+  // These are duplicated from ValueBase to define defaults for the docs.
+  /**
+   * The slider's minimum value.
+   * @default 0
+   */
+  minValue?: number,
+  /**
+   * The slider's maximum value.
+   * @default 100
+   */
+  maxValue?: number,
+  /**
+   * The slider's step value.
+   * @default 1
+   */
+  step?: number
 }
 
 export interface SliderThumbProps extends FocusableProps, Validation, LabelableProps {
