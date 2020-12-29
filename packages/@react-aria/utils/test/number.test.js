4f6ea11bce6e51a886bbb0241859e777343369fd
matamatanot <matamatanot@gmail.com>
2020-12-29 12:49:35 +0900

    chore: replace assert.equal with assert.strictEqual

--- packages/@react-aria/utils/test/number.test.js
+++ packages/@react-aria/utils/test/number.test.js
@@ -4,17 +4,17 @@ import {clamp, snapValueToStep} from '../src/number';
 describe('number utils', () => {
   describe('clamp', () => {
     it('should constrain value between min and max', () => {
-      assert.equal(clamp(5, -1, 1), 1);
-      assert.equal(clamp(-5, -1, 1), -1);
+      assert.strictEqual(clamp(5, -1, 1), 1);
+      assert.strictEqual(clamp(-5, -1, 1), -1);
     });
   });
 
   describe('snapValueToStep', () => {
     it('should snap value to nearest step based on min and max', () => {
-      assert.equal(snapValueToStep(2, -0.5, 100, 3), 2.5);
-      assert.equal(snapValueToStep(-6.2, -2.5, 100, 3), -2.5);
-      assert.equal(snapValueToStep(106.2, -2.5, 100, 3), 99.5);
-      assert.equal(snapValueToStep(-0.009999, -0.5, 0.5, 0.01), -0.01);
+      assert.strictEqual(snapValueToStep(2, -0.5, 100, 3), 2.5);
+      assert.strictEqual(snapValueToStep(-6.2, -2.5, 100, 3), -2.5);
+      assert.strictEqual(snapValueToStep(106.2, -2.5, 100, 3), 99.5);
+      assert.strictEqual(snapValueToStep(-0.009999, -0.5, 0.5, 0.01), -0.01);
     });
   });
 });
