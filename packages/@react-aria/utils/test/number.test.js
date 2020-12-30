6bfd948319d0f3605eb4ce7ccd0577b62e029881
matamatanot <matamatanot@gmail.com>
2020-12-31 07:12:39 +0900

    chore: jest expect

--- packages/@react-aria/utils/test/number.test.js
+++ packages/@react-aria/utils/test/number.test.js
@@ -1,20 +1,19 @@
-import assert from 'assert';
 import {clamp, snapValueToStep} from '../src/number';
 
 describe('number utils', () => {
   describe('clamp', () => {
     it('should constrain value between min and max', () => {
-      assert.strictEqual(clamp(5, -1, 1), 1);
-      assert.strictEqual(clamp(-5, -1, 1), -1);
+      expect(clamp(5, -1, 1).toBe(1));
+      expect(clamp(-5, -1, 1).toBe(1));
     });
   });
 
   describe('snapValueToStep', () => {
     it('should snap value to nearest step based on min and max', () => {
-      assert.strictEqual(snapValueToStep(2, -0.5, 100, 3), 2.5);
-      assert.strictEqual(snapValueToStep(-6.2, -2.5, 100, 3), -2.5);
-      assert.strictEqual(snapValueToStep(106.2, -2.5, 100, 3), 99.5);
-      assert.strictEqual(snapValueToStep(-0.009999, -0.5, 0.5, 0.01), -0.01);
+      expect(snapValueToStep(2, -0.5, 100, 3).toBe(2.5));
+      expect(snapValueToStep(-6.2, -2.5, 100, 3).toBe(-2.5));
+      expect(snapValueToStep(106.2, -2.5, 100, 3).toBe(99.5));
+      expect(snapValueToStep(-0.009999, -0.5, 0.5, 0.01).toBe(-0.01));
     });
   });
 });
