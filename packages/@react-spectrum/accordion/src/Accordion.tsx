d30865c8ac5106f436a1ef778bc9a8a8f3fefeef
so99ynoodles <so99ynoodles@gmail.com>
2021-03-05 01:54:20 +0900

    Plop

---
+++ packages/@react-spectrum/accordion/src/Accordion.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import {classNames, useDOMRef, useStyleProps} from '@react-spectrum/utils';
+import {DOMRef} from '@react-types/shared';
+import {filterDOMProps} from '@react-aria/utils';
+import React from 'react';
+import {SpectrumAccordionProps} from '@react-types/accordion';
+import styles from '@adobe/spectrum-css-temp/components/accordion/vars.css';
+import {useAccordion} from '@react-aria/accordion';
+import {useProviderProps} from '@react-spectrum/provider';
+
+
+function Accordion(props: SpectrumAccordionProps, ref: DOMRef<HTMLDivElement>) {
+  // Grabs specific props from the closest Provider (see https://react-spectrum.adobe.com/react-spectrum/Provider.html#property-groups). Remove if your component doesn't support any of the listed props.
+  props = useProviderProps(props);
+  // Handles RSP specific style options, UNSAFE_style, and UNSAFE_className props (see https://react-spectrum.adobe.com/react-spectrum/styling.html#style-props)
+  let {styleProps} = useStyleProps(props);
+  let ariaProps = useAccordion(props, state);
+  let domRef = useDOMRef(ref);
+
+  return (
+    <div
+      {...filterDOMProps(props)}
+      {...ariaProps}
+      {...styleProps}
+      ref={domRef}
+      className={classNames(styles, '', styleProps.className)} />
+  );
+}
+
+/**
+ * TODO: Add description of component here.
+ */
+const _Accordion = React.forwardRef(Accordion);
+export {_Accordion as Accordion};
