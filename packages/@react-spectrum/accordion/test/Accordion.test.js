c3e37cf89f4bcd5a60adbe1733c6bd28be7dc11e
so99ynoodles <so99ynoodles@gmail.com>
2021-03-05 04:46:07 +0900

    Temp

--- packages/@react-spectrum/accordion/test/Accordion.test.js
+++ packages/@react-spectrum/accordion/test/Accordion.test.js
@@ -11,8 +11,8 @@
  */
 
 import {Accordion} from '../';
-import {render} from '@testing-library/react';
 import React from 'react';
+import {render} from '@testing-library/react';
 
 describe('Accordion', function () {
   it.each`
