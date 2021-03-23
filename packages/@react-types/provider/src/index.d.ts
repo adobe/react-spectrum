4ff1873cf5f83a6d561177f13f387e01a10de6b2
so99ynoodles <so99ynoodles@gmail.com>
2021-03-23 18:04:41 +0900

    Add a comment

--- packages/@react-types/provider/src/index.d.ts
+++ packages/@react-types/provider/src/index.d.ts
@@ -19,6 +19,7 @@ export interface Breakpoints {
   S?: number,
   M?: number,
   L?: number,
+  // Currently, it only deals with pixels, but we need to fix it to accept em or rem as well.
   [custom: string]: number
 }
 
