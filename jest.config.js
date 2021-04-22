025ef021b30325746c1e4fd7458ea8db73ece734
Rob Snow <rsnow@adobe.com>
2021-04-21 18:59:53 -0700

    use a speedier glob with more accurate file list fix logic with awaiting a promise.race

--- jest.config.js
+++ jest.config.js
@@ -171,7 +171,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/?(*.)+(test).[tj]s?(x)"
+    "packages/**/*.test.[tj]s?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
