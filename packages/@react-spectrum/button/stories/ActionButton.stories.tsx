22e802067ef1be051eb2e723cedbd91c1b121c05
Rob Snow <rsnow@adobe.com>
2021-05-19 11:49:27 -0700

    fix lint

--- packages/@react-spectrum/button/stories/ActionButton.stories.tsx
+++ packages/@react-spectrum/button/stories/ActionButton.stories.tsx
@@ -14,7 +14,7 @@ import {action} from '@storybook/addon-actions';
 import {ActionButton} from '../';
 import Add from '@spectrum-icons/workflow/Add';
 import {Flex} from '@react-spectrum/layout';
-import React, {useState} from 'react';
+import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {Text} from '@react-spectrum/text';
 import {View} from '@react-spectrum/view';
