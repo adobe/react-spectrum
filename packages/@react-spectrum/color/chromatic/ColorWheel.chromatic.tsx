ca4a3f31e39521bafc7036f7a61804fa0f243195
Daniel Lu <dl1644@gmail.com>
2021-06-02 13:46:59 -0700

    adding color component chromatic stories

---
+++ packages/@react-spectrum/color/chromatic/ColorWheel.chromatic.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import {Meta, Story} from '@storybook/react';
+import React from 'react';
+import {ColorWheel} from '../';
+import {SpectrumColorWheelProps} from '@react-types/color';
+
+const meta: Meta<SpectrumColorWheelProps> = {
+  title: 'ColorWheel'
+};
+
+export default meta;
+
+const Template = (): Story<SpectrumColorWheelProps> => (args) => (
+  <ColorWheel {...args} />
+);
+
+export const Default = Template().bind({});
+Default.args = {defaultValue: 'hsl(0, 100%, 50%)'};
+
+export const Disabled = Template().bind({});
+Disabled.args = {...Default.args, isDisabled: true};
+
+export const CustomSize = Template().bind({});
+CustomSize.args = {...Default.args, size: 'size-5000'};
