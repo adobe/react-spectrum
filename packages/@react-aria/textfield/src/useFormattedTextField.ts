0a6476d2d984867cb485406d6b726ee364bdddcd
Reid Barber <reid@reidbarber.com>
2021-06-17 17:18:36 -0500

    Fix to clear line up to selection

--- packages/@react-aria/textfield/src/useFormattedTextField.ts
+++ packages/@react-aria/textfield/src/useFormattedTextField.ts
@@ -78,7 +78,10 @@ export function useFormattedTextField(props: AriaTextFieldProps, state: Formatte
             : input.value.slice(0, input.selectionStart) + input.value.slice(input.selectionEnd);
           break;
         case 'deleteSoftLineBackward':
-          nextValue = '';
+          nextValue = input.value.slice(input.selectionStart);
+          break;
+        case 'deleteHardLineBackward':
+          nextValue = input.value.slice(input.selectionStart);
           break;
         default:
           if (e.data != null) {
