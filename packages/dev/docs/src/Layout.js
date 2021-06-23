2dbc95946fcc0f98bc0c125626a79ed458c0f9f1
Michael Jordan <mijordan@adobe.com>
2021-06-23 13:51:28 -0400

    fix(#1999): Improve accessibility in DocSearch results
    Update to #2016 to improve accessibility in DocSearch results so that they better implement the WAI-ARIA ComboBox design pattern.
    
    - Fixes a few other docs issues identified with aXe DevTools.
    - Fixes a couple console error messages with themeSwitcherButton and hamburgerButton.

--- packages/dev/docs/src/Layout.js
+++ packages/dev/docs/src/Layout.js
@@ -342,7 +342,7 @@ function Nav({currentPageName, pages}) {
           </a>
         }
         <a href={isBlog ? '/index.html' : './index.html'} className={docStyles.homeBtn} id="nav-title-id">
-          <svg viewBox="0 0 30 26" fill="#E1251B" aria-label="Adobe">
+          <svg viewBox="0 0 30 26" fill="#E1251B" role="img" aria-label="Adobe">
             <polygon points="19,0 30,0 30,26" />
             <polygon points="11.1,0 0,0 0,26" />
             <polygon points="15,9.6 22.1,26 17.5,26 15.4,20.8 10.2,20.8" />
