2f0b06b4c167cce665b001c6566391f4311f09b0
Devon Govett <devongovett@gmail.com>
2021-06-25 14:47:36 -0700

    Use Image component to fix base url

--- packages/dev/docs/src/ExampleCard.js
+++ packages/dev/docs/src/ExampleCard.js
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import {Image} from './Image';
 import React from 'react';
 import styles from './ExampleCard.css';
 import typographyStyles from '@adobe/spectrum-css-temp/components/typography/vars.css';
@@ -6,7 +7,7 @@ import typographyStyles from '@adobe/spectrum-css-temp/components/typography/var
 export function ExampleCard(props) {
   return (
     <a href={props.url} rel="noreferrer" target="_blank" className={clsx(typographyStyles['spectrum-Body3'], styles.exampleCard)}>
-      <img src={props.preview} alt={props.title} />
+      <Image src={props.preview} alt={props.title} className={null} />
       <div className={styles.cardTitle}>{props.title}</div>
       <div className={clsx(typographyStyles['spectrum-Body4'], styles.cardDescription)}>{props.description}</div>
     </a>
