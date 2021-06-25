52ab854ad4134e978d858acbcc2a284bc093b0c4
Devon Govett <devongovett@gmail.com>
2021-06-24 19:00:19 -0700

    Add links to codesandbox examples to show styling

---
+++ packages/dev/docs/src/ExampleCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ExampleCard.css';
+import typographyStyles from '@adobe/spectrum-css-temp/components/typography/vars.css';
+import clsx from 'clsx';
+
+export function ExampleCard(props) {
+  return (
+    <a href={props.url} rel="noreferrer" target="_blank" className={clsx(typographyStyles['spectrum-Body3'], styles.exampleCard)}>
+      <img src={props.preview} alt={props.title} />
+      <div className={styles.cardTitle}>{props.title}</div>
+      <div className={clsx(typographyStyles['spectrum-Body4'], styles.cardDescription)}>{props.description}</div>
+    </a>
+  );
+}
