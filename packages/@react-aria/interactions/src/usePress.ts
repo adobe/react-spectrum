87b372221dfacfd135837f638d56ca26513cd1c4
Robert Snow <rsnow@adobe.com>
2021-06-25 17:41:37 -0700

    [WIP] - Make form submission fix in usePress instead of in useButton (#1997)
    Make fix in usePress instead of in useButton

--- packages/@react-aria/interactions/src/usePress.ts
+++ packages/@react-aria/interactions/src/usePress.ts
@@ -211,7 +211,9 @@ export function usePress(props: PressHookProps): PressResult {
     let pressProps: HTMLAttributes<HTMLElement> = {
       onKeyDown(e) {
         if (isValidKeyboardEvent(e.nativeEvent)) {
-          e.preventDefault();
+          if (shouldPreventDefaultKeyboard(e.target as Element)) {
+            e.preventDefault();
+          }
           e.stopPropagation();
 
 
@@ -262,7 +264,9 @@ export function usePress(props: PressHookProps): PressResult {
 
     let onKeyUp = (e: KeyboardEvent) => {
       if (state.isPressed && isValidKeyboardEvent(e)) {
-        e.preventDefault();
+        if (shouldPreventDefaultKeyboard(e.target as Element)) {
+          e.preventDefault();
+        }
         e.stopPropagation();
 
         state.isPressed = false;
@@ -664,6 +668,10 @@ function isOverTarget(point: EventPoint, target: HTMLElement) {
 function shouldPreventDefault(target: Element) {
   // We cannot prevent default if the target is inside a draggable element.
   return !target.closest('[draggable="true"]');
+}
+
+function shouldPreventDefaultKeyboard(target: Element) {
+  return target.getAttribute('type') !== 'submit';
 }
 
 function isVirtualPointerEvent(event: PointerEvent) {
