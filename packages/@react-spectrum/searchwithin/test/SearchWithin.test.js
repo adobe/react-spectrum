2fa8e22d683eb072c27ff3780033e8d85b622a33
Reid Barber <reid@reidbarber.com>
2021-07-12 11:15:59 -0500

    add SearchWithin tests

--- packages/@react-spectrum/searchwithin/test/SearchWithin.test.js
+++ packages/@react-spectrum/searchwithin/test/SearchWithin.test.js
@@ -9,44 +9,101 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-// import {Item, Picker, SearchField, SearchWithin} from '../';
-// import {Provider} from '@react-spectrum/provider';
-// import React from 'react';
-// import {render} from '@testing-library/react';
-// import scaleMedium from '@adobe/spectrum-css-temp/vars/spectrum-medium-unique.css';
-// import themeLight from '@adobe/spectrum-css-temp/vars/spectrum-light-unique.css';
+import {act, render} from '@testing-library/react';
+import {Item, Picker} from '@react-spectrum/picker';
+import {Provider} from '@react-spectrum/provider';
+import React from 'react';
+import {SearchField} from '@react-spectrum/searchfield';
+import {SearchWithin} from '../src';
+import {theme} from '@react-spectrum/theme-default';
+import {triggerPress} from '@react-spectrum/test-utils';
+import {typeText} from '@react-spectrum/test-utils';
 
-// let theme = {
-//   light: themeLight,
-//   medium: scaleMedium
-// };
+let defaultProps = {
+  label: 'Test'
+};
+
+function renderSearchWithin(props = {}, searchFieldProps = {}, pickerProps = {}) {
+  return render(
+    <Provider theme={theme}>
+      <SearchWithin {...defaultProps} {...props}>
+        <SearchField placeholder="Search" {...searchFieldProps} />
+        <Picker defaultSelectedKey="all" {...pickerProps}>
+          <Item key="all">All</Item>
+          <Item key="campaigns">Campaigns</Item>
+          <Item key="audiences">Audiences</Item>
+          <Item key="tags">Tags</Item>
+        </Picker>
+      </SearchWithin>
+    </Provider>  
+  );
+}
 
 describe('SearchWithin', function () {
+  beforeAll(function () {
+    jest.useFakeTimers();
+  });
 
-  // let defaultProps = {
-  //   label: 'Test'
-  // };
+  afterEach(() => {
+    jest.clearAllMocks();
+    act(() => jest.runAllTimers());
+  });
 
-  // const ExampleSearchWithin = React.forwardRef((props = {}, ref) => (
-  //   <Provider theme={theme}>
-  //     <SearchWithin {...defaultProps} {...props} ref={ref}>
-  //       <SearchField placeholder="Search" />
-  //       <Picker defaultSelectedKey="all">
-  //         <Item key="all">All</Item>
-  //         <Item key="campaigns">Campaigns</Item>
-  //         <Item key="audiences">Audiences</Item>
-  //         <Item key="tags">Tags</Item>
-  //       </Picker>
-  //     </SearchWithin>
-  //   </Provider>
-  //   ));
+  afterAll(function () {
+    jest.restoreAllMocks();
+  });
 
-  // function renderSearchWithin(props = {}) {
-  //   return render(<ExampleSearchWithin {...props} />);
-  // }
-  
-  it('temp test', function () {
+  it('renders correctly', function () {
+    let {getAllByText, getByRole, getByPlaceholderText} = renderSearchWithin();
 
-    expect(true).toBeTruthy();
+    let searchfield = getByPlaceholderText('Search');
+    expect(searchfield).toBeVisible();
+    expect(searchfield).toHaveAttribute('type', 'search');
+
+    let button = getByRole('button');
+    expect(button).toBeVisible();
+    expect(button).toHaveAttribute('aria-haspopup', 'listbox');
+
+    let label = getAllByText('Test')[0];
+    expect(label).toBeVisible();
+  });
+
+  it('can type in search and get onChange', function () {
+    let onChange = jest.fn();
+    let {getByPlaceholderText} = renderSearchWithin({}, {onChange});
+    let searchField = getByPlaceholderText('Search');
+    expect(searchField).toHaveAttribute('value', '');
+
+    act(() => {searchField.focus();});
+    typeText(searchField, 'test search');
+    act(() => {searchField.blur();});
+    expect(searchField).toHaveAttribute('value', 'test search');
+    expect(onChange).toBeCalledTimes(11);
+  });
+
+  it('can open menu and get onChange', function () {
+    let onOpenChange = jest.fn();
+    let {getByRole} = renderSearchWithin({}, {}, {onOpenChange});
+
+    let picker = getByRole('button');
+    triggerPress(picker);
+
+    let listbox = getByRole('listbox');
+    expect(listbox).toBeVisible();
+    expect(onOpenChange).toBeCalledTimes(1);
+    expect(onOpenChange).toHaveBeenCalledWith(true);
+  });
+
+  it('searchfield and picker are labelled correctly', function () {
+    let {getByRole, getAllByText, getByPlaceholderText} = renderSearchWithin();
+
+    let searchfield = getByPlaceholderText('Search');
+    let picker = getByRole('button');
+    triggerPress(picker);
+
+    let listbox = getByRole('listbox');
+    let label = getAllByText('Test')[0];
+    expect(listbox).toHaveAttribute('aria-labelledby', label.id);
+    expect(searchfield).toHaveAttribute('aria-labelledby', label.id);
   });
 });
