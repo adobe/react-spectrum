21573a927c87f85446e2d768f9da424889e5d8b3
Tom Schindl <tom.schindl@bestsolution.at>
2021-07-14 19:20:03 +0200

    use CSF syntax for storybook

--- packages/@react-spectrum/menu/stories/ActionMenu.stories.tsx
+++ packages/@react-spectrum/menu/stories/ActionMenu.stories.tsx
@@ -1,14 +1,25 @@
 import {action} from '@storybook/addon-actions';
-import {ActionMenu} from '../src/ActionMenu';
+import {ActionMenu} from '..';
 import {Item} from '../';
+import {Meta, Story} from '@storybook/react';
 import React from 'react';
-import {storiesOf} from '@storybook/react';
+import {SpectrumActionMenuProps} from '@react-types/menu';
 
-storiesOf('ActionMenu', module)
-  .add('default menu (static)', () => (
-    <ActionMenu onAction={action('action')}>
-      <Item key="one">One</Item>
-      <Item key="two">Two</Item>
-      <Item key="three">Three</Item>
-    </ActionMenu>
-      ));
+
+const meta: Meta<SpectrumActionMenuProps<object>> = {
+  title: 'ActionMenu',
+  component: ActionMenu
+};
+
+export default meta;
+
+const Template = <T extends object>(): Story<SpectrumActionMenuProps<T>> => () => (
+  <ActionMenu onAction={action('action')}>
+    <Item key="one">One</Item>
+    <Item key="two">Two</Item>
+    <Item key="three">Three</Item>
+  </ActionMenu>
+);
+
+export const Default = Template().bind({});
+Default.args = {};
