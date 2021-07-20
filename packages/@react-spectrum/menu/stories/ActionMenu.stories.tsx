434903154600585c1a930e7d012abeec17c44e6b
Tom Schindl <tom.schindl@bestsolution.at>
2021-07-20 22:09:57 +0200

    added stories to represent almost all properties

--- packages/@react-spectrum/menu/stories/ActionMenu.stories.tsx
+++ packages/@react-spectrum/menu/stories/ActionMenu.stories.tsx
@@ -12,11 +12,13 @@
 
 import {action} from '@storybook/addon-actions';
 import {ActionMenu} from '..';
+import {Alignment} from '@react-types/shared';
+import {Flex} from '../../layout';
 import {Item} from '../';
 import {Meta, Story} from '@storybook/react';
-import React from 'react';
+import {Picker} from '../../picker';
+import React, {useState} from 'react';
 import {SpectrumActionMenuProps} from '@react-types/menu';
-
 
 const meta: Meta<SpectrumActionMenuProps<object>> = {
   title: 'ActionMenu',
@@ -33,6 +35,85 @@ const Template = <T extends object>(): Story<SpectrumActionMenuProps<T>> => (arg
   </ActionMenu>
 );
 
+type Direction = 'bottom' | 'top' | 'left' | 'right' | 'start' | 'end';
+const directionItems = [
+  {
+    key: 'bottom',
+    label: 'Bottom'
+  },
+  {
+    key: 'top',
+    label: 'Top'
+  },
+  {
+    key: 'left',
+    label: 'Left'
+  },
+  {
+    key: 'right',
+    label: 'Right'
+  },
+  {
+    key: 'start',
+    label: 'Start'
+  },
+  {
+    key: 'end',
+    label: 'End'
+  }];
+const alignItems = [
+  {
+    key: 'start',
+    label: 'Start'
+  },
+  {
+    key: 'end',
+    label: 'End'
+  }
+];
+
+function isOfDirection(key: string): key is Direction {
+  return directionItems.map(e => e.key).includes(key);
+}
+
+function isOfAlignment(key: string): key is Alignment {
+  return alignItems.map(e => e.key).includes(key);
+}
+
+function DirectionAlignment() {
+  const [align, setAlignment] = useState<Alignment>('start');
+  const [direction, setDirection] = useState<Direction>('bottom');
+
+  const handleAlignChange = (key) => {
+    if (isOfAlignment(key)) {
+      setAlignment(key);
+    }
+  };
+
+  const handleDirectionChange = (key) => {
+    if (isOfDirection(key)) {
+      setDirection(key);
+    }
+  };
+
+  return (<Flex alignItems="end" columnGap={10}>
+    <Picker label="Align" items={alignItems} selectedKey={align} onSelectionChange={handleAlignChange}>
+      {(item) => <Item key={item.key}>{item.label}</Item>}
+    </Picker>
+    <Picker label="Direction" items={directionItems} selectedKey={direction} onSelectionChange={handleDirectionChange}>
+      {(item) => <Item key={item.key}>{item.label}</Item>}
+    </Picker>
+    <ActionMenu 
+      onAction={action('action')} 
+      align={align} 
+      direction={direction}>
+      <Item key="one">One</Item>
+      <Item key="two">Two</Item>
+      <Item key="three">Three</Item>
+    </ActionMenu>
+  </Flex>);
+}
+
 export const Default = Template().bind({});
 Default.args = {};
 
@@ -43,4 +124,12 @@ export const DOMId = Template().bind({});
 DOMId.args = {id: 'my-action-menu'};
 
 export const Quiet = Template().bind({});
-Quiet.args = {direction: 'top', align: 'end', isQuiet: true};
+Quiet.args = {isQuiet: true};
+
+export const Disabled = Template().bind({});
+Disabled.args = {isDisabled: true};
+
+export const AutoFocus = Template().bind({});
+AutoFocus.args = {autoFocus: true};
+
+export const DirectionAlign = () => <DirectionAlignment />;
