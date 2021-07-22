91f70a1434fd10ddd7825e169a8e81ee98921a66
Rob Snow <rsnow@adobe.com>
2021-07-22 12:43:31 -0700

    hack to make storybook use createRoot

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,29 @@
 import {configureActions} from '@storybook/addon-actions';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {VerticalCenter} from './layout';
 import {withProviderSwitcher} from './custom-addons/provider';
+
+const nodes = new Map();
+ReactDOM.render = (app, rootNode) => {
+  let root = nodes.get(rootNode);
+  if (!root) {
+    root = ReactDOM.createRoot(rootNode); // depending on your react version this might be `.createRoot`
+    nodes.set(rootNode, root);
+  }
+  root.render(app);
+};
+
+ReactDOM.unmountComponentAtNode = (component) => {
+  const root = nodes.get(component);
+  if (root) {
+    root.unmount();
+    return true;
+  } else {
+    console.error("ReactDOM injection: can't unmount the given component");
+    return false;
+  }
+};
 
 // decorator order matters, the last one will be the outer most
 
