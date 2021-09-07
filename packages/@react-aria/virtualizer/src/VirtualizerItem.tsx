42ead5e380c228f78750c019afee4b0bf4a0a17c
Daniel Lu <dl1644@gmail.com>
2021-09-07 10:25:50 -0700

    moving contain style changes to card specific style
    this way we arent affecting table and stuff. Need to test the performance hit before I change virtualizer item

--- packages/@react-aria/virtualizer/src/VirtualizerItem.tsx
+++ packages/@react-aria/virtualizer/src/VirtualizerItem.tsx
@@ -69,7 +69,7 @@ export function layoutInfoToStyle(layoutInfo: LayoutInfo, dir: Direction, parent
     opacity: layoutInfo.opacity,
     zIndex: layoutInfo.zIndex,
     transform: layoutInfo.transform,
-    contain: 'size layout style'
+    contain: 'size layout style paint'
   };
 
   cache.set(layoutInfo, style);
