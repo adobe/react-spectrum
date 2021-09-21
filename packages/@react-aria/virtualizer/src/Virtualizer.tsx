cc02b5a5dcdceb40269b6aab2fb695e561609bfe
Devon Govett <devongovett@gmail.com>
2021-09-20 17:18:12 -0700

    Revert virtualizer changes

--- packages/@react-aria/virtualizer/src/Virtualizer.tsx
+++ packages/@react-aria/virtualizer/src/Virtualizer.tsx
@@ -12,7 +12,6 @@
 
 import {Collection} from '@react-types/shared';
 import {focusWithoutScrolling, mergeProps, useLayoutEffect} from '@react-aria/utils';
-import {getInteractionModality} from '@react-aria/interactions';
 import {Layout, Rect, ReusableView, useVirtualizerState, VirtualizerState} from '@react-stately/virtualizer';
 import React, {FocusEvent, HTMLAttributes, Key, ReactElement, RefObject, useCallback, useEffect, useRef} from 'react';
 import {ScrollView} from './ScrollView';
@@ -127,7 +126,7 @@ export function useVirtualizer<T extends object, V, W>(props: VirtualizerOptions
       return;
     }
 
-    if (focusedKey !== lastFocusedKey.current && getInteractionModality() !== 'pointer') {
+    if (focusedKey !== lastFocusedKey.current) {
       if (scrollToItem) {
         scrollToItem(focusedKey);
       } else {
