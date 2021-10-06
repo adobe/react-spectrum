514aa52816ce32342de345db3b928a3bb9597489
Daniel Lu <dl1644@gmail.com>
2021-10-06 14:04:05 -0700

    updating config and adding commit hash to dist folder

--- scripts/verdaccio.sh
+++ scripts/verdaccio.sh
@@ -13,16 +13,16 @@ fi
 
 function cleanup {
   lsof -ti tcp:4000 | xargs kill
-  # TODO: figure out what of the below doesn't need to happen on CI
-  rm -rf storage/ ~/.config/verdaccio/storage/ $output
-  git tag -d $(git tag -l)
-  git fetch
-  git reset --hard HEAD~1
-  npm set registry $original_registry
+
   if [ "$ci" = false ];
   then
     # Clean up generated dists if run locally
     rm -rf **/dist
+    rm -rf storage/ ~/.config/verdaccio/storage/ $output
+    git tag -d $(git tag -l)
+    git fetch
+    git reset --hard HEAD~1
+    npm set registry $original_registry
   fi
 }
 
@@ -52,7 +52,7 @@ then
   make website-production
   # Rename the dist folder from dist/production/docs to dist/verdaccio/docs
   # If building the sample app, move the contents of the build folder to dist/verdaccio/build or something
-  mv dist/production/ dist/verdaccio/
+  mv dist/production/ dist/`git rev-parse HEAD`/verdaccio/
 else
   # Wait for user input to do cleanup
   read -n 1 -p "Press a key to close server and cleanup"
