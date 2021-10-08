6ee6c0a62dd8f7979c0207884e6477662488c6dc
Rob Snow <rsnow@adobe.com>
2021-10-08 11:52:03 -0700

    Remove dead code

--- jest.config.js
+++ jest.config.js
@@ -101,7 +101,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|styl)$': 'identity-obj-proxy',
-    '\\.\\./Icon/.*$': '<rootDir>/__mocks__/iconMock.js',
     ...mocks
   },
 
