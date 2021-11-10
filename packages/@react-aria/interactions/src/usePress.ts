e7095bbe90a20019e5b26ba128809805197422a3
Daniel Lu <dl1644@gmail.com>
2021-11-10 12:56:02 -0800

    adding bug link for Safari pointer pressure

--- packages/@react-aria/interactions/src/usePress.ts
+++ packages/@react-aria/interactions/src/usePress.ts
@@ -742,6 +742,8 @@ function isVirtualPointerEvent(event: PointerEvent) {
   // If the pointer size is zero, then we assume it's from a screen reader.
   // Android TalkBack double tap will sometimes return a event with width and height of 1
   // and pointerType === 'mouse' so we need to check for a specific combination of event attributes.
+  // Cannot use "event.pressure === 0" as the sole check due to Safari pointer events always returning pressure === 0
+  // instead of .5, see https://bugs.webkit.org/show_bug.cgi?id=206216
   return (
     (event.width === 0 && event.height === 0) ||
     (event.buttons === 1 &&
