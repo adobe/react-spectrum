b9953807f09475219df062a673e4d09aed75e4c4
Aaron Miller <amiller@watheia.org>
2021-11-17 02:08:03 +0000

    Merge branch 'issue-2558' of https://github.com/watheia/react-spectrum into issue-2558

--- scripts/brotli.js
+++ scripts/brotli.js
@@ -1,34 +1,35 @@
 /*
  * Copyright 2020 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * This file is licensed to you under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 
-const fs = require("fs");
-const glob = require("fast-glob");
-const zlib = require("zlib");
+const fs = require('fs');
+const glob = require('fast-glob');
+const zlib = require('zlib');
+// const PQueue = require('p-queue').default;
 const pqueue = async (...args) =>
-  import("p-queue").then(({ default: PQueue }) => new PQueue(...args));
+  import('p-queue').then(({ default: PQueue }) => new PQueue(...args));
 
 function compress(file) {
   return new Promise((resolve, reject) => {
     fs.createReadStream(file)
       .pipe(zlib.createBrotliCompress())
-      .pipe(fs.createWriteStream(file + ".br"))
-      .on("error", (err) => reject(err))
-      .on("close", () => resolve());
+      .pipe(fs.createWriteStream(file + '.br'))
+      .on("error', (err) => reject(err))
+      .on('close', () => resolve());
   });
 }
 
 async function run() {
-  const files = glob.sync("dist/production/**.{js,css,html,map}");
-  const queue = await pqueue({ concurrency: 10 });
+  const files = glob.sync('dist/production/**.{js,css,html,map}');
+  const queue = await pqueue({concurrency: 10});
   for (let file of files) {
     queue.add(() => compress(file));
   }
