adobe/react-spectrum

commit b0e1531420aa45137ea3791074b2c378f9343c49

preventing clicks/taps on focusable elements from scrolling virtualized lists

packages/@react-aria/virtualizer/src/Virtualizer.tsx
@@ -12,6 +12,7 @@
 
 import {Collection} from '@react-types/shared';
 import {focusWithoutScrolling, mergeProps, useLayoutEffect} from '@react-aria/utils';
+import {getInteractionModality} from '@react-aria/interactions';
 import {Layout, Rect, ReusableView, useVirtualizerState, VirtualizerState} from '@react-stately/virtualizer';
 import React, {FocusEvent, HTMLAttributes, Key, ReactElement, RefObject, useCallback, useEffect, useRef} from 'react';
 import {ScrollView} from './ScrollView';
@@ -126,7 +127,7 @@ export function useVirtualizer<T extends object, V, W>(props: VirtualizerOptions
       return;
     }
 
-    if (focusedKey !== lastFocusedKey.current) {
+    if (focusedKey !== lastFocusedKey.current && getInteractionModality() !== 'pointer') {
       if (scrollToItem) {
         scrollToItem(focusedKey);
       } else {

commit b2c22c5409d05707cee295274208384cf738dacf

fixing scroll into view regression
reverting change made in b0e1531420aa45137ea3791074b2c378f9343c49 since it breaks scrolling a focused item into view when a menu is open

packages/@react-aria/virtualizer/src/Virtualizer.tsx
@@ -12,7 +12,6 @@
 
 import {Collection} from '@react-types/shared';
 import {focusWithoutScrolling, mergeProps, useLayoutEffect} from '@react-aria/utils';
-import {getInteractionModality} from '@react-aria/interactions';
 import {Layout, Rect, ReusableView, useVirtualizerState, VirtualizerState} from '@react-stately/virtualizer';
 import React, {FocusEvent, HTMLAttributes, Key, ReactElement, RefObject, useCallback, useEffect, useRef} from 'react';
 import {ScrollView} from './ScrollView';
@@ -129,8 +128,8 @@ export function useVirtualizer<T extends object, V, W>(props: VirtualizerOptions
     if (virtualizer.visibleRect.height === 0) {
       return;
     }
-    let modality = getInteractionModality();
-    if (focusedKey !== lastFocusedKey.current && modality !== 'pointer') {
+
+    if (focusedKey !== lastFocusedKey.current) {
       if (scrollToItem) {
         scrollToItem(focusedKey);
       } else {