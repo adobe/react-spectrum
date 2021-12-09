302874cafd4f01ebf6fafa78cefafe717d06e035
Reid Barber <reid@reidbarber.com>
2021-12-09 17:31:07 -0600

    add custom test renderer with strict mode wrapper

---
+++ scripts/customRTL.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+
+const Wrapper = ({children}) => (
+  <React.StrictMode>
+    {children}
+  </React.StrictMode>
+);
+
+const customRender = (ui, options) =>
+  render(ui, {
+    wrapper: Wrapper,
+    ...options
+  }
+);
+
+// Re-export everything.
+export * from '@testing-library/react';
+
+// Override render method.
+export {customRender as render};
