b6c9d5f184849488184f90ee4247452abbd62607
Justin Skinner <jskinner@adobe.com>
2021-12-09 12:23:10 -0800

    Added a new prop 'isPortal' to trigger a render with 'ProviderWrapper' for componetns rendered in ReactDOM.createPortal

--- packages/@react-spectrum/provider/src/Provider.tsx
+++ packages/@react-spectrum/provider/src/Provider.tsx
@@ -92,7 +92,7 @@ function Provider(props: ProviderProps, ref: DOMRef<HTMLDivElement>) {
   let contents = children;
   let domProps = filterDOMProps(otherProps);
   let {styleProps} = useStyleProps(otherProps, undefined, {matchedBreakpoints});
-  if (!prevContext || props.locale || theme !== prevContext.theme || colorScheme !== prevContext.colorScheme || scale !== prevContext.scale || Object.keys(domProps).length > 0 || otherProps.UNSAFE_className || Object.keys(styleProps.style).length > 0) {
+  if (!prevContext || props.isPortal || props.locale || theme !== prevContext.theme || colorScheme !== prevContext.colorScheme || scale !== prevContext.scale || Object.keys(domProps).length > 0 || otherProps.UNSAFE_className || Object.keys(styleProps.style).length > 0) {
     contents = (
       <ProviderWrapper {...props} UNSAFE_style={{isolation: !prevContext ? 'isolate' : undefined, ...styleProps.style}} ref={ref}>
         {contents}
