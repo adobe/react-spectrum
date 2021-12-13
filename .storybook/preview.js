6f0399dc87906eb1880208d13b7f45ee564bf36b
Reid Barber <reid@reidbarber.com>
2021-12-13 10:26:17 -0600

    add strict mode to storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -20,7 +20,9 @@ export const parameters = {
 export const decorators = [
   story => (
     <VerticalCenter style={{alignItems: 'center', minHeight: '100vh', boxSizing: 'border-box', display: 'flex', justifyContent: 'center'}}>
-      {story()}
+      <React.StrictMode>
+        {story()}
+      </React.StrictMode>
     </VerticalCenter>
   ),
   withProviderSwitcher
