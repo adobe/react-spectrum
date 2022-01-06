e6b0dfe4af865511d097518d1f110ef327d45f01
Reid Barber <reid@reidbarber.com>
2022-01-06 14:27:13 -0600

    fix act import in checkbox tests

--- packages/@react-spectrum/checkbox/test/Checkbox.test.js
+++ packages/@react-spectrum/checkbox/test/Checkbox.test.js
@@ -9,10 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
+import {act, render} from '../../../../lib/customRTL';
 import {Checkbox} from '../';
 import React from 'react';
-import {render} from '../../../../lib/customRTL';
 import userEvent from '@testing-library/user-event';
 
 
