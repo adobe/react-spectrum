8c8cfc3a50987f9362480cac6a23fed8cbe2d1a2
Yusong Wu <1391530737@qq.com>
2022-01-10 19:26:38 -0800

    Update packages/dev/test-utils/src/events.ts
    Co-authored-by: Robert Snow <snowystinger@gmail.com>

--- packages/dev/test-utils/src/events.ts
+++ packages/dev/test-utils/src/events.ts
@@ -33,8 +33,9 @@ export const DEFAULT_LONG_PRESS_TIME = 500;
 
 export function triggerLongPress(element, opts = {}) {
   fireEvent.pointerDown(element, {pointerType: 'touch', ...opts});
-  // @ts-ignore
-  act(() => jest.advanceTimersByTime(DEFAULT_LONG_PRESS_TIME));
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_LONG_PRESS_TIME);
+  });
   fireEvent.pointerUp(element, {pointerType: 'touch', ...opts});
 }
 
