359fa5150420fd53001709e0c1e2c0af22ab51c4
Michael Jordan <mijordan@adobe.com>
2022-02-11 19:20:24 -0500

    fix(#2766): refactor intesectionObserver logic into focusSafely

--- packages/@react-aria/focus/src/focusSafely.ts
+++ packages/@react-aria/focus/src/focusSafely.ts
@@ -13,25 +13,64 @@
 import {focusWithoutScrolling, runAfterTransition} from '@react-aria/utils';
 import {getInteractionModality} from '@react-aria/interactions';
 
+// Define an IntersectionObserver to evaluate whether the browser should scroll an element receiving focus into view.
+const intersectionObserver = (() => {
+  const intersectionObserverOptions:IntersectionObserverInit = {
+    root: undefined,
+    rootMargin: '0px', 
+    threshold: 1
+  };
+
+  const scrollIntoViewOptions:ScrollIntoViewOptions = {
+    behavior: 'auto',
+    block: 'nearest',
+    inline: 'nearest'
+  };
+
+  const intersectionObserverCallback = (entries:Array<IntersectionObserverEntry>, observer:IntersectionObserver) => {
+    entries.forEach(entry => {
+      // If the element receiving focus is not within the browser viewport, scroll it into view 
+      if (!entry.isIntersecting) {
+        runAfterTransition(() => entry.target.scrollIntoView(scrollIntoViewOptions));
+      }
+      observer.unobserve(entry.target);
+    });
+  };
+
+  try {
+    return new IntersectionObserver(intersectionObserverCallback, intersectionObserverOptions);
+  } catch (err) {
+    return undefined;
+  }
+})();
+
 /**
  * A utility function that focuses an element while avoiding undesired side effects such
  * as page scrolling and screen reader issues with CSS transitions.
  */
 export function focusSafely(element: HTMLElement) {
+  const modality = getInteractionModality();
+
   // If the user is interacting with a virtual cursor, e.g. screen reader, then
   // wait until after any animated transitions that are currently occurring on
   // the page before shifting focus. This avoids issues with VoiceOver on iOS
   // causing the page to scroll when moving focus if the element is transitioning
   // from off the screen.
-  if (getInteractionModality() === 'virtual') {
+  if (modality === 'virtual') {
     let lastFocusedElement = document.activeElement;
     runAfterTransition(() => {
       // If focus did not move and the element is still in the document, focus it.
       if (document.activeElement === lastFocusedElement && document.contains(element)) {
         focusWithoutScrolling(element);
+        if (intersectionObserver) {
+          intersectionObserver.observe(element);
+        }
       }
     });
   } else {
     focusWithoutScrolling(element);
+    if (intersectionObserver && modality !== 'pointer') {
+      intersectionObserver.observe(element);
+    }
   }
 }
