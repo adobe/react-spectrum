dedc05b4a64e6edc6a60607e287896267c97bb43
Reid Barber <reid@reidbarber.com>
2022-02-24 11:41:20 -0600

    add test for losing tabIndex=-1 on blur

--- packages/@react-aria/landmark/test/useLandmark.test.tsx
+++ packages/@react-aria/landmark/test/useLandmark.test.tsx
@@ -998,4 +998,31 @@ describe('LandmarkManager', function () {
     expect(document.activeElement).toBe(nav);
     expect(nav).toHaveAttribute('tabIndex', '-1');
   });
+  it('loses the tabIndex=-1 if something else is focused', function () {
+    let {getByRole, getAllByRole} = render(
+      <div>
+        <Navigation>
+          <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </Navigation>
+        <Main>
+          <TextField label="First Name" />
+        </Main>
+      </div>
+    );
+    let nav = getByRole('navigation');
+
+    fireEvent.keyDown(document.activeElement, {key: 'F6'});
+    fireEvent.keyUp(document.activeElement, {key: 'F6'});
+    expect(nav).toHaveAttribute('tabIndex', '-1');
+    expect(document.activeElement).toBe(nav);
+
+    userEvent.tab();
+
+    expect(document.activeElement).toBe(getAllByRole('link')[0]);
+    expect(nav).not.toHaveAttribute('tabIndex');
+  });
 });
