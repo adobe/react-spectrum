ebd6bc2c9269969181d978fd40fa84424edacf9e
Robert Snow <rsnow@adobe.com>
2022-02-24 16:07:22 -0800

    Color area (#2483)
    * Initial ColorArea implementation
    
    * refactor aria back to stately
    
    * more refactoring
    
    * refactor step logic and defaults
    
    * tests and subsequent fixes from tests
    
    * Add uncontrolled to tests
    
    * Add size story
    
    * use snaptovalue and add page steps
    
    * remove dead code
    
    * rename variable to make more sense, fix description
    
    * Add isDisabled based on Spectrum designs
    
    * add min size
    
    * add prop description
    
    * Get back to no useMove changes
    
    * Combine the useMove frequently appears with useKeyboard  (#2508)
    
    * Combine the useMove frequently appears with useKeyboard
    
    * fix merge mistake
    
    * Remove isPage from useMove, add modifier keys
    
    * continue propagation for keys we don't handle
    
    * have aria pass through the step size
    
    Co-authored-by: Rob Snow <rsnow@adobe.com>
    
    * ColorArea: remove aria-roledescription on iOS/Android (#2547)
    
    * ColorArea: remove aria-roledescription on iOS/Android
    
    To better reflect behavior of slider inputs with mobile screen readers on iOS and Android, each input should be labeled by its individual channel name and we should remove aria-roledescription so the input is identified simply as a slider control.
    
    * ColorArea: change aria-valuetext for iOS/Android
    
    On iOS/Android, the aria-valuetext for each slider within the ColorArea control should just be the value for that channel, and need not include the value for the second channel. The value for all three channels is included as the title attribute.
    
    * ColorArea: include channel name in aria-valuetext for iOS/Android
    
    * Add formatting for color name and value based on locale
    
    * allow for differences in formatting strings
    
    * fix lint
    
    * @trivial remove extra whitespace/redundant afterEach
    
    * fix: tests
    
    * fix remaining issues before merges on main make it in
    
    * incorporate michaels a11y and locale fixes
    
    Co-authored-by: Michael Jordan <mijordan@adobe.com>
    Co-authored-by: Daniel Lu <dl1644@gmail.com>
    Co-authored-by: GitHub <octobot@github.com>

---
+++ packages/@react-spectrum/color/stories/ColorArea.stories.tsx
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import {action} from '@storybook/addon-actions';
+import {ColorArea, ColorSlider} from '../';
+import {ColorChannel, SpectrumColorAreaProps} from '@react-types/color';
+import {Flex} from '@adobe/react-spectrum';
+import {Meta, Story} from '@storybook/react';
+import {parseColor} from '@react-stately/color';
+import React, {useState} from 'react';
+import {Text} from '@react-spectrum/text';
+
+
+const meta: Meta<SpectrumColorAreaProps> = {
+  title: 'ColorArea',
+  component: ColorArea
+};
+
+export default meta;
+
+const Template: Story<SpectrumColorAreaProps> = (args) => (
+  <ColorAreaExample {...args} />
+);
+
+let RGB: Set<ColorChannel> = new Set(['red', 'green', 'blue']);
+let difference = (a, b): Set<ColorChannel> => new Set([...a].filter(x => !b.has(x)));
+
+function ColorAreaExample(props: SpectrumColorAreaProps) {
+  let {xChannel, yChannel, isDisabled} = props;
+  let channels = new Set([xChannel, yChannel]);
+  let zChannel: ColorChannel = difference(RGB, channels).keys().next().value;
+  let [color, setColor] = useState(props.defaultValue || parseColor('#ff00ff'));
+  return (<div role="group" aria-label="RGB Color Picker">
+    <Flex gap="size-500" alignItems="center">
+      <Flex direction="column" gap="size-50" alignItems="center">
+        <ColorArea
+          {...props}
+          value={color}
+          onChange={(e) => {
+            if (props.onChange) {
+              props.onChange(e);
+            }
+            setColor(e);
+          }} />
+        <ColorSlider
+          value={color}
+          onChange={(e) => {
+            if (props.onChange) {
+              props.onChange(e);
+            }
+            setColor(e);
+          }}
+          onChangeEnd={props.onChangeEnd}
+          channel={zChannel}
+          isDisabled={isDisabled} />
+      </Flex>
+      <Flex direction="column" alignItems="center" gap="size-100" minWidth={'size-2000'}>
+        <div role="img" aria-label={`color swatch: ${color.toString('rgb')}`} title={`${color.toString('hex')}`} style={{width: '100px', height: '100px', background: color.toString('css')}} />
+        <Text>{color.toString('hex')}</Text>
+      </Flex>
+    </Flex>
+  </div>);
+}
+
+export let XBlueYGreen = Template.bind({});
+XBlueYGreen.storyName = 'RGB xChannel="blue", yChannel="green"';
+XBlueYGreen.args = {xChannel: 'blue', yChannel: 'green', onChange: action('onChange'), onChangeEnd: action('onChangeEnd')};
+
+export let XGreenYBlue = Template.bind({});
+XGreenYBlue.storyName = 'RGB xChannel="green", yChannel="blue"';
+XGreenYBlue.args = {...XBlueYGreen.args, xChannel: 'green', yChannel: 'blue'};
+
+export let XBlueYRed = Template.bind({});
+XBlueYRed.storyName = 'RGB xChannel="blue", yChannel="red"';
+XBlueYRed.args = {...XBlueYGreen.args, xChannel: 'blue', yChannel: 'red'};
+
+export let XRedYBlue = Template.bind({});
+XRedYBlue.storyName = 'GB xChannel="red", yChannel="blue"';
+XRedYBlue.args = {...XBlueYGreen.args, xChannel: 'red', yChannel: 'blue'};
+
+export let XRedYGreen = Template.bind({});
+XRedYGreen.storyName = 'RGB xChannel="red", yChannel="green"';
+XRedYGreen.args = {...XBlueYGreen.args, xChannel: 'red', yChannel: 'green'};
+
+export let XGreenYRed = Template.bind({});
+XGreenYRed.storyName = 'RGB xChannel="green", yChannel="red"';
+XGreenYRed.args = {...XBlueYGreen.args, xChannel: 'green', yChannel: 'red'};
+
+export let XBlueYGreenStep16 = Template.bind({});
+XBlueYGreenStep16.storyName = 'RGB xChannel="blue", yChannel="green", step="16"';
+XBlueYGreenStep16.args = {...XBlueYGreen.args, xChannelStep: 16, yChannelStep: 16};
+
+export let XBlueYGreenPageStep32 = Template.bind({});
+XBlueYGreenPageStep32.storyName = 'RGB xChannel="blue", yChannel="green", pageStep="32"';
+XBlueYGreenPageStep32.args = {...XBlueYGreen.args, xChannelPageStep: 32, yChannelPageStep: 32};
+
+/* TODO: what does a disabled color area look like? */
+export let XBlueYGreenisDisabled = Template.bind({});
+XBlueYGreenisDisabled.storyName = 'RGB xChannel="blue", yChannel="green", isDisabled';
+XBlueYGreenisDisabled.args = {...XBlueYGreen.args, isDisabled: true};
+
+/* TODO: how do we visually label and how to do we aria-label */
+export let XBlueYGreenAriaLabelled = Template.bind({});
+XBlueYGreenAriaLabelled.storyName = 'RGB xChannel="blue", yChannel="green", aria-label="foo"';
+XBlueYGreenAriaLabelled.args = {...XBlueYGreen.args, label: undefined, ariaLabel: 'foo'};
+
+export let XBlueYGreenSize3000 = Template.bind({});
+XBlueYGreenSize3000.storyName = 'RGB xChannel="blue", yChannel="green", size="size-3000"';
+XBlueYGreenSize3000.args = {...XBlueYGreen.args, size: 'size-3000'};
+
+export let XBlueYGreenSize600 = Template.bind({});
+XBlueYGreenSize600.storyName = 'RGB xChannel="blue", yChannel="green", size="size-600"';
+XBlueYGreenSize600.args = {...XBlueYGreen.args, size: 'size-600'};
