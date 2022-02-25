1b23709a0461f6e5d57804059a5412eee8f26fce
Reid Barber <reid@reidbarber.com>
2022-02-25 15:26:04 -0600

    fix the changing hooks warning logic

--- packages/@react-spectrum/list/src/ListView.tsx
+++ packages/@react-spectrum/list/src/ListView.tsx
@@ -109,7 +109,7 @@ function ListView<T extends object>(props: ListViewProps<T>, ref: DOMRef<HTMLDiv
   } = props;
   let isListDraggable = !!dragHooks;
   let dragHooksProvided = useRef(isListDraggable);
-  if (dragHooksProvided.current && !isListDraggable) {
+  if (dragHooksProvided.current !== isListDraggable) {
     console.warn('Drag hooks were provided during one render, but not another. This should be avoided as it may produce unexpected behavior.');
   }
   let domRef = useDOMRef(ref);
