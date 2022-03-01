8240301ed8ebbd4fb46dff68bfe0a4711d3afd10
Danny North <dnorth@adobe.com>
2022-03-01 14:31:15 -0700

    move useColumnResizeState static functions to utils file. Reorganize functions based on usage.

--- packages/@react-stately/table/src/useColumnResizeWidthState.ts
+++ packages/@react-stately/table/src/useColumnResizeWidthState.ts
@@ -1,5 +1,6 @@
 
 import {ColumnProps} from '@react-types/table';
+import {getContentWidth, getDynamicColumnWidths, getMaxWidth, getMinWidth, isStatic, mapToArray, parseStaticWidth} from './utils';
 import {GridNode} from '@react-types/grid';
 import {Key, MutableRefObject, useRef, useState} from 'react';
 import {useLayoutEffect} from '@react-aria/utils';
@@ -47,17 +48,13 @@ export default function useColumnResizeWidthState<T>(
     setColumnWidthsForRef(widths);
   }
 
-  function getContentWidth(widths) {
-    return Array.from(widths).map(e => e[1]).reduce((acc, cur) => acc + cur, 0);
-  }
-
   function resizeColumn(column: GridNode<T>, newWidth: number) : { key: Key, width: number }[] {
     // copy the columnWidths map and set the new width for the column being resized
     let widths = new Map<Key, number>(columnWidthsRef.current);
     widths.set(columns[columns.length - 1].key, 0);
     widths.set(column.key, Math.max(
-      getMinWidth(column.props.minWidth),
-      Math.min(Math.floor(newWidth), getMaxWidth(column.props.maxWidth))
+      getMinWidth(column.props.minWidth, tableWidth.current),
+      Math.min(Math.floor(newWidth), getMaxWidth(column.props.maxWidth, tableWidth.current))
     ));
 
     // keep track of all columns that have been seized
@@ -94,26 +91,22 @@ export default function useColumnResizeWidthState<T>(
     return mapToArray(allAffectedColumns);
   }
 
-  function mapToArray(columnWidthsMap: Map<Key, number>): { key: Key, width: number }[] {
-    return Array.from(columnWidthsMap, ([key, width]) => ({key, width}));
-  }
-
   function buildColumnWidths(affectedColumns: GridNode<T>[], availableSpace: number): Map<Key, number> {
     const widths = new Map<Key, number>();
     let remainingSpace = availableSpace;
-    
+
     const {staticColumns, dynamicColumns} = getStaticAndDynamicColumns(affectedColumns);
 
     staticColumns.forEach(column => {
       let width = getRealColumnWidth(column);
-      let w = parseStaticWidth(width);
+      let w = parseStaticWidth(width, tableWidth.current);
       widths.set(column.key, w);
       remainingSpace -= w;
     });
 
     // dynamic columns
     if (dynamicColumns.length > 0) {
-      const newColumnWidths = getDynamicColumnWidths(dynamicColumns, remainingSpace);
+      const newColumnWidths = getDynamicColumnWidths(dynamicColumns, remainingSpace, tableWidth.current);
       for (let column of newColumnWidths) {
         widths.set(column.key, column.calculatedWidth);
       }
@@ -133,112 +126,6 @@ export default function useColumnResizeWidthState<T>(
     let props = column.props as ColumnProps<T>;
     return resizedColumns?.has(column.key) ? columnWidthsRef.current.get(column.key) : props.width ?? props.defaultWidth ?? getDefaultWidth?.(column.props) ?? '1fr';
   }
-
-  function isStatic(width: number | string): boolean {
-    return width !== null && width !== undefined && (typeof width === 'number' || width.match(/^(\d+)%$/) !== null);
-  }
-
-  function parseStaticWidth(width: number | string): number {
-    if (typeof width === 'string') {
-      let match = width.match(/^(\d+)%$/);
-      if (!match) {
-        throw new Error('Only percentages or numbers are supported for static column widths');
-      }
-      return tableWidth.current * (parseInt(match[1], 10) / 100);
-    }
-    return width;
-  }
-
-  function getDynamicColumnWidths(remainingColumns: GridNode<T>[], remainingSpace: number) {
-    let columns = mapColumns(remainingColumns, remainingSpace);
-  
-    columns.sort((a, b) => b.delta - a.delta);
-    columns = solveWidths(columns, remainingSpace);
-    columns.sort((a, b) => a.index - b.index);
-  
-    return columns;
-  }
-
-  type mappedColumn = GridNode<T> & {
-    index: number,
-    delta: number,
-    calculatedWidth?: number
-  };
-
-  function mapColumns(remainingColumns: GridNode<T>[], remainingSpace: number): mappedColumn[] {
-    let remainingFractions = remainingColumns.reduce(
-      (sum, column) => sum + parseFractionalUnit(column.props.defaultWidth),
-      0
-    );
-  
-    let columns = [...remainingColumns].map((column, index) => {
-      const targetWidth =
-        (parseFractionalUnit(column.props.defaultWidth) * remainingSpace) / remainingFractions;
-  
-      return {
-        ...column,
-        index,
-        delta: Math.max(
-          0,
-          getMinWidth(column.props.minWidth) - targetWidth,
-          targetWidth - getMaxWidth(column.props.maxWidth)
-        )
-      };
-    });
-  
-    return columns;
-  }
-
-  function solveWidths(remainingColumns: mappedColumn[], remainingSpace: number): mappedColumn[] {
-    let remainingFractions = remainingColumns.reduce(
-      (sum, col) => sum + parseFractionalUnit(col.props.defaultWidth),
-      0
-    );
-  
-    for (let i = 0; i < remainingColumns.length; i++) {
-      const column = remainingColumns[i];
-  
-      const targetWidth =
-        (parseFractionalUnit(column.props.defaultWidth) * remainingSpace) / remainingFractions;
-  
-      let width = Math.max(
-        getMinWidth(column.props.minWidth),
-        Math.min(Math.floor(targetWidth), getMaxWidth(column.props.maxWidth))
-      );
-      column.calculatedWidth = width;
-      remainingSpace -= width;
-      remainingFractions -= parseFractionalUnit(column.props.defaultWidth);
-    }
-  
-    return remainingColumns;
-  }
-
-  function parseFractionalUnit(width: string): number {
-    if (!width) {
-      return 1;
-    } 
-    let match = width.match(/^(\d+)(?=fr$)/);
-    // if width is the incorrect format, just deafult it to a 1fr
-    if (!match) {
-      console.warn(`width: ${width} is not a supported format, width should be a number (ex. 150), percentage (ex. '50%') or fr unit (ex. '2fr')`, 
-      'defaulting to \'1fr\'');
-      return 1;
-    }
-    return parseInt(match[0], 10);
-  }
-
-  function getMinWidth(minWidth: number | string): number {
-    return minWidth !== undefined && minWidth !== null
-      ? parseStaticWidth(minWidth)
-      : 75;
-  }
-  
-  function getMaxWidth(maxWidth: number | string): number {
-    return maxWidth !== undefined && maxWidth !== null
-      ? parseStaticWidth(maxWidth)
-      : Infinity;
-  }
-
 
   return [columnWidthsRef, resizeColumn, setTableWidth, onColumnResizeStart, onColumnResizeEnd];
 }
