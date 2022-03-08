c3b881f5747ef5dda11c64c28214b9c1caa0c007
Devon Govett <devongovett@gmail.com>
2022-03-07 16:05:20 -0800

    Aria docs

--- packages/@react-aria/calendar/src/useCalendarCell.ts
+++ packages/@react-aria/calendar/src/useCalendarCell.ts
@@ -41,8 +41,21 @@ interface CalendarCellAria {
   isSelected: boolean,
   /** Whether the cell is focused. */
   isFocused: boolean,
-  /** Whether the cell is disabled. */
+  /**
+   * Whether the cell is disabled, according to the calendar's `minValue, `maxValue`, and `isDisabled` props.
+   * Disabled dates are not focusable, and cannot be selected by the user. They are typically
+   * displayed with a dimmed appearance.
+   */
   isDisabled: boolean,
+  /**
+   * Whether the cell is unavailable, according to the calendar's `isDateUnavailable` prop. Unavailable dates remain
+   * focusable, but cannot be selected by the user. They should be displayed with a visual affordance to indicate they
+   * are unavailable, such as a different color or a strikethrough.
+   *
+   * Note that because they are focusable, unavailable dates must meet a 4.5:1 color contrast ratio,
+   * [as defined by WCAG](https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html).
+   */
+  isUnavailable: boolean,
   /**
    * Whether the cell is outside the visible range of the calendar.
    * For example, dates before the first day of a month in the same week.
@@ -70,7 +83,8 @@ export function useCalendarCell(props: AriaCalendarCellProps, state: CalendarSta
   let isSelected = state.isSelected(date);
   let isFocused = state.isCellFocused(date);
   isDisabled = isDisabled || state.isCellDisabled(date);
-  let isSelectable = !isDisabled && !state.isCellUnavailable(date);
+  let isUnavailable = state.isCellUnavailable(date);
+  let isSelectable = !isDisabled && !isUnavailable;
 
   // For performance, reuse the same date object as before if the new date prop is the same.
   // This allows subsequent useMemo results to be reused.
@@ -289,6 +303,7 @@ export function useCalendarCell(props: AriaCalendarCellProps, state: CalendarSta
     isFocused,
     isSelected,
     isDisabled,
+    isUnavailable,
     isOutsideVisibleRange: date.compare(state.visibleRange.start) < 0 || date.compare(state.visibleRange.end) > 0,
     formattedDate
   };
