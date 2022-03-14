1e2ee481b6d2f0f254f891ceaf2e477fb20cd874
Danny North <dnorth@adobe.com>
2022-03-14 15:37:59 -0600

    removing the react-aria dependency in react-stately.
    utilizing useLayoutEffect because in the RSP version, this hook would end up depending on actual DOM measurements. That may not be true for other implementations of this hook so its possible requiring it isnt neceessary.

--- packages/@react-aria/utils/src/useLayoutEffect.ts
+++ packages/@react-aria/utils/src/useLayoutEffect.ts
@@ -10,11 +10,4 @@
  * governing permissions and limitations under the License.
  */
 
-import React from 'react';
-
-// During SSR, React emits a warning when calling useLayoutEffect.
-// Since neither useLayoutEffect nor useEffect run on the server,
-// we can suppress this by replace it with a noop on the server.
-export const useLayoutEffect = typeof window !== 'undefined'
-  ? React.useLayoutEffect
-  : () => {};
+export {useLayoutEffect} from '@react-stately/utils';
--- packages/@react-stately/utils/src/useLayoutEffect.ts
+++ packages/@react-stately/utils/src/useLayoutEffect.ts
@@ -10,6 +10,11 @@
  * governing permissions and limitations under the License.
  */
 
-export * from './useControlledState';
-export * from './number';
-export * from './useLayoutEffect';
+import React from 'react';
+
+// During SSR, React emits a warning when calling useLayoutEffect.
+// Since neither useLayoutEffect nor useEffect run on the server,
+// we can suppress this by replace it with a noop on the server.
+export const useLayoutEffect = typeof window !== 'undefined'
+  ? React.useLayoutEffect
+  : () => {};
