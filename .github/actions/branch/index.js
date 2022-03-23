78be86028efaa6e09adc0ac3937045c871a0ca4e
Daniel Lu <dl1644@gmail.com>
2022-03-22 17:32:12 -0700

    only create/update a new branch if the trigger comment was made in a fork pr

--- .github/actions/branch/index.js
+++ .github/actions/branch/index.js
@@ -13,40 +13,44 @@ async function run() {
       ...context.repo,
       pull_number: prNumber
     });
-    const branch = `${data.user.login}-${data.head.ref}`;
-    const ref = `refs/heads/${branch}`;
-    const sha = data.merge_commit_sha;
 
-    let res;
-    // Look up if branch for fork PR exists in base repo
-    try {
-      res = await octokit.repos.getBranch({
-        ...context.repo,
-        branch
-      });
-    } catch (error) {
-      if (!(error.name === 'HttpError' && error.status === 404)) {
-        throw error;
-      } else {
-        // If branch doesn't exist for the forked PR, create one so we can get a
-        // build for it and return
-        await octokit.git.createRef({
+    // Only create/update a branch if the PR from a fork
+    if (data.head.repo.full_name !== context.payload.repository.full_name) {
+      const branch = `${data.user.login}-${data.head.ref}`;
+      const ref = `refs/heads/${branch}`;
+      const sha = data.merge_commit_sha;
+
+      let res;
+      // Look up if branch for fork PR exists in base repo
+      try {
+        res = await octokit.repos.getBranch({
           ...context.repo,
-          ref,
-          sha
+          branch
         });
-        return;
+      } catch (error) {
+        if (!(error.name === 'HttpError' && error.status === 404)) {
+          throw error;
+        } else {
+          // If branch doesn't exist for the forked PR, create one so we can get a
+          // build for it and return
+          await octokit.git.createRef({
+            ...context.repo,
+            ref,
+            sha
+          });
+          return;
+        }
       }
-    }
 
-    // If branch already exists update it to match fork PR state.
-    if (res.status === 200) {
-      await octokit.git.updateRef({
-        ...context.repo,
-        sha,
-        ref: `heads/${branch}`,
-        force: true
-      })
+      // If branch already exists update it to match fork PR state.
+      if (res.status === 200) {
+        await octokit.git.updateRef({
+          ...context.repo,
+          sha,
+          ref: `heads/${branch}`,
+          force: true
+        })
+      }
     }
   } catch (error) {
     core.setFailed(error.message);
