3ab8e0eaba34d1ae46ffacb27e2ee05bfdede542
Tiffany Lin <tiffanyl@adobe.com>
2022-05-06 08:42:56 -0700

    redo changes removed by merge conflict

--- packages/@react-spectrum/list/src/ListView.tsx
+++ packages/@react-spectrum/list/src/ListView.tsx
@@ -92,6 +92,8 @@ interface ListViewProps<T> extends CollectionBase<T>, DOMProps, AriaLabelingProp
   isQuiet?: boolean,
   /** The current loading state of the ListView. Determines whether or not the progress circle should be shown. */
   loadingState?: LoadingState,
+  /** Sets how the text overflow will behave. */
+  overflowMode?: 'truncate' | 'wrap',
   /** Sets what the ListView should render when there is no content to display. */
   renderEmptyState?: () => JSX.Element,
   /**
@@ -112,6 +114,7 @@ function ListView<T extends object>(props: ListViewProps<T>, ref: DOMRef<HTMLDiv
     onLoadMore,
     loadingState,
     isQuiet,
+    overflowMode = 'truncate',
     onAction,
     dragHooks,
     ...otherProps
@@ -197,7 +200,8 @@ function ListView<T extends object>(props: ListViewProps<T>, ref: DOMRef<HTMLDiv
             {
               'react-spectrum-ListView--quiet': isQuiet,
               'react-spectrum-ListView--draggable': isListDraggable,
-              'react-spectrum-ListView--loadingMore': loadingState === 'loadingMore'
+              'react-spectrum-ListView--loadingMore': loadingState === 'loadingMore',
+              'react-spectrum-ListView--wrap': overflowMode === 'wrap'
             },
             styleProps.className
           )
