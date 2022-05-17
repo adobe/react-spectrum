907214cdf5b093b33da79cd487ed11b5cfff4ca3
Reid Barber <reid@reidbarber.com>
2022-05-17 16:18:37 -0500

    fix grid-area for description

--- packages/@react-spectrum/list/src/ListViewItem.tsx
+++ packages/@react-spectrum/list/src/ListViewItem.tsx
@@ -12,7 +12,7 @@
 import {Checkbox} from '@react-spectrum/checkbox';
 import ChevronLeftMedium from '@spectrum-icons/ui/ChevronLeftMedium';
 import ChevronRightMedium from '@spectrum-icons/ui/ChevronRightMedium';
-import {classNames, ClearSlots, SlotProvider} from '@react-spectrum/utils';
+import {classNames, ClearSlots, SlotProvider, unwrapDOMRef, useHasChild} from '@react-spectrum/utils';
 import {Content} from '@react-spectrum/view';
 import {CSSTransition} from 'react-transition-group';
 import type {DraggableItemResult, DroppableItemResult} from '@react-aria/dnd';
@@ -62,6 +62,7 @@ export function ListViewItem<T>(props: ListViewItemProps<T>) {
     isDisabled
   }, state, rowRef);
   let {checkboxProps} = useListSelectionCheckbox({key: item.key}, state);
+  let hasDescription = useHasChild(`.${listStyles['react-spectrum-ListViewItem-description']}`, rowRef);
 
   let draggableItem: DraggableItemResult;
   if (isListDraggable) {
@@ -172,7 +173,8 @@ export function ListViewItem<T>(props: ListViewItemProps<T>) {
               'react-spectrum-ListViewItem--dropTarget': !!isDropTarget,
               'react-spectrum-ListViewItem--firstRow': isFirstRow,
               'react-spectrum-ListViewItem--lastRow': isLastRow,
-              'react-spectrum-ListViewItem--isFlushBottom': isFlushWithContainerBottom
+              'react-spectrum-ListViewItem--isFlushBottom': isFlushWithContainerBottom,
+              'react-spectrum-ListViewItem--hasDescription': hasDescription
             }
           )
         }
