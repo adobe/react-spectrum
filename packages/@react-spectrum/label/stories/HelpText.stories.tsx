5a4973e206dcb824effd7c4be57f2ba7a0daf024
Kyle Taborski <ktabors@yahoo.com>
2022-05-20 15:47:41 -0700

    helptext & label storybook codemod csf 3.0

--- packages/@react-spectrum/label/stories/HelpText.stories.tsx
+++ packages/@react-spectrum/label/stories/HelpText.stories.tsx
@@ -9,199 +9,179 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 import { Flex } from '@react-spectrum/layout';
 import React, { useState } from 'react';
 import { SpectrumTextFieldProps } from '@react-types/textfield';
 import { TextField } from '@react-spectrum/textfield';
-
 export default {
   title: 'HelpText',
-
   parameters: {
-    providerSwitcher: { status: 'positive' },
+    providerSwitcher: {
+      status: 'positive',
+    },
   },
 };
-
-export const Description = () => render({ description: 'Password must be at least 8 characters.' });
-
-Description.story = {
+export const Description = {
+  render: () =>
+    render({
+      description: 'Password must be at least 8 characters.',
+    }),
   name: 'description',
 };
-
-export const ErrorMessage = () =>
-  render({
-    errorMessage: 'Create a password with at least 8 characters.',
-    validationState: 'invalid',
-  });
-
-ErrorMessage.story = {
+export const ErrorMessage = {
+  render: () =>
+    render({
+      errorMessage: 'Create a password with at least 8 characters.',
+      validationState: 'invalid',
+    }),
   name: 'error message',
 };
-
-export const DescriptionAndErrorMessage = () => {
-  let [value, setValue] = useState('');
-
-  return (
-    <TextField
-      label="Empty field"
-      description="This input is only valid when it's empty."
-      errorMessage="Remove input."
-      value={value}
-      onChange={setValue}
-      validationState={value.length ? 'invalid' : undefined}
-    />
-  );
-};
-
-DescriptionAndErrorMessage.story = {
+export const DescriptionAndErrorMessage = {
+  render: () => {
+    let [value, setValue] = useState('');
+    return (
+      <TextField
+        label="Empty field"
+        description="This input is only valid when it's empty."
+        errorMessage="Remove input."
+        value={value}
+        onChange={setValue}
+        validationState={value.length ? 'invalid' : undefined}
+      />
+    );
+  },
   name: 'description and error message',
 };
-
-export const ErrorMessageWithNoDescription = () => {
-  let [value, setValue] = useState('');
-
-  return (
-    <TextField
-      label="Empty field"
-      errorMessage="Remove input."
-      value={value}
-      onChange={setValue}
-      validationState={value.length ? 'invalid' : 'valid'}
-    />
-  );
-};
-
-ErrorMessageWithNoDescription.story = {
+export const ErrorMessageWithNoDescription = {
+  render: () => {
+    let [value, setValue] = useState('');
+    return (
+      <TextField
+        label="Empty field"
+        errorMessage="Remove input."
+        value={value}
+        onChange={setValue}
+        validationState={value.length ? 'invalid' : 'valid'}
+      />
+    );
+  },
   name: 'error message with no description',
 };
-
-export const DescriptionValidationStateValid = () =>
-  render({
-    label: 'Nickname',
-    description: "Enter your nickname, or leave blank if you don't have one.",
-    validationState: 'valid',
-  });
-
-DescriptionValidationStateValid.story = {
+export const DescriptionValidationStateValid = {
+  render: () =>
+    render({
+      label: 'Nickname',
+      description: "Enter your nickname, or leave blank if you don't have one.",
+      validationState: 'valid',
+    }),
   name: 'description, validationState: valid',
 };
-
-export const DescriptionAndErrorMessageValidationStateValid = () =>
-  render({
-    label: 'Valid field',
-    description: 'The error message will never render because validationState is "valid".',
-    errorMessage: 'Uninformative error message', // Won't render
-    validationState: 'valid',
-  });
-
-DescriptionAndErrorMessageValidationStateValid.story = {
+export const DescriptionAndErrorMessageValidationStateValid = {
+  render: () =>
+    render({
+      label: 'Valid field',
+      description: 'The error message will never render because validationState is "valid".',
+      errorMessage: 'Uninformative error message',
+      // Won't render
+      validationState: 'valid',
+    }),
   name: 'description and error message, validationState: valid',
 };
-
-export const Disabled = () =>
-  render({ description: 'Password must be at least 8 characters.', isDisabled: true });
-
-Disabled.story = {
+export const Disabled = {
+  render: () =>
+    render({
+      description: 'Password must be at least 8 characters.',
+      isDisabled: true,
+    }),
   name: 'disabled',
 };
-
-export const LabelAlignEnd = () =>
-  render({
-    description: 'Password must be at least 8 characters.',
-    labelAlign: 'end',
-  });
-
-LabelAlignEnd.story = {
+export const LabelAlignEnd = {
+  render: () =>
+    render({
+      description: 'Password must be at least 8 characters.',
+      labelAlign: 'end',
+    }),
   name: 'labelAlign: end',
 };
-
-export const LabelPositionSide = () =>
-  render({
-    description: 'Password must be at least 8 characters.',
-    labelPosition: 'side',
-  });
-
-LabelPositionSide.story = {
+export const LabelPositionSide = {
+  render: () =>
+    render({
+      description: 'Password must be at least 8 characters.',
+      labelPosition: 'side',
+    }),
   name: 'labelPosition: side',
 };
-
-export const LabelAlignEndLabelPositionSide = () =>
-  render({
-    description: 'Password must be at least 8 characters.',
-    labelAlign: 'end',
-    labelPosition: 'side',
-  });
-
-LabelAlignEndLabelPositionSide.story = {
+export const LabelAlignEndLabelPositionSide = {
+  render: () =>
+    render({
+      description: 'Password must be at least 8 characters.',
+      labelAlign: 'end',
+      labelPosition: 'side',
+    }),
   name: 'labelAlign: end, labelPosition: side',
 };
-
-export const NoVisibleLabel = () =>
-  render({
-    label: null,
-    'aria-label': 'Password',
-    description: 'Password must be at least 8 characters.',
-  });
-
-NoVisibleLabel.story = {
+export const NoVisibleLabel = {
+  render: () =>
+    render({
+      label: null,
+      'aria-label': 'Password',
+      description: 'Password must be at least 8 characters.',
+    }),
   name: 'no visible label',
 };
-
-export const NoVisibleLabelLabelPositionSide = () =>
-  render({
-    label: null,
-    'aria-label': 'Password',
-    description: 'Password must be at least 8 characters.',
-    labelPosition: 'side',
-  });
-
-NoVisibleLabelLabelPositionSide.story = {
+export const NoVisibleLabelLabelPositionSide = {
+  render: () =>
+    render({
+      label: null,
+      'aria-label': 'Password',
+      description: 'Password must be at least 8 characters.',
+      labelPosition: 'side',
+    }),
   name: 'no visible label, labelPosition: side',
 };
-
-export const CustomWidth = () =>
-  render({
-    label: 'Password',
-    description: 'Password must be at least 8 characters.',
-    width: '100px',
-  });
-
-CustomWidth.story = {
+export const CustomWidth = {
+  render: () =>
+    render({
+      label: 'Password',
+      description: 'Password must be at least 8 characters.',
+      width: '100px',
+    }),
   name: 'custom width',
 };
-
-export const CustomWidthLabelPositionSide = () =>
-  render({
-    label: 'Password',
-    description: 'Password must be at least 8 characters.',
-    width: '440px',
-    labelPosition: 'side',
-  });
-
-CustomWidthLabelPositionSide.story = {
+export const CustomWidthLabelPositionSide = {
+  render: () =>
+    render({
+      label: 'Password',
+      description: 'Password must be at least 8 characters.',
+      width: '440px',
+      labelPosition: 'side',
+    }),
   name: 'custom width, labelPosition: side',
 };
-
-export const DescriptionAndCustomDescription = () =>
-  renderCustomDescription({ description: 'Password must be at least 8 characters.' });
-
-DescriptionAndCustomDescription.story = {
+export const DescriptionAndCustomDescription = {
+  render: () =>
+    renderCustomDescription({
+      description: 'Password must be at least 8 characters.',
+    }),
   name: 'description and custom description',
 };
-
-export const ContainerWithTextAlignmentSet = () => (
-  <Flex direction="column" gap="size-200" UNSAFE_style={{ textAlign: 'center' }}>
-    <TextField label="Password" description="Enter a single digit number." />
-    <TextField
-      label="Password 2"
-      errorMessage="Create a password with at least 8 characters."
-      validationState="invalid"
-    />
-  </Flex>
-);
-
-ContainerWithTextAlignmentSet.story = {
+export const ContainerWithTextAlignmentSet = {
+  render: () => (
+    <Flex
+      direction="column"
+      gap="size-200"
+      UNSAFE_style={{
+        textAlign: 'center',
+      }}
+    >
+      <TextField label="Password" description="Enter a single digit number." />
+      <TextField
+        label="Password 2"
+        errorMessage="Create a password with at least 8 characters."
+        validationState="invalid"
+      />
+    </Flex>
+  ),
   name: 'container with text alignment set',
 };
 
--- packages/@react-spectrum/label/stories/Label.stories.tsx
+++ packages/@react-spectrum/label/stories/Label.stories.tsx
@@ -9,77 +9,92 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 import { Label } from '../';
 import React from 'react';
 import { SpectrumLabelProps } from '@react-types/label';
 import { TextField } from '@react-spectrum/textfield';
-
 export default {
   title: 'Label',
-
   parameters: {
-    providerSwitcher: { status: 'positive' },
+    providerSwitcher: {
+      status: 'positive',
+    },
   },
 };
-
 export const Default = () => render({});
-export const LabelAlignStart = () => render({ labelAlign: 'start', width: '100%' });
-
-LabelAlignStart.story = {
+export const LabelAlignStart = {
+  render: () =>
+    render({
+      labelAlign: 'start',
+      width: '100%',
+    }),
   name: 'labelAlign: start',
 };
-
-export const LabelAlignEnd = () => render({ labelAlign: 'end', width: '100%' });
-
-LabelAlignEnd.story = {
+export const LabelAlignEnd = {
+  render: () =>
+    render({
+      labelAlign: 'end',
+      width: '100%',
+    }),
   name: 'labelAlign: end',
 };
-
-export const LabelPositionSideLabelAlignStart = () =>
-  render({ labelPosition: 'side', labelAlign: 'start', width: 80 });
-
-LabelPositionSideLabelAlignStart.story = {
+export const LabelPositionSideLabelAlignStart = {
+  render: () =>
+    render({
+      labelPosition: 'side',
+      labelAlign: 'start',
+      width: 80,
+    }),
   name: 'labelPosition: side, labelAlign: start',
 };
-
-export const LabelPositionSideLabelAlignEnd = () =>
-  render({ labelPosition: 'side', labelAlign: 'end', width: 80 });
-
-LabelPositionSideLabelAlignEnd.story = {
+export const LabelPositionSideLabelAlignEnd = {
+  render: () =>
+    render({
+      labelPosition: 'side',
+      labelAlign: 'end',
+      width: 80,
+    }),
   name: 'labelPosition: side, labelAlign: end',
 };
-
-export const IsRequired = () => render({ isRequired: true });
-
-IsRequired.story = {
+export const IsRequired = {
+  render: () =>
+    render({
+      isRequired: true,
+    }),
   name: 'isRequired',
 };
-
-export const NecessityIndicatorIcon = () =>
-  render({ isRequired: true, necessityIndicator: 'icon' });
-
-NecessityIndicatorIcon.story = {
+export const NecessityIndicatorIcon = {
+  render: () =>
+    render({
+      isRequired: true,
+      necessityIndicator: 'icon',
+    }),
   name: 'necessityIndicator: icon',
 };
-
-export const NecessityIndicatorLabel = () =>
-  render({ isRequired: true, necessityIndicator: 'label' });
-
-NecessityIndicatorLabel.story = {
+export const NecessityIndicatorLabel = {
+  render: () =>
+    render({
+      isRequired: true,
+      necessityIndicator: 'label',
+    }),
   name: 'necessityIndicator: label',
 };
-
-export const IsRequiredFalseNecessityIndicatorLabel = () =>
-  render({ isRequired: false, necessityIndicator: 'label' });
-
-IsRequiredFalseNecessityIndicatorLabel.story = {
+export const IsRequiredFalseNecessityIndicatorLabel = {
+  render: () =>
+    render({
+      isRequired: false,
+      necessityIndicator: 'label',
+    }),
   name: 'isRequired: false, necessityIndicator: label',
 };
 
 function render(props: SpectrumLabelProps = {}) {
   return (
-    <div style={{ whiteSpace: 'nowrap' }}>
+    <div
+      style={{
+        whiteSpace: 'nowrap',
+      }}
+    >
       <Label {...props} for="test">
         Test
       </Label>
