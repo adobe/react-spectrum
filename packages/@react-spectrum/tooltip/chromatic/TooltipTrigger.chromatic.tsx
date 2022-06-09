dd35bdd55e3b5ee75ea53251056391d8980d0195
Kyle Taborski <ktabors@yahoo.com>
2022-06-09 16:49:01 -0700

    chromatic TooltipTrigger polishing the CSF3 codemod and combining stories

--- packages/@react-spectrum/tooltip/chromatic/TooltipTrigger.chromatic.tsx
+++ packages/@react-spectrum/tooltip/chromatic/TooltipTrigger.chromatic.tsx
@@ -9,40 +9,86 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { ActionButton } from '@react-spectrum/button';
-import { Meta, Story } from '@storybook/react';
+import {ActionButton} from '@react-spectrum/button';
+import {ComponentMeta, ComponentStoryObj} from '@storybook/react';
 import React from 'react';
-import { SpectrumTooltipTriggerProps } from '@react-types/tooltip';
-import { Tooltip, TooltipTrigger } from '../';
-const meta: Meta<SpectrumTooltipTriggerProps> = {
+import {Tooltip, TooltipTrigger} from '../';
+
+type TooltipTriggerStory = ComponentStoryObj<typeof TooltipTrigger>;
+
+export default {
   title: 'TooltipTrigger',
   component: TooltipTrigger,
+  args: {
+    defaultOpen: true,
+    children: [
+      <ActionButton>Trigger Tooltip</ActionButton>,
+      <Tooltip>Tooltip message.</Tooltip>
+    ]
+  },
   parameters: {
     chromaticProvider: {
       colorSchemes: ['light'],
       locales: ['en-US'],
       scales: ['medium'],
-      disableAnimations: true,
+      disableAnimations: true
     },
     // chromatic needs a bit more time than disableAnimations allows
     chromatic: {
       pauseAnimationAtEnd: true,
-    },
+    }
+  }
+} as ComponentMeta<typeof TooltipTrigger>;
+
+export const Default: TooltipTriggerStory = {};
+
+export const PlacementStart: TooltipTriggerStory = {
+  args: {placement: 'start'}
+}
+
+export const Offset50: TooltipTriggerStory = {
+  args: {offset: 50}
+}
+
+export const CrossOffset50: TooltipTriggerStory = {
+  args: {crossOffset: 50}
+}
+
+export const ContainerPadding50AtEdge: TooltipTriggerStory = {
+  args: {containerPadding: 50},
+  decorators: [(Story) => (
+    <div style={{width: '100%'}}>
+      <Story />
+    </div>
+  )]
+}
+
+export const ArrowPositioningAtEdge: TooltipTriggerStory = {
+  args: {
+    children: [
+      <ActionButton>Trigger Tooltip</ActionButton>,
+      <Tooltip>Long tooltip message that just goes on and on.</Tooltip>
+    ]
   },
+  decorators: [(Story) => (
+    <div style={{width: '100%'}}>
+      <Story />
+    </div>
+  )]
 };
-export default meta;
 
-const Template = (): Story<SpectrumTooltipTriggerProps> => (args) =>
-  (
-    <TooltipTrigger {...args} defaultOpen>
-      <ActionButton>Trigger Tooltip</ActionButton>
-      <Tooltip>Tooltip message.</Tooltip>
-    </TooltipTrigger>
-  );
-
-export const Default = Template().bind({});
-Default.args = {};
-export const PlacementStart = Template().bind({});
-PlacementStart.args = { ...Default.args, placement: 'start' };
-export const PlacementEnd = Template().bind({});
-PlacementEnd.args = { ...Default.args, placement: 'end' };
+export const PlacementSideFlip: TooltipTriggerStory = {
+  args: {
+    placement: 'start',
+    shouldFlip: false,
+    children: [
+      <ActionButton>Trigger Tooltip</ActionButton>,
+      <Tooltip>Long tooltip message that just goes on and on again.</Tooltip>
+    ]
+  },
+  decorators: [(Story) => (
+    <div style={{width: '100%'}}>
+      <Story />
+    </div>
+  )]
+}
