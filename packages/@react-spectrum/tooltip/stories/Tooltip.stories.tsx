b3d6397b2d3c333e298cd878cb39917fcd175716
Kyle Taborski <ktabors@yahoo.com>
2022-06-08 21:05:13 -0700

    refactored Tooltip to 2 stories with controls

--- packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
+++ packages/@react-spectrum/tooltip/stories/Tooltip.stories.tsx
@@ -9,128 +9,55 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+import {ComponentMeta, ComponentStoryObj} from '@storybook/react';
 import React from 'react';
-import { Tooltip } from '../src';
+import {Tooltip} from '../src';
+
+type TooltipStory = ComponentStoryObj<typeof Tooltip>;
+
+const argTypes = {
+  placement: {
+    control: 'radio',
+    defaultValue: 'top',
+    options: ['top', 'bottom', 'left', 'right', 'start', 'end']
+  },
+  variant: {
+    control: 'radio',
+    defaultValue: undefined,
+    options: [undefined, 'neutral', 'info', 'positive', 'negative']
+  },
+  showIcon: {
+    control: 'boolean',
+    defaultValue: false
+  },
+  isOpen: {
+    control: {disable: true}
+  }
+};
+
 export default {
   title: 'Tooltip',
-};
-export const Default = {
-  render: () => render('This is a tooltip.'),
-  name: 'default',
-};
-export const PlacementLeft = {
-  render: () =>
-    render('This is a tooltip.', {
-      placement: 'left',
-    }),
-  name: 'placement: left',
-};
-export const PlacementRight = {
-  render: () =>
-    render('This is a tooltip.', {
-      placement: 'right',
-    }),
-  name: 'placement: right',
-};
-export const PlacementTop = {
-  render: () =>
-    render('This is a tooltip.', {
-      placement: 'top',
-    }),
-  name: 'placement: top',
-};
-export const PlacementBottom = {
-  render: () =>
-    render('This is a tooltip.', {
-      placement: 'bottom',
-    }),
-  name: 'placement: bottom',
-};
-export const VariantNeutral = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'neutral',
-    }),
-  name: 'variant: neutral',
-};
-export const VariantPositive = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'positive',
-    }),
-  name: 'variant: positive',
-};
-export const VariantNegative = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'negative',
-    }),
-  name: 'variant: negative',
-};
-export const VariantInfo = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'info',
-    }),
-  name: 'variant: info',
-};
-export const VariantNeutralIcon = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'neutral',
-      showIcon: true,
-    }),
-  name: 'variant: neutral, icon',
-};
-export const VariantPositiveIcon = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'positive',
-      showIcon: true,
-    }),
-  name: 'variant: positive, icon',
-};
-export const VariantNegativeIcon = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'negative',
-      showIcon: true,
-    }),
-  name: 'variant: negative, icon',
-};
-export const VariantInfoIcon = {
-  render: () =>
-    render('This is a tooltip.', {
-      variant: 'info',
-      showIcon: true,
-    }),
-  name: 'variant: info, icon',
-};
-export const LongContent = {
-  render: () => render(longMarkup),
-  name: 'long content',
-};
-const longMarkup = (
-  <div>
-    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
-    sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
-    Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
-    commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
-    ipsum rutrum orci, sagittis tempus lacus enim ac dui.
-  </div>
-);
+  component: Tooltip,
+  args: {
+    isOpen: true,
+    children: 'This is a tooltip'
+  },
+  argTypes: argTypes
+} as ComponentMeta<typeof Tooltip>;
 
-function render(content, props = {}) {
-  return (
-    <div
-      style={{
-        display: 'inline-block',
-      }}
-    >
-      <Tooltip {...props} isOpen>
-        {content}
-      </Tooltip>
-    </div>
-  );
-}
+export const Default: TooltipStory = {};
+
+export const LongContent: TooltipStory = {
+  args: {
+    children: (
+      <div>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero
+        sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+        Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed,
+        commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+        ipsum rutrum orci, sagittis tempus lacus enim ac dui.
+      </div>
+    )
+  }
+};
