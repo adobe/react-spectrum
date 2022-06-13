10701e52da6e430ee95947fe38e1bd271979a33a
yihuiliao <yliao@adobe.com>
2022-06-13 16:39:00 -0400

    reinstated useeffect to change background color for now

--- examples/rsp-cra-18/src/App.tsx
+++ examples/rsp-cra-18/src/App.tsx
@@ -8,13 +8,13 @@ function App() {
   let [selected, setSelection] = React.useState(false);
 
   // changes the background color of the document based on the status of 'selected'
-  // React.useEffect(() => {
-  //   document.body.style.backgroundColor = selected ? "#f5f5f5" : "#1e1e1e";
+  React.useEffect(() => {
+    document.body.style.backgroundColor = selected ? "#f5f5f5" : "#1e1e1e";
 
-  //   return () => {
-  //     document.body.style.backgroundColor = selected ? "#f5f5f5" : "#1e1e1e";
-  //   };
-  // }, [selected]);
+    return () => {
+      document.body.style.backgroundColor = selected ? "#f5f5f5" : "#1e1e1e";
+    };
+  }, [selected]);
 
   return (
     <Provider theme={defaultTheme} 
