05a3d0832b52a9bf6b4cb3b4b27c830f3adeed9e
Reid Barber <reid@reidbarber.com>
2022-07-18 16:54:47 -0500

    switch from template and use parcel for sandbox

--- packages/dev/parcel-transformer-mdx-docs/MDXTransformer.js
+++ packages/dev/parcel-transformer-mdx-docs/MDXTransformer.js
@@ -17,6 +17,7 @@ const {fragmentUnWrap, fragmentWrap} = require('./MDXFragments');
 const yaml = require('js-yaml');
 const dprint = require('dprint-node');
 const t = require('@babel/types');
+const {getExampleSandpack, sandpackImports} = require('./SandpackFiles');
 
 const IMPORT_MAPPINGS = {
   '@react-spectrum/theme-default': {
@@ -26,190 +27,6 @@ const IMPORT_MAPPINGS = {
     theme: 'darkTheme'
   }
 };
-
-const indexHtml = `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>React Spectrum Example</title>
-  </head>
-  <body>
-      <style>
-        body {
-          min-height: 100vh;
-          margin: 0;
-        }
-        #root {
-          min-height: 100vh;
-        }
-        #root > div {
-          padding: 40px;
-          min-height: calc(100vh - 80px);
-        }
-      </style>
-    <div id="root"></div>
-  </body>
-</html>`;
-
-const formatExampleCode = (code, fixIndention = false) => fixIndention ? code.replace(/`/g, '\\`').replace(/\n/g, '\n    ').trim() : code.replace(/`/g, '\\`').trim();
-
-const formatImports = (imports) => imports.join('\n').replace(/`/g, '\\`');
-
-const getIndexFile = (componentName, colorScheme = 'light') => `import React from "react";
-import ReactDOM from "react-dom";
-import { Provider, defaultTheme } from "@adobe/react-spectrum";
-import ${componentName} from "./${componentName}";
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Provider theme={defaultTheme} colorScheme="${colorScheme}">
-    <${componentName} />
-  </Provider>,
-  rootElement
-);
-`;
-
-const getExampleFile = (exampleCode, imports) => `import React from "react";
-${formatImports(imports)}
-
-export default function Example() {
-  return (
-    ${formatExampleCode(exampleCode, true)}
-  );
-}
-`;
-
-const getNamedExampleFile = (exampleCode, imports) => `import React from "react";
-${formatImports(imports)}
-
-export default ${formatExampleCode(exampleCode)}
-`;
-
-const getExampleSandpack = (id, provider, code, imports, componentName, named = false) => `
-function CustomSandpack(props) {
-  const { code } = useActiveCode();
-  const { sandpack } = useSandpack();
-  const { refresh } = useSandpackNavigation();
-
-  React.useEffect(() => {
-    // Detect when dark/light mode is changed and update index.js file.
-    const observer = new MutationObserver((mutations) => {
-      if (mutations[0].target.style.cssText.includes('dark')) {
-        sandpack.updateFile('/index.js', props.indexFiles.dark);
-      } else {
-        sandpack.updateFile('/index.js', props.indexFiles.light);
-      }
-    });
-
-    observer.observe(document.documentElement, {attributeFilter: ['style']});
-
-    return () => {
-      observer.disconnect();
-    }
-  }, []);
-
-  const openInCodeSandboxButtonRef = React.useRef(null);
-
-  return (
-    <SandpackLayout
-      style={{ flexDirection: "column", border: 'none' }}>
-      <SandpackThemeProvider
-        theme={{
-          colors: {
-            surface1: 'var(--hljs-background)',
-            surface2: 'var(--hljs-background)',
-            surface3: 'var(--hljs-background)',
-            clickable: 'var(--hljs-color)',
-            base: 'var(--hljs-color)',
-            activeBackground: 'var(--hljs-background)',
-            hover: 'var(--hljs-color)'
-          },
-          syntax: {
-            comment: 'var(--hljs-comment-color)',
-            definition: 'var(--hljs-section-color)',
-            keyword: 'var(--hljs-keyword-color)',
-            plain: 'var(--hljs-color)', // Unsure
-            property: 'var(--hljs-attribute-color)',
-            punctuation: 'var(--hljs-color)',
-            static: 'var(--hljs-literal-color)',
-            string: 'var(--hljs-string-color)',
-            tag: 'var(--hljs-section-color)'
-          }
-        }}
-      >
-        <SandpackCodeEditor showTabs={false} wrapContent={true} style={{height: 'auto'}} />
-        <SandpackPreview
-          style={{display: 'block', background: 'none', border: 'none'}}
-          actionsChildren={
-            <ButtonGroup>
-              <TooltipTrigger>
-                <ActionButton
-                  isQuiet
-                  aria-label="Copy example code"
-                  onPress={() => {
-                    navigator.clipboard.writeText(code.trim());
-                  }}
-                >
-                  <Copy size="S" />
-                </ActionButton>
-                <Tooltip>Copy code</Tooltip>
-              </TooltipTrigger>
-              <TooltipTrigger>
-                <ActionButton
-                  isQuiet
-                  aria-label="Refresh example"
-                  onPress={() => refresh()}
-                >
-                  <Refresh size="S" />
-                </ActionButton>
-                <Tooltip>Refresh</Tooltip>
-              </TooltipTrigger>
-              <TooltipTrigger>
-                <ActionButton
-                  isQuiet
-                  aria-label="Open example in CodeSandbox"
-                  onPress={() => openInCodeSandboxButtonRef.current.firstChild.click()}
-                >
-                  <LinkOut size="S" />
-                </ActionButton>
-                <Tooltip>Open in CodeSandbox</Tooltip>
-              </TooltipTrigger>
-            </ButtonGroup>
-          }
-          showRefreshButton={false}
-          showOpenInCodeSandbox={false} />
-          <div ref={openInCodeSandboxButtonRef} style={{ display: "none" }}>
-            <UnstyledOpenInCodeSandboxButton>
-              Open in CodeSandbox
-            </UnstyledOpenInCodeSandboxButton>
-          </div>
-        </SandpackThemeProvider>
-    </SandpackLayout>
-  );
-}
-
-ReactDOM.render(
-  <${provider} UNSAFE_style={{padding: 0}}>
-    <SandpackProvider
-      template="react"
-      files={{"/${componentName}.js":
-          {
-            code: \`${named ? getNamedExampleFile(code, imports) : getExampleFile(code, imports)}\`,
-            active: true
-          },
-        "/index.js": localStorage.theme === 'dark' ? \`${getIndexFile(componentName, 'dark')}\` : \`${getIndexFile(componentName, 'light')}\`,
-        "/public/index.html": \`${indexHtml}\`
-      }}
-      customSetup={{
-        dependencies: {
-          "@adobe/react-spectrum": "latest",
-        }
-      }}>
-      <CustomSandpack indexFiles={{light: \`${getIndexFile(componentName, 'light')}\`, dark: \`${getIndexFile(componentName, 'dark')}\` }} />
-    </SandpackProvider>
-  </${provider}>,
-  document.getElementById("${id}"));`; 
-
 
 module.exports = new Transformer({
   async loadConfig({config}) {
@@ -529,22 +346,7 @@ module.exports = new Transformer({
       clientBundle += `import React from 'react';
 import ReactDOM from 'react-dom';
 import {Example as ExampleProvider} from '@react-spectrum/docs/src/ThemeSwitcher';
-${shouldImportSandpack ?
-`import {
-  SandpackProvider,
-  SandpackLayout,
-  SandpackCodeEditor,
-  SandpackPreview,
-  SandpackThemeProvider,
-  useActiveCode,
-  useSandpack,
-  useSandpackNavigation,
-  UnstyledOpenInCodeSandboxButton
-} from "@codesandbox/sandpack-react";
-import Copy from '@spectrum-icons/workflow/Copy';
-import Refresh from '@spectrum-icons/workflow/Refresh';
-import LinkOut from '@spectrum-icons/workflow/LinkOut';
-import {ActionButton, ButtonGroup, Tooltip, TooltipTrigger} from '@adobe/react-spectrum';` : ''}
+${shouldImportSandpack ? sandpackImports : ''}
 ${exampleCode.join('\n')}
 export default {};
 `;
---
+++ packages/dev/parcel-transformer-mdx-docs/SandpackFiles.js
@@ -0,0 +1,207 @@
+const indexHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React Spectrum Example</title>
+  </head>
+  <body>
+      <style>
+        body {
+          min-height: 100vh;
+          margin: 0;
+        }
+        #app {
+          min-height: 100vh;
+        }
+        #app > div {
+          padding: 40px;
+          min-height: calc(100vh - 80px);
+        }
+      </style>
+    <div id="app"></div>
+  </body>
+</html>`;
+
+const sandpackImports = `import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackThemeProvider,
+  useActiveCode,
+  useSandpack,
+  useSandpackNavigation,
+  UnstyledOpenInCodeSandboxButton
+} from "@codesandbox/sandpack-react";
+import Copy from '@spectrum-icons/workflow/Copy';
+import Refresh from '@spectrum-icons/workflow/Refresh';
+import LinkOut from '@spectrum-icons/workflow/LinkOut';
+import {ActionButton, ButtonGroup, Tooltip, TooltipTrigger} from '@adobe/react-spectrum';`;
+
+const formatExampleCode = (code, fixIndention = false) => fixIndention ? code.replace(/`/g, '\\`').replace(/\n/g, '\n    ').trim() : code.replace(/`/g, '\\`').trim();
+
+const formatImports = (imports) => imports.join('\n').replace(/`/g, '\\`');
+
+const getIndexFile = (componentName, colorScheme = 'light') => `import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
+import ${componentName} from "./${componentName}";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+  <Provider theme={defaultTheme} colorScheme="${colorScheme}">
+    <${componentName} />
+  </Provider>
+);
+`;
+
+const getExampleFile = (exampleCode, imports) => `import React from "react";
+${formatImports(imports)}
+
+export default function Example() {
+  return (
+    ${formatExampleCode(exampleCode, true)}
+  );
+}
+`;
+
+const getNamedExampleFile = (exampleCode, imports) => `import React from "react";
+${formatImports(imports)}
+
+export default ${formatExampleCode(exampleCode)}
+`;
+
+const getExampleSandpack = (id, provider, code, imports, componentName, named = false) => `
+function CustomSandpack(props) {
+  const { code } = useActiveCode();
+  const { sandpack } = useSandpack();
+  const { refresh } = useSandpackNavigation();
+
+  React.useEffect(() => {
+    // Detect when dark/light mode is changed and update index.js file.
+    const observer = new MutationObserver((mutations) => {
+      if (mutations[0].target.style.cssText.includes('dark')) {
+        sandpack.updateFile('/index.js', props.indexFiles.dark);
+      } else {
+        sandpack.updateFile('/index.js', props.indexFiles.light);
+      }
+    });
+
+    observer.observe(document.documentElement, {attributeFilter: ['style']});
+
+    return () => {
+      observer.disconnect();
+    }
+  }, []);
+
+  const openInCodeSandboxButtonRef = React.useRef(null);
+
+  return (
+    <SandpackLayout
+      style={{ flexDirection: "column", border: 'none' }}>
+      <SandpackThemeProvider
+        theme={{
+          colors: {
+            surface1: 'var(--hljs-background)',
+            surface2: 'var(--hljs-background)',
+            surface3: 'var(--hljs-background)',
+            clickable: 'var(--hljs-color)',
+            base: 'var(--hljs-color)',
+            activeBackground: 'var(--hljs-background)',
+            hover: 'var(--hljs-color)'
+          },
+          syntax: {
+            comment: 'var(--hljs-comment-color)',
+            definition: 'var(--hljs-section-color)',
+            keyword: 'var(--hljs-keyword-color)',
+            plain: 'var(--hljs-color)', // Unsure
+            property: 'var(--hljs-attribute-color)',
+            punctuation: 'var(--hljs-color)',
+            static: 'var(--hljs-literal-color)',
+            string: 'var(--hljs-string-color)',
+            tag: 'var(--hljs-section-color)'
+          }
+        }}
+      >
+        <SandpackCodeEditor showTabs={false} wrapContent={true} style={{height: 'auto'}} />
+        <SandpackPreview
+          style={{display: 'block', background: 'none', border: 'none'}}
+          actionsChildren={
+            <ButtonGroup>
+              <TooltipTrigger>
+                <ActionButton
+                  isQuiet
+                  aria-label="Copy example code"
+                  onPress={() => {
+                    navigator.clipboard.writeText(code.trim());
+                  }}
+                >
+                  <Copy size="S" />
+                </ActionButton>
+                <Tooltip>Copy code</Tooltip>
+              </TooltipTrigger>
+              <TooltipTrigger>
+                <ActionButton
+                  isQuiet
+                  aria-label="Refresh example"
+                  onPress={() => refresh()}
+                >
+                  <Refresh size="S" />
+                </ActionButton>
+                <Tooltip>Refresh</Tooltip>
+              </TooltipTrigger>
+              <TooltipTrigger>
+                <ActionButton
+                  isQuiet
+                  aria-label="Open example in CodeSandbox"
+                  onPress={() => openInCodeSandboxButtonRef.current.firstChild.click()}
+                >
+                  <LinkOut size="S" />
+                </ActionButton>
+                <Tooltip>Open in CodeSandbox</Tooltip>
+              </TooltipTrigger>
+            </ButtonGroup>
+          }
+          showRefreshButton={false}
+          showOpenInCodeSandbox={false} />
+          <div ref={openInCodeSandboxButtonRef} style={{ display: "none" }}>
+            <UnstyledOpenInCodeSandboxButton>
+              Open in CodeSandbox
+            </UnstyledOpenInCodeSandboxButton>
+          </div>
+        </SandpackThemeProvider>
+    </SandpackLayout>
+  );
+}
+
+ReactDOM.render(
+  <${provider} UNSAFE_style={{padding: 0}}>
+    <SandpackProvider
+      files={{
+        "/${componentName}.js":
+          {
+            code: \`${named ? getNamedExampleFile(code, imports) : getExampleFile(code, imports)}\`,
+            active: true
+          },
+        "/index.js": localStorage.theme === 'dark' ? \`${getIndexFile(componentName, 'dark')}\` : \`${getIndexFile(componentName, 'light')}\`,
+        "/index.html": \`${indexHtml}\`
+      }}
+      customSetup={{
+        dependencies: {
+          "@adobe/react-spectrum": "latest",
+          react: "^18.0.0",
+          "react-dom": "^18.0.0",
+        },
+        entry: "/index.js",
+        main: "/index.html",
+        environment: "parcel",
+      }}>
+      <CustomSandpack indexFiles={{light: \`${getIndexFile(componentName, 'light')}\`, dark: \`${getIndexFile(componentName, 'dark')}\` }} />
+    </SandpackProvider>
+  </${provider}>,
+  document.getElementById("${id}"));`; 
+
+module.exports = {getExampleSandpack, sandpackImports};
+
