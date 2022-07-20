1063dc7832a5d65b98c278f9ea93eec8fa9a0fb9
Daniel Lu <dl1644@gmail.com>
2022-07-20 16:46:34 -0700

    fixing build

--- scripts/verdaccioPkgSize.js
+++ scripts/verdaccioPkgSize.js
@@ -8,11 +8,15 @@ verdaccioPkgSize();
 // Creates a file listing the tarball sizes of every Verdaccio published package
 // Assumes that the verdaccio db json exists and that the store for the packages is ~/.config/verdaccio/storage/
 function verdaccioPkgSize() {
-  let verdaccioDBPath = path.join(__dirname, '..', 'storage', '.verdaccio-db.json');
-  let publishedPackages = JSON.parse(fs.readFileSync(verdaccioDBPath), 'utf').list;
   let verdaccioStorePath = exec('echo ~/.config/verdaccio/storage/').toString().trim();
 
+  if (!fs.existsSync(verdaccioStorePath)) {
+    verdaccioStorePath = path.join(__dirname, '..', 'verdaccio', 'storage/');
+  }
+
   let json = {};
+  let verdaccioDBPath = path.join(__dirname, '..', 'storage', '.verdaccio-db.json');
+  let publishedPackages = JSON.parse(fs.readFileSync(verdaccioDBPath), 'utf').list;
   for (let pkg of publishedPackages) {
     let tarballPath = glob.sync(`**/${pkg}/*.tgz`, {cwd: verdaccioStorePath});
     let size = fs.statSync(verdaccioStorePath + tarballPath).size / 1000;
