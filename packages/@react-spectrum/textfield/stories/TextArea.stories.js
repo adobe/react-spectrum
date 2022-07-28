94b3845182bb0df861febcc8dea395f5d0c92e76
Robert Snow <rsnow@adobe.com>
2022-07-27 17:18:18 -0700

    Revert "Readonly textfield textarea (#3277)" (#3352)

--- packages/@react-spectrum/textfield/stories/TextArea.stories.js
+++ packages/@react-spectrum/textfield/stories/TextArea.stories.js
@@ -19,58 +19,6 @@ import React, {useState} from 'react';
 import {storiesOf} from '@storybook/react';
 import {TextArea} from '../';
 
-const parameters = {
-  args: {
-    isReadOnly: true,
-    isQuiet: false,
-    isRequired: false,
-    isDisabled: false,
-    autoFocus: true,
-    validationState: '',
-    description: '',
-    errorMessage: '',
-    value: ''
-  },
-  argTypes: {
-    isQuiet: {
-      control: {type: 'boolean'}
-    },
-    isRequired: {
-      control: {type: 'boolean'}
-    },
-    isDisabled: {
-      control: {type: 'boolean'}
-    },
-    autoFocus: {
-      control: {type: 'boolean'}
-    },
-    validationState: {
-      control: {
-        type: 'radio',
-        options: ['', 'invalid', 'valid', '']
-      }
-    },
-    description: {
-      control: {
-        type: 'radio',
-        options: ['', 'Please enter a street address']
-      }
-    },
-    errorMessage: {
-      control: {
-        type: 'radio',
-        options: ['', 'please enter a valid street address']
-      }
-    },
-    value: {
-      control: {
-        type: 'radio',
-        options: ['', 'foo  '.repeat(20)]
-      }
-    }
-  }
-};
-
 storiesOf('TextArea', module)
   .addParameters({providerSwitcher: {status: 'positive'}})
   .add(
@@ -199,24 +147,7 @@ storiesOf('TextArea', module)
     () => <ControlledTextArea />
   )
   .add('in flex', () => renderInFlexRowAndBlock())
-  .add('in flex validation state', () => renderInFlexRowAndBlock({validationState: 'invalid'}))
-  .add('test: isReadOnly, with controls',
-    (args) => (   
-      <TextArea
-        label="Comments"
-        onChange={action('change')}
-        onFocus={action('focus')}
-        onBlur={action('blur')}
-        UNSAFE_className="custom_classname"
-        {...args} />
-    ), parameters
-  )
-  .add('test: isReadOnly, defaultValue',
-    () => render({isReadOnly: true, defaultValue: 'foo  '.repeat(10)})
-  )
-  .add('test: isReadOnly, with icon, value = icon',
-    () => render({isReadOnly: true, icon: <Info />,  value: 'icon'})
-  );
+  .add('in flex validation state', () => renderInFlexRowAndBlock({validationState: 'invalid'}));
 
 function render(props = {}) {
   return (
@@ -235,7 +166,6 @@ function ControlledTextArea(props) {
   return (
     <>
       <TextArea label="megatron" value={value} onChange={setValue} {...props} isQuiet />
-      <TextArea label="megatron" value={value} onChange={setValue} {...props} isReadOnly />
       <Button variant="primary" onPress={() => setValue('decepticons are evil transformers and should be kicked out of earth')}>Set Text</Button>
     </>
   );
