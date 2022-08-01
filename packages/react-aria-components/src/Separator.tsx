f22aece46e466907654a49833f8cd95df8ab35f1
Devon Govett <devongovett@gmail.com>
2022-08-01 08:56:19 -0700

    Convert separator

---
+++ packages/react-aria-components/src/Separator.tsx
@@ -0,0 +1,32 @@
+import {SeparatorProps as AriaSeparatorProps, useSeparator} from 'react-aria';
+import React, {createContext, ElementType, ForwardedRef, forwardRef} from 'react';
+import {StyleProps, useContextProps} from './utils';
+
+interface SeparatorProps extends AriaSeparatorProps, StyleProps {}
+
+export const SeparatorContext = createContext<SeparatorProps>({});
+
+function Separator(props: SeparatorProps, ref: ForwardedRef<Element>) {
+  [props, ref] = useContextProps(props, ref, SeparatorContext);
+  let {elementType, orientation, style, className} = props;
+  let Element = (elementType as ElementType) || 'hr';
+  if (Element === 'hr' && orientation === 'vertical') {
+    Element = 'div';
+  }
+
+  let {separatorProps} = useSeparator({
+    elementType,
+    orientation
+  });
+
+  return (
+    <Element
+      {...separatorProps}
+      style={style}
+      className={className}
+      ref={ref} />
+  );
+}
+
+const _Separator = forwardRef(Separator);
+export {_Separator as Separator};
