bb7aa460330423893b099db84e9cf567f289b710
Devon Govett <devongovett@gmail.com>
2022-08-05 16:31:49 -0700

    Revert focusing the first item for now

--- packages/@react-aria/selection/src/useSelectableCollection.ts
+++ packages/@react-aria/selection/src/useSelectableCollection.ts
@@ -354,21 +354,6 @@ export function useSelectableCollection(options: AriaSelectableCollectionOptions
   }, []);
 
   useEffect(() => {
-    // If the focusedKey becomes null while the collection has focus, move focus to the first item.
-    let focusedKey = manager.focusedKey;
-    if (manager.isFocused && focusedKey == null) {
-      let navigateToFirstKey = (key: Key | undefined) => {
-        if (key != null) {
-          focusedKey = key;
-          manager.setFocusedKey(key);
-          if (selectOnFocus) {
-            manager.replaceSelection(key);
-          }
-        }
-      };
-      navigateToFirstKey(manager.firstSelectedKey ?? delegate.getFirstKey());
-    }
-
     // If not virtualized, scroll the focused element into view when the focusedKey changes.
     // When virtualized, Virtualizer handles this internally.
     if (!isVirtualized && manager.focusedKey && scrollRef?.current) {
