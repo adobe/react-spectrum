35cda5ea9111ff6a3fe452f417f2f3b641e73b5a
Devon Govett <devongovett@gmail.com>
2022-08-05 10:08:03 -0700

    ts

--- packages/@react-aria/combobox/src/useComboBox.ts
+++ packages/@react-aria/combobox/src/useComboBox.ts
@@ -27,7 +27,7 @@ import {useLocalizedStringFormatter} from '@react-aria/i18n';
 import {useMenuTrigger} from '@react-aria/menu';
 import {useTextField} from '@react-aria/textfield';
 
-interface AriaComboBoxOptions<T> extends Omit<AriaComboBoxProps<T>, 'children'> {
+export interface AriaComboBoxOptions<T> extends Omit<AriaComboBoxProps<T>, 'children'> {
   /** The ref for the input element. */
   inputRef: RefObject<HTMLInputElement>,
   /** The ref for the list box popover. */
