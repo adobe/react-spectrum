3c5bf21e1a43c52d7570cd99c3dfdd616b8b010f
Daniel Lu <dl1644@gmail.com>
2022-08-08 09:31:07 -0700

    remove --no-scope-hoist

--- scripts/buildWebsite.js
+++ scripts/buildWebsite.js
@@ -59,7 +59,7 @@ async function build() {
     browserslist: packageJSON.browserslist,
     scripts: {
       // Add a public url if provided via arg (for verdaccio prod doc website build since we want a commit hash)
-      build: `DOCS_ENV=production PARCEL_WORKER_BACKEND=process parcel build 'docs/*/*/docs/*.mdx' 'packages/dev/docs/pages/**/*.mdx' --no-scope-hoist ${publicUrlFlag}`,
+      build: `DOCS_ENV=production PARCEL_WORKER_BACKEND=process parcel build 'docs/*/*/docs/*.mdx' 'packages/dev/docs/pages/**/*.mdx' ${publicUrlFlag}`,
       postinstall: 'patch-package'
     }
   };
