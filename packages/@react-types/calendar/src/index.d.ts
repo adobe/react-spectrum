e9c42bb2f5be027207fb2bad6a5feada8cc3609a
Daniel Lu <dl1644@gmail.com>
2022-08-25 11:12:47 -0700

    properly extend from calendar aria types

--- packages/@react-types/calendar/src/index.d.ts
+++ packages/@react-types/calendar/src/index.d.ts
@@ -69,7 +69,7 @@ export interface AriaCalendarProps<T extends DateValue> extends CalendarProps<T>
 
 export interface AriaRangeCalendarProps<T extends DateValue> extends RangeCalendarProps<T>, DOMProps, AriaLabelingProps {}
 
-export interface SpectrumCalendarProps<T extends DateValue> extends CalendarProps<T>, StyleProps {
+export interface SpectrumCalendarProps<T extends DateValue> extends AriaCalendarProps<T>, StyleProps {
   /**
    * The number of months to display at once. Up to 3 months are supported.
    * @default 1
@@ -77,7 +77,7 @@ export interface SpectrumCalendarProps<T extends DateValue> extends CalendarProp
   visibleMonths?: number
 }
 
-export interface SpectrumRangeCalendarProps<T extends DateValue> extends RangeCalendarProps<T>, StyleProps {
+export interface SpectrumRangeCalendarProps<T extends DateValue> extends AriaRangeCalendarProps<T>, StyleProps {
   /**
    * The number of months to display at once. Up to 3 months are supported.
    * @default 1
