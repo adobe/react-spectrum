167f18e6c07f5ffa0074d26ee14919b44938aac9
Devon Govett <devongovett@gmail.com>
2022-09-07 14:42:08 -0700

    Rename StaticField to LabeledValue

--- packages/@react-spectrum/labeledvalue/src/index.ts
+++ packages/@react-spectrum/labeledvalue/src/index.ts
@@ -12,4 +12,5 @@
 
 /// <reference types="css-module-types" />
 
-export * from './StaticField';
+export type {SpectrumLabeledValueProps} from './LabeledValue';
+export {LabeledValue} from './LabeledValue';
