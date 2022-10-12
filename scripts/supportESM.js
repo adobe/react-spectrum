73b37a3cabf6a68738aedae1d94ad89e23bd63b7
Rob Snow <rsnow@adobe.com>
2022-10-11 19:39:15 -0700

    fix exports field

--- scripts/supportESM.js
+++ scripts/supportESM.js
@@ -16,22 +16,23 @@ function run() {
 
   for (let packageJSON of packages) {
     let json = JSON.parse(fs.readFileSync(packageJSON), 'utf8');
-    if (!json.exports) {
-      let newPackageJSON = {};
-      for (let [field, value] of Object.entries(json)) {
-        newPackageJSON[field] = value;
-        if (field === 'main') {
-          newPackageJSON.exports = value;
+    if (json.exports) {
+      delete json.exports;
+    }
+    let newPackageJSON = {};
+    for (let [field, value] of Object.entries(json)) {
+      newPackageJSON[field] = value;
+      if (field === 'main') {
+        newPackageJSON.exports = `./${value}`;
 
-          if (argv.dryRun) {
-            console.log('setting main field in ', json.name);
-          }
+        if (argv.dryRun) {
+          console.log('setting main field in ', json.name);
         }
       }
-      if (!argv.dryRun) {
-        let pkg = JSON.stringify(newPackageJSON, null, 2);
-        fs.writeFileSync(packageJSON, `${pkg}\n`);
-      }
+    }
+    if (!argv.dryRun) {
+      let pkg = JSON.stringify(newPackageJSON, null, 2);
+      fs.writeFileSync(packageJSON, `${pkg}\n`);
     }
   }
 }
