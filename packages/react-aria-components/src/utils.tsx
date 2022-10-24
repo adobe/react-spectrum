473c6e058b01183e833a693708773d0fe657dddf
Devon Govett <devongovett@gmail.com>
2022-10-23 20:49:17 -0700

    Add missing prop descriptions

--- packages/react-aria-components/src/utils.tsx
+++ packages/react-aria-components/src/utils.tsx
@@ -48,20 +48,26 @@ export function Provider<A, B, C, D, E, F, G, H>({values, children}: ProviderPro
 }
 
 export interface StyleProps {
+  /** The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the element. */
   className?: string,
+  /** The inline [style](https://developer.mozilla.org/en-US/docs/Web/API/Element/style) for the element. */
   style?: CSSProperties
 }
 
 export interface DOMProps extends StyleProps {
+  /** The children of the component. */
   children?: ReactNode
 }
 
 export interface StyleRenderProps<T> {
+  /** The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the element. A function may be provided to compute the class based on component state. */
   className?: string | ((values: T) => string),
+  /** The inline [style](https://developer.mozilla.org/en-US/docs/Web/API/Element/style) for the element. A function may be provided to compute the style based on component state. */
   style?: CSSProperties | ((values: T) => CSSProperties)
 }
 
 export interface RenderProps<T> extends StyleRenderProps<T> {
+  /** The children of the component. A function may be provided to alter the children based on component state. */
   children?: ReactNode | ((values: T) => ReactNode)
 }
 
@@ -96,6 +102,7 @@ export function useRenderProps<T>({className, style, children, defaultClassName,
 
 export type WithRef<T, E> = T & {ref?: React.ForwardedRef<E>};
 export interface SlotProps {
+  /** A slot name for the component. Slots allow the component to receive props from a parent component. */
   slot?: string
 }
 
