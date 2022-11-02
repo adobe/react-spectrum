dfba094240cdcc49f3a08e319f4cbff465fa11fe
Daniel Lu <dl1644@gmail.com>
2022-11-02 11:35:34 -0700

    adding storybook test runner

---
+++ .storybook/test-runner.js
@@ -0,0 +1,19 @@
+const { injectAxe, checkA11y } = require('axe-playwright');
+
+/*
+* See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
+* to learn more about the test-runner hooks API.
+*/
+module.exports = {
+  async preRender(page) {
+    await injectAxe(page);
+  },
+  async postRender(page) {
+    await checkA11y(page, '#root', {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+    });
+  },
+};
