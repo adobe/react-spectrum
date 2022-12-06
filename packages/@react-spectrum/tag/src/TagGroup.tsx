af7d23ebcd14478418a2b9a552f7509084f745d9
Reid Barber <reid@reidbarber.com>
2022-12-06 17:37:33 -0600

    add FocusScope

--- packages/@react-spectrum/tag/src/TagGroup.tsx
+++ packages/@react-spectrum/tag/src/TagGroup.tsx
@@ -13,6 +13,7 @@
 import {ActionButton} from '@react-spectrum/button';
 import {classNames, useDOMRef, useStyleProps} from '@react-spectrum/utils';
 import {DOMRef} from '@react-types/shared';
+import {FocusScope} from '@react-aria/focus';
 import {GridCollection, useGridState} from '@react-stately/grid';
 import {mergeProps, useLayoutEffect, useResizeObserver, useValueEffect} from '@react-aria/utils';
 import React, {ReactElement, useCallback, useMemo, useState} from 'react';
@@ -148,34 +149,36 @@ function TagGroup<T extends object>(props: SpectrumTagGroupProps<T>, ref: DOMRef
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let {tabIndex, role, ...otherGridProps} = gridProps;
   return (
-    <div
-      {...mergeProps(styleProps, tagGroupProps, otherGridProps)}
-      className={
+    <FocusScope>
+      <div
+        {...mergeProps(styleProps, tagGroupProps, otherGridProps)}
+        className={
         classNames(
           styles,
           'spectrum-Tags',
           styleProps.className
         )
       }
-      role={state.collection.size ? 'grid' : null}
-      ref={domRef}>
-      {visibleTags.map(item => (
-        <Tag
-          {...item.childNodes[0].props}
-          key={item.key}
-          item={item}
-          state={state}
-          allowsRemoving={allowsRemoving}
-          onRemove={onRemove}>
-          {item.childNodes[0].rendered}
-        </Tag>
+        role={state.collection.size ? 'grid' : null}
+        ref={domRef}>
+        {visibleTags.map(item => (
+          <Tag
+            {...item.childNodes[0].props}
+            key={item.key}
+            item={item}
+            state={state}
+            allowsRemoving={allowsRemoving}
+            onRemove={onRemove}>
+            {item.childNodes[0].rendered}
+          </Tag>
       ))}
-      {maxRows != null && visibleTagCount < gridCollection.size &&
+        {maxRows != null && visibleTagCount < gridCollection.size &&
         <ActionButton isQuiet onPress={() => setIsCollapsed(!isCollapsed)}>
           {isCollapsed ? `Show all (${gridCollection.size})` : 'Show less '}
         </ActionButton>
       }
-    </div>
+      </div>
+    </FocusScope>
   );
 }
 
