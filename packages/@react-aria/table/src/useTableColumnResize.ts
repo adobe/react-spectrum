b228c3a162d2f40c0b1ae92bdc12b671d4e6321a
Daniel Lu <dl1644@gmail.com>
2023-03-03 12:04:23 -0800

    fix logic

--- packages/@react-aria/table/src/useTableColumnResize.ts
+++ packages/@react-aria/table/src/useTableColumnResize.ts
@@ -168,7 +168,7 @@ export function useTableColumnResize<T>(props: AriaTableColumnResizeProps<T>, st
   }
   let value = Math.floor(state.getColumnWidth(item.key));
   let modality = useInteractionModality();
-  let description = triggerRef?.current != null && (modality === 'keyboard' || modality === 'virtual') && !isResizing.current ? stringFormatter.format('resizerDescription') : undefined;
+  let description = triggerRef?.current == null && (modality === 'keyboard' || modality === 'virtual') && !isResizing.current ? stringFormatter.format('resizerDescription') : undefined;
   let descriptionProps = useDescription(description);
   let ariaProps = {
     'aria-label': props.label,
