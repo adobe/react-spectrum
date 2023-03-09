9dde16062566b2ba85e800fd85c6ef08231007f5
Michael Jordan <mijordan@adobe.com>
2023-03-09 15:38:58 -0500

    fix(#4176): VisuallyHidden triggering overflow in Chrome
    Use `px` units.

--- packages/@react-aria/visually-hidden/src/VisuallyHidden.tsx
+++ packages/@react-aria/visually-hidden/src/VisuallyHidden.tsx
@@ -33,12 +33,12 @@ const styles: CSSProperties = {
   border: 0,
   clip: 'rect(0 0 0 0)',
   clipPath: 'inset(50%)',
-  height: 1,
-  margin: -1,
+  height: '1px',
+  margin: '-1px',
   overflow: 'hidden',
   padding: 0,
   position: 'absolute',
-  width: 1,
+  width: '1px',
   whiteSpace: 'nowrap'
 };
 
