00c86a2163f272bcdb3b17f46ce9c3e3f9356834
Reid Barber <reid@reidbarber.com>
2023-03-10 10:08:45 -0600

    fix table tests

--- packages/@react-spectrum/table/test/Table.test.js
+++ packages/@react-spectrum/table/test/Table.test.js
@@ -1751,13 +1751,12 @@ describe('TableView', function () {
         // Ensure we have the correct sticky cells in the right order.
         let row = cell.closest('[role=row]');
         let cells = within(row).getAllByRole(role => role === 'gridcell' || role === 'rowheader');
-        // TODO: Fix handling of sticky cells
-        expect(cells).toHaveLength(17);
-        // expect(cells[0]).toHaveAttribute('aria-colindex', '1'); // checkbox
-        expect(cells[0]).toHaveAttribute('aria-colindex', '2'); // rowheader
-        expect(cells[1]).toHaveAttribute('aria-colindex', '6'); // persisted
-        expect(cells[1]).toBe(cell);
-        expect(cells[2]).toHaveAttribute('aria-colindex', '14'); // first visible
+        expect(cells).toHaveLength(18);
+        expect(cells[0]).toHaveAttribute('aria-colindex', '1'); // checkbox
+        expect(cells[1]).toHaveAttribute('aria-colindex', '2'); // rowheader
+        expect(cells[2]).toHaveAttribute('aria-colindex', '6'); // persisted
+        expect(cells[2]).toBe(cell);
+        expect(cells[3]).toHaveAttribute('aria-colindex', '14'); // first visible
 
         // Moving focus should scroll the new focused item into view
         moveFocus('ArrowLeft');
--- packages/@react-spectrum/table/test/TableSizing.test.tsx
+++ packages/@react-spectrum/table/test/TableSizing.test.tsx
@@ -347,8 +347,7 @@ describe('TableViewSizing', function () {
         for (let row of bodyRows) {
           for (let [index, cell] of row.childNodes.entries()) {
             if (index === 0) {
-              // TODO: This should be 2. Why did this change?
-              expect((cell as HTMLElement).style.zIndex).toBe('1');
+              expect((cell as HTMLElement).style.zIndex).toBe('2');
             } else {
               expect((cell as HTMLElement).style.zIndex).toBe('1');
             }
