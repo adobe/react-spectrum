7b2749e34f2fc8e32cbc33411f47c55fa6a6a0cb
Devon Govett <devongovett@gmail.com>
2023-03-15 12:23:02 -0700

    Fixes

--- packages/react-aria-components/src/Dialog.tsx
+++ packages/react-aria-components/src/Dialog.tsx
@@ -9,15 +9,15 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import {AriaDialogProps, useDialog, useOverlayTrigger} from 'react-aria';
-import {ButtonContext} from './Button';
-import {ContextValue, DOMProps, Provider, SlotProps, useContextProps} from './utils';
-import {filterDOMProps} from '@react-aria/utils';
-import {HeadingContext} from './Heading';
-import {ModalContext} from './Modal';
-import {OverlayTriggerProps, useOverlayTriggerState} from 'react-stately';
-import {PopoverContext} from './Popover';
-import React, {createContext, ForwardedRef, forwardRef, ReactNode, useRef} from 'react';
+import { AriaDialogProps, useDialog, useOverlayTrigger } from 'react-aria';
+import { ButtonContext } from './Button';
+import { ContextValue, DOMProps, Provider, SlotProps, useContextProps } from './utils';
+import { filterDOMProps } from '@react-aria/utils';
+import { HeadingContext } from './Heading';
+import { ModalContext } from './Modal';
+import { OverlayTriggerProps, useOverlayTriggerState } from 'react-stately';
+import { PopoverContext } from './Popover';
+import React, { createContext, ForwardedRef, forwardRef, ReactNode, useRef } from 'react';
 
 export interface DialogTriggerProps extends OverlayTriggerProps {
   children: ReactNode
@@ -28,7 +28,7 @@ interface DialogRenderProps {
 }
 
 export interface DialogProps extends AriaDialogProps, DOMProps, SlotProps {
-  children: ReactNode | ((opts: DialogRenderProps) => ReactNode),
+  children?: ReactNode | ((opts: DialogRenderProps) => ReactNode),
   onClose?: () => void
 }
 
@@ -39,17 +39,17 @@ export const DialogContext = createContext<ContextValue<DialogProps, HTMLElement
  */
 export function DialogTrigger(props: DialogTriggerProps) {
   let state = useOverlayTriggerState(props);
-  
+
   let buttonRef = useRef();
-  let {triggerProps, overlayProps} = useOverlayTrigger({type: 'dialog'}, state, buttonRef);
+  let { triggerProps, overlayProps } = useOverlayTrigger({ type: 'dialog' }, state, buttonRef);
 
   return (
     <Provider
       values={[
-        [ModalContext, {state}],
-        [DialogContext, {...overlayProps, onClose: state.close}],
-        [ButtonContext, {...triggerProps, isPressed: state.isOpen, ref: buttonRef}],
-        [PopoverContext, {state, triggerRef: buttonRef}]
+        [ModalContext, { state }],
+        [DialogContext, { ...overlayProps, onClose: state.close }],
+        [ButtonContext, { ...triggerProps, isPressed: state.isOpen, ref: buttonRef }],
+        [PopoverContext, { state, triggerRef: buttonRef }]
       ]}>
       {props.children}
     </Provider>
@@ -59,15 +59,15 @@ export function DialogTrigger(props: DialogTriggerProps) {
 
 function Dialog(props: DialogProps, ref: ForwardedRef<HTMLElement>) {
   [props, ref] = useContextProps(props, ref, DialogContext);
-  let {dialogProps, titleProps} = useDialog(props, ref);
-  
+  let { dialogProps, titleProps } = useDialog(props, ref);
+
   let children = props.children;
   if (typeof children === 'function') {
     children = children({
       close: props.onClose
     });
   }
-  
+
   return (
     <section
       {...filterDOMProps(props)}
@@ -80,7 +80,7 @@ function Dialog(props: DialogProps, ref: ForwardedRef<HTMLElement>) {
         values={[
           [ButtonContext, undefined],
           // TODO: clear context within dialog content?
-          [HeadingContext, {...titleProps, level: 2}]
+          [HeadingContext, { ...titleProps, level: 2 }]
         ]}>
         {children}
       </Provider>
@@ -92,4 +92,4 @@ function Dialog(props: DialogProps, ref: ForwardedRef<HTMLElement>) {
  * A dialog is an overlay shown above other content in an application.
  */
 const _Dialog = forwardRef(Dialog);
-export {_Dialog as Dialog};
+export { _Dialog as Dialog };
