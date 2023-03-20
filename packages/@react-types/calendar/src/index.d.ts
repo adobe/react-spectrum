12b4f8ac6471adda11614265661d775a8113f83b
Prachi Arora <pracarora@paypal.com>
2023-03-20 22:11:07 +0530

    feat(RangeCalendar): Added anchorDate functionality - fixed lint issues

--- packages/@react-types/calendar/src/index.d.ts
+++ packages/@react-types/calendar/src/index.d.ts
@@ -10,106 +10,79 @@
  * governing permissions and limitations under the License.
  */
 
-import {
-  AriaLabelingProps,
-  DOMProps,
-  RangeValue,
-  StyleProps,
-  ValidationState,
-  ValueBase,
-} from "@react-types/shared";
-import {
-  CalendarDate,
-  CalendarDateTime,
-  ZonedDateTime,
-} from "@internationalized/date";
-import { ReactNode } from "react";
+import {AriaLabelingProps, DOMProps, RangeValue, StyleProps, ValidationState, ValueBase} from '@react-types/shared';
+import {CalendarDate, CalendarDateTime, ZonedDateTime} from '@internationalized/date';
+import {ReactNode} from 'react';
 
 export type DateValue = CalendarDate | CalendarDateTime | ZonedDateTime;
-type MappedDateValue<T> = T extends ZonedDateTime
-  ? ZonedDateTime
-  : T extends CalendarDateTime
-  ? CalendarDateTime
-  : T extends CalendarDate
-  ? CalendarDate
-  : never;
+type MappedDateValue<T> =
+  T extends ZonedDateTime ? ZonedDateTime :
+  T extends CalendarDateTime ? CalendarDateTime :
+  T extends CalendarDate ? CalendarDate :
+  never;
 
 export interface CalendarPropsBase {
   /** The minimum allowed date that a user may select. */
-  minValue?: DateValue;
+  minValue?: DateValue,
   /** The maximum allowed date that a user may select. */
-  maxValue?: DateValue;
+  maxValue?: DateValue,
   /** Callback that is called for each date of the calendar. If it returns true, then the date is unavailable. */
-  isDateUnavailable?: (date: DateValue) => boolean;
+  isDateUnavailable?: (date: DateValue) => boolean,
   /** Fixed date of the range. */
-  anchorDate?: CalendarDate;
+  anchorDate?: CalendarDate,
   /**
    * Whether the calendar is disabled.
    * @default false
    */
-  isDisabled?: boolean;
+  isDisabled?: boolean,
   /**
    * Whether the calendar value is immutable.
    * @default false
    */
-  isReadOnly?: boolean;
+  isReadOnly?: boolean,
   /**
    * Whether to automatically focus the calendar when it mounts.
    * @default false
    */
-  autoFocus?: boolean;
+  autoFocus?: boolean,
   /** Controls the currently focused date within the calendar. */
-  focusedValue?: DateValue;
+  focusedValue?: DateValue,
   /** The date that is focused when the calendar first mounts (uncountrolled). */
-  defaultFocusedValue?: DateValue;
+  defaultFocusedValue?: DateValue,
   /** Handler that is called when the focused date changes. */
-  onFocusChange?: (date: CalendarDate) => void;
+  onFocusChange?: (date: CalendarDate) => void,
   /** Whether the current selection is valid or invalid according to application logic. */
-  validationState?: ValidationState;
+  validationState?: ValidationState,
   /** An error message to display when the selected value is invalid. */
-  errorMessage?: ReactNode;
+  errorMessage?: ReactNode
 }
 
 export type DateRange = RangeValue<DateValue>;
-export interface CalendarProps<T extends DateValue>
-  extends CalendarPropsBase,
-    ValueBase<T, MappedDateValue<T>> {}
-export interface RangeCalendarProps<T extends DateValue>
-  extends CalendarPropsBase,
-    ValueBase<RangeValue<T>, RangeValue<MappedDateValue<T>>> {
+export interface CalendarProps<T extends DateValue> extends CalendarPropsBase, ValueBase<T, MappedDateValue<T>> {}
+export interface RangeCalendarProps<T extends DateValue> extends CalendarPropsBase, ValueBase<RangeValue<T>, RangeValue<MappedDateValue<T>>> {
   /**
    * When combined with `isDateUnavailable`, determines whether non-contiguous ranges,
    * i.e. ranges containing unavailable dates, may be selected.
    */
-  allowsNonContiguousRanges?: boolean;
+  allowsNonContiguousRanges?: boolean
 }
 
-export interface AriaCalendarProps<T extends DateValue>
-  extends CalendarProps<T>,
-    DOMProps,
-    AriaLabelingProps {}
+export interface AriaCalendarProps<T extends DateValue> extends CalendarProps<T>, DOMProps, AriaLabelingProps {}
 
-export interface AriaRangeCalendarProps<T extends DateValue>
-  extends RangeCalendarProps<T>,
-    DOMProps,
-    AriaLabelingProps {}
+export interface AriaRangeCalendarProps<T extends DateValue> extends RangeCalendarProps<T>, DOMProps, AriaLabelingProps {}
 
-export interface SpectrumCalendarProps<T extends DateValue>
-  extends AriaCalendarProps<T>,
-    StyleProps {
+export interface SpectrumCalendarProps<T extends DateValue> extends AriaCalendarProps<T>, StyleProps {
   /**
    * The number of months to display at once. Up to 3 months are supported.
    * @default 1
    */
-  visibleMonths?: number;
+  visibleMonths?: number
 }
 
-export interface SpectrumRangeCalendarProps<T extends DateValue>
-  extends AriaRangeCalendarProps<T>,
-    StyleProps {
+export interface SpectrumRangeCalendarProps<T extends DateValue> extends AriaRangeCalendarProps<T>, StyleProps {
   /**
    * The number of months to display at once. Up to 3 months are supported.
    * @default 1
    */
-  visibleMonths?: number;
+  visibleMonths?: number
 }
