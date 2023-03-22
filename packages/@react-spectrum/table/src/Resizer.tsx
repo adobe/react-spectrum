6adb313035c5062b0081b70db4700c6779aae8b6
Rob Snow <rsnow@adobe.com>
2023-03-22 15:40:26 -0700

    fix lint

--- packages/@react-spectrum/table/src/Resizer.tsx
+++ packages/@react-spectrum/table/src/Resizer.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable jsx-a11y/role-supports-aria-props */
-import { classNames } from '@react-spectrum/utils';
-import { ColumnSize } from '@react-types/table';
-import { FocusRing } from '@react-aria/focus';
-import { getInteractionModality } from '@react-aria/interactions';
-import { GridNode } from '@react-types/grid';
+import {classNames} from '@react-spectrum/utils';
+import {ColumnSize} from '@react-types/table';
+import {FocusRing} from '@react-aria/focus';
+import {getInteractionModality} from '@react-aria/interactions';
+import {GridNode} from '@react-types/grid';
 // @ts-ignore
 import intlMessages from '../intl/*.json';
-import { mergeProps } from '@react-aria/utils';
-import React, { Key, RefObject, useEffect, useState } from 'react';
+import {mergeProps} from '@react-aria/utils';
+import React, {Key, RefObject, useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import styles from '@adobe/spectrum-css-temp/components/table/vars.css';
 import rspStyles from './table.css';
-import { useLocale, useLocalizedStringFormatter } from '@react-aria/i18n';
-import { useTableColumnResize } from '@react-aria/table';
-import { useTableContext, useVirtualizerContext } from './TableView';
-import { VisuallyHidden } from '@react-aria/visually-hidden';
+import styles from '@adobe/spectrum-css-temp/components/table/vars.css';
+import {useLocale, useLocalizedStringFormatter} from '@react-aria/i18n';
+import {useTableColumnResize} from '@react-aria/table';
+import {useTableContext, useVirtualizerContext} from './TableView';
+import {VisuallyHidden} from '@react-aria/visually-hidden';
 
 interface ResizerProps<T> {
   column: GridNode<T>,
@@ -32,14 +32,14 @@ let CURSOR_CLASSES = {
 };
 
 function Resizer<T>(props: ResizerProps<T>, ref: RefObject<HTMLInputElement>) {
-  let { column, showResizer } = props;
-  let { isEmpty, layout } = useTableContext();
+  let {column, showResizer} = props;
+  let {isEmpty, layout} = useTableContext();
   // Virtualizer re-renders, but these components are all cached
   // in order to get around that and cause a rerender here, we use context
   // but we don't actually need any value, they are available on the layout object
   useVirtualizerContext();
   let stringFormatter = useLocalizedStringFormatter(intlMessages);
-  let { direction } = useLocale();
+  let {direction} = useLocale();
   let [directionClass, setDirectionClass] = React.useState(null);
 
   let [isPointerDown, setIsPointerDown] = useState(false);
@@ -54,15 +54,15 @@ function Resizer<T>(props: ResizerProps<T>, ref: RefObject<HTMLInputElement>) {
         setIsPointerDown(false);
       }
     };
-    document.addEventListener('pointerdown', setDown, { capture: true });
-    document.addEventListener('pointerup', setUp, { capture: true });
+    document.addEventListener('pointerdown', setDown, {capture: true});
+    document.addEventListener('pointerup', setUp, {capture: true});
     return () => {
-      document.removeEventListener('pointerdown', setDown, { capture: true });
-      document.removeEventListener('pointerup', setUp, { capture: true });
+      document.removeEventListener('pointerdown', setDown, {capture: true});
+      document.removeEventListener('pointerup', setUp, {capture: true});
     };
   }, []);
 
-  let { inputProps, resizerProps } = useTableColumnResize<unknown>(
+  let {inputProps, resizerProps} = useTableColumnResize<unknown>(
     mergeProps(props, {
       label: stringFormatter.format('columnResizer'),
       isDisabled: isEmpty,
@@ -137,16 +137,16 @@ function Resizer<T>(props: ResizerProps<T>, ref: RefObject<HTMLInputElement>) {
         role="presentation"
         className={classNames(styles, 'spectrum-Table-columnResizerPlaceholder')} />
       <CursorOverlay show={isResizing && isPointerDown}>
-        <div className={directionClass} style={{ position: 'fixed', top: 0, left: 0, bottom: 0, right: 0 }} />
+        <div className={directionClass} style={{position: 'fixed', top: 0, left: 0, bottom: 0, right: 0}} />
       </CursorOverlay>
     </>
   );
 }
 
 function CursorOverlay(props) {
-  let { show, children } = props;
+  let {show, children} = props;
   return show ? ReactDOM.createPortal(children, document.body) : null;
 }
 
 const _Resizer = React.forwardRef(Resizer);
-export { _Resizer as Resizer };
+export {_Resizer as Resizer};
