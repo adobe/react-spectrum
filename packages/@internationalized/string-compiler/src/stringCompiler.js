c4d916154e2a58348aa2a498ad5c7c4d6baf6462
Rob Snow <rsnow@adobe.com>
2023-03-23 14:02:47 -0700

    more different

--- packages/@internationalized/string-compiler/src/stringCompiler.js
+++ packages/@internationalized/string-compiler/src/stringCompiler.js
@@ -67,7 +67,7 @@ function compileParts(parts, inline = false, pluralValue = '') {
         break;
       case TYPE.number:
         // TODO: Eventually this will need to support the formatting options that are possible here.
-        // They're in part.parsedOptions. Would require changes to the formatter to support it though.
+        //  They're in part.parsedOptions. Would require changes to the formatter to support it though.
         res += '${formatter.number(args.' + part.value + ')}';
         usesFormatter = true;
         break;
