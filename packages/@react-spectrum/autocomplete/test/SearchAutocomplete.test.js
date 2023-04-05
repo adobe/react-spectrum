8e52af01be9e4f7f0818491ecb5f7e5aa496b226
Reid Barber <reid@reidbarber.com>
2023-04-05 14:09:49 -0500

    update test

--- packages/@react-spectrum/autocomplete/test/SearchAutocomplete.test.js
+++ packages/@react-spectrum/autocomplete/test/SearchAutocomplete.test.js
@@ -824,13 +824,14 @@ describe('SearchAutocomplete', function () {
       expect(searchAutocomplete).not.toHaveAttribute('aria-activedescendant');
     });
 
-    it('input events are only called once', function () {
+    it('input events are only fired once', function () {
       let onKeyDown = jest.fn();
       let onKeyUp = jest.fn();
       let onFocus = jest.fn();
       let onInputChange = jest.fn();
+      let onFocusChange = jest.fn();
       let onBlur = jest.fn();
-      let {getByRole} = renderSearchAutocomplete({onKeyDown, onKeyUp, onFocus, onInputChange, onBlur});
+      let {getByRole} = renderSearchAutocomplete({onKeyDown, onKeyUp, onFocus, onInputChange, onBlur, onFocusChange});
 
       let searchAutocomplete = getByRole('combobox');
       typeText(searchAutocomplete, 'w');
@@ -843,6 +844,7 @@ describe('SearchAutocomplete', function () {
       expect(onKeyUp).toHaveBeenCalledTimes(1);
       expect(onFocus).toHaveBeenCalledTimes(1);
       expect(onInputChange).toHaveBeenCalledTimes(1);
+      expect(onFocusChange).toHaveBeenCalledTimes(1);
       expect(onBlur).toHaveBeenCalledTimes(0);
 
       act(() => {
