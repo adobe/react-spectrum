18020d8b0b1df3f4a15b4fbe2468113f3d96ebed
Scott <snow@adobe.com>
2023-05-04 14:41:44 -0700

    add story to test tooltip click ignore

--- packages/@react-spectrum/tooltip/test/Tooltip.test.js
+++ packages/@react-spectrum/tooltip/test/Tooltip.test.js
@@ -11,7 +11,7 @@
  */
 
 import React from 'react';
-import {render} from '@react-spectrum/test-utils';
+import {render, fireEvent} from '@react-spectrum/test-utils';
 import {Tooltip} from '../';
 
 describe('Tooltip', function () {
