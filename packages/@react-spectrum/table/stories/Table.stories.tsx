6c3cd4b1e56a0f73f266e858fe40fb2004772f66
Rob Snow <rsnow@adobe.com>
2023-05-08 16:41:11 -0700

    support empty state

--- packages/@react-spectrum/table/stories/Table.stories.tsx
+++ packages/@react-spectrum/table/stories/Table.stories.tsx
@@ -215,6 +215,7 @@ function BaseTableHeader<T extends object>(props: TableHeaderProps<T>) {
     children: props.children,
     items: props.columns
   });
+
   let renderer = typeof props.children === 'function' ? props.children : null;
   return (
     <CollectionRendererContext.Provider value={renderer}>
@@ -256,6 +257,11 @@ function TableHeaderRow(props) {
 
   // @ts-ignore
   return <headerrow multiple={props}>{children}</headerrow>;
+}
+
+function ColumnPlaceholder(props) {
+  // @ts-ignore
+  return <placeholder />;
 }
 
 export function Column<T extends object>(props: ColumnProps<T>): JSX.Element {
@@ -405,6 +411,63 @@ export const StaticNestedColumns: TableStory = {
     </TableView>
   ),
   name: 'static with nested columns'
+};
+
+let manyColumns = [];
+for (let i = 0; i < 100; i++) {
+  manyColumns.push({name: 'Column ' + i, key: 'C' + i});
+}
+manyColumns[0].isRowHeader = true;
+
+let manyRows = [];
+for (let i = 0; i < 1000; i++) {
+  let row = {id: 'R' + i};
+  for (let j = 0; j < 100; j++) {
+    row['C' + j] = `${i}, ${j}`;
+  }
+
+  manyRows.push(row);
+}
+
+function renderEmptyState() {
+  return (
+    <IllustratedMessage>
+      <svg width="150" height="103" viewBox="0 0 150 103">
+        <path d="M133.7,8.5h-118c-1.9,0-3.5,1.6-3.5,3.5v27c0,0.8,0.7,1.5,1.5,1.5s1.5-0.7,1.5-1.5V23.5h119V92c0,0.3-0.2,0.5-0.5,0.5h-118c-0.3,0-0.5-0.2-0.5-0.5V69c0-0.8-0.7-1.5-1.5-1.5s-1.5,0.7-1.5,1.5v23c0,1.9,1.6,3.5,3.5,3.5h118c1.9,0,3.5-1.6,3.5-3.5V12C137.2,10.1,135.6,8.5,133.7,8.5z M15.2,21.5V12c0-0.3,0.2-0.5,0.5-0.5h118c0.3,0,0.5,0.2,0.5,0.5v9.5H15.2z M32.6,16.5c0,0.6-0.4,1-1,1h-10c-0.6,0-1-0.4-1-1s0.4-1,1-1h10C32.2,15.5,32.6,15.9,32.6,16.5z M13.6,56.1l-8.6,8.5C4.8,65,4.4,65.1,4,65.1c-0.4,0-0.8-0.1-1.1-0.4c-0.6-0.6-0.6-1.5,0-2.1l8.6-8.5l-8.6-8.5c-0.6-0.6-0.6-1.5,0-2.1c0.6-0.6,1.5-0.6,2.1,0l8.6,8.5l8.6-8.5c0.6-0.6,1.5-0.6,2.1,0c0.6,0.6,0.6,1.5,0,2.1L15.8,54l8.6,8.5c0.6,0.6,0.6,1.5,0,2.1c-0.3,0.3-0.7,0.4-1.1,0.4c-0.4,0-0.8-0.1-1.1-0.4L13.6,56.1z" />
+      </svg>
+      <Heading>No results</Heading>
+      <Content>No results found, press <Link onPress={action('linkPress')}>here</Link> for more info.</Content>
+    </IllustratedMessage>
+  );
+}
+
+function EmptyStateTable(props) {
+  let [show, setShow] = useState(false);
+  let [sortDescriptor, setSortDescriptor] = useState({});
+  return (
+    <Flex direction="column">
+      <ActionButton width="100px" onPress={() => setShow(show => !show)}>Toggle items</ActionButton>
+      <TableView aria-label="TableView with empty state" width={700} height={400} {...props} renderEmptyState={renderEmptyState} selectionMode="multiple" sortDescriptor={sortDescriptor} onSortChange={setSortDescriptor}>
+        <TableHeader columns={manyColumns}>
+          {column =>
+            <Column allowsResizing allowsSorting minWidth={100} isRowHeader={column.isRowHeader}>{column.name}</Column>
+          }
+        </TableHeader>
+        <TableBody items={show ? manyRows : []}>
+          {item =>
+            (<Row key={item.foo} columns={manyColumns}>
+              {column => <Cell>{item[column.key]}</Cell>}
+            </Row>)
+          }
+        </TableBody>
+      </TableView>
+    </Flex>
+  );
+}
+
+export const EmptyStateStory: TableStory = {
+  render: (args) => <EmptyStateTable {...args} />,
+  name: 'renderEmptyState'
 };
 
 export const ResizingUncontrolledStaticWidths: TableStory = {
