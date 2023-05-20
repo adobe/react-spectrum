cc8ac7bce4731edc638e7954af3955e57b489a02
Devon Govett <devongovett@gmail.com>
2023-05-20 13:33:37 -0700

    Fix useUpdateEffect

--- packages/@react-aria/utils/src/useUpdateEffect.ts
+++ packages/@react-aria/utils/src/useUpdateEffect.ts
@@ -15,13 +15,15 @@ import {EffectCallback, useEffect, useRef} from 'react';
 // Like useEffect, but only called for updates after the initial render.
 export function useUpdateEffect(effect: EffectCallback, dependencies: any[]) {
   const isInitialMount = useRef(true);
+  const lastDeps = useRef<any[] | null>(null);
 
   useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
-    } else {
+    } else if (!lastDeps.current || dependencies.some((dep, i) => dep !== lastDeps[i])) {
       effect();
     }
+    lastDeps.current = dependencies;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, dependencies);
 }
