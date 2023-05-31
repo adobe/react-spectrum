0e372ef229231bbe5c9d35e25c4c284ff064d6bf
Devon Govett <devongovett@gmail.com>
2023-05-31 13:47:34 -0700

    Focus collection when focusedKey becomes null

--- packages/@react-aria/selection/src/useSelectableCollection.ts
+++ packages/@react-aria/selection/src/useSelectableCollection.ts
@@ -364,6 +364,7 @@ export function useSelectableCollection(options: AriaSelectableCollectionOptions
 
   // If not virtualized, scroll the focused element into view when the focusedKey changes.
   // When virtualized, Virtualizer handles this internally.
+  let lastFocusedKey = useRef(manager.focusedKey);
   useEffect(() => {
     let modality = getInteractionModality();
     if (manager.isFocused && manager.focusedKey != null && scrollRef?.current) {
@@ -377,6 +378,13 @@ export function useSelectableCollection(options: AriaSelectableCollectionOptions
         }
       }
     }
+
+    // If the focused key becomes null (e.g. the last item is deleted), focus the whole collection.
+    if (manager.isFocused && manager.focusedKey == null && lastFocusedKey.current != null) {
+      focusSafely(ref.current);
+    }
+
+    lastFocusedKey.current = manager.focusedKey;
   }, [isVirtualized, scrollRef, manager.focusedKey, manager.isFocused, ref]);
 
   let handlers = {
