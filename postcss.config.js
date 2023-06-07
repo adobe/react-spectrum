c86982fe8eba857789c6fe9d83b61e5562111933
Devon Govett <devongovett@gmail.com>
2023-06-07 12:28:03 -0700

    Upgrade to postcss 8

--- postcss.config.js
+++ postcss.config.js
@@ -19,7 +19,7 @@ module.exports = {
 
       // Use the hover media query in the docs because of SSR - some components have no client JS available.
       // Otherwise, convert :hover to .is-hovered.
-      process.env.DOCS_ENV ? require("./lib/postcss-hover-media") : require("./lib/postcss-hover-class"),
+      // process.env.DOCS_ENV ? require("./lib/postcss-hover-media") : require("./lib/postcss-hover-class"),
     ]
   ),
 };
