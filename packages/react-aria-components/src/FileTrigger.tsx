fc00f831900a165a550ccf9de45366a4aea717e1
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2023-06-14 14:28:12 -0700

    rac dropzone (#4451)
    * initialize dropzone
    
    * added some comments for clarification
    
    * edit spacing
    
    * added additonal props and passed ref
    
    * added dropzone to storybook
    
    * update index.ts
    
    * fixed linting
    
    * fix linting, added actions to dropzone story
    
    * fixed focus
    
    * allow copy/paste, update render props, styling
    
    * can copy, added stories, dropzone with input
    
    * fix linting
    
    * update focus, added css
    
    * update stories, add data attribute
    
    * add isVirtualDragging to DropZone
    
    * single hidden button, added virtual drag
    
    * add tests, update dropzone
    
    * clean up, add story
    
    * fix tests
    
    * fix
    
    * minor fix
    
    * minor minor fix
    
    * add files to react-aria/utils, update css, mobile dropzone
    
    * remove console.log
    
    * update utils, add press events to dropzone
    
    * update aria-label, add focusProps to wrapper div, usePress updates
    
    * create FileTrigger
    
    * cleanup/add stories
    
    * add prop to filetrigger, update comments, change stories
    
    * change comments
    
    * update useDrop hook to accomodate dropButton
    
    * drop target fixes
    
    * changes to dnd with button ref, press responder, remove onInputChange
    
    * fix prop names, update dnd
    
    * add aria label to DropZone, update onChange
    
    * fix onChange in FileTrigger

---
+++ packages/react-aria-components/src/FileTrigger.tsx
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import {AriaLabelingProps} from '@react-types/shared';
+import {ContextValue, DOMProps, SlotProps, useContextProps} from './utils';
+import {filterDOMProps} from '@react-aria/utils';
+import {Input} from './Input';
+import {PressResponder} from '@react-aria/interactions';
+import React, {createContext, ForwardedRef, forwardRef, useRef} from 'react';
+import {VisuallyHidden} from 'react-aria';
+
+export interface FileTriggerProps extends SlotProps, DOMProps, AriaLabelingProps {
+  /**
+   * Specifies what mime type of files are allowed.
+   */
+  acceptedFileTypes?: Array<string>,
+  /**
+   * Whether multiple files can be selected.
+   */
+  allowsMultiple?: boolean,
+  /**
+   * Specifies the use of a media capture mechanism to capture the media on the spot.
+   */
+  defaultCamera?: 'user' | 'environment',
+  /**
+   * Handler when a user selects a file.
+   */
+  onChange?: (files: FileList | null) => void
+}
+
+export const FileTriggerContext = createContext<ContextValue<FileTriggerProps, HTMLDivElement>>(null);
+
+function FileTrigger(props: FileTriggerProps, ref: ForwardedRef<HTMLDivElement>) {
+  [props, ref] = useContextProps(props, ref, FileTriggerContext);
+  let {onChange, acceptedFileTypes, className, allowsMultiple, defaultCamera, children, ...otherProps} = props;
+  let inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div
+      className={className || 'react-aria-FileTrigger'}
+      {...filterDOMProps(otherProps)}
+      ref={ref}
+      slot={props.slot}>
+      <PressResponder onPress={() => inputRef.current?.click()}>
+        {children}
+      </PressResponder>
+      <VisuallyHidden>
+        <Input 
+          type="file" 
+          ref={inputRef} 
+          accept={acceptedFileTypes?.toString()} 
+          onChange={(e) => onChange?.(e.target.files)} 
+          capture={defaultCamera} 
+          multiple={allowsMultiple} /> 
+      </VisuallyHidden>
+    </div>
+  );
+}
+
+/**
+ * A FileTrigger allows a user to access the file system with either a Button or Link.
+ */
+const _FileTrigger = forwardRef(FileTrigger);
+export {_FileTrigger  as FileTrigger};
