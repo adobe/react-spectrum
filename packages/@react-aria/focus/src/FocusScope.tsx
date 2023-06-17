f72263cbf80a98fb2fb570cc7ab9a9bf16f0fda9
Kyle Taborski <ktabors@yahoo.com>
2023-06-17 12:06:16 -0700

    comment improvements
    Co-authored-by: Robert Snow <rsnow@adobe.com>

--- packages/@react-aria/focus/src/FocusScope.tsx
+++ packages/@react-aria/focus/src/FocusScope.tsx
@@ -382,7 +382,7 @@ function useFocusContainment(scopeRef: RefObject<Element[]>, contain: boolean) {
     };
   }, [scopeRef, contain]);
 
-  // Changed useEffect to useLayoutEffect to prevent an exception with onBlur and focusFirstInScope where scope changes on an unmount
+  // Changed useEffect to useLayoutEffect so this is guaranteed to run before our async synthetic blur
   // eslint-disable-next-line arrow-body-style
   useLayoutEffect(() => {
     return () => {
