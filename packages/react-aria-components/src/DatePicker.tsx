10eb786cdbf2ed07f69082600caed80a2c32e469
Reid Barber <reid@reidbarber.com>
2023-06-19 11:33:31 -0500

    add data attributes to datepicker

--- packages/react-aria-components/src/DatePicker.tsx
+++ packages/react-aria-components/src/DatePicker.tsx
@@ -233,7 +233,16 @@ function DateRangePicker<T extends DateValue>(props: DateRangePickerProps<T>, re
           }
         }]
       ]}>
-      <div {...mergeProps(focusProps, hoverProps)} {...DOMProps} {...renderProps} ref={ref} slot={props.slot} />
+      <div
+        {...mergeProps(focusProps, hoverProps)}
+        {...DOMProps}
+        {...renderProps}
+        ref={ref}
+        slot={props.slot}
+        data-validation-state={state.validationState || undefined}
+        data-hovered={isHovered || undefined}
+        data-focus-visible={isFocusVisible || undefined}
+        data-disabled={props.isDisabled || undefined} />
     </Provider>
   );
 }
