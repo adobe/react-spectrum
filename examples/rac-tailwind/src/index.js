e1fcb7756c7e4a5ac9ecf0e35ed56b6858061c3e
Daniel Lu <dl1644@gmail.com>
2023-06-22 12:57:49 -0700

    fixing more typos

--- examples/rac-tailwind/src/index.js
+++ examples/rac-tailwind/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './App';
 
 if (ReactDOM.version.startsWith('18')) {
   let ReactDOMClient = require('react-dom/client');
@@ -7,7 +7,7 @@ if (ReactDOM.version.startsWith('18')) {
     document.getElementById('root')
   );
   root.render(
-      <App />
+    <App />
   );
 } else {
   ReactDOM.render(
