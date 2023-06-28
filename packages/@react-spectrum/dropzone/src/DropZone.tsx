e9146c6194f50cc4983fa08018284de7d3aa77db
Yihui Liao <liao.yihui18@gmail.com>
2023-06-27 18:25:45 -0700

    add css, trying stuff in dropzone

--- packages/@react-spectrum/dropzone/src/DropZone.tsx
+++ packages/@react-spectrum/dropzone/src/DropZone.tsx
@@ -12,10 +12,10 @@
 
 import {AriaLabelingProps, DOMProps, FocusableRefValue, StyleProps} from '@react-types/shared';
 import {classNames, createFocusableRef, SlotProvider, useStyleProps} from '@react-spectrum/utils';
-import {DropOptions, mergeProps} from 'react-aria';
+import {DropOptions, mergeProps, useDrop} from 'react-aria';
 import {filterDOMProps} from '@react-aria/utils';
 import {DropZone as RACDropZone} from 'react-aria-components';
-import React, {ReactNode, Ref, useImperativeHandle, useRef, useState} from 'react';
+import React, {ReactNode, Ref, useEffect, useImperativeHandle, useRef, useState} from 'react';
 import styles from '@adobe/spectrum-css-temp/components/dropzone/vars.css';
 export interface SpectrumDropZoneProps extends Omit<DropOptions, 'getDropOperationForPoint' | 'ref' | 'hasDropButton'>, DOMProps, StyleProps, AriaLabelingProps {
   children: ReactNode
@@ -32,8 +32,7 @@ function DropZone(props: SpectrumDropZoneProps, ref: Ref<DropZoneRef>) {
   let domRef = useRef<HTMLDivElement>(null);
   let inputRef = useRef<HTMLInputElement>(null);
 
-  // will need to think about this a bit more but i think this is a state we will need to track for styling purposes
-  let [isFilled, setIsFilled] = useState(false); // what can we use to determine if dropzone is filled? onDrop? onChange? how will this work... look into useEffect for this?
+  let [isFilled, setIsFilled] = useState(false);
 
   let domProps = filterDOMProps(props, {labelable: true});
   delete domProps.id;
@@ -45,21 +44,11 @@ function DropZone(props: SpectrumDropZoneProps, ref: Ref<DropZoneRef>) {
       return inputRef.current;
     }
   }));
-
-  console.log('isFilled', isFilled);
-
-  // we're gonna need that banner thing to appear when it is a droptarget...but how will we get isDropTarget?
-  // that will appear when isDropTarget is true? what animations do we want to do? what should that banner even be?
-  // does that banner only appear when the drop target isFilled?
-  // what should the screen reader experience be? (should read off)
-
-  // isDisabled? validation states? errorMessage? (let's ask design)
   
   return (
     <RACDropZone
       {...mergeProps(otherProps, domProps)}
-      // {...styleProps} // not really sure what to do about this (an error keeps popping up)
-      onDrop={() => setIsFilled(true)} // is this the right way to do this? (will need to look into this) in what conditions would it be set to false?
+      {...styleProps} // not really sure what to do about this
       className={
       classNames(
         styles,
@@ -67,12 +56,21 @@ function DropZone(props: SpectrumDropZoneProps, ref: Ref<DropZoneRef>) {
         styleProps.className
       )} 
       ref={domRef}>
-      {isFilled && 
-        <div>
+      {isFilled && // isDropTarget (but this idk how to get this value...)
+        <div
+          className={
+            classNames(
+              styles,
+              'spectrum-Dropzone-banner'
+            )
+          }>
           Drop file to replace
         </div>}
       <SlotProvider
-        slots={{illustration: {UNSAFE_className: classNames(styles, 'spectrum-IllustratedMessage')}}}> 
+        slots={{
+          illustration: {UNSAFE_className: classNames(styles, 'spectrum-IllustratedMessage')}, 
+          // content: {UNSAFE_className: classNames(styles, 'spectrum-IllustratedMessage-description'), onChange: () => setIsFilled(true)}
+        }}> 
         {children}
       </SlotProvider>
     </RACDropZone>
