54ac61b05e4d441964bc08fbc196143c98c17670
Daniel Lu <dl1644@gmail.com>
2023-07-13 12:23:34 -0700

    another alterative

--- packages/@react-stately/layout/src/TableLayout.ts
+++ packages/@react-stately/layout/src/TableLayout.ts
@@ -576,9 +576,17 @@ export class TableLayout<T> extends ListLayout<T> {
     let res = super.getInitialLayoutInfo(layoutInfo);
 
     // If this insert was the result of async loading, remove the zoom effect and just keep the fade in.
-    if (this.wasLoading) {
-      res.transform = null;
-    }
+    // if (this.wasLoading) {
+    res.transform = null;
+    // }
+
+    return res;
+  }
+
+  getFinalLayoutInfo(layoutInfo: LayoutInfo) {
+    let res = super.getFinalLayoutInfo(layoutInfo);
+
+    res.transform = null;
 
     return res;
   }
