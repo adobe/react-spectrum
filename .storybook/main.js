eec35fc20b5940659d9497386f4dd8b8145b3f3c
Daniel Lu <dl1644@gmail.com>
2023-07-24 16:23:03 -0700

    fix/ignore various story only aXe issues (FocusScope, List, Checkbox, Meter, SideNav

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
     builder: "storybook-builder-parcel",
   },
   // TODO: revert this back to  stories: ['../packages/**/stories/*.stories.{js,jsx,ts,tsx}'], when done testing individual stories
-  stories: ['../packages/@react-spectrum/toast/stories/*.stories.{js,jsx,ts,tsx}'],
+  stories: ['../packages/@react-spectrum/meter/stories/*.stories.{js,jsx,ts,tsx}'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-a11y',
