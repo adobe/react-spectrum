77fbf106d412426d829fffce39c7abe45e0ccc64
Devon Govett <devongovett@gmail.com>
2023-08-02 17:35:27 -0700

    Table column resizing support for RAC (#4785)

--- packages/@react-spectrum/table/src/TableViewBase.tsx
+++ packages/@react-spectrum/table/src/TableViewBase.tsx
@@ -20,7 +20,6 @@ import {
   classNames,
   useDOMRef,
   useFocusableRef,
-  useIsMobileDevice,
   useStyleProps,
   useUnwrapDOMRef
 } from '@react-spectrum/utils';
@@ -823,7 +822,6 @@ function ResizableTableColumnHeader(props) {
     headerRowHovered,
     setIsInResizeMode,
     isEmpty,
-    onFocusedResizer,
     isInResizeMode,
     headerMenuOpen,
     setHeaderMenuOpen
@@ -877,39 +875,8 @@ function ResizableTableColumnHeader(props) {
     return options;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [allowsSorting]);
-  let isMobile = useIsMobileDevice();
 
   let resizingColumn = layout.resizingColumn;
-  let prevResizingColumn = useRef(null);
-  let timeout = useRef(null);
-  useEffect(() => {
-    if (prevResizingColumn.current !== resizingColumn &&
-      resizingColumn != null &&
-      resizingColumn === column.key) {
-      if (timeout.current) {
-        clearTimeout(timeout.current);
-      }
-      // focusSafely won't actually focus because the focus moves from the menuitem to the body during the after transition wait
-      // without the immediate timeout, Android Chrome doesn't move focus to the resizer
-      let focusResizer = () => {
-        resizingRef.current.focus();
-        onFocusedResizer();
-        timeout.current = null;
-      };
-      if (isMobile) {
-        timeout.current = setTimeout(focusResizer, 400);
-        return;
-      }
-      timeout.current = setTimeout(focusResizer, 0);
-    }
-    prevResizingColumn.current = resizingColumn;
-  }, [resizingColumn, column.key, isMobile, onFocusedResizer, resizingRef, prevResizingColumn, timeout]);
-
-  // eslint-disable-next-line arrow-body-style
-  useEffect(() => {
-    return () => clearTimeout(timeout.current);
-  }, []);
-
   let showResizer = !isEmpty && ((headerRowHovered && getInteractionModality() !== 'keyboard') || resizingColumn != null);
   let alignment = 'start';
   let menuAlign = 'start' as 'start' | 'end';
--- packages/react-aria-components/src/Table.tsx
+++ packages/react-aria-components/src/Table.tsx
@@ -1,16 +1,17 @@
 import {AriaLabelingProps} from '@react-types/shared';
 import {BaseCollection, CollectionContext, CollectionProps, CollectionRendererContext, ItemRenderProps, NodeValue, useCachedChildren, useCollection, useCollectionChildren, useCollectionItemRef} from './Collection';
-import {buildHeaderRows} from '@react-stately/table';
+import {buildHeaderRows, TableColumnResizeState} from '@react-stately/table';
 import {ButtonContext} from './Button';
 import {CheckboxContext} from './Checkbox';
-import {ContextValue, defaultSlot, forwardRefType, Provider, RenderProps, SlotProps, StyleProps, StyleRenderProps, useContextProps, useRenderProps} from './utils';
-import {DisabledBehavior, DraggableCollectionState, DroppableCollectionState, Node, SelectionBehavior, SelectionMode, SortDirection, TableState, useTableState} from 'react-stately';
+import {ColumnSize, ColumnStaticSize, TableCollection as ITableCollection, TableProps as SharedTableProps} from '@react-types/table';
+import {ContextValue, defaultSlot, DOMProps, forwardRefType, Provider, RenderProps, SlotProps, StyleProps, StyleRenderProps, useContextProps, useRenderProps} from './utils';
+import {DisabledBehavior, DraggableCollectionState, DroppableCollectionState, Node, SelectionBehavior, SelectionMode, SortDirection, TableState, useTableColumnResizeState, useTableState} from 'react-stately';
 import {DragAndDropHooks, DropIndicator, DropIndicatorContext, DropIndicatorProps} from './useDragAndDrop';
-import {DraggableItemResult, DragPreviewRenderer, DropIndicatorAria, DroppableCollectionResult, FocusScope, ListKeyboardDelegate, mergeProps, useFocusRing, useHover, useTable, useTableCell, useTableColumnHeader, useTableHeaderRow, useTableRow, useTableRowGroup, useTableSelectAllCheckbox, useTableSelectionCheckbox, useVisuallyHidden} from 'react-aria';
-import {filterDOMProps, useObjectRef} from '@react-aria/utils';
+import {DraggableItemResult, DragPreviewRenderer, DropIndicatorAria, DroppableCollectionResult, FocusScope, ListKeyboardDelegate, mergeProps, useFocusRing, useHover, useLocale, useTable, useTableCell, useTableColumnHeader, useTableColumnResize, useTableHeaderRow, useTableRow, useTableRowGroup, useTableSelectAllCheckbox, useTableSelectionCheckbox, useVisuallyHidden} from 'react-aria';
+import {filterDOMProps, useLayoutEffect, useObjectRef, useResizeObserver} from '@react-aria/utils';
 import {GridNode} from '@react-types/grid';
-import {TableCollection as ITableCollection, TableProps as SharedTableProps} from '@react-types/table';
-import React, {createContext, ForwardedRef, forwardRef, Key, ReactElement, ReactNode, RefObject, useCallback, useContext, useEffect, useMemo, useRef} from 'react';
+import React, {createContext, ForwardedRef, forwardRef, Key, ReactElement, ReactNode, RefObject, useCallback, useContext, useEffect, useMemo, useRef, useState} from 'react';
+import ReactDOM from 'react-dom';
 
 class TableCollection<T> extends BaseCollection<T> implements ITableCollection<T> {
   headerRows: GridNode<T>[] = [];
@@ -176,11 +177,79 @@ class TableCollection<T> extends BaseCollection<T> implements ITableCollection<T
   }
 }
 
+interface ResizableTableContainerContextValue {
+  tableWidth: number,
+  // Dependency inject useTableColumnResizeState so it doesn't affect bundle size unless you're using ResizableTableContainer.
+  useTableColumnResizeState: typeof useTableColumnResizeState,
+  onResizeStart?: (widths: Map<Key, ColumnSize>) => void,
+  onResize?: (widths: Map<Key, ColumnSize>) => void,
+  onResizeEnd?: (widths: Map<Key, ColumnSize>) => void
+}
+
+const ResizableTableContainerContext = createContext<ResizableTableContainerContextValue | null>(null);
+
+export interface ResizableTableContainerProps extends DOMProps {
+  /**
+   * Handler that is called when a user starts a column resize.
+   */
+  onResizeStart?: (widths: Map<Key, ColumnSize>) => void,
+  /**
+   * Handler that is called when a user performs a column resize.
+   * Can be used with the width property on columns to put the column widths into
+   * a controlled state.
+   */
+  onResize?: (widths: Map<Key, ColumnSize>) => void,
+  /**
+   * Handler that is called after a user performs a column resize.
+   * Can be used to store the widths of columns for another future session.
+   */
+  onResizeEnd?: (widths: Map<Key, ColumnSize>) => void
+}
+
+function ResizableTableContainer(props: ResizableTableContainerProps, ref: ForwardedRef<HTMLDivElement>) {
+  let objectRef = useObjectRef(ref);
+  let [width, setWidth] = useState(0);
+  useResizeObserver({
+    ref: objectRef,
+    onResize() {
+      setWidth(objectRef.current?.clientWidth);
+    }
+  });
+
+  useLayoutEffect(() => {
+    setWidth(objectRef.current?.clientWidth);
+  }, [objectRef]);
+
+  let ctx = useMemo(() => ({
+    tableWidth: width,
+    useTableColumnResizeState,
+    onResizeStart: props.onResizeStart,
+    onResize: props.onResize,
+    onResizeEnd: props.onResizeEnd
+  }), [width, props.onResizeStart, props.onResize, props.onResizeEnd]);
+
+  return (
+    <div
+      {...filterDOMProps(props as any)}
+      ref={objectRef}
+      className={props.className || 'react-aria-ResizableTableContainer'}
+      style={props.style}>
+      <ResizableTableContainerContext.Provider value={ctx}>
+        {props.children}
+      </ResizableTableContainerContext.Provider>
+    </div>
+  );
+}
+
+const _ResizableTableContainer = forwardRef(ResizableTableContainer);
+export {_ResizableTableContainer as ResizableTableContainer};
+
 interface InternalTableContextValue {
   state: TableState<unknown>,
   dragAndDropHooks?: DragAndDropHooks,
   dragState?: DraggableCollectionState,
-  dropState?: DroppableCollectionState
+  dropState?: DroppableCollectionState,
+  layoutState?: TableColumnResizeState<unknown>
 }
 
 export const TableContext = createContext<ContextValue<TableProps, HTMLTableElement>>(null);
@@ -318,11 +387,25 @@ function Table(props: TableProps, ref: ForwardedRef<HTMLTableElement>) {
     allowsDragging: isListDraggable
   }), [selectionBehavior, selectionMode, disallowEmptySelection, isListDraggable]);
 
+  let style = renderProps.style;
+  let tableContainerContext = useContext(ResizableTableContainerContext);
+  let layoutState: TableColumnResizeState<unknown> | undefined = undefined;
+  if (tableContainerContext) {
+    layoutState = tableContainerContext.useTableColumnResizeState({
+      tableWidth: tableContainerContext.tableWidth
+    }, state);
+    style = {
+      ...style,
+      tableLayout: 'fixed',
+      width: 'fit-content'
+    };
+  }
+
   return (
     <>
       <Provider
         values={[
-          [InternalTableContext, {state, dragAndDropHooks, dragState, dropState}],
+          [InternalTableContext, {state, dragAndDropHooks, dragState, dropState, layoutState}],
           [DropIndicatorContext, {render: TableDropIndicatorWrapper}]
         ]}>
         <FocusScope>
@@ -330,6 +413,7 @@ function Table(props: TableProps, ref: ForwardedRef<HTMLTableElement>) {
             {...filterDOMProps(props)}
             {...renderProps}
             {...mergeProps(gridProps, focusProps, droppableCollection?.collectionProps)}
+            style={style}
             ref={ref}
             slot={props.slot}
             data-drop-target={isRootDropTarget || undefined}
@@ -404,6 +488,11 @@ const _TableHeader = /*#__PURE__*/ (forwardRef as forwardRefType)(TableHeader);
 export {_TableHeader as TableHeader};
 
 export interface ColumnRenderProps {
+  /* 
+   * Whether the item is currently hovered with a mouse.
+   * @selector [data-hovered]
+   */
+  isHovered: boolean,
   /**
    * Whether the item is currently focused.
    * @selector [data-focused]
@@ -425,10 +514,18 @@ export interface ColumnRenderProps {
    */
   sortDirection?: SortDirection,
   /**
-   * Whether the item is currently hovered with a mouse.
-   * @selector [data-hovered]
+   * Whether the column is currently being resized.
+   * @selector [data-resizing]
    */
-  isHovered: boolean
+  isResizing: boolean,
+  /**
+   * Triggers sorting for this column in the given direction.
+   */
+  sort(direction: SortDirection): void,
+  /**
+   * Starts column resizing if the table is contained in a `<ResizableTableContainer>` element.
+   */
+  startResize(): void
 }
 
 export interface ColumnProps<T = object> extends RenderProps<ColumnRenderProps> {
@@ -442,7 +539,15 @@ export interface ColumnProps<T = object> extends RenderProps<ColumnRenderProps> 
   /** Whether a column is a [row header](https://www.w3.org/TR/wai-aria-1.1/#rowheader) and should be announced by assistive technology during row navigation. */
   isRowHeader?: boolean,
   /** A string representation of the column's contents, used for accessibility announcements. */
-  textValue?: string
+  textValue?: string,
+  /** The width of the column. This prop only applies when the `<Table>` is wrapped in a `<ResizableTableContainer>`. */
+  width?: ColumnSize | null,
+  /** The default width of the column. This prop only applies when the `<Table>` is wrapped in a `<ResizableTableContainer>`. */
+  defaultWidth?: ColumnSize | null,
+  /** The minimum width of the column. This prop only applies when the `<Table>` is wrapped in a `<ResizableTableContainer>`. */
+  minWidth?: ColumnStaticSize | null,
+  /** The maximum width of the column. This prop only applies when the `<Table>` is wrapped in a `<ResizableTableContainer>`. */
+  maxWidth?: ColumnStaticSize | null
 }
 
 function Column<T extends object>(props: ColumnProps<T>, ref: ForwardedRef<HTMLTableCellElement>): JSX.Element {
@@ -684,6 +789,13 @@ function TableHeaderRow<T>({item}: {item: GridNode<T>}) {
   );
 }
 
+interface ColumnResizerContextValue {
+  column: GridNode<unknown>,
+  triggerRef: RefObject<HTMLDivElement>
+}
+
+const ColumnResizerContext = createContext<ColumnResizerContextValue | null>(null);
+
 function TableColumnHeader<T>({column}: {column: GridNode<T>}) {
   let ref = useObjectRef<HTMLTableHeaderCellElement>(column.props.ref);
   let {state} = useContext(InternalTableContext)!;
@@ -695,6 +807,18 @@ function TableColumnHeader<T>({column}: {column: GridNode<T>}) {
   let {isFocused, isFocusVisible, focusProps} = useFocusRing();
   let {hoverProps, isHovered} = useHover({});
 
+  let {layoutState} = useContext(InternalTableContext)!;
+  let isResizing = false;
+  if (layoutState) {
+    isResizing = layoutState.resizingColumn === column.key;
+  } else {
+    for (let prop in ['width', 'defaultWidth', 'minWidth', 'maxWidth']) {
+      if (prop in column.props) {
+        console.warn(`The ${prop} prop on a <Column> only applies when a <Table> is wrapped in a <ResizableTableContainer>. If you aren't using column resizing, you can set the width of a column with CSS.`);
+      }
+    }
+  }
+
   let props: ColumnProps<unknown> = column.props;
   let renderProps = useRenderProps({
     ...props,
@@ -702,28 +826,171 @@ function TableColumnHeader<T>({column}: {column: GridNode<T>}) {
     children: column.rendered,
     defaultClassName: 'react-aria-Column',
     values: {
+      isHovered,
       isFocused,
       isFocusVisible,
       allowsSorting: column.props.allowsSorting,
       sortDirection: state.sortDescriptor?.column === column.key
         ? state.sortDescriptor.direction
         : undefined,
-      isHovered
+      isResizing,
+      startResize: () => {
+        if (layoutState) {
+          layoutState.startResize(column.key);
+          state.setKeyboardNavigationDisabled(true);
+        } else {
+          throw new Error('Wrap your <Table> in a <ResizableTableContainer> to enable column resizing');
+        }
+      },
+      sort: (direction) => {
+        state.sort(column.key, direction);
+      }
     }
   });
+
+  let style = renderProps.style;
+  if (layoutState) {
+    style = {...style, width: layoutState.getColumnWidth(column.key)};
+  }
 
   return (
     <th
       {...mergeProps(filterDOMProps(props as any), columnHeaderProps, focusProps, hoverProps)}
       {...renderProps}
+      style={style}
       colSpan={column.colspan}
       ref={ref}
       data-focused={isFocused || undefined}
-      data-focus-visible={isFocusVisible || undefined}>
-      {renderProps.children}
+      data-focus-visible={isFocusVisible || undefined}
+      data-resizing={isResizing || undefined}>
+      <ColumnResizerContext.Provider value={{column, triggerRef: ref}}>
+        {renderProps.children}
+      </ColumnResizerContext.Provider>
     </th>
   );
 }
+
+export interface ColumnResizerRenderProps {
+  /**
+   * Whether the resizer is currently hovered with a mouse.
+   * @selector [data-hovered]
+   */
+  isHovered: boolean,
+  /**
+   * Whether the resizer is currently focused.
+   * @selector [data-focused]
+   */
+  isFocused: boolean,
+  /**
+   * Whether the resizer is currently keyboard focused.
+   * @selector [data-focus-visible]
+   */
+  isFocusVisible: boolean,
+  /**
+   * Whether the resizer is currently being resized.
+   * @selector [data-resizing]
+   */
+  isResizing: boolean,
+  /**
+   * The direction that the column is currently resizable.
+   * @selector [data-resizable-direction="right | left | both"]
+   */
+  resizableDirection: 'right' | 'left' | 'both'
+}
+
+export interface ColumnResizerProps extends RenderProps<ColumnResizerRenderProps> {
+  /** A custom accessibility label for the resizer. */
+  'aria-label'?: string
+}
+
+function ColumnResizer(props: ColumnResizerProps, ref: ForwardedRef<HTMLDivElement>) {
+  let {layoutState} = useContext(InternalTableContext)!;
+  if (!layoutState) {
+    throw new Error('Wrap your <Table> in a <ResizableTableContainer> to enable column resizing');
+  }
+
+  let {onResizeStart, onResize, onResizeEnd} = useContext(ResizableTableContainerContext)!;
+  let {column, triggerRef} = useContext(ColumnResizerContext)!;
+  let inputRef = useRef<HTMLInputElement>(null);
+  let {resizerProps, inputProps, isResizing} = useTableColumnResize(
+    {
+      column,
+      // TODO: translate
+      'aria-label': props['aria-label'] || 'Resizer',
+      onResizeStart,
+      onResize,
+      onResizeEnd,
+      triggerRef
+    },
+    layoutState,
+    inputRef
+  );
+  let {focusProps, isFocused, isFocusVisible} = useFocusRing();
+  let {hoverProps, isHovered} = useHover({});
+
+  let isEResizable = layoutState.getColumnMinWidth(column.key) >= layoutState.getColumnWidth(column.key);
+  let isWResizable = layoutState.getColumnMaxWidth(column.key) <= layoutState.getColumnWidth(column.key);
+  let {direction} = useLocale();
+  let resizableDirection: ColumnResizerRenderProps['resizableDirection'] = 'both';
+  if (isEResizable) {
+    resizableDirection = direction === 'rtl' ? 'right' : 'left';
+  } else if (isWResizable) {
+    resizableDirection = direction === 'rtl' ? 'left' : 'right';
+  } else {
+    resizableDirection = 'both';
+  }
+
+  let objectRef = useObjectRef(ref);
+  let [cursor, setCursor] = useState('');
+  useEffect(() => {
+    let style = window.getComputedStyle(objectRef.current);
+    setCursor(style.cursor);
+  }, [objectRef, resizableDirection]);
+
+  let renderProps = useRenderProps({
+    ...props,
+    defaultClassName: 'react-aria-ColumnResizer',
+    values: {
+      isFocused,
+      isFocusVisible,
+      isResizing,
+      isHovered,
+      resizableDirection
+    }
+  });
+
+  let [isMouseDown, setMouseDown] = useState(false);
+  let onPointerDown = (e: PointerEvent) => {
+    if (e.pointerType === 'mouse') {
+      setMouseDown(true);
+    }
+  };
+
+  if (!isResizing && isMouseDown) {
+    setMouseDown(false);
+  }
+
+  return (
+    <div
+      ref={objectRef}
+      role="presentation"
+      {...renderProps}
+      {...mergeProps(resizerProps, {onPointerDown})}
+      data-hovered={isHovered || undefined}
+      data-focused={isFocused || undefined}
+      data-focus-visible={isFocusVisible || undefined}
+      data-resizing={isResizing || undefined}
+      data-resizable-direction={resizableDirection}>
+      <input
+        ref={inputRef}
+        {...mergeProps(inputProps, focusProps, hoverProps)} />
+      {isResizing && isMouseDown && ReactDOM.createPortal(<div style={{position: 'fixed', top: 0, left: 0, bottom: 0, right: 0, cursor}} />, document.body)}
+    </div>
+  );
+}
+
+const _ColumnResizer = forwardRef(ColumnResizer);
+export {_ColumnResizer as ColumnResizer};
 
 function TableRow<T>({item}: {item: GridNode<T>}) {
   let ref = useObjectRef<HTMLTableRowElement>(item.props.ref);
