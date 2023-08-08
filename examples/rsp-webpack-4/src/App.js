5b492a4cd94825fb5b72017ee28a6ad6ec836167
Daniel Lu <dl1644@gmail.com>
2023-08-08 11:35:31 -0700

    Updating test app with expandable rows table and InlineAlert (#4866)
    * adding treeble and inline alert to test apps
    
    * test build
    
    * Update examples/rsp-webpack-4/src/App.js
    
    * test app fix
    
    * revert some ci changes
    
    * take two of build fix
    
    * make table shorter
    
    * forgot next 17
    
    * revert ci changes
    
    * fixing warnings
    
    * Revert "revert ci changes"
    
    This reverts commit 64e73840d06361dafacdae0bd25d0b436fc49b2e.
    
    * Revert "Revert "revert ci changes""
    
    This reverts commit 7f5bd8de45761f22baa4c80159b7f66e93319605.

--- examples/rsp-webpack-4/src/App.js
+++ examples/rsp-webpack-4/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import {Provider, defaultTheme, Item} from '@adobe/react-spectrum'
+import {Provider, defaultTheme, Item, TagGroup, Cell, Column, Row, TableBody, TableHeader, TableView, Content, Heading} from '@adobe/react-spectrum';
 import Lighting from './Lighting';
 import {useState} from 'react'
 import BodyContent from './BodyContent';
-import {TagGroup} from '@react-spectrum/tag';
+import {enableTableNestedRows} from '@react-stately/flags';
+import {InlineAlert} from '@react-spectrum/inlinealert'
+
+let columns = [
+  {name: 'Foo', key: 'foo'},
+  {name: 'Bar', key: 'bar'},
+  {name: 'Baz', key: 'baz'}
+];
+
+let nestedItems = [
+  {foo: 'Lvl 1 Foo 1', bar: 'Lvl 1 Bar 1', baz: 'Lvl 1 Baz 1', childRows: [
+    {foo: 'Lvl 2 Foo 1', bar: 'Lvl 2 Bar 1', baz: 'Lvl 2 Baz 1', childRows: [
+      {foo: 'Lvl 3 Foo 1', bar: 'Lvl 3 Bar 1', baz: 'Lvl 3 Baz 1'}
+    ]},
+    {foo: 'Lvl 2 Foo 2', bar: 'Lvl 2 Bar 2', baz: 'Lvl 2 Baz 2'}
+  ]}
+];
 
 function App() {
   let [selected, setSelection] = useState(false);
+  enableTableNestedRows();
 
   return (
     <Provider theme={defaultTheme}
@@ -21,6 +38,24 @@ function App() {
           <Item>Shopping</Item>
         </TagGroup>
         <BodyContent />
+        <TableView aria-label="example table with nested rows" UNSTABLE_allowsExpandableRows width={500} height={200} >
+          <TableHeader columns={columns}>
+            {column => <Column>{column.name}</Column>}
+          </TableHeader>
+          <TableBody items={nestedItems}>
+            {(item) =>
+              (<Row key={item.foo} UNSTABLE_childItems={item.childRows}>
+                {(key) => {
+                  return <Cell>{item[key]}</Cell>;
+                }}
+              </Row>)
+            }
+          </TableBody>
+        </TableView>
+        <InlineAlert>
+          <Heading>Payment Information</Heading>
+          <Content>Enter your billing address, shipping address, and payment method to complete your purchase.</Content>
+        </InlineAlert>
       </div>
     </Provider>
   );
