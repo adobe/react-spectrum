ede9c892907a0c26df29ac42ebe97954c3c141ef
Reid Barber <reid@reidbarber.com>
2023-08-18 14:24:24 -0500

    cleanup

--- packages/@react-spectrum/menu/src/SafeTriangle.tsx
+++ packages/@react-spectrum/menu/src/SafeTriangle.tsx
@@ -30,6 +30,8 @@ export function SafeTriangle(props: SafeTriangleProps): ReactElement {
   } = props;
   let [mousePosition, setMousePosition] = useState<{mouseX: number, mouseY: number}>({mouseX: null, mouseY: null});
   let {mouseX, mouseY} = mousePosition;
+  let [style, setStyle] = useState<React.CSSProperties>({height: 0, width: 0});
+
   let updateMousePosition = (e: MouseEvent) => {
     setMousePosition({mouseX: e.clientX, mouseY: e.clientY});
   };
@@ -40,46 +42,37 @@ export function SafeTriangle(props: SafeTriangleProps): ReactElement {
     return () => window.removeEventListener('mousemove', updateMousePosition);
   }, []);
 
-  let x = 0;
-  let y = 0;
-  let width = 0;
-  let height = 0;
-  let direction: 'left' | 'right';
-  let subMenuNode = subMenuRef.current?.UNSAFE_getDOMNode() as HTMLElement;
-  if (subMenuNode) {
-    let rect = subMenuNode.getBoundingClientRect();
-    x = rect.left;
-    y = rect.top;
-    width = mouseX > x ? 0 : x - mouseX + 5;
-    height = rect.height;
-    direction = x > triggerRef.current?.getBoundingClientRect().left ? 'right' : 'left';
-  } else {
-    return null;
-  }
+  useEffect(() => {
+    if (subMenuRef.current && triggerRef.current) {
+      let {left: x, top: y, height} = (subMenuRef.current?.UNSAFE_getDOMNode() as HTMLElement).getBoundingClientRect();
+      let offset = triggerRef.current?.getBoundingClientRect().width;
+      let direction = x > triggerRef.current?.getBoundingClientRect().left ? 'right' : 'left';
 
-  if (direction === 'right' && mouseX > x) {
-    return null;
-  } else if (direction === 'left' && mouseX < x) {
-    return null;
-  }
+      let isVisible = (direction === 'right' && mouseX < x) || (direction === 'left' && mouseX > x);
 
-  let offset = triggerRef.current?.getBoundingClientRect().width;
-  let top = triggerRef.current?.getBoundingClientRect().top - triggerRef.current?.parentElement.getBoundingClientRect().top;
-  let left = -1 * (x - mouseX) + offset - 7;
-  let clipPath = `polygon(100% 0%, 0% ${(100 * (mouseY - y)) / height}%, 100% 100%)`;
+      setStyle(isVisible ? {
+        top: triggerRef.current?.getBoundingClientRect().top - triggerRef.current?.parentElement.getBoundingClientRect().top,
+        left: -1 * (x - mouseX) + offset - 7,
+        height,
+        width: mouseX > x ? 0 : x - mouseX + 5,
+        clipPath: `polygon(100% 0%, 0% ${(100 * (mouseY - y)) / height}%, 100% 100%)`
+      } : {
+        height: 0,
+        width: 0
+      });
+    }
+
+  }, [mouseX, mouseY, subMenuRef, triggerRef]);
 
   return (
     <div
+      aria-hidden="false"
       style={{
         zIndex: 1,
         position: 'absolute',
         // TODO: make transparent
         backgroundColor: 'red',
-        top,
-        left,
-        height,
-        width,
-        clipPath
+        ...style
       }} />
   );
 }
