38db634d5432de24657eac9beea86874a016cfea
Reid Barber <reid@reidbarber.com>
2023-09-27 15:57:27 -0500

    add missing textValue

--- packages/@react-spectrum/menu/stories/MenuTrigger.stories.tsx
+++ packages/@react-spectrum/menu/stories/MenuTrigger.stories.tsx
@@ -814,7 +814,7 @@ function MenuWithUnavailableSometimes(props) {
             </ContextualHelpTrigger>
             <Item key="3">Three</Item>
             <ContextualHelpTrigger isUnavailable>
-              <Item key="bar">
+              <Item key="bar" textValue="Four">
                 <Text>Four</Text>
                 <Text slot={'description'}>Shut the door</Text>
               </Item>
