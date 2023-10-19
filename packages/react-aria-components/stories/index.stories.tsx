d05e84a31e8550c990915f1059a0510427201fca
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-10-19 12:40:36 +0900

    fix: lint

--- packages/react-aria-components/stories/index.stories.tsx
+++ packages/react-aria-components/stories/index.stories.tsx
@@ -865,20 +865,20 @@ export const TableExample = () => {
 
 export const TableDynamicExample = () => {
   let columns = [
-    {name: "Name", key: "name", isRowHeader: true},
-    {name: "Type", key: "type"},
-    {name: "Date Modified", key: "date"},
+    {name: 'Name', key: 'name', isRowHeader: true},
+    {name: 'Type', key: 'type'},
+    {name: 'Date Modified', key: 'date'}
   ];
 
   let rows = [
-    {id: 1, name: "Games", date: "6/7/2020", type: "File folder"},
-    {id: 2, name: "Program Files", date: "4/7/2021", type: "File folder"},
-    {id: 3, name: "bootmgr", date: "11/20/2010", type: "System file"},
-    {id: 4, name: "log.txt", date: "1/18/20167", type: "Text Document"},
+    {id: 1, name: 'Games', date: '6/7/2020', type: 'File folder'},
+    {id: 2, name: 'Program Files", date: "4/7/2021', type: 'File folder'},
+    {id: 3, name: 'bootmgr', date: '11/20/2010', type: 'System file'},
+    {id: 4, name: 'log.txt', date: '1/18/20167', type: 'Text Document'}
   ];
 
   return (
-    <Table aria-label="Files">
+    <Table aria-label='Files'>
       <TableHeader columns={columns}>
         {(column) => (
           <Column isRowHeader={column.isRowHeader}>{column.name}</Column>
