4b4f636397df12a65446702a48633c6ed42f31fe
Reid Barber <reid@reidbarber.com>
2023-11-03 15:55:34 -0500

    lint

--- packages/react-aria-components/test/Group.test.js
+++ packages/react-aria-components/test/Group.test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Adobe. All rights reserved.
+ * Copyright 2023 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,9 +9,8 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
-import {Group, GroupContext} from '..';
 import {fireEvent, pointerMap, render} from '@react-spectrum/test-utils';
+import {Group, GroupContext} from '..';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 
