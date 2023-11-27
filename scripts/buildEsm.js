db4469eebcc5e27e118ab9888a5649374b70d68f
Devon Govett <devongovett@gmail.com>
2023-11-27 14:09:11 -0800

    fix esm build script

--- scripts/buildEsm.js
+++ scripts/buildEsm.js
@@ -3,9 +3,11 @@ const glob = require('glob');
 
 let pkg = process.argv[process.argv.length - 1];
 let regex = /([a-z]{2}-[A-Z]{2})\.module\.js/g;
-let js = fs.readFileSync(`${pkg}/dist/module.js`, 'utf8');
-js = js.replace(regex, '$1.mjs');
-fs.writeFileSync(`${pkg}/dist/import.mjs`, js);
+if (fs.existsSync(`${pkg}/dist/module.js`)) {
+  let js = fs.readFileSync(`${pkg}/dist/module.js`, 'utf8');
+  js = js.replace(regex, '$1.mjs');
+  fs.writeFileSync(`${pkg}/dist/import.mjs`, js);
+}
 
 for (let file of glob.sync(`${pkg}/dist/*.module.js`)) {
   fs.copyFileSync(file, file.replace(regex, '$1.mjs'));
