3760f571bf66ef38b435195d5057ffa1c6b40c09
Devon Govett <devongovett@gmail.com>
2023-12-03 01:43:10 -0500

    more styling

--- starters/tailwind/src/ListBox.tsx
+++ starters/tailwind/src/ListBox.tsx
@@ -55,7 +55,7 @@ export interface ListBoxSectionProps<T> extends SectionProps<T> {
 export function ListBoxSection<T extends object>(props: ListBoxSectionProps<T>) {
   return (
     <Section className="first:-mt-[5px] after:content-[''] after:block after:h-[5px]">
-      <Header className="text-sm font-semibold text-gray-500 px-3 py-1 sticky -top-[5px] -mt-px -mx-1 z-10 bg-gray-100/60 backdrop-blur-md supports-[-moz-appearance:none]:bg-gray-100 border-y [&+*]:mt-1">{props.title}</Header>
+      <Header className="text-sm font-semibold text-gray-500 px-4 py-1 truncate sticky -top-[5px] -mt-px -mx-1 z-10 bg-gray-100/60 backdrop-blur-md supports-[-moz-appearance:none]:bg-gray-100 border-y [&+*]:mt-1">{props.title}</Header>
       <Collection items={props.items}>
         {props.children}
       </Collection>
