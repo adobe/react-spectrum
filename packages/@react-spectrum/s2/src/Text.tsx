d6bbb2693c281d2e7bc5b1eff7abd6988db27568
Rob Snow <rsnow@adobe.com>
2023-12-07 10:41:15 +1100

    Add our wrapper elements for basic slots

--- packages/@react-spectrum/s2/src/Text.tsx
+++ packages/@react-spectrum/s2/src/Text.tsx
@@ -2,18 +2,28 @@ import { useDOMRef, useSlotProps, useStyleProps } from "@react-spectrum/utils";
 import {Text as RACText} from 'react-aria-components';
 import {filterDOMProps} from "@react-aria/utils";
 import { forwardRef } from "react";
+import { TextProps } from "@adobe/react-spectrum";
+import { DOMRef } from "@react-types/shared";
 
-export let Text = forwardRef((props, ref) => {
+
+function Text(props: TextProps, ref: DOMRef) {
   props = useSlotProps(props, 'text');
   let {
     children,
     ...otherProps
   } = props;
   let {styleProps} = useStyleProps(otherProps);
+  let domRef = useDOMRef(ref);
 
   return (
-    <RACText {...filterDOMProps(otherProps)} {...styleProps} ref={ref}>
+    <RACText {...filterDOMProps(otherProps)} {...styleProps} ref={domRef}>
       {children}
     </RACText>
   );
-});
+}
+
+/**
+ * Text represents text with no specific semantic meaning.
+ */
+const _Text = forwardRef(Text);
+export {_Text as Text};
