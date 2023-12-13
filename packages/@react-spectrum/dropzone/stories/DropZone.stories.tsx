29510be945f98a4defdd36001415a38887835a36
Kyle Taborski <ktabors@yahoo.com>
2023-12-13 15:46:18 -0800

    Strict Typescript - Dropzone, VisuallyHidden (#5566)
    * checkboxgroup stately: fixing boolean and undefined assignment
    
    * inlineAlert: better typing on variables
    
    * VisuallyHidden: protecting against nulls
    
    * dropzone stories typescript improvements
    
    * improving visuallyalert tests changes and inlinealert is more accurate
    
    * add tsconfig update for the changes
    
    * removing conflicts
    
    * fixing a partial revert
    
    * dropzone lint fixes

--- packages/@react-spectrum/dropzone/stories/DropZone.stories.tsx
+++ packages/@react-spectrum/dropzone/stories/DropZone.stories.tsx
@@ -16,9 +16,9 @@ import {Cell, Column, Row, TableBody, TableHeader, TableView} from '@react-spect
 import {classNames} from '@react-spectrum/utils';
 import {Content} from '@react-spectrum/view';
 import {Draggable} from '@react-aria/dnd/stories/dnd.stories';
+import {DropEvent, FileDropItem, TextDropItem, useDrag} from 'react-aria';
 import {DropZone} from '../';
 import File from '@spectrum-icons/illustrations/File';
-import {FileDropItem, TextDropItem, useDrag} from 'react-aria';
 import {FileTrigger, Text} from 'react-aria-components';
 import {Heading} from '@react-spectrum/text';
 import {IllustratedMessage} from '@react-spectrum/illustratedmessage';
@@ -28,6 +28,13 @@ import React, {useState} from 'react';
 import {SpectrumDropZoneProps} from '../src/DropZone';
 import styles from './styles.css';
 import Upload from '@spectrum-icons/illustrations/Upload';
+
+interface FileFilledSource {
+  src?: string,
+  id?: number,
+  type: string,
+  name: string
+}
 
 type StoryArgs = SpectrumDropZoneProps;
 
@@ -46,8 +53,8 @@ export const withDraggable = {
 
 export const customAriaLabel = {
   render: (args) => (
-    <DropZoneWithDraggable 
-      {...args} 
+    <DropZoneWithDraggable
+      {...args}
       aria-label="custom label" />
   )
 };
@@ -60,7 +67,7 @@ export const withButton = {
 
 export const customBannerMessage = {
   render: (args) => (
-    <Example 
+    <Example
       {...args}
       replaceMessage="This is a custom message" />
   )
@@ -95,25 +102,25 @@ function Example(props) {
   ];
 
   let [isFilled, setIsFilled] = useState(false);
-  let [filledSrc, setFilledSrc] = useState(null);
-  
+  let [filledSrc, setFilledSrc] = useState<Iterable<FileFilledSource> | undefined>(undefined);
+
   return (
     <>
-      <DropZone 
+      <DropZone
         {...props}
         width="size-3000"
         height="size-3000"
         isFilled={isFilled}
-        onDrop={async (e) => { 
+        onDrop={async (e) => {
           let items = e.items.filter((item) => item.kind === 'file') as FileDropItem[];
           if (items.length > 0) {
-            const rows = items.map((item, index) => ({name: item.name, type: item.type, id: index}));
+            const rows = items.map((item, index) => ({name: item.name, type: item.type, id: index} as FileFilledSource));
             setFilledSrc(rows);
             setIsFilled(true);
           }
         }}
         onDropEnter={action('onDropEnter')}
-        onDropExit={action('onDropExit')} 
+        onDropExit={action('onDropExit')}
         onPaste={action('onPaste')}>
         <IllustratedMessage>
           <Upload />
@@ -125,9 +132,9 @@ function Example(props) {
           <Content>
             <FileTrigger
               allowsMultiple
-              onSelect={(e) => {
-                let files = Array.from(e);
-                let rows = files.map((file, index) => ({name: file.name, type: file.type, id: index}));
+              onSelect={(e: FileList | null) => {
+                let files = Array.from(e || []);
+                let rows = files.map((file, index) => ({name: file.name, type: file.type, id: index} as FileFilledSource));
                 setFilledSrc(rows);
                 setIsFilled(true);
               }}>
@@ -147,7 +154,7 @@ function Example(props) {
               {column.name}
             </Column>)}
         </TableHeader>
-        {isFilled ? 
+        {isFilled ?
           <TableBody items={filledSrc}>
             {item => (
               <Row>
@@ -166,23 +173,23 @@ function Example(props) {
 
 function DropZoneWithDraggable(props) {
   let [isFilled, setIsFilled] = useState(false);
-  let [filledSrc, setFilledSrc] = useState(null);
+  let [filledSrc, setFilledSrc] = useState<string | null>(null);
 
   return (
     <>
       <Draggable />
-      <DropZone 
+      <DropZone
         {...props}
         isFilled={isFilled}
-        onDrop={async (e) => {
-          let items = await Promise.all(e.items.filter((item) => item.kind === 'text' && item.types.has('text/plain')).map((item: TextDropItem) => item.getText('text/plain')));
+        onDrop={async (e: DropEvent) => {
+          let items = await Promise.all(e.items.filter((item) => item.kind === 'text' && item.types.has('text/plain')).map((item) => (item as TextDropItem).getText('text/plain')));
           if (items.length > 0) {
             setIsFilled(true);
             setFilledSrc(items.join('\n'));
           }
         }}
         onDropEnter={action('onDropEnter')}
-        onDropExit={action('onDropExit')} 
+        onDropExit={action('onDropExit')}
         onPaste={action('onPaste')}>
         <IllustratedMessage>
           <Upload />
@@ -200,28 +207,28 @@ function DropZoneWithDraggable(props) {
 
 function DropZoneWithButton(props) {
   let [isFilled, setIsFilled] = useState(false);
-  let [filledSrc, setFilledSrc] = useState(null);
+  let [filledSrc, setFilledSrc] = useState<FileFilledSource | null>(null);
 
   return (
     <>
-      <DropZone 
+      <DropZone
         {...props}
         width="size-3000"
         height="size-3000"
         isFilled={isFilled}
         UNSAFE_className={classNames(styles, isFilled && 'is-filled')}
-        onDrop={async (e) => { 
+        onDrop={async (e) => {
           let item = e.items.find((item) => item.kind === 'file') as FileDropItem;
           if (item) {
             setFilledSrc({
-              src: URL.createObjectURL(await item.getFile()), 
+              src: URL.createObjectURL(await item.getFile()),
               type: item.type,
               name: item.name});
             setIsFilled(true);
           }
         }}
         onDropEnter={action('onDropEnter')}
-        onDropExit={action('onDropExit')} 
+        onDropExit={action('onDropExit')}
         onPaste={action('onPaste')}>
         <IllustratedMessage>
           <Upload />
@@ -233,7 +240,7 @@ function DropZoneWithButton(props) {
           <Content>
             <FileTrigger
               onSelect={(e) => {
-                let files = Array.from(e);
+                let files = Array.from(e || []);
                 let src = files.map(file => URL.createObjectURL(file));
                 let type = files.map(file => file.type);
                 let name = files.map(file => file.name);
@@ -248,10 +255,10 @@ function DropZoneWithButton(props) {
           </Content>
         </IllustratedMessage>
       </DropZone>
-      {isFilled && 
+      {isFilled &&
         <div className={styles.files}>
           <File />
-          {filledSrc.name}
+          {filledSrc?.name}
         </div>}
     </>
   );
@@ -285,7 +292,7 @@ function DropZoneFilled(props) {
           });
         }}
         onDropEnter={action('onDropEnter')}
-        onDropExit={action('onDropExit')} 
+        onDropExit={action('onDropExit')}
         onPaste={action('onPaste')}>
         <img className={styles.images} alt="a starry sky" src={filledSrc} />
       </DropZone>
