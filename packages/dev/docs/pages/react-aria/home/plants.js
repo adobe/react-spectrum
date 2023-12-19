b52ed4af9a53156c70da50417b0cfad2320d4ac2
Devon Govett <devongovett@gmail.com>
2023-12-19 12:55:35 -0500

    lint

---
+++ packages/dev/docs/pages/react-aria/home/plants.js
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+export default [
+  {
+    id: 1,
+    common_name: 'Aloe',
+    scientific_name: ['Aloe vera'],
+    watering: 'Minimum',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/aloe.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 2,
+    common_name: 'Blue Jacaranda',
+    scientific_name: ['Jacaranda mimosifolia'],
+    watering: 'Minimum',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/jacaranda.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 3,
+    common_name: 'Oleander',
+    scientific_name: ['Nerium oleander'],
+    watering: 'Minimum',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/oleander.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 4,
+    common_name: 'Poplar',
+    scientific_name: ['Populus'],
+    watering: 'Average',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/poplar.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 5,
+    common_name: 'Zanzibar Gem',
+    scientific_name: ['Zamioculcas'],
+    watering: 'Average',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/zz.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 6,
+    common_name: 'Morning Glory',
+    scientific_name: ['Ipomoea'],
+    watering: 'Frequent',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/morning.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 7,
+    common_name: 'Christmas Bush',
+    scientific_name: ['Ceratopetalum gummiferum'],
+    watering: 'Average',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/xmas.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 8,
+    common_name: 'Gardenia',
+    scientific_name: ['Gardenia jasminoides'],
+    watering: 'Average',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/gardenia.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 9,
+    common_name: 'Spider Plant',
+    scientific_name: ['Chlorophytum comosum'],
+    watering: 'Average',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/spider.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 10,
+    common_name: 'Chinese Money Plant',
+    scientific_name: ['Pilea peperomioides'],
+    watering: 'Average',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/money.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 11,
+    common_name: 'Fiddle Leaf Fig',
+    scientific_name: ['Ficus lyrata'],
+    watering: 'Average',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/fig.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 12,
+    common_name: 'Tuberous Sword Fern',
+    scientific_name: ['Nephrolepis cordifolia'],
+    watering: 'Frequent',
+    sunlight: ['part shade'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/fern.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 13,
+    common_name: 'Star Jasmine',
+    scientific_name: ['Trachelospermum jasminoides'],
+    watering: 'Frequent',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/star.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 14,
+    common_name: 'Split-leaf Philodendron',
+    scientific_name: ['Monstera deliciosa'],
+    watering: 'Frequent',
+    sunlight: ['part shade'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/monstera.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 15,
+    common_name: 'Agapanthus',
+    scientific_name: ['Agapanthus praecox'],
+    watering: 'Minimum',
+    sunlight: ['full sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/agapanthus.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 16,
+    common_name: 'Tree Fern',
+    scientific_name: ['Cyatheaceae'],
+    watering: 'Frequent',
+    sunlight: ['part sun'],
+    cycle: 'Annual',
+    default_image: {
+      thumbnail: new URL('plants/tree_fern.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 17,
+    common_name: 'Striped Dracaena',
+    scientific_name: ['Asparagaceae'],
+    watering: 'Average',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/dracaena.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 18,
+    common_name: 'Delta Maidenhair Fern',
+    scientific_name: ['Adiantum raddianum'],
+    watering: 'Average',
+    sunlight: ['part shade'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/maidenhair.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 19,
+    common_name: 'Ivy',
+    scientific_name: ['Hedera'],
+    watering: 'Frequent',
+    sunlight: ['part sun'],
+    cycle: 'Perennial',
+    default_image: {
+      thumbnail: new URL('plants/ivy.jpg?as=webp', import.meta.url).toString()
+    }
+  },
+  {
+    id: 20,
+    common_name: 'Nasturtium',
+    scientific_name: ['Tropaeolum'],
+    watering: 'Average',
+    sunlight: ['full sun'],
+    cycle: 'Annual',
+    default_image: {
+      thumbnail: new URL('plants/nasturtium.jpg?as=webp', import.meta.url).toString()
+    }
+  }
+];
