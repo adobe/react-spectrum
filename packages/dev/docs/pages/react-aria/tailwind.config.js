8c3f9599fb38bd7b5ed5cb99d28a8b77f7fe2d0d
Devon Govett <devongovett@gmail.com>
2024-01-08 12:40:24 -0500

    lint

--- packages/dev/docs/pages/react-aria/tailwind.config.js
+++ packages/dev/docs/pages/react-aria/tailwind.config.js
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
