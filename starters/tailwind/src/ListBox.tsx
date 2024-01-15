b6849b8a74985c024e4e9574510cad0806a3921b
Devon Govett <devongovett@gmail.com>
2024-01-14 20:06:08 -0500

    fix listbox selected outline color in dark mode

--- starters/tailwind/src/ListBox.tsx
+++ starters/tailwind/src/ListBox.tsx
@@ -32,7 +32,7 @@ export const itemStyles = tv({
   variants: {
     isSelected: {
       false: 'text-slate-700 dark:text-zinc-300 hover:bg-slate-200 dark:hover:bg-zinc-700 -outline-offset-2',
-      true: 'bg-blue-600 text-white forced-colors:bg-[Highlight] forced-colors:text-[HighlightText] [&:has(+[data-selected])]:rounded-b-none [&+[data-selected]]:rounded-t-none -outline-offset-4 outline-white forced-colors:outline-[HighlightText]'
+      true: 'bg-blue-600 text-white forced-colors:bg-[Highlight] forced-colors:text-[HighlightText] [&:has(+[data-selected])]:rounded-b-none [&+[data-selected]]:rounded-t-none -outline-offset-4 outline-white dark:outline-white forced-colors:outline-[HighlightText]'
     },
     isDisabled: {
       true: 'text-slate-300 dark:text-zinc-600 forced-colors:text-[GrayText]'
