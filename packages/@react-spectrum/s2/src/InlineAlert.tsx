31236f2d3325c27482135882da31080ebd3034eb
Yihui Liao <yliao@adobe.com>
2024-01-26 15:07:43 -0800

    Inline Alert (#12)
    * initialize inline alert
    
    * add styling for inline alert
    
    * lock file?
    
    * address some comments
    
    * address comments
    
    * update gridTemplateAreas syntax
    
    Co-authored-by: Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
    Co-authored-by: Devon Govett <devongovett@gmail.com>

---
+++ packages/@react-spectrum/s2/src/InlineAlert.tsx
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import AlertTriangle from '../s2wf-icons/assets/react/s2IconAlertTriangle20N';
+import CheckmarkCircle from '../s2wf-icons/assets/react/s2IconCheckmarkCircle20N';
+import {DOMProps, DOMRef, StyleProps} from '@react-types/shared';
+import {filterDOMProps} from '@react-aria/utils';
+import {focusRing} from './style-utils.ts' with {type: 'macro'};
+import InfoCircle from '../s2wf-icons/assets/react/s2IconInfoCircle20N';
+import {mergeStyles} from '../style-macro/runtime.ts';
+import React, {ReactNode, useEffect, useRef} from 'react';
+import {SlotProvider, useDOMRef} from '@react-spectrum/utils';
+import {style} from '../style-macro/spectrum-theme.ts' with {type: 'macro'};
+import {useFocusRing} from 'react-aria';
+
+export interface SpectrumInlineAlertProps extends DOMProps, StyleProps, InlineStylesProps {
+  /**
+   * The contents of the Inline Alert.
+   */
+  children: ReactNode,
+  /**
+   * Whether to automatically focus the Inline Alert when it first renders.
+   */
+  autoFocus?: boolean
+}
+
+interface InlineStylesProps {
+  /**
+   * The semantic tone of a Inline Alert. 
+   * @default neutral
+   */
+  variant?: 'informative' | 'positive' | 'notice' | 'negative' | 'neutral',
+  /**
+   * The visual style of the Inline Alert.
+   * @default border
+   */
+  style?: 'border' | 'subtleFill' | 'boldFill'
+}
+
+const inlineAlert = style<InlineStylesProps & {isFocusVisible?: boolean}>({
+  ...focusRing(),
+  display: 'inline-block',
+  position: 'relative',
+  boxSizing: 'border-box',
+  maxWidth: 80,
+  fontSize: 'base',
+  fontFamily: 'sans',
+  padding: 6,
+  borderRadius: 'lg',
+  borderStyle: 'solid',
+  borderWidth: 2,
+  borderColor: {
+    style: {
+      border: {
+        variant: {
+          informative: 'informative-800',
+          positive: 'positive-700',
+          notice: 'notice-700',
+          negative: 'negative-800',
+          neutral: 'gray-700' // is there a semantic color name for neutral?
+        }
+      },
+      subtleFill: 'transparent',
+      boldFill: 'transparent'
+    }
+  },
+  backgroundColor: {
+    variant: {
+      informative: {
+        style: {
+          border: 'gray-25',
+          subtleFill: 'informative-200',
+          boldFill: 'informative'
+        }
+      },
+      positive: {
+        style: {
+          border: 'gray-25',
+          subtleFill: 'positive-200',
+          boldFill: 'positive'
+        }
+      },
+      notice: {
+        style: {
+          border: 'gray-25',
+          subtleFill: 'notice-200',
+          boldFill: 'notice' // TODO: check with design since this doesn't pass contrast in dark mode
+        }
+      },
+      negative: {
+        style: {
+          border: 'gray-25',
+          subtleFill: 'negative-200',
+          boldFill: 'negative'
+        }
+      },
+      neutral: {
+        style: {
+          border: 'gray-25',
+          subtleFill: 'gray-100',
+          boldFill: 'neutral-subdued'
+        }
+      }
+    }
+  }
+});
+
+const icon = style<InlineStylesProps>({
+  gridArea: 'icon',
+  fill: {
+    default: {
+      variant: {
+        informative: 'blue-800', // TODO: update semantic aliases for icon colors after devon's PR is merged in 
+        positive: 'green-700',
+        notice: 'orange-700',
+        negative: 'red-800'
+      }
+    },
+    style: {
+      boldFill: 'transparent-white-900'
+    },
+    forcedColors: 'ButtonText'
+  }
+});
+
+const grid = style<InlineStylesProps>({
+  display: 'grid',
+  columnGap: 6,
+  gridTemplateColumns: '1fr auto',
+  gridTemplateRows: 'auto auto auto',
+  width: 'full',
+  gridTemplateAreas: [
+    'heading icon',
+    'content content'
+  ]
+});
+
+let ICONS = {
+  informative: InfoCircle,
+  positive: CheckmarkCircle,
+  notice: AlertTriangle,
+  negative: AlertTriangle,
+  neutral: undefined
+};
+
+function InlineAlert(props: SpectrumInlineAlertProps, ref: DOMRef<HTMLDivElement>) {
+  let {
+    children,
+    variant = 'neutral',
+    style: styleFill = 'border',
+    autoFocus
+  } = props;
+
+  let domRef = useDOMRef(ref);
+
+  let slots = {
+    heading: {UNSAFE_className: style({
+      marginTop: 0, 
+      color: {
+        style: {
+          border: 'gray-900',
+          subtleFill: 'gray-900',
+          boldFill: 'transparent-white-900'
+        }
+      }, 
+      gridArea: 'heading'})({variant, style: styleFill})
+    },
+    content: {UNSAFE_className: style({
+      color: {
+        style: {
+          border: 'gray-900',
+          subtleFill: 'gray-800',
+          boldFill: 'transparent-white-800'
+        } 
+      },
+      gridArea: 'content'})({variant, style: styleFill})
+    }
+  };
+
+  let Icon = null;
+  let iconAlt = '';
+  if (variant in ICONS) {
+    Icon = ICONS[variant];
+    iconAlt = variant;
+  }
+
+  let {isFocusVisible, focusProps} = useFocusRing({autoFocus: props.autoFocus});
+  let autoFocusRef = useRef(props.autoFocus);
+  useEffect(() => {
+    if (autoFocusRef.current && domRef.current) {
+      domRef.current.focus();
+    }
+    autoFocusRef.current = false;
+  }, [domRef]);
+
+  return (
+    <div
+      {...filterDOMProps(props)}
+      {...focusProps}
+      ref={domRef}
+      tabIndex={autoFocus ? -1 : undefined}
+      autoFocus={autoFocus}
+      role="alert"
+      className={mergeStyles(props.UNSAFE_className, inlineAlert({
+        variant,
+        style: styleFill,
+        isFocusVisible
+      }))}>
+      <div 
+        className={grid({...props})}>
+        <SlotProvider slots={slots}>
+          {Icon && <Icon
+            className={icon({...props})}
+            aria-label={iconAlt} />}
+          {children}
+        </SlotProvider>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Inline alerts display a non-modal message associated with objects in a view.
+ * These are often used in form validation, providing a place to aggregate feedback related to multiple fields.
+ */
+const _InlineAlert = React.forwardRef(InlineAlert);
+export {_InlineAlert as InlineAlert};
---
+++ packages/@react-spectrum/s2/stories/InlineAlert.stories.tsx
@@ -0,0 +1,45 @@
+import {InlineAlert} from '../src/InlineAlert'; 
+import type {Meta} from '@storybook/react';
+import {Heading, Content} from '@adobe/react-spectrum';
+import {useState} from 'react';
+import {Button} from '../src/Button';
+
+const meta: Meta<typeof InlineAlert> = {
+  component: InlineAlert,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?node-id=2696%3A5096&mode=dev'
+    }
+  }
+};
+
+export default meta;
+
+export const Example = (args: any) => (
+  <InlineAlert {...args}>
+    <Heading>Payment Information</Heading>
+    <Content>
+      There was an error processing your payment. Please check that your card information is correct, then try again.
+    </Content>
+  </InlineAlert>
+);
+
+export const DynamicExample = (args: any) => {
+  let [shown, setShown] = useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onPress={() => setShown(!shown)}>{shown ? 'Hide Alert' : 'Show Alert'}</Button>
+      {shown &&
+        <InlineAlert {...args} autoFocus>
+          <Heading>Payment Information</Heading>
+          <Content>
+            There was an error processing your payment. Please check that your card information is correct, then try again.
+          </Content>
+        </InlineAlert>
+      }
+    </>
+  );
+};
