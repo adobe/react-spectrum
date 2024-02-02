81d084c8462099033df8801908cfcf7b418045a0
Michael Jordan <mijordan@adobe.com>
2024-02-02 10:32:12 -0500

    #4110 #4111 Color hue/color name accessibility/i18n

--- packages/@react-stately/color/test/Color.test.tsx
+++ packages/@react-stately/color/test/Color.test.tsx
@@ -267,4 +267,42 @@ describe('Color', function () {
       expect(parseColor('hsb(0, 1%, 0%)').toString('hsl')).toBe('hsl(0, 0%, 0%)');
     });
   });
+
+  describe('css color name', function () {
+    it('should parse a css color name', function () {
+      let color = parseColor('papayawhip');
+      expect(color.getChannelValue('red')).toBe(255);
+      expect(color.getChannelValue('green')).toBe(239);
+      expect(color.getChannelValue('blue')).toBe(213);
+      expect(color.getChannelValue('alpha')).toBe(1);
+      expect(color.toString('hex')).toBe('#FFEFD5');
+      expect(color.toString('rgba')).toBe('rgba(255, 239, 213, 1)');
+      expect(color.toString('css')).toBe('rgba(255, 239, 213, 1)');
+    });
+
+    it('should throw on invalid css colorname value', function () {
+      expect(() => parseColor('bluebish')).toThrow('Invalid color value: bluebish');
+    });
+  });
+
+  describe('#getColorName', function () {
+    it('should return localized color name', function () {
+      let color = parseColor('papayawhip');
+      expect(color.getColorName('en-US')).toBe('Papaya Whip');
+      color = parseColor('hsl(30, 0%, 22.5%)');
+      expect(color.getColorName('en-US')).toBe('Gray 23%');
+      color = parseColor('hsl(30, 50%, 100%)');
+      expect(color.getColorName('en-US')).toBe('White');
+      color = parseColor('hsl(30, 100%, 0%)');
+      expect(color.getColorName('en-US')).toBe('Black');
+      color = parseColor('#E78B8B');
+      expect(color.getColorName('en-US')).toBe('Pink');
+      color = parseColor('#E7E7B8');
+      expect(color.getColorName('en-US')).toBe('Pale, Light Grayish Olive');
+      color = parseColor('hsl(30, 39%, 25%)');
+      expect(color.getColorName('en-US')).toBe('Brown');
+      color = parseColor('hsb(200, 60%, 62%)');
+      expect(color.getColorName('en-US')).toBe('Moderate Cornflower Blue');
+    });
+  });
 });
