35772315e66fa1139decbb0721b444e51de0de56
Devon Govett <devongovett@gmail.com>
2024-02-06 17:04:02 -0500

    Mark RAC and RSP monopackages as client-only

--- packages/@adobe/react-spectrum/src/index.ts
+++ packages/@adobe/react-spectrum/src/index.ts
@@ -10,6 +10,10 @@
  * governing permissions and limitations under the License.
  */
 
+// Mark as a client only package. This will cause a build time error if you try 
+// to import it from a React Server Component in a framework like Next.js.
+import 'client-only';
+
 export {ActionGroup} from '@react-spectrum/actiongroup';
 export {Badge} from '@react-spectrum/badge';
 export {Breadcrumbs} from '@react-spectrum/breadcrumbs';
