5be176393737f3b38670d5712780b87fd7a8a5be
Rob Snow <rsnow@adobe.com>
2024-02-20 12:29:36 +1100

    Also apply it to the range picker

--- packages/react-aria-components/stories/DatePicker.stories.tsx
+++ packages/react-aria-components/stories/DatePicker.stories.tsx
@@ -148,7 +148,8 @@ export const DateRangePickerTriggerWidthExample = () => (
         color: 'CanvasText',
         border: '1px solid gray',
         padding: 20,
-        width: 'calc(var(--trigger-width) - 20px * 2 - 1px * 2)'
+        boxSizing: 'border-box',
+        width: 'var(--trigger-width)'
       }}>
       <Dialog>
         <RangeCalendar>
