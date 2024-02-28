d58af25ea201278c7d956bfb61dcc372a3fa365b
Kyle Taborski <taborski@adobe.com>
2024-02-28 12:55:45 -0800

    Picker input field styling (#32)
    * renaming to picker and adding initial style macro changes
    
    * field and input styles added
    
    * two quick style fixes from feedback
    
    * removing staticColor, fixing isDisabled, and improving some prop handling
    
    * lint spacing fixes
    
    * fixing the focusRing after menu close
    
    * quiet style as element with rounded ends
    
    * undid testing mode
    
    * implementing the isQuiet focus ring more closely, but looks less correct
    
    * chevron icon cleanup
    
    * refining styles based on feedback
    
    Co-authored-by: Kyle Taborski <ktabors@yahoo.com>

---
+++ packages/@react-spectrum/s2/src/Picker.tsx
@@ -0,0 +1,208 @@
+import {baseColor, style} from '../style-macro/spectrum-theme' with {type: 'macro'};
+import {
+  Button,
+  ButtonRenderProps,
+  ListBox,
+  ListBoxItem,
+  ListBoxItemProps,
+  Popover,
+  Select as AriaSelect,
+  SelectProps as AriaSelectProps,
+  SelectRenderProps as AriaSelectRenderProps,
+  SelectValue,
+  ValidationResult
+} from 'react-aria-components';
+import ChevronIcon from '../ui-icons/S2_ChevronSize100.svg';
+import {field, focusRing} from './style-utils' with {type: 'macro'};
+import {
+  FieldLabel,
+  HelpText
+} from './Field';
+import {FormContext, useFormProps} from './Form';
+import {pressScale} from './pressScale';
+import React, {useContext, useRef} from 'react';
+import {SpectrumLabelableProps} from '@react-types/shared';
+
+export interface PickerStyleProps {
+  size?: 'S' | 'M' | 'L' | 'XL',
+  isQuiet?: boolean
+}
+
+export interface PickerProps<T extends object>
+  extends Omit<AriaSelectProps<T>, 'children'>, PickerStyleProps, SpectrumLabelableProps {
+  description?: string,
+  errorMessage?: string | ((validation: ValidationResult) => string),
+  label?: string,
+  items?: Iterable<T>,
+  children: React.ReactNode | ((item: T) => React.ReactNode)
+}
+
+export interface PickerSelectProps extends PickerStyleProps, AriaSelectRenderProps {}
+
+export interface PickerButtonProps extends PickerStyleProps, ButtonRenderProps {}
+
+const selectWrapper = style<PickerSelectProps & SpectrumLabelableProps & {isInForm?: boolean}>({
+  ...field(),
+  width: {
+    default: 52,
+    size: {
+      S: 44,
+      L: 56,
+      XL: 60
+    },
+    isQuiet: 'fit'
+  }
+});
+
+const inputButton = style<PickerButtonProps | AriaSelectRenderProps>({
+  ...focusRing(),
+  outlineStyle: {/* The focus ring shows up after menu close if default and isFocusVisible from focusRing() aren't added to this definition. */
+    default: 'none',
+    isFocusVisible: 'solid',
+    isQuiet: 'none'
+  },
+  gridArea: 'input',
+  fontFamily: 'sans',
+  fontSize: 'control',
+  display: 'flex',
+  textAlign: 'start',
+  borderStyle: 'none',
+  borderRadius: 'control',
+  alignItems: 'center',
+  height: 'control',
+  columnGap: {
+    default: 'text-to-control',
+    isQuiet: 'text-to-visual'
+  },
+  paddingX: {
+    default: 'edge-to-text',
+    isQuiet: 0
+  },
+  paddingBottom: {
+    isQuiet: '[calc((var(--field-height) - 1lh) / 2)]'
+  },
+  backgroundColor: {
+    default: baseColor('gray-100'),
+    isOpen: 'gray-200',
+    isDisabled: 'disabled',
+    isQuiet: 'transparent'
+  },
+  color: {
+    default: 'neutral',
+    isDisabled: 'disabled'
+  }
+});
+
+const quietFocusLine = style({
+  width: 'full',
+  height: 0.5,
+  position: 'absolute',
+  bottom: 0,
+  borderRadius: 'full',
+  backgroundColor: {
+    default: 'blue-800',
+    forcedColors: 'Highlight'
+  }
+});
+
+const valueStyles = style({
+  flex: '1',
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap'
+});
+
+const iconStyles = style({
+  rotate: 90,
+  size: {
+    default: 2.5,
+    size: {
+      L: 3,
+      XL: 3.5
+    }
+  },
+  '--iconPrimary': {
+    type: 'fill',
+    value: 'currentColor'
+  }
+});
+
+export function Picker<T extends object>(props: PickerProps<T>) {
+  let ref = useRef(null);
+  let formContext = useContext(FormContext);
+  props = useFormProps(props);
+  let {
+    label,
+    description,
+    errorMessage,
+    children,
+    items,
+    isInvalid,
+    isQuiet,
+    size = 'M',
+    labelPosition = 'top',
+    labelAlign = 'start',
+    necessityIndicator,
+    ...pickerProps
+  } = props;
+
+  return (
+    <AriaSelect
+      {...pickerProps}
+      className={renderProps => selectWrapper({
+        ...renderProps,
+        isInForm: !!formContext,
+        isQuiet: isQuiet,
+        labelPosition,
+        size: size
+      })}>
+      {({isDisabled, isFocusVisible}) => (
+        <>
+          <FieldLabel
+            isDisabled={isDisabled}
+            isRequired={props.isRequired}
+            size={size}
+            labelPosition={labelPosition}
+            labelAlign={labelAlign}
+            necessityIndicator={necessityIndicator}>
+            {label}
+          </FieldLabel>
+          <Button
+            ref={ref}
+            style={pressScale(ref)}
+            className={renderProps => inputButton({
+              ...renderProps,
+              isQuiet: isQuiet,
+              size: size
+            })}>
+            <SelectValue
+              className={valueStyles()} />
+            <ChevronIcon
+              className={iconStyles({
+                isDisabled: isDisabled,
+                isQuiet: isQuiet,
+                size: size
+              })} />
+            {isFocusVisible && isQuiet && <span className={quietFocusLine()} /> }
+          </Button>
+          <HelpText
+            size={size}
+            isDisabled={isDisabled}
+            isInvalid={isInvalid}
+            description={description}>
+            {errorMessage}
+          </HelpText>
+          <Popover>
+            <ListBox items={items}>
+              {children}
+            </ListBox>
+          </Popover>
+        </>
+      )}
+    </AriaSelect>
+  );
+}
+
+export function PickerItem(props: ListBoxItemProps) {
+  return <ListBoxItem {...props} />;
+}
---
+++ packages/@react-spectrum/s2/stories/Picker.stories.tsx
@@ -0,0 +1,48 @@
+import {Picker, PickerItem} from '../src/Picker';
+import React from 'react';
+import {style} from '../style-macro/spectrum-theme' with {type: 'macro'};
+
+import type {Meta} from '@storybook/react';
+
+const meta: Meta<typeof Picker> = {
+  component: Picker,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?type=design&node-id=739-1453&mode=design&t=rZcwwKQ0qsEp7G8L-0'
+    }
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+
+export const Example = (args: any) => (
+  <section
+    className={style({
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+      padding: 2,
+      color: 'neutral',
+      backgroundColor: {
+        staticColor: {
+          black: {default: 'yellow-400', dark: 'yellow-1100'},
+          white: {default: 'blue-900', dark: 'blue-500'}
+        }
+      }
+    })({staticColor: args.staticColor})}>
+    <Picker {...args}>
+      <PickerItem>Chocolate</PickerItem>
+      <PickerItem>Mint</PickerItem>
+      <PickerItem>Strawberry</PickerItem>
+      <PickerItem>Vanilla</PickerItem>
+      <PickerItem>Chocolate Chip Cookie Dough</PickerItem>
+    </Picker>
+  </section>
+);
+
+Example.args = {
+  label: 'Ice cream flavor'
+};
