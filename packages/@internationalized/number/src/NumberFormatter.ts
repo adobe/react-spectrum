3cc1ad61d4af4bac0fa2e23aed699247941eace3
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-03-03 00:46:35 +0900

    Keep number parsing and formatting warnings in production

--- packages/@internationalized/number/src/NumberFormatter.ts
+++ packages/@internationalized/number/src/NumberFormatter.ts
@@ -196,10 +196,8 @@ export function numberFormatSignDisplayPolyfill(numberFormat: Intl.NumberFormat,
       let noSign = numberFormat.format(num);
       // ignore RTL/LTR marker character
       let minus = negative.replace(noSign, '').replace(/\u200e|\u061C/, '');
-      if (process.env.NODE_ENV !== 'production') {
-        if ([...minus].length !== 1) {
-          console.warn('@react-aria/i18n polyfill for NumberFormat signDisplay: Unsupported case');
-        }
+      if ([...minus].length !== 1) {
+        console.warn('@react-aria/i18n polyfill for NumberFormat signDisplay: Unsupported case');
       }
       let positive = negative.replace(noSign, '!!!').replace(minus, '+').replace('!!!', noSign);
       return positive;
