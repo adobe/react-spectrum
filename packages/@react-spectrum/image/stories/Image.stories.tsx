d968f7e845982cb64b84e85c6008080011cf7bb0
Rob Snow <rsnow@adobe.com>
2024-03-06 17:38:09 +1100

    fix lint

--- packages/@react-spectrum/image/stories/Image.stories.tsx
+++ packages/@react-spectrum/image/stories/Image.stories.tsx
@@ -9,8 +9,8 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import {ComponentMeta, ComponentStory, ComponentStoryObj} from '@storybook/react';
-import {Image, SpectrumImageProps} from '../';
+import {ComponentMeta, ComponentStoryObj} from '@storybook/react';
+import {Image} from '../';
 import React, {useState} from 'react';
 
 export type ImageStory = ComponentStoryObj<typeof Image>;
