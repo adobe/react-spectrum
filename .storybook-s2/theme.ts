6427c04a25605a4a082c699f4c39e068ab7336aa
Reid Barber <chabarbe@adobe.com>
2024-03-26 14:23:43 -0500

    Add logo to storybook build (#122)
    * add storybook logo
    
    * Use italics for tagline
    
    Co-authored-by: Reid Barber <reid@reidbarber.com>

---
+++ .storybook-s2/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+// @ts-ignore
+import logo from './logo.svg';
+
+export default create({
+  base: 'light',
+  brandTitle: 'React Spectrum - Spectrum 2 Preview',
+  brandImage: logo,
+});
