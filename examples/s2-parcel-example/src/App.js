a3624807b69a55e5b3008c89dee09e4f2461a16b
Danielle Robinson <darobins@adobe.com>
2024-03-27 14:38:57 -0700

    Add parcel example  (#137)
    * add parcel
    
    * Update examples/parcel-example/package.json
    
    Co-authored-by: Devon Govett <govett@adobe.com>
    
    Co-authored-by: Devon Govett <govett@adobe.com>

---
+++ examples/s2-parcel-example/src/App.js
@@ -0,0 +1,17 @@
+import "@react/experimental-s2/page.css";
+import { style } from "@react/experimental-s2/style" with { type: "macro" };
+import { Button } from "@react/experimental-s2";
+
+export function App() {
+  return (
+    <main>
+      <Button
+        styles={style({
+          marginStart: 16,
+        })}
+      >
+        Hello Spectrum 2!
+      </Button>
+    </main>
+  );
+}
---
+++ examples/s2-parcel-example/src/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
