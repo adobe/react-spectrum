d1621d6113c64a0d371a51c88ef09504df82ec94
TMH-SE <hieuhutieu98@gmail.com>
2024-03-28 01:09:26 +0700

    fix: correct floating precision in snapValueToStep

--- packages/@react-stately/utils/src/number.ts
+++ packages/@react-stately/utils/src/number.ts
@@ -18,33 +18,38 @@ export function clamp(value: number, min: number = -Infinity, max: number = Infi
   return newValue;
 }
 
+export function roundToStepPrecision(value: number, step: number) {
+  let roundedValue = value;
+  let stepString = step.toString();
+  let pointIndex = stepString.indexOf('.');
+  let precision = pointIndex >= 0 ? stepString.length - pointIndex : 0;
+  if (precision > 0) {
+    let pow = Math.pow(10, precision);
+    roundedValue = Math.round(roundedValue * pow) / pow;
+  }
+  return roundedValue;
+}
+
 export function snapValueToStep(value: number, min: number | undefined, max: number | undefined, step: number): number {
   min = Number(min);
   max = Number(max);
   let remainder = ((value - (isNaN(min) ? 0 : min)) % step);
-  let snappedValue = Math.abs(remainder) * 2 >= step
+  let snappedValue = roundToStepPrecision(Math.abs(remainder) * 2 >= step
     ? value + Math.sign(remainder) * (step - Math.abs(remainder))
-    : value - remainder;
+    : value - remainder, step);
 
   if (!isNaN(min)) {
     if (snappedValue < min) {
       snappedValue = min;
     } else if (!isNaN(max) && snappedValue > max) {
-      snappedValue = min + Math.floor((max - min) / step) * step;
+      snappedValue = min + Math.floor(roundToStepPrecision((max - min) / step, step)) * step;
     }
   } else if (!isNaN(max) && snappedValue > max) {
-    snappedValue = Math.floor(max / step) * step;
+    snappedValue = Math.floor(roundToStepPrecision(max / step, step)) * step;
   }
 
   // correct floating point behavior by rounding to step precision
-  let string = step.toString();
-  let index = string.indexOf('.');
-  let precision = index >= 0 ? string.length - index : 0;
-
-  if (precision > 0) {
-    let pow = Math.pow(10, precision);
-    snappedValue = Math.round(snappedValue * pow) / pow;
-  }
+  snappedValue = roundToStepPrecision(snappedValue, step);
 
   return snappedValue;
 }
