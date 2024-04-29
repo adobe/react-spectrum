5c8bf5c3f89e0640380fe8425bc380842ab7de31
Michael Jordan <mijordan@adobe.com>
2024-04-29 10:47:25 -0400

    lint fix: remove duplicated import

--- packages/@react-spectrum/toast/src/Toast.tsx
+++ packages/@react-spectrum/toast/src/Toast.tsx
@@ -15,11 +15,10 @@ import {Button, ClearButton} from '@react-spectrum/button';
 import {classNames, useDOMRef, useStyleProps} from '@react-spectrum/utils';
 import CrossMedium from '@spectrum-icons/ui/CrossMedium';
 import {DOMProps, DOMRef} from '@react-types/shared';
-import {filterDOMProps} from '@react-aria/utils';
+import {filterDOMProps, mergeProps} from '@react-aria/utils';
 import InfoMedium from '@spectrum-icons/ui/InfoMedium';
 // @ts-ignore
 import intlMessages from '../intl/*.json';
-import {mergeProps} from '@react-aria/utils';
 import {QueuedToast, ToastState} from '@react-stately/toast';
 import React from 'react';
 import styles from '@adobe/spectrum-css-temp/components/toast/vars.css';
