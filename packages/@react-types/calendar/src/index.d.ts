8cbc6122aa42729aa1166e1b1f82e9b15cd62219
Ishank Sharma <16836510+ishank-s@users.noreply.github.com>
2024-05-13 14:05:17 +0530

    ts-strict  calender fix types (#6201)
    Contributor help <3

--- packages/@react-types/calendar/src/index.d.ts
+++ packages/@react-types/calendar/src/index.d.ts
@@ -67,7 +67,7 @@ export interface CalendarPropsBase {
 
 export type DateRange = RangeValue<DateValue> | null;
 export interface CalendarProps<T extends DateValue | null> extends CalendarPropsBase, ValueBase<T | null, MappedDateValue<T>> {}
-export interface RangeCalendarProps<T extends DateValue | null> extends CalendarPropsBase, ValueBase<RangeValue<T> | null, RangeValue<MappedDateValue<T>>> {
+export interface RangeCalendarProps<T extends DateValue | null> extends CalendarPropsBase, ValueBase<RangeValue<T>> {
   /**
    * When combined with `isDateUnavailable`, determines whether non-contiguous ranges,
    * i.e. ranges containing unavailable dates, may be selected.
