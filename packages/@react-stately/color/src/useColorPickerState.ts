d4166499a5e0dc5517cc6a038e1495ee493f757f
Reid Barber <reid@reidbarber.com>
2024-05-17 14:44:44 -0500

    make color required

--- packages/@react-stately/color/src/useColorPickerState.ts
+++ packages/@react-stately/color/src/useColorPickerState.ts
@@ -4,13 +4,13 @@ import {useColor} from './useColor';
 import {useControlledState} from '@react-stately/utils';
 import {ValueBase} from '@react-types/shared';
 
-export interface ColorPickerProps extends ValueBase<string | Color | null, Color> {}
+export interface ColorPickerProps extends ValueBase<string | Color, Color> {}
 
 export interface ColorPickerState {
   /** The current color value of the color picker. */
   color: Color,
   /** Sets the current color value of the color picker. */
-  setColor(color: Color | null): void
+  setColor(color: Color): void
 }
 
 export function useColorPickerState(props: ColorPickerProps): ColorPickerState {
@@ -21,7 +21,9 @@ export function useColorPickerState(props: ColorPickerProps): ColorPickerState {
   return {
     color,
     setColor(color) {
-      setColor(color || parseColor('#000000'));
+      if (color != null) {
+        setColor(color || parseColor('#000000'));
+      }
     }
   };
 }
