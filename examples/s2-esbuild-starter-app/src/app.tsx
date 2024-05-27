ef933d8e6587bfd6136c961209b7a0665f1b103d
Rob Snow <rsnow@adobe.com>
2024-05-27 10:31:54 +1000

    Esbuild starter (#112)
    * Esbuild starter

---
+++ examples/s2-esbuild-starter-app/src/app.tsx
@@ -0,0 +1,20 @@
+
+import "@react/experimental-s2/page.css";
+import { style } from "@react/experimental-s2/style" with { type: "macro" };
+import { Button } from "@react/experimental-s2";
+
+function App() {
+  return (
+    <main>
+      <Button
+        styles={style({
+          marginStart: 16,
+        })}
+      >
+        Hello Spectrum 2!
+      </Button>
+    </main>
+  );
+}
+
+export default App;
