7e029cdf85bf3641730e5fd96ee35ee043fdd9c8
Tsvetomir Lazarov <cvetomir.lazarov@gmail.com>
2024-05-28 14:34:32 +0300

    Export `HiddenContext` from the utils in `react-aria-components`
    This will give user land components the opportunity to use the context to replace components like the Popover in the Select component without breaking the functionality of the Select.

--- packages/react-aria-components/src/index.ts
+++ packages/react-aria-components/src/index.ts
@@ -29,7 +29,7 @@ export {ColorSwatchPicker, ColorSwatchPickerItem, ColorSwatchPickerContext} from
 export {ColorThumb} from './ColorThumb';
 export {ColorWheel, ColorWheelTrack, ColorWheelTrackContext, ColorWheelStateContext} from './ColorWheel';
 export {ComboBox, ComboBoxContext, ComboBoxStateContext} from './ComboBox';
-export {composeRenderProps, DEFAULT_SLOT, Provider, useContextProps, useSlottedContext} from './utils';
+export {composeRenderProps, DEFAULT_SLOT, Provider, useContextProps, useSlottedContext, HiddenContext } from './utils';
 export {DateField, DateInput, DateSegment, TimeField, DateFieldContext, TimeFieldContext, DateFieldStateContext, TimeFieldStateContext} from './DateField';
 export {DatePicker, DateRangePicker, DatePickerContext, DateRangePickerContext, DatePickerStateContext, DateRangePickerStateContext} from './DatePicker';
 export {DialogTrigger, Dialog, DialogContext, OverlayTriggerStateContext} from './Dialog';
