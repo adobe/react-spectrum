167d94ca2286c6dd6a80a7da2b51a1ded51b17cd
Rob Snow <rsnow@adobe.com>
2024-06-05 21:50:03 +1000

    TS Strict Color Stately

--- packages/@react-stately/color/test/Color.test.tsx
+++ packages/@react-stately/color/test/Color.test.tsx
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import {ColorFormat} from '@react-types/color';
+import {ColorChannel, ColorFormat} from '@react-types/color';
 import fc from 'fast-check';
 import {getDeltaE00} from 'delta-e';
 import {parseColor} from '../src/Color';
@@ -202,11 +202,11 @@ describe('Color', function () {
 
 
     let rgb = fc.tuple(fc.integer({min: 0, max: 255}), fc.integer({min: 0, max: 255}), fc.integer({min: 0, max: 255}))
-      .map(([r, g, b]) => (['rgb', `rgb(${r}, ${g}, ${b})`, [r, g, b]]));
+      .map(([r, g, b]) => (['rgb' as ColorFormat, `rgb(${r}, ${g}, ${b})`, [r, g, b]]));
     let hsl = fc.tuple(fc.integer({min: 0, max: 360}), fc.integer({min: 0, max: 100}), fc.integer({min: 0, max: 100}))
-      .map(([h, s, l]) => (['hsl', `hsl(${h}, ${s}%, ${l}%)`, [h, s, l]]));
+      .map(([h, s, l]) => (['hsl' as ColorFormat, `hsl(${h}, ${s}%, ${l}%)`, [h, s, l]]));
     let hsb = fc.tuple(fc.integer({min: 0, max: 360}), fc.integer({min: 0, max: 100}), fc.integer({min: 0, max: 100}))
-      .map(([h, s, b]) => (['hsb', `hsb(${h}, ${s}%, ${b}%)`, [h, s, b]]));
+      .map(([h, s, b]) => (['hsb' as ColorFormat, `hsb(${h}, ${s}%, ${b}%)`, [h, s, b]]));
     let options = fc.record({
       colorSpace: fc.oneof(fc.constant('rgb'), fc.constant('hsl'), fc.constant('hsb')),
       color: fc.oneof(rgb, hsl, hsb)
@@ -232,6 +232,7 @@ describe('Color', function () {
     };
 
     it('can perform round trips', () => {
+      // @ts-ignore
       fc.assert(fc.property(options, ({colorSpace, color}: {colorSpace: ColorFormat, color: [string, string, number[]]}) => {
         let testColor = parseColor(color[1]);
         let convertedColor = testColor.toString(colorSpace);
