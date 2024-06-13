d9f8a2b387478551d7ad3aeaadb67f977decbae9
Rob Snow <rsnow@adobe.com>
2024-06-13 17:22:19 +1000

    fix test

--- packages/@react-spectrum/card/test/CardView.test.js
+++ packages/@react-spectrum/card/test/CardView.test.js
@@ -1199,12 +1199,7 @@ describe('CardView', function () {
       let grid = tree.getByRole('grid');
       grid.scrollTop = 3000;
       fireEvent.scroll(grid);
-      let isReact19 = parseInt(React.version, 10) >= 19;
-      if (isReact19) {
-        expect(onLoadMore).toHaveBeenCalledTimes(2);
-      } else {
-        expect(onLoadMore).toHaveBeenCalledTimes(1);
-      }
+      expect(onLoadMore).toHaveBeenCalledTimes(1);
     });
 
     it.each`
