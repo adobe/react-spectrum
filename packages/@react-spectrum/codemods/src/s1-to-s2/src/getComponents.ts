16b6407c51a047e9381c91f797ed297ebc6859a2
Reid Barber <reid@reidbarber.com>
2024-08-06 10:32:50 -0500

    update component index path

--- packages/@react-spectrum/codemods/src/s1-to-s2/src/getComponents.ts
+++ packages/@react-spectrum/codemods/src/s1-to-s2/src/getComponents.ts
@@ -6,7 +6,7 @@ import traverse from '@babel/traverse';
 export function getComponents(): Set<string> {
   // Determine list of available components in S2 from index.ts
   let availableComponents = new Set<string>();
-  const indexPath = process.env.NODE_ENV === 'test' ? path.resolve(__dirname, '../../../../@react-spectrum/s2/src/index.ts') : require.resolve('@react-spectrum/s2/src/index.ts');
+  const indexPath = process.env.NODE_ENV === 'test' ? path.resolve(__dirname, '../../../../../@react-spectrum/s2/src/index.ts') : require.resolve('@react-spectrum/s2/src/index.ts');
   let index = parse(readFileSync(indexPath, 'utf8'), {sourceType: 'module', plugins: ['typescript']});
   traverse(index, {
     ExportNamedDeclaration(path) {
