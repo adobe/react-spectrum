178130c21ec0764ba0bd5f85daa649fe58664052
Reid Barber <reid@reidbarber.com>
2024-08-08 17:22:25 -0500

    Revert "try renaming transformer functions"
    This reverts commit b2177f7621a8067a2539b8bdcc13ab717da38e42.

--- packages/@react-spectrum/codemods/src/s1-to-s2/src/codemods/codemod.ts
+++ packages/@react-spectrum/codemods/src/s1-to-s2/src/codemods/codemod.ts
@@ -29,7 +29,7 @@ interface Options {
   components?: string
 }
 
-export default function transformerS1ToS2(file: FileInfo, api: API, options: Options) {
+export default function transformer(file: FileInfo, api: API, options: Options) {
   let j = api.jscodeshift;
   let root = j(file.source);
   let componentsToTransform = options.components ? new Set(options.components.split(',').filter(s => availableComponents.has(s))) : availableComponents;
@@ -220,4 +220,4 @@ export default function transformerS1ToS2(file: FileInfo, api: API, options: Opt
   return root.toSource();
 }
 
-transformerS1ToS2.parser = 'tsx';
+transformer.parser = 'tsx';
