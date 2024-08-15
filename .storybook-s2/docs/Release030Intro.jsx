495538aa358f2d038b0027030aeccccfbd9b4436
Daniel Lu <dl1644@gmail.com>
2024-08-15 16:57:18 -0700

    some small edits

--- .storybook-s2/docs/Release030Intro.jsx
+++ .storybook-s2/docs/Release030Intro.jsx
@@ -7,15 +7,15 @@ export function Release030Intro() {
   return (
     <>
       <P>
-        With this end of summer release we've been busy adding a number of new components such as NumberField, Tabs, AlertDialog, TagGroup, and AvatarGroup.
+        With this end of summer release we've been busy adding a number of new components such as AlertDialog, AvatarGroup, NumberField, Tabs, and TagGroup.
         If you're using a previous version of Spectrum 2, please update your dependencies to <Code>@react-spectrum/s2@^0.3.0</Code>.
         In your code you'll need to update your imports to the new package name:
       </P>
-      <Pre>{highlight(`import {...} from '@react-spectrum/alertdialog';`)}</Pre>
+      <Pre>{highlight(`import {...} from '@react-spectrum/s2';`)}</Pre>
       <P>and in your package.json:</P>
       <Pre>{highlight(`"@react-spectrum/s2": "^0.3.0"`)}</Pre>
 
-      <P>We've added new documentation that shows the included <Link href="/?path=/docs/workflow-icons--docs">workflow icons</Link> and <Link href="/?path=/docs/illustrations--docs">illustrations</Link>. If your project has custom icons or illustrations, you can use the <Link href="">CLI tool</Link>.</P>
+      <P>We've also added new documentation for Spectrum 2 <Link href="/?path=/docs/workflow-icons--docs">workflow icons</Link> and <Link href="/?path=/docs/illustrations--docs">illustrations</Link>. If your project has custom icons or illustrations, you can use the <Link href="">CLI tool</Link>.</P>
 
       <P>To help teams migrate their projects to Spectrum 2, we've added a codemod to help update to the new components. Please read the <Link href="/?path=/docs/migrating--docs">migration documentation</Link> for more information.</P>
     </>
