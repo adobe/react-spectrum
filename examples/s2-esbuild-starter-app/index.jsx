0c4f7d59a487b26670258e67be6a560ea94bea20
Rob Snow <rsnow@adobe.com>
2024-08-20 08:38:37 +1000

    Esbuild testing

--- examples/s2-esbuild-starter-app/index.jsx
+++ examples/s2-esbuild-starter-app/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from "react-dom";
 import App from './src/app';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />)
