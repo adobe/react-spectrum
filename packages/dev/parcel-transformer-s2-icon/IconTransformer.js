faf0e18467231422cb7a06eb92a74d04e271f1e8
Devon Govett <devongovett@gmail.com>
2024-08-19 15:54:36 -0700

    August release (#6905)

--- packages/dev/parcel-transformer-s2-icon/IconTransformer.js
+++ packages/dev/parcel-transformer-s2-icon/IconTransformer.js
@@ -80,10 +80,11 @@ function template(asset, iconName, svg) {
   if (/^[0-9]/.test(importName)) {
     iconRename = '_' + importName;
   }
-  let context = asset.pipeline === 'illustration' ? 'IllustrationContext' : 'IconContext';
+  let normalizedPath = asset.filePath.replaceAll('\\', '/');
+  let context = asset.pipeline === 'illustration' || normalizedPath.includes('@react-spectrum/s2/spectrum-illustrations') ? 'IllustrationContext' : 'IconContext';
   return (
 `
-import {IconProps, ${context}, IconContextValue} from '${asset.filePath.includes('@react-spectrum/s2') ? '~/src/Icon' : '@react-spectrum/s2'}';
+import {IconProps, ${context}, IconContextValue} from '${normalizedPath.includes('@react-spectrum/s2') ? '~/src/Icon' : '@react-spectrum/s2'}';
 import {SVGProps, useRef} from 'react';
 import {useContextProps} from 'react-aria-components';
 
