ad6d1e7ff0803c7b9d5dc66ec6600d077f08da62
Reid Barber <reid@reidbarber.com>
2024-09-16 15:00:17 -0500

    Revert "remove @ts-ignore"
    This reverts commit 88c1604c54c787c69a7d69b350ef556b38e96c6a.

--- packages/@react-aria/disclosure/src/useDisclosure.ts
+++ packages/@react-aria/disclosure/src/useDisclosure.ts
@@ -49,6 +49,7 @@ export function useDisclosure(props: AriaDisclosureProps, state: DisclosureState
     // Until React supports hidden="until-found": https://github.com/facebook/react/pull/24741
     if (supportsBeforeMatch && contentRef.current && !isControlled && !isDisabled) {
       if (state.isExpanded) {
+        // @ts-ignore
         contentRef.current.hidden = undefined;
       } else {
         // @ts-ignore
