d57bd8d906ec8f943285798269bbbfd84d2da3eb
Reid Barber <reid@reidbarber.com>
2024-09-26 18:24:16 -0500

    fix+tests: fix Disclosure bugs and add tests (#7096)
    * add disclosure tests
    
    * lint/cleanup
    
    * Pass isExpanded to useDisclosure as well
    
    ---------
    
    Co-authored-by: Robert Snow <rsnow@adobe.com>
    Co-authored-by: Devon Govett <devongovett@gmail.com>

--- packages/@react-aria/disclosure/src/useDisclosure.ts
+++ packages/@react-aria/disclosure/src/useDisclosure.ts
@@ -51,17 +51,15 @@ export function useDisclosure(props: AriaDisclosureProps, state: DisclosureState
   let supportsBeforeMatch = !isSSR && 'onbeforematch' in document.body;
 
   // @ts-ignore https://github.com/facebook/react/pull/24741
-  useEvent(ref, 'beforematch', supportsBeforeMatch ? () => state.expand() : null);
+  useEvent(ref, 'beforematch', supportsBeforeMatch && !isControlled ? () => state.expand() : null);
 
   useEffect(() => {
     // Until React supports hidden="until-found": https://github.com/facebook/react/pull/24741
     if (supportsBeforeMatch && ref?.current && !isControlled && !isDisabled) {
       if (state.isExpanded) {
-        // @ts-ignore
-        ref.current.hidden = undefined;
+        ref.current.removeAttribute('hidden');
       } else {
-        // @ts-ignore
-        ref.current.hidden = 'until-found';
+        ref.current.setAttribute('hidden', 'until-found');
       }
     }
   }, [isControlled, ref, props.isExpanded, state, supportsBeforeMatch, isDisabled]);
@@ -72,7 +70,7 @@ export function useDisclosure(props: AriaDisclosureProps, state: DisclosureState
       'aria-expanded': state.isExpanded,
       'aria-controls': contentId,
       onPress: (e) => {
-        if (e.pointerType !== 'keyboard') {
+        if (!isDisabled && e.pointerType !== 'keyboard') {
           state.toggle();
         }
       },
