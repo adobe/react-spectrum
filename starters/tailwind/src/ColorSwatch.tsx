a79adcf0f9d3f94c8836a71231af21302415d93e
Devon Govett <devongovett@gmail.com>
2024-09-30 13:54:30 -0700

    Add color components to tailwind starter

---
+++ starters/tailwind/src/ColorSwatch.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ColorSwatch as AriaColorSwatch, ColorSwatchProps} from 'react-aria-components';
+import { composeTailwindRenderProps } from './utils';
+
+export function ColorSwatch(props: ColorSwatchProps) {
+  return (
+    <AriaColorSwatch 
+      {...props}
+      className={composeTailwindRenderProps(props.className, 'w-8 h-8 rounded border border-black/10')}
+      style={({color}) => ({
+        background: `linear-gradient(${color}, ${color}),
+          repeating-conic-gradient(#CCC 0% 25%, white 0% 50%) 50% / 16px 16px`
+      })} />
+  );
+}
