a147e96f5a4b67bde4a6e5ec67811eb83949a11b
Daniel Lu <dl1644@gmail.com>
2024-10-01 13:12:33 -0700

    test chromatic font loader

--- .chromatic/preview.js
+++ .chromatic/preview.js
@@ -2,7 +2,7 @@ import {configureActions} from '@storybook/addon-actions';
 import React from 'react';
 import {VerticalCenter} from './layout';
 import {withChromaticProvider} from './custom-addons/chromatic';
-
+import isChromatic from 'chromatic/isChromatic';
 
 // decorator order matters, the last one will be the outer most
 
@@ -29,3 +29,24 @@ export const decorators = [
   ),
   withChromaticProvider
 ];
+
+// Use the document.fonts API to check if fonts have loaded
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/fonts API to
+const fontLoader = async () => {
+  console.log('gawegaweg')
+  return {
+    fonts: await document.fonts.ready,
+  }
+};
+
+/* 👇 It's configured as a global loader
+ * See https://storybook.js.org/docs/writing-stories/loaders
+ * to learn more about loaders
+ */
+const loaders = isChromatic() && document.fonts ? [fontLoader] : [];
+
+const preview = {
+  loaders: [fontLoader]
+};
+
+export default preview;
