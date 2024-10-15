9f095b5b2ddca92a4d445712c756fa915e2e97f4
GitHub <snowystinger@gmail.com>
2024-10-16 08:56:19 +1100

    remove extraneous comment

--- packages/@react-aria/select/src/HiddenSelect.tsx
+++ packages/@react-aria/select/src/HiddenSelect.tsx
@@ -81,18 +81,6 @@ export function useHiddenSelect<T>(props: AriaHiddenSelectOptions, state: Select
   // The solution is to use <VisuallyHidden> to hide the elements, which clips the elements to a
   // 1px rectangle. In addition, we hide from screen readers with aria-hidden, and make the <select>
   // non tabbable with tabIndex={-1}.
-  //
-  // In mobile browsers, there are next/previous buttons above the software keyboard for navigating
-  // between fields in a form. These only support native form inputs that are tabbable. In order to
-  // support those, an additional hidden input is used to marshall focus to the button. It is tabbable
-  // except when the button is focused, so that shift tab works properly to go to the actual previous
-  // input in the form. Using the <select> for this also works, but Safari on iOS briefly flashes
-  // the native menu on focus, so this isn't ideal. A font-size of 16px or greater is required to
-  // prevent Safari from zooming in on the input when it is focused.
-  //
-  // If the current interaction modality is null, then the user hasn't interacted with the page yet.
-  // In this case, we set the tabIndex to -1 on the input element so that automated accessibility
-  // checkers don't throw false-positives about focusable elements inside an aria-hidden parent.
   return {
     containerProps: {
       ...visuallyHiddenProps,
