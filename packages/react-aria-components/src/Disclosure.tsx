4f85a359f81eb4fe00ac463d76265c3cd4409096
Reid Barber <reid@reidbarber.com>
2024-10-21 14:57:14 -0500

    fix: Accordion/Disclosure release prep (#7216)
    * RAC Disclosure SSR tests
    
    * tests for nested disclosures and interactive adjacent elements
    
    * DisclosurePanel: add DOMProps and render props type/table
    
    * update v3 stories and chromatic
    
    * update v3 Accordion deps
    
    * add v3 Accordion SSR test
    
    * update DisclosureGroupProps type to remove collection in jsdoc
    
    * more v3 accordion tests
    
    * yarn.lock
    
    * lint
    
    * add isQuiet to v3 Accordion/Disclosure
    
    * add focus ring style
    
    * update stories/chromatic
    
    * lint
    
    * remove active outline
    
    * lint
    
    * add isQuiet to v3 docs

--- packages/react-aria-components/src/Disclosure.tsx
+++ packages/react-aria-components/src/Disclosure.tsx
@@ -181,7 +181,15 @@ function Disclosure(props: DisclosureProps, ref: ForwardedRef<HTMLDivElement>) {
   );
 }
 
-export interface DisclosurePanelProps extends RenderProps<{}> {
+export interface DisclosurePanelRenderProps {
+  /**
+   * Whether keyboard focus is within the disclosure panel.
+   * @selector [data-focus-visible-within]
+   */
+  isFocusVisibleWithin: boolean
+}
+
+export interface DisclosurePanelProps extends RenderProps<DisclosurePanelRenderProps>, DOMProps {
   /**
    * The accessibility role for the disclosure's panel.
    * @default 'group'
@@ -207,8 +215,10 @@ function DisclosurePanel(props: DisclosurePanelProps, ref: ForwardedRef<HTMLDivE
       isFocusVisibleWithin
     }
   });
+  let DOMProps = filterDOMProps(props);
   return (
     <div
+      {...DOMProps}
       ref={mergeRefs(ref, panelRef)}
       {...mergeProps(panelProps, focusWithinProps)}
       {...renderProps}
