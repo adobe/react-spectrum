7a376e37bceee1f00ef20f739fe6f406fbfeff74
Devon Govett <devongovett@gmail.com>
2024-10-28 17:42:28 -0700

    Update webpack and vite configs to be more optimized

--- examples/s2-vite-project/src/App.tsx
+++ examples/s2-vite-project/src/App.tsx
@@ -10,20 +10,122 @@
  * governing permissions and limitations under the License.
  */
 
+import React, { useState } from "react";
 import "@react-spectrum/s2/page.css";
+import {
+  ActionButton,
+  ActionMenu,
+  Button,
+  ButtonGroup,
+  Divider,
+  Heading,
+  LinkButton,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  SubmenuTrigger,
+  Text,
+  ToggleButton,
+} from "@react-spectrum/s2";
+import Edit from "@react-spectrum/s2/icons/Edit";
+import Section from "./Section";
 import { style } from "@react-spectrum/s2/style" with { type: "macro" };
-import { Button } from "@react-spectrum/s2";
+
+const Lazy = React.lazy(() => import('./Lazy'));
 
 function App() {
+  let [isLazyLoaded, setLazyLoaded] = useState(false);
   return (
     <main>
-      <Button
-        styles={style({
-          marginStart: 16,
+      <Heading
+        styles={style({ font: "heading-xl", textAlign: "center" })}
+        level={1}
+      >
+        Spectrum 2 + Webpack
+      </Heading>
+      <div
+        className={style({
+          maxWidth: 288,
+          margin: "auto",
         })}
       >
-        Hello Spectrum 2!
-      </Button>
+        <Divider />
+      </div>
+      <div
+        className={style({
+          display: "flex",
+          flexDirection: "column",
+          gap: 16,
+          alignItems: "center"
+        })}
+      >
+        <Section title="Buttons">
+          <ButtonGroup>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <ActionButton>
+              <Edit />
+              <Text>Action Button</Text>
+            </ActionButton>
+            <ToggleButton>Toggle Button</ToggleButton>
+            <LinkButton
+              variant="primary"
+              href="https://adobe.com"
+              target="_blank"
+            >
+              Link Button
+            </LinkButton>
+          </ButtonGroup>
+        </Section>
+
+        <Section title="Collections">
+          <ActionMenu>
+            <MenuItem>Action Menu Item 1</MenuItem>
+            <MenuItem>Action Menu Item 2</MenuItem>
+            <MenuItem>Action Menu Item 3</MenuItem>
+          </ActionMenu>
+          <MenuTrigger>
+            <ActionButton>Menu</ActionButton>
+            <Menu onAction={(key) => alert(key.toString())}>
+              <MenuItem id="cut">Cut</MenuItem>
+              <MenuItem id="copy">Copy</MenuItem>
+              <MenuItem id="paste">Paste</MenuItem>
+              <MenuItem id="replace">Replace</MenuItem>
+              <SubmenuTrigger>
+                <MenuItem id="share">Share</MenuItem>
+                <Menu onAction={(key) => alert(key.toString())}>
+                  <MenuItem id="copy-ink">Copy Link</MenuItem>
+                  <SubmenuTrigger>
+                    <MenuItem id="email">Email</MenuItem>
+                    <Menu onAction={(key) => alert(key.toString())}>
+                      <MenuItem id="attachment">Email as Attachment</MenuItem>
+                      <MenuItem id="link">Email as Link</MenuItem>
+                    </Menu>
+                  </SubmenuTrigger>
+                  <MenuItem id="sms">SMS</MenuItem>
+                </Menu>
+              </SubmenuTrigger>
+              <MenuItem id="delete">Delete</MenuItem>
+            </Menu>
+          </MenuTrigger>
+          <MenuTrigger>
+            <ActionButton>Menu Trigger</ActionButton>
+            <Menu>
+              <MenuItem href="/foo">
+                Link to /foo
+              </MenuItem>
+              <MenuItem>Cut</MenuItem>
+              <MenuItem>Copy</MenuItem>
+              <MenuItem>Paste</MenuItem>
+            </Menu>
+          </MenuTrigger>
+        </Section>
+
+        {!isLazyLoaded && <ActionButton onPress={() => setLazyLoaded(true)}>Load more</ActionButton>}
+        {isLazyLoaded && <React.Suspense fallback={<>Loading</>}>
+          <Lazy />
+        </React.Suspense>}
+      </div>
     </main>
   );
 }
