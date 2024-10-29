01c439a4aeca5d5015775e7b51ffdb9eb1482bd8
GitHub <snowystinger@gmail.com>
2024-10-29 14:17:12 -0500

    fix lint

--- packages/react-aria-components/src/Tabs.tsx
+++ packages/react-aria-components/src/Tabs.tsx
@@ -18,7 +18,7 @@ import {ContextValue, Provider, RenderProps, SlotProps, StyleRenderProps, useCon
 import {filterDOMProps, useObjectRef} from '@react-aria/utils';
 import {Collection as ICollection, Node, TabListState, useTabListState} from 'react-stately';
 import React, {createContext, ForwardedRef, forwardRef, JSX, useContext, useMemo} from 'react';
-import { useFocusable } from '@react-aria/focus';
+import {useFocusable} from '@react-aria/focus';
 
 export interface TabsProps extends Omit<AriaTabListProps<any>, 'items' | 'children'>, RenderProps<TabsRenderProps>, SlotProps {}
 
--- packages/react-aria-components/src/Tooltip.tsx
+++ packages/react-aria-components/src/Tooltip.tsx
@@ -20,7 +20,7 @@ import {OverlayArrowContext} from './OverlayArrow';
 import {OverlayTriggerProps, TooltipTriggerProps, TooltipTriggerState, useTooltipTriggerState} from 'react-stately';
 import React, {createContext, ForwardedRef, forwardRef, ReactNode, useContext, useRef, useState} from 'react';
 import {useLayoutEffect} from '@react-aria/utils';
-import { useObjectRef } from '../../@react-aria/utils';
+import {useObjectRef} from '../../@react-aria/utils';
 
 export interface TooltipTriggerComponentProps extends TooltipTriggerProps {
   children: ReactNode
