7c5bb7d356af328de5fad07db581c19dad905b9e
Kyle Taborski <ktabors@yahoo.com>
2024-10-30 12:47:52 -0700

    adding description and illustration (instead of icon)

--- packages/@react-spectrum/s2/stories/SelectBoxGroup.stories.tsx
+++ packages/@react-spectrum/s2/stories/SelectBoxGroup.stories.tsx
@@ -1,23 +1,17 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
+/*
+ * Copyright 2024 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright 2024 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- **************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-import Bell from '../s2wf-icons/S2_Icon_Bell_20_N.svg';
-import Heart from '../s2wf-icons/S2_Icon_Heart_20_N.svg';
 import type {Meta, StoryObj} from '@storybook/react';
+import Server from '../spectrum-illustrations/linear/Server';
 import {SelectBox, SelectBoxGroup, Text} from '../src';
 
 const meta: Meta<typeof SelectBoxGroup> = {
@@ -49,12 +43,13 @@ export const Example: Story = {
     return (
       <SelectBoxGroup {...args}>
         <SelectBox value="Bells">
-          <Bell />
+          <Server />
           <Text slot="label">Bells</Text>
         </SelectBox>
         <SelectBox value="Heart">
-          <Heart />
-          <Text slot="label">Heart</Text>
+          <Server />
+          <Text slot="label">Select box label</Text>
+          <Text slot="description">A description that explains more context and details, to supplement the message of the label.</Text>
         </SelectBox>
       </SelectBoxGroup>
     );
