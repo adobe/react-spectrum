35d39509555845274667b16e76e72e12c9a07c4e
GitHub <snowystinger@gmail.com>
2024-11-15 16:09:21 +1100

    fix types for tailwind

--- starters/tailwind/src/ListBox.tsx
+++ starters/tailwind/src/ListBox.tsx
@@ -90,7 +90,7 @@ export function DropdownItem(props: ListBoxItemProps) {
 
 export interface DropdownSectionProps<T> extends SectionProps<T> {
   title?: string
-  items: any
+  items?: any
 }
 
 export function DropdownSection<T extends object>(props: DropdownSectionProps<T>) {
