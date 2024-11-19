b3a4d6c1134aae882aa1dcfce64efba1d8f4308d
Alec Mev <alec@mev.earth>
2024-11-19 01:24:25 +0000

    fix: add optional property types (#6872)
    * Fix optional property types
    
    * Use vanilla Intl.ResolvedDateTimeFormatOptions
    
    * Define DOMProps via HTMLAttributes
    
    * add id back into the docs
    
    * fix lint
    
    ---------
    
    Co-authored-by: Robert Snow <rsnow@adobe.com>
    Co-authored-by: Yihui Liao <44729383+yihuiliao@users.noreply.github.com>

--- packages/@react-types/shared/src/dom.d.ts
+++ packages/@react-types/shared/src/dom.d.ts
@@ -59,7 +59,7 @@ export interface DOMProps {
   /**
    * The element's unique identifier. See [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id).
    */
-  id?: string
+  id?: string | undefined
 }
 
 export interface FocusableDOMProps extends DOMProps {
