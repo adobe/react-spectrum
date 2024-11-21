e729223b010960708badb8e54032a6fe277d168a
Robert Snow <rsnow@adobe.com>
2024-11-21 11:53:23 +1100

    Update packages/@react-aria/focus/src/FocusScope.tsx
    Co-authored-by: Devon Govett <devongovett@gmail.com>

--- packages/@react-aria/focus/src/FocusScope.tsx
+++ packages/@react-aria/focus/src/FocusScope.tsx
@@ -692,7 +692,6 @@ function useRestoreFocus(scopeRef: RefObject<Element[] | null>, restoreFocus?: b
         restoreFocus
         && nodeToRestore
         && (
-           
           ((ownerDocument.activeElement && isElementInChildScope(ownerDocument.activeElement, scopeRef)) || (ownerDocument.activeElement === ownerDocument.body && shouldRestoreFocus(scopeRef)))
         )
       ) {
