5757c3a1cecb77d9e5c2d40b74f55eb353dbd7d6
Kyle Taborski <ktabors@yahoo.com>
2024-12-04 14:12:17 -0800

    fix: S2 examples Popover and add CustomDialog (#7459)
    * fix: using Popover and s2-parcel example has meta veiwport defined
    
    * adding CustomDialog example
    
    * vw not vh

--- examples/s2-parcel-example/src/Lazy.js
+++ examples/s2-parcel-example/src/Lazy.js
@@ -13,6 +13,7 @@ import {
   ButtonGroup,
   Checkbox,
   CheckboxGroup,
+  CloseButton,
   ColorArea,
   ColorField,
   ColorSlider,
@@ -23,6 +24,7 @@ import {
   ComboBoxItem,
   Content,
   ContextualHelp,
+  CustomDialog,
   Dialog,
   DialogContainer,
   DialogTrigger,
@@ -44,6 +46,7 @@ import {
   NumberField,
   Picker,
   PickerItem,
+  Popover,
   ProgressBar,
   ProgressCircle,
   Radio,
@@ -67,6 +70,7 @@ import {
   Tooltip,
   TooltipTrigger,
 } from "@react-spectrum/s2";
+import Checkmark from '@react-spectrum/s2/illustrations/gradient/generic1/Checkmark';
 import Cloud from "@react-spectrum/s2/illustrations/linear/Cloud";
 import DropToUpload from "@react-spectrum/s2/illustrations/linear/DropToUpload";
 import Edit from "@react-spectrum/s2/icons/Edit";
@@ -247,15 +251,27 @@ export default function Lazy() {
           </Dialog>
         </DialogTrigger>
 
-        <DialogTrigger type="popover">
-          <ActionButton>Disk Status</ActionButton>
-          <Dialog>
-            <Heading>C://</Heading>
+        <DialogTrigger>
+          <ActionButton>Illustration</ActionButton>
+          <CustomDialog size="M">
+            <div className={style({display: 'flex', flexDirection: 'column', rowGap: 8, alignItems: 'center'})}>
+              <Checkmark />
+              <Heading slot="title" styles={style({font: 'heading-lg', textAlign: 'center', marginY: 0})}>Thank you!</Heading>
+              <p className={style({font: 'body', textAlign: 'center', marginY: 0})}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <CloseButton styles={style({position: 'absolute', top: 12, insetEnd: 12})} />
+            </div>
+          </CustomDialog>
+        </DialogTrigger>
 
-            <Content>
+        <DialogTrigger>
+          <ActionButton>Disk Status</ActionButton>
+          <Popover>
+            <Heading styles={style({font: 'heading', marginTop: 0, marginBottom: 20})}>C://</Heading>
+
+            <Content styles={style({font: 'ui'})}>
               <Text>50% disk space remaining.</Text>
             </Content>
-          </Dialog>
+          </Popover>
         </DialogTrigger>
 
         <TooltipTrigger>
