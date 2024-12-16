5cc25e8ad44e5dfe927121a39973ba264b1f7017
chirokas <157580465+chirokas@users.noreply.github.com>
2024-12-16 12:13:49 +0000

    Fix type

--- packages/@react-aria/utils/src/inertValue.ts
+++ packages/@react-aria/utils/src/inertValue.ts
@@ -1,10 +1,10 @@
 import {version} from 'react';
 
-export function inertValue(value: any) {
+export function inertValue(value?: boolean) {
   const pieces = version.split('.');
   const major = parseInt(pieces[0], 10);
   if (major >= 19) {
-    return !!value;
+    return value;
   }
   // compatibility with React < 19
   return value ? 'true' : undefined;
