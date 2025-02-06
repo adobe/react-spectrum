9285000bae5f85091d723d5698299166ce4e4af5
Devon Govett <devongovett@gmail.com>
2025-02-05 21:35:43 -0500

    fix waterfall layout options

--- packages/@react-stately/layout/src/index.ts
+++ packages/@react-stately/layout/src/index.ts
@@ -12,6 +12,7 @@
 export type {GridLayoutOptions} from './GridLayout';
 export type {ListLayoutOptions, LayoutNode} from './ListLayout';
 export type {TableLayoutProps} from './TableLayout';
+export type {WaterfallLayoutOptions} from './WaterfallLayout';
 export {GridLayout} from './GridLayout';
 export {ListLayout} from './ListLayout';
 export {TableLayout} from './TableLayout';
