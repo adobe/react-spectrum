fc3c18bf8b07aa1a4c1e7a51ec9c490a29621232
GitHub <snowystinger@gmail.com>
2025-02-20 14:49:38 +1100

    fix lint and add interfaces

--- examples/rsp-next-ts/pages/index.tsx
+++ examples/rsp-next-ts/pages/index.tsx
@@ -249,46 +249,46 @@ export default function Home() {
             </TableView>
             <AutocompleteExample />
 
-          <div style={{width: '300px', height: '150px', overflow: 'auto'}}>
-            <TreeView disabledKeys={['projects-1']} aria-label="test static tree">
-              <TreeViewItem id="Photos" textValue="Photos">
-                <TreeViewItemContent>
-                  <Text>Photos</Text>
-                  <Folder />
-                </TreeViewItemContent>
-              </TreeViewItem>
-              <TreeViewItem id="projects" textValue="Projects">
-                <TreeViewItemContent>
-                  <Text>Projects</Text>
-                  <Folder />
-                </TreeViewItemContent>
-                <TreeViewItem id="projects-1" textValue="Projects-1">
+            <div style={{width: '300px', height: '150px', overflow: 'auto'}}>
+              <TreeView disabledKeys={['projects-1']} aria-label="test static tree">
+                <TreeViewItem id="Photos" textValue="Photos">
                   <TreeViewItemContent>
-                    <Text>Projects-1</Text>
+                    <Text>Photos</Text>
                     <Folder />
                   </TreeViewItemContent>
-                  <TreeViewItem id="projects-1A" textValue="Projects-1A">
+                </TreeViewItem>
+                <TreeViewItem id="projects" textValue="Projects">
+                  <TreeViewItemContent>
+                    <Text>Projects</Text>
+                    <Folder />
+                  </TreeViewItemContent>
+                  <TreeViewItem id="projects-1" textValue="Projects-1">
                     <TreeViewItemContent>
-                      <Text>Projects-1A</Text>
+                      <Text>Projects-1</Text>
+                      <Folder />
+                    </TreeViewItemContent>
+                    <TreeViewItem id="projects-1A" textValue="Projects-1A">
+                      <TreeViewItemContent>
+                        <Text>Projects-1A</Text>
+                        <FileTxt />
+                      </TreeViewItemContent>
+                    </TreeViewItem>
+                  </TreeViewItem>
+                  <TreeViewItem id="projects-2" textValue="Projects-2">
+                    <TreeViewItemContent>
+                      <Text>Projects-2</Text>
+                      <FileTxt />
+                    </TreeViewItemContent>
+                  </TreeViewItem>
+                  <TreeViewItem id="projects-3" textValue="Projects-3">
+                    <TreeViewItemContent>
+                      <Text>Projects-3</Text>
                       <FileTxt />
                     </TreeViewItemContent>
                   </TreeViewItem>
                 </TreeViewItem>
-                <TreeViewItem id="projects-2" textValue="Projects-2">
-                  <TreeViewItemContent>
-                    <Text>Projects-2</Text>
-                    <FileTxt />
-                  </TreeViewItemContent>
-                </TreeViewItem>
-                <TreeViewItem id="projects-3" textValue="Projects-3">
-                  <TreeViewItemContent>
-                    <Text>Projects-3</Text>
-                    <FileTxt />
-                  </TreeViewItemContent>
-                </TreeViewItem>
-              </TreeViewItem>
-            </TreeView>
-          </div>
+              </TreeView>
+            </div>
           </Section>
 
           <Section title="Color">
