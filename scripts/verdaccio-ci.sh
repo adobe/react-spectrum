f030ba2f5c149f1fb272f5543327759a38e82f1e
GitHub <snowystinger@gmail.com>
2025-02-27 13:36:14 +1100

    don't stop server on exit

--- scripts/verdaccio-ci.sh
+++ scripts/verdaccio-ci.sh
@@ -3,15 +3,6 @@
 port=4000
 output="output.out"
 
-function cleanup {
-  echo "Cleaning up"
-  # lsof doesn't work in circleci
-  netstat -tpln | awk -F'[[:space:]/:]+' '$5 == 4000 {print $(NF-2)}' | xargs kill
-}
-
-trap cleanup EXIT
-trap "exit 1" INT ERR
-
 set -e
 
 # Start verdaccio and send it to the background
