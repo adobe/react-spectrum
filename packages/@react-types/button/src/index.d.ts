fb3714d2ab66ef59c156166cac9492609843894c
Robert Snow <snowystinger@gmail.com>
2025-03-11 08:16:03 +1100

    Update packages/@react-types/button/src/index.d.ts

--- packages/@react-types/button/src/index.d.ts
+++ packages/@react-types/button/src/index.d.ts
@@ -99,7 +99,7 @@ export interface SpectrumButtonProps<T extends ElementType = 'button'> extends A
   /**
    * The `<form>` element to associate the button with.
    * The value of this attribute must be the id of a `<form>` in the same document.
-   * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#form
+   * See [form attribute](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#form).
    */
   form?: string
 }
