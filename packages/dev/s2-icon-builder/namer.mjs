40ece33e93cb4a4961c196a4a5b42ee2b53b2a4b
Robert Snow <snowystinger@gmail.com>
2025-04-24 10:49:49 -0700

    move some more things around

--- packages/dev/s2-icon-builder/namer.mjs
+++ packages/dev/s2-icon-builder/namer.mjs
@@ -6,7 +6,7 @@ export default new Namer({
     let mainAsset = bundle.getMainEntry();
     if (bundle.needsStableName && bundle.target.distEntry) {
       console.log(bundle.target.distEntry);
-      return false;
+      return bundle.target.distEntry;
     }
     let ext = '.' + bundle.type;
     if (bundle.type === 'js') {
@@ -18,10 +18,10 @@ export default new Namer({
     if (m) {
       if (originalExt === '.svg') {
         console.log(m[1] + '/internal/' + name + ext);
-        return false;
+        return m[1] + '/internal/' + name + ext;
       }
       console.log(m[1] + '/' + name + ext);
-      return false;
+      return m[1] + '/' + name + ext;
     }
     console.log(name
       .replace(/^S2_Icon_(.*?)(Size\d+)?_\d+(?:x\d+)?_N$/, '$1')
@@ -31,6 +31,13 @@ export default new Namer({
       })
       .replace(/\.module$/, '_module')
       + ext);
-    return false;
+    return name
+      .replace(/^S2_Icon_(.*?)(Size\d+)?_\d+(?:x\d+)?_N$/, '$1')
+      .replace(/^S2_(fill|lin)_(.+)_(generic\d)_(\d+)$/, (m, type, name, style) => {
+        name = name[0].toUpperCase() + name.slice(1).replace(/_/g, '');
+        return 'gradient/' + style + '/' + name;
+      })
+      .replace(/\.module$/, '_module')
+      + ext;
   }
 });
