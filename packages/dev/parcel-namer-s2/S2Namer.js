cc1608d45c2effc79c23dcb812b7417525cce827
Robert Snow <snowystinger@gmail.com>
2025-05-07 17:26:02 +1000

    try updating the namer

--- packages/dev/parcel-namer-s2/S2Namer.js
+++ packages/dev/parcel-namer-s2/S2Namer.js
@@ -16,6 +16,29 @@ const path = require('path');
 module.exports = new Namer({
   name({bundle}) {
     let mainAsset = bundle.getMainEntry();
+
+    console.log(mainAsset?.filePath);
+    if (!mainAsset?.filePath.includes('react-spectrum/packages')) {
+      if (bundle.needsStableName && bundle.target.distEntry) {
+        return bundle.target.distEntry;
+      }
+      let ext = '.' + bundle.type;
+      if (bundle.type === 'js') {
+        ext = bundle.env.outputFormat === 'esmodule' ? '.mjs' : '.cjs';
+      }
+      let originalExt = path.extname(mainAsset.filePath);
+      let name = path.basename(mainAsset.filePath, originalExt).replace(/\*/g, 'intlStrings');
+
+      return name
+        .replace(/^S2_Icon_(.*?)(Size\d+)?_\d+(?:x\d+)?_N$/, '$1')
+        .replace(/^S2_(fill|lin)_(.+)_(generic\d)_(\d+)$/, (m, type, name, style) => {
+          name = name[0].toUpperCase() + name.slice(1).replace(/_/g, '');
+          return 'gradient/' + style + '/' + name;
+        })
+        .replace(/\.module$/, '_module')
+        + ext;
+    }
+
     if (mainAsset?.filePath.includes('@react-spectrum/s2')) {
       if (bundle.needsStableName && bundle.target.distEntry) {
         return bundle.target.distEntry;
