a9bb8ea0da29792985a770deeca266f8726b2405
Robert Snow <snowystinger@gmail.com>
2025-05-09 09:56:46 +1000

    fix version number

--- scripts/verdaccio-build-icon-builder.sh
+++ scripts/verdaccio-build-icon-builder.sh
@@ -65,7 +65,7 @@ cat > icon-library-test/package.json << EOF
     "react-dom": "^18.0.0 || ^19.0.0-rc.1"
   },
   "devDependencies": {
-    "@react-spectrum/s2-icon-builder": "^1.0.0",
+    "@react-spectrum/s2-icon-builder": ">=0.2.3",
     "@react-spectrum/s2": "latest",
     "react": "^19.0.0",
     "react-dom": "^19.0.0"
