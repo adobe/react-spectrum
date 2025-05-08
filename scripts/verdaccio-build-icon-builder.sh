fd8932b9b2d9105955ddf32a68c2617f05f293e2
Robert Snow <snowystinger@gmail.com>
2025-05-08 12:19:02 +1000

    Add real application to test icon builder

--- scripts/verdaccio-build-icon-builder.sh
+++ scripts/verdaccio-build-icon-builder.sh
@@ -17,6 +17,11 @@ else
     echo "Verdaccio is NOT running on port 4000."
 fi
 
+# Rename the dist folder from dist/production/docs to verdaccio_dist/COMMIT_HASH_BEFORE_PUBLISH/verdaccio/docs
+# This is so we can have verdaccio build in a separate stream from deploy and deploy_prod
+verdaccio_path=verdaccio_dist/`git rev-parse HEAD~0`/verdaccio
+mkdir -p $verdaccio_path
+
 yarn config set npmPublishRegistry --home $registry
 yarn config set npmRegistryServer --home $registry
 yarn config set npmAlwaysAuth --home false
@@ -39,7 +44,7 @@ mkdir icon-library-test
 touch icon-library-test/package.json
 cat > icon-library-test/package.json << EOF
 {
-  "name": "icon-library-test",
+  "name": "@react-spectrum/icon-library-test",
   "version": "1.0.0",
   "exports": {
     "./*": {
@@ -71,12 +76,33 @@ touch icon-library-test/yarn.lock
 cp ../../packages/@react-spectrum/s2/s2wf-icons/S2_Icon_3D_20_N.svg icon-library-test/src/S2_Icon_3D_20_N.svg
 cp ../../packages/@react-spectrum/s2/s2wf-icons/S2_Icon_AlignRight_20_N.svg icon-library-test/src/S2_Icon_AlignRight_20_N.svg
 cd icon-library-test
-ls src
+echo "Installing and building icon library"
 yarn install --no-immutable
 yarn transform-icons -i './src/*.svg' -o ./ --isLibrary
 
 ls .
 
-cd ..
+yarn config set npmPublishRegistry $registry
+yarn config set npmRegistryServer $registry
+yarn config set npmAlwaysAuth false
+yarn config set npmAuthToken abc
+yarn config set unsafeHttpWhitelist localhost
+npm set registry $registry
+
+git config --global user.email octobot@github.com
+git config --global user.name GitHub Actions
+
+# Publish icon package to verdaccio
+echo "Publishing icon package to verdaccio"
+yarn npm publish --tag latest
+
+echo "Building icon builder fixture"
+cd ../../../scripts/icon-builder-fixture
+yarn install --no-immutable
+yarn build --public-url ./
+
+echo "Moving icon builder fixture to verdaccio"
+mv dist ../../$verdaccio_path/icon-builder-fixture
+
 
 netstat -tpln | awk -F'[[:space:]/:]+' '$5 == 4000 {print $(NF-2)}' | xargs kill
