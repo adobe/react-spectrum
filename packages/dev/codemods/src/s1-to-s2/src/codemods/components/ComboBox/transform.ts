66986b8ffcf64b1f0052c80142b70c2815a3b658
Reid Barber <reid@reidbarber.com>
2025-05-15 14:54:59 -0500

    update comment

--- packages/dev/codemods/src/s1-to-s2/src/codemods/components/ComboBox/transform.ts
+++ packages/dev/codemods/src/s1-to-s2/src/codemods/components/ComboBox/transform.ts
@@ -11,11 +11,9 @@ import * as t from '@babel/types';
  * Transforms ComboBox:
  * - Change menuWidth value from a DimensionValue to a pixel value.
  * - Remove isQuiet (it is no longer supported in Spectrum 2).
- * - Comment out loadingState (it has not been implemented yet).
  * - Remove placeholder (it is no longer supported in Spectrum 2).
  * - Change validationState="invalid" to isInvalid.
  * - Remove validationState="valid" (it is no longer supported in Spectrum 2).
- * - Comment out onLoadMore (it has not been implemented yet).
  */
 export default function transformComboBox(path: NodePath<t.JSXElement>) {
   // Change menuWidth value from a DimensionValue to a pixel value
