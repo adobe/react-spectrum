4a9231b6f552b62c36d73bb7b453aa8a3481550a
Robert Snow <snowystinger@gmail.com>
2025-05-28 11:40:54 +1000

    fix tailwind starter

--- starters/tailwind/stories/Breadcrumbs.stories.tsx
+++ starters/tailwind/stories/Breadcrumbs.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Breadcrumb, Breadcrumbs } from '../src/Breadcrumbs';
 
-import type { Meta } from '@storybook/react';
+import { Meta } from '@storybook/react';
 
 const meta: Meta<typeof Breadcrumbs> = {
   component: Breadcrumbs,
--- starters/tailwind/stories/TagGroup.stories.tsx
+++ starters/tailwind/stories/TagGroup.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta } from '@storybook/react';
+import { Meta } from '@storybook/react';
 import React from 'react';
 import { Tag, TagGroup } from '../src/TagGroup';
 
