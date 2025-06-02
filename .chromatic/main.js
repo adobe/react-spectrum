a080a686875e304b5fe20927fa37edf6a9ab2183
Robert Snow <snowystinger@gmail.com>
2025-06-02 14:46:03 +1000

    remember to turn on accessibility addon for chromatic

--- .chromatic/main.js
+++ .chromatic/main.js
@@ -8,7 +8,9 @@ module.exports = {
     '../packages/**/chromatic/**/*.stories.@(js|jsx|ts|tsx)',
     '../packages/@react-spectrum/s2/chromatic/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  addons: process.env.NODE_ENV === 'production' ? [] : [
+  addons: process.env.NODE_ENV === 'production' ? [
+    '@storybook/addon-a11y'
+  ] : [
     '@storybook/addon-actions',
     '@storybook/addon-a11y'
   ],
