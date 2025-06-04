95d4e715971e327e717825bc1839cfbdf47e9208
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2025-06-04 15:13:43 -0700

    add s2 release notes

--- .storybook-s2/docs/Release030Intro.jsx
+++ .storybook-s2/docs/Release030Intro.jsx
@@ -16,3 +16,46 @@ export function Release030Intro() {
     </>
   )
 }
+
+export function Release0100Intro() {
+  return (
+    <>
+      <P>
+        We have a new option to our icon builder so that users can build a library of icons more easily using the same processing we apply to our own S2 Icon library. 
+      </P>
+      <P>
+        To run the new option, create a new npm project with an appropriate package.json, such as
+      </P>
+      <Pre>{highlight(`{
+  "name": "<<PACKAGE NAME>>",
+  "version": "<<VERSION>>",
+  "exports": {
+    "./*": {
+      "types": "./*.d.ts",
+      "module": "./*.mjs",
+      "import": "./*.mjs",
+      "require": "./*.cjs"
+    }
+  },
+  "peerDependencies": {
+    "@react-spectrum/s2": ">=0.8.0",
+    "react": "^18.0.0 || ^19.0.0-rc.1",
+    "react-dom": "^18.0.0 || ^19.0.0-rc.1"
+  },
+  "devDependencies": {
+    "@react-spectrum/s2-icon-builder": ">=0.2.3",
+    "@react-spectrum/s2": "latest",
+    "react": "^19.0.0",
+    "react-dom": "^19.0.0"
+  }
+}`)}</Pre>
+      <P>Copy your svg icons into a <Code>src</Code> directory in the project.</P>
+      <P>Then run:</P>
+      <Pre>{
+      highlight(`yarn install --no-immutable
+yarn transform-icons -i './src/*.svg' -o ./ --isLibrary`)}
+      </Pre>
+    </>
+  )
+}
+
