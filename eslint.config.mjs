b109b75ef5aac29051bf9dc17d6549cd9dba11f7
Devon Govett <devongovett@gmail.com>
2025-06-06 14:23:09 -0700

    eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -54,7 +54,8 @@ export default [{
         "starters/**/*",
         "scripts/icon-builder-fixture/**/*",
         "packages/@react-spectrum/s2/icon.d.ts",
-        "packages/@react-spectrum/s2/spectrum-illustrations"
+        "packages/@react-spectrum/s2/spectrum-illustrations",
+        "packages/dev/s2-docs/pages/**"
     ],
 }, ...compat.extends("eslint:recommended"), {
     plugins: {
