017577aff35e8f952b6e319e951bd5d2da0171f2
Reid Barber <reid@reidbarber.com>
2025-06-09 10:47:20 -0500

    use link icon on headings

--- packages/dev/s2-docs/src/Headings.tsx
+++ packages/dev/s2-docs/src/Headings.tsx
@@ -1,15 +1,16 @@
 'use client';
 
-import {style} from '@react-spectrum/s2/style' with {type: 'macro'};
+import {iconStyle, style} from '@react-spectrum/s2/style' with {type: 'macro'};
 import {useHover} from '@react-aria/interactions';
 import {useFocusRing} from '@react-aria/focus';
 import {Link} from '@react-spectrum/s2';
+import LinkIcon from '@react-spectrum/s2/icons/Link';
 
 function anchorId(children) {
   return children.replace(/\s/g, '-').replace(/[^a-zA-Z0-9-_]/g, '').toLowerCase();
 }
 
-function AnchorLink({anchorId, isHovered}) {
+function AnchorLink({anchorId, isHovered, level}) {
   let {isFocusVisible, focusProps} = useFocusRing({within: true});
   const href = `#${anchorId}`;
   // TODO: Do these links need aria-labels or aria-labelledby?
@@ -20,10 +21,21 @@ function AnchorLink({anchorId, isHovered}) {
         isHovered: 1,
         isFocusVisible: 1
       },
-      marginStart: 8,
-      transition: '[opacity 0.2s ease-in-out]'
-    })({isHovered, isFocusVisible})}>
-      <Link href={href}>#</Link>
+      marginStart: {
+        default: 8,
+        level: {
+          3: 4,
+          4: 4
+        }
+      },
+      transition: '[opacity 0.2s ease-in-out]',
+    })({isHovered, isFocusVisible, level})}>
+      <Link href={href}>
+        <LinkIcon
+          styles={iconStyle({size: 'S', marginBottom: 4})} 
+          UNSAFE_style={{marginBottom: (level === 3 || level === 4) ? 0 : undefined}}
+          />
+      </Link>
     </span>
   );
 }
@@ -34,7 +46,7 @@ export function H2({children, ...props}) {
   return (
     <h2 {...props} id={id} className={style({font: 'heading-xl', marginTop: 48, marginBottom: 24})} {...hoverProps}>
       {children}
-      <AnchorLink anchorId={id} isHovered={isHovered} />
+      <AnchorLink anchorId={id} isHovered={isHovered} level={2} />
     </h2>
   );
 }
@@ -45,7 +57,7 @@ export function H3({children, ...props}) {
   return (
     <h3 {...props} id={id} className={style({font: 'heading', marginTop: 32, marginBottom: 2})} {...hoverProps}>
       {children}
-      <AnchorLink anchorId={id} isHovered={isHovered} />
+      <AnchorLink anchorId={id} isHovered={isHovered} level={3} />
     </h3>
   );
 }
@@ -56,7 +68,7 @@ export function H4({children, ...props}) {
   return (
     <h4 {...props} id={id} className={style({font: 'heading-sm'})} {...hoverProps}>
       {children}
-      <AnchorLink anchorId={id} isHovered={isHovered} />
+      <AnchorLink anchorId={id} isHovered={isHovered} level={4} />
     </h4>
   );
 }
