e0f8ac0276928c8fe23087841145bd449b0d6891
Devon Govett <devongovett@gmail.com>
2025-06-09 17:01:46 -0700

    Revert "Revert "chore: Use inert in ariaHideOutside (#8317)" (#8371)"
    This reverts commit 26f9102de5ae9ff2057812714c151241a29118fe.

--- packages/@react-spectrum/picker/src/Picker.tsx
+++ packages/@react-spectrum/picker/src/Picker.tsx
@@ -154,7 +154,8 @@ export const Picker = React.forwardRef(function Picker<T extends object>(props: 
         hideArrow
         state={state}
         triggerRef={unwrappedTriggerRef}
-        scrollRef={listboxRef}>
+        scrollRef={listboxRef}
+        shouldContainFocus>
         {listbox}
       </Popover>
     );
