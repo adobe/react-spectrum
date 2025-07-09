849e52ae8a97f78a59de747b296618076bee9661
Lucas Weng <30640930+lucasweng@users.noreply.github.com>
2025-07-09 08:17:56 +0800

    fix: prevent form submission when required Select has more than 300 options and no selection (#8280)
    * fix: prevent form submission when required Select has more than 300 options and no selection
    
    * fix: only block form submission if the validation behavior is native
    
    * fix: lint errors
    
    * fix: prevent invalid form control error and native error popup for visually hidden required input
    
    * fix: prevent visually hidden text input from affecting keyboard navigation
    
    * refactor: use display: none instead of visually hidden to hide the input
    
    * fix conflict where select would steal focus on invalid from previous invalid components
    
    ---------
    
    Co-authored-by: Robert Snow <rsnow@adobe.com>
    Co-authored-by: Robert Snow <snowystinger@gmail.com>

--- packages/react-aria-components/stories/Select.stories.tsx
+++ packages/react-aria-components/stories/Select.stories.tsx
@@ -18,7 +18,13 @@ import {UNSTABLE_ListBoxLoadingSentinel} from '../src/ListBox';
 import {useAsyncList} from 'react-stately';
 
 export default {
-  title: 'React Aria Components/Select'
+  title: 'React Aria Components/Select',
+  argTypes: {
+    validationBehavior: {
+      control: 'select',
+      options: ['native', 'aria']
+    }
+  }
 };
 
 export const SelectExample = () => (
@@ -64,7 +70,11 @@ export const SelectRenderProps = () => (
   </Select>
 );
 
-let manyItems = [...Array(100)].map((_, i) => ({id: i, name: `Item ${i}`}));
+let makeItems = (length: number) => Array.from({length}, (_, i) => ({
+  id: i,
+  name: `Item ${i}`
+}));
+let manyItems = makeItems(100);
 
 const usStateOptions = [
   {id: 'AL', name: 'Alabama'},
@@ -273,4 +283,26 @@ export const SelectSubmitExample = () => (
     <Button type="submit">Submit</Button>
     <Button type="reset">Reset</Button>
   </Form>
+);
+
+// Test case for https://github.com/adobe/react-spectrum/issues/8034
+// Required select validation cannot currently be tested in the jsdom environment.
+// In jsdom, forms are submitted even when required fields are empty.
+// See: https://github.com/jsdom/jsdom/issues/2898
+export const RequiredSelectWithManyItems = (props) => (
+  <form>
+    <Select {...props} name="select" isRequired>
+      <Label style={{display: 'block'}}>Required Select with many items</Label>
+      <Button>
+        <SelectValue />
+        <span aria-hidden="true" style={{paddingLeft: 5}}>▼</span>
+      </Button>
+      <Popover>
+        <ListBox items={makeItems(301)} className={styles.menu}>
+          {item => <MyListBoxItem>{item.name}</MyListBoxItem>}
+        </ListBox>
+      </Popover>
+    </Select>
+    <Button type="submit">Submit</Button>
+  </form>
 );
