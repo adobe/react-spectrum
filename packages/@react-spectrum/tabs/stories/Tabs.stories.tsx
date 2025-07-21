28df9407cc09ebefae70a081405ddd1414d2dafa
Robert Snow <snowystinger@gmail.com>
2025-07-22 09:49:27 +1000

    fix story

--- packages/@react-spectrum/tabs/stories/Tabs.stories.tsx
+++ packages/@react-spectrum/tabs/stories/Tabs.stories.tsx
@@ -57,7 +57,7 @@ OrientationVertical.story = {
   name: 'orientation: vertical'
 };
 
-export const OrientationVerticalWrap: TabsStory = () => renderWithVerticalWrap({maxWidth: 90, wrap: true});
+export const OrientationVerticalWrap: TabsStory = () => renderWithVerticalWrap({wrap: true});
 
 OrientationVerticalWrap.story = {
   name: 'orientation: vertical, wrap: true'
@@ -758,7 +758,7 @@ function renderWithVerticalWrap(props = {}) {
       <TabList {...props}>
         <Item key="val1">User Profile Settings</Item>
         <Item key="val2">バナーおよびディスプレイ広告</Item>
-        <Item key="val3"><span style={{wordBreak: 'break-word'}}>Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz</span></Item>
+        <Item key="val3">Rindfleischetikettierungsüberwachungsaufgabenübertragungsgesetz</Item>
         <Item key="val4">Tab 4</Item>
         <Item key="val5">Tab 5</Item>
       </TabList>
