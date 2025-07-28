e643550f89925c57fc7407fe48ba7c91d8aa09d5
Reid Barber <reid@reidbarber.com>
2025-07-28 14:24:38 -0500

    Revert "fix z-index in copy/share ActionButtonGroup to avoid showing over dialog"
    This reverts commit e5f538da18625aa8af49fec0e1d5155286e594a0.

--- packages/dev/s2-docs/src/CodePlatter.tsx
+++ packages/dev/s2-docs/src/CodePlatter.tsx
@@ -67,7 +67,7 @@ export function CodePlatter({children, shareUrl, files, type, registryUrl}: Code
 
   return (
     <div className={platterStyle}>
-      <div className={style({display: 'flex', justifyContent: 'end', float: 'inline-end', padding: 16, position: 'relative'})}>
+      <div className={style({display: 'flex', justifyContent: 'end', float: 'inline-end', padding: 16, position: 'relative', zIndex: 1})}>
         <ActionButtonGroup
           orientation="vertical"
           isQuiet
