f39cf2e0d1589fa93d8a1d0367fedc1cbf8aeb5c
Reid Barber <reid@reidbarber.com>
2025-08-14 10:23:16 -0500

    add 'use client' to app/layout.tsx

--- examples/s2-next-macros/src/app/layout.tsx
+++ examples/s2-next-macros/src/app/layout.tsx
@@ -10,6 +10,8 @@
  * governing permissions and limitations under the License.
  */
 
+'use client';
+
 import {ClientProviders} from './provider';
 import type { Metadata } from "next";
 import { Provider } from '@react-spectrum/s2';
