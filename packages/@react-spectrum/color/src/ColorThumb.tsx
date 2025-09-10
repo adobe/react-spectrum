2e49eaf0cb99b81dad71baf5c26e72d1b24d0b88
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2025-09-10 16:41:05 -0700

    fix: position color loupe relative to the color handle in V3 ColorWheel

--- packages/@react-spectrum/color/src/ColorThumb.tsx
+++ packages/@react-spectrum/color/src/ColorThumb.tsx
@@ -50,7 +50,7 @@ function ColorThumb(props: ColorThumbProps): JSX.Element {
 }
 
 // ColorLoupe is rendered in a portal so that it breaks out of clipped/scrolling containers (e.g. popovers).
-function ColorLoupe({isOpen, valueCSS, containerRef, loupeRef, style}: any) {
+function ColorLoupe({isOpen, valueCSS, containerRef, loupeRef, style, getPosition}: any) {
   let patternId = useId();
 
   // Get the bounding rectangle of the container (e.g. ColorArea/ColorSlider).
@@ -69,11 +69,15 @@ function ColorLoupe({isOpen, valueCSS, containerRef, loupeRef, style}: any) {
   let thumbTop = style.top || '50%';
   if (typeof thumbTop === 'string' && thumbTop.endsWith('%')) {
     thumbTop = parseFloat(style.top || '50%') / 100 * containerRect.height;
+  } else if (typeof thumbTop === 'string' && thumbTop.endsWith('px')) {
+    thumbTop = parseFloat(thumbTop);
   }
 
   let thumbLeft = style.left || '50%';
   if (typeof thumbLeft === 'string' && thumbLeft.endsWith('%')) {
     thumbLeft = parseFloat(thumbLeft || '50%') / 100 * containerRect.width;
+  } else if (typeof thumbLeft=== 'string' && thumbLeft.endsWith('px')) {
+    thumbLeft = parseFloat(thumbLeft);
   }
 
   return (
