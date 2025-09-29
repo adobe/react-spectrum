5ede892f6e9493260818d795006ebb04d2b57702
Reid Barber <reid@reidbarber.com>
2025-09-28 20:25:00 -0500

    docs: Add more new docs content (#8874)
    * add Getting Started page
    
    * add Styling guide
    
    * remove RAC imports from S2 docs examples
    
    * add Migration page
    
    * update links in Release Notes
    
    * lint
    
    * add index page
    
    * lint
    
    * fix mobile nav in dark mode
    
    * add missing S2 component descriptions to JSDoc
    
    * add No Results state for Icons and Illustrations search
    
    * Add note to Illustration docs about pipeline name
    
    * add coming soon pages for S2 Collections, Drag and Drop, and Selection
    
    * rename  S2 guide pages for better slugs
    
    * git casing fix
    
    * add version badge to S2 Toast docs
    
    * fix todo link
    
    * add Internationalized docs
    
    * use BetaApp icon for Beta bage
    
    * fix Icons and Illustrations search items
    
    * improve RAC Button example
    
    * fix UserCard props type
    
    * make S2 style macro supported CSS properties list responsive
    
    * enable horizontal scrolling BundlerSwitcher control
    
    * lint
    
    * fix 'Esc' keyboard listener
    
    * add README.md for @react-spectrum/mcp
    
    * have mcp server load markdown docs from CDN
    
    * fix search menu background
    
    * lint

--- packages/@react-spectrum/s2/src/RangeCalendar.tsx
+++ packages/@react-spectrum/s2/src/RangeCalendar.tsx
@@ -64,6 +64,9 @@ const headerStyles = style({
   width: 'full'
 });
 
+/**
+ * RangeCalendars display a grid of days in one or more months and allow users to select a contiguous range of dates.
+ */
 export const RangeCalendar = /*#__PURE__*/ (forwardRef as forwardRefType)(function RangeCalendar<T extends DateValue>(props: RangeCalendarProps<T>, ref: ForwardedRef<HTMLDivElement>) {
   [props, ref] = useSpectrumContextProps(props, ref, RangeCalendarContext);
   let {
