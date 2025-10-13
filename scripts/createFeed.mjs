ecc7c9871dfbdc0d19941faa89cfb094f79bfae4
Robert Snow <rsnow@adobe.com>
2025-10-14 08:14:52 +1100

    chore: Remove dead dependencies (#8941)
    * chore: remove dead dependencies and update some
    
    * turn on verdaccio
    
    * update lockfile
    
    * attempt building starters outside of the global workspace
    
    * finish commit
    
    * Revert starter build on wrong branch
    
    This reverts commit 202c7ed9f1c0d32259e316441dc95343fbf1adf4.
    
    * remove all plop templates
    
    * delete dead docs
    
    * Revert "turn on verdaccio"
    
    This reverts commit 71030ab2cee1d57c6c5006a19b8674d88bc3674b.
    
    * turn on verdaccio
    
    * fix types
    
    * remove nyc reference
    
    * Revert "turn on verdaccio"
    
    This reverts commit 0d97a9526973b884e63d5f1992de927ea7a0a417.
    
    ---------
    
    Co-authored-by: Danni <darobins@adobe.com>

--- scripts/createFeed.mjs
+++ scripts/createFeed.mjs
@@ -3,14 +3,14 @@
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 
-import glob from 'glob';
+import {globSync} from 'glob';
 import fs from 'fs';
 import xml from "xml";
 import process from 'process';
@@ -73,7 +73,7 @@ let options = {
 })();
 
 function getFeed(type) {
-  let files = glob.sync(`packages/dev/docs/pages/${type}/*.mdx`, {ignore: [`packages/dev/docs/pages/${type}/index.mdx`]});
+  let files = globSync(`packages/dev/docs/pages/${type}/*.mdx`, {ignore: [`packages/dev/docs/pages/${type}/index.mdx`]});
   let posts = [];
 
   for (let file of files) {
