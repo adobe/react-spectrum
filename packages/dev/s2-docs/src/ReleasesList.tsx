2a5f1595ce69bd86f22e5cbfe9177677c1ad2396
Robert Snow <snowystinger@gmail.com>
2025-10-28 14:38:06 +1100

    fix rendering inside description

--- packages/dev/s2-docs/src/ReleasesList.tsx
+++ packages/dev/s2-docs/src/ReleasesList.tsx
@@ -1,6 +1,7 @@
 
 import {Link} from './Link';
 import type {Page} from '@parcel/rsc';
+import {renderHTMLfromMarkdown} from './types';
 import {style} from '@react-spectrum/s2/style' with {type: 'macro'};
 
 export function ReleasesList({pages}: {pages: Page[]}) {
@@ -8,14 +9,14 @@ export function ReleasesList({pages}: {pages: Page[]}) {
     return new Date(b.exports?.date).getTime() - new Date(a.exports?.date).getTime();
   });
   return (
-    <article className={style({marginY: 40, display: 'flex', flexDirection: 'column', gap: 40})}>
+    <article className={style({marginY: 40, display: 'flex', flexDirection: 'column', gap: 40, maxWidth: 600})}>
       {releases.map(release => (
         <div key={release.name}>
           <header className={style({marginBottom: 12})}>
             <h2 className={style({font: 'heading-lg', margin: 0})}><Link href={release.url}>{release.exports?.title}</Link></h2>
             <Time date={release.exports?.date} />
           </header>
-          <p className={style({font: 'body', margin: 0})}>{release.exports?.description}</p>
+          <p className={style({font: 'body', margin: 0})}>{renderHTMLfromMarkdown(release.exports?.description, {forceInline: false, forceBlock: true})}</p>
         </div>
       ))}
     </article>
