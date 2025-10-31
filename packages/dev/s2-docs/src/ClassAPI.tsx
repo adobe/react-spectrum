50c9e075dc2af6e8619b251a85b841b188cba80c
Daniel Lu <dl1644@gmail.com>
2025-10-31 11:46:06 -0700

    clean up old code, no more switcher

--- packages/dev/s2-docs/src/ClassAPI.tsx
+++ packages/dev/s2-docs/src/ClassAPI.tsx
@@ -1,30 +1,14 @@
 import {InterfaceType, setLinks, TInterface} from './types';
 import React from 'react';
-import {style} from '@react-spectrum/s2/style' with {type: 'macro'};
 
-
-const wrapper = style({
-  paddingX: {
-    default: 0,
-    isInSwitcher: 32
-  },
-  paddingBottom: {
-    default: 0,
-    isInSwitcher: 32
-  }
-});
 interface ClassAPIProps {
   class: TInterface,
-  links: any,
-  // TODO: replace by making this a client component if we can do that
-  isInSwitcher?: boolean
+  links: any
 }
 
-export function ClassAPI({class: c, links, isInSwitcher}: ClassAPIProps) {
+export function ClassAPI({class: c, links}: ClassAPIProps) {
   setLinks(links);
   return (
-    <div className={wrapper({isInSwitcher})}>
-      <InterfaceType {...c} />
-    </div>
+    <InterfaceType {...c} />
   );
 }
