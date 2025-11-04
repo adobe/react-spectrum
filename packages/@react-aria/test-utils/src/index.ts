f8ec2fb32e6437d1951c4bbbf7665e433bac0dce
Daniel Lu <dl1644@gmail.com>
2025-11-04 11:28:21 -0800

    export only the type

--- packages/@react-aria/test-utils/src/index.ts
+++ packages/@react-aria/test-utils/src/index.ts
@@ -14,18 +14,16 @@ export {triggerLongPress} from './events';
 export {installMouseEvent, installPointerEvent} from './testSetup';
 export {pointerMap} from './userEventMaps';
 export {User} from './user';
-// TODO: had to export these for the docs, but not sure why I didn't have to do
-// so for the v3 docs?
-export {CheckboxGroupTester} from './checkboxgroup';
-export {ComboBoxTester} from './combobox';
-export {DialogTester} from './dialog';
-export {GridListTester} from './gridlist';
-export {ListBoxTester} from './listbox';
-export {MenuTester} from './menu';
-export {RadioGroupTester} from './radiogroup';
-export {SelectTester} from './select';
-export {TableTester} from './table';
-export {TabsTester} from './tabs';
-export {TreeTester} from './tree';
+export type {CheckboxGroupTester} from './checkboxgroup';
+export type {ComboBoxTester} from './combobox';
+export type {DialogTester} from './dialog';
+export type {GridListTester} from './gridlist';
+export type {ListBoxTester} from './listbox';
+export type {MenuTester} from './menu';
+export type {RadioGroupTester} from './radiogroup';
+export type {SelectTester} from './select';
+export type {TableTester} from './table';
+export type {TabsTester} from './tabs';
+export type {TreeTester} from './tree';
 
 export type {UserOpts} from './types';
