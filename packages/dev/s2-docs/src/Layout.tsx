4ddc5899a550e39254bbc3a0350aff5b74ca0f0f
Reid Barber <reid@reidbarber.com>
2025-11-05 14:35:13 -0600

    fix layout

--- packages/dev/s2-docs/src/Layout.tsx
+++ packages/dev/s2-docs/src/Layout.tsx
@@ -343,32 +343,3 @@ export function Time({date}: {date: string}) {
     </time>
   );
 }
-
-export function BlogPostLayout(props: PageProps & {children: ReactElement<any>}) {
-  let {currentPage, children} = props;
-  let date = currentPage.exports?.date as string | undefined;
-  let author = currentPage.exports?.author as string | undefined;
-  let authorLink = currentPage.exports?.authorLink as string | undefined;
-  
-  // TODO: author/time not rendering
-  const blogComponents = {
-    ...components,
-    h1: ({children: h1Children, ...h1Props}) => (
-      <header className={style({display: 'flex', flexDirection: 'column', gap: 8, marginBottom: 32})}>
-        <h1 {...h1Props} id="top" style={{'--width-per-em': getTextWidth(h1Children)} as any} className={h1}>{h1Children}</h1>
-        {author && (
-          <address className={style({font: 'body-sm', fontStyle: 'normal'})}>
-            By {authorLink ? <Link href={authorLink} rel="author">{author}</Link> : author}
-          </address>
-        )}
-        {date && <Time date={date} />}
-      </header>
-    )
-  };
-  
-  return (
-    <Layout {...props}>
-      {React.cloneElement(children, {components: blogComponents})}
-    </Layout>
-  );
-}
