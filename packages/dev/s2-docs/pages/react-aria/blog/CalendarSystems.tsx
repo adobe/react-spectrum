8afa735f8c5a2d29d19a7230168c8fc74e0633bc
Reid Barber <reid@reidbarber.com>
2025-11-05 17:24:47 -0600

    docs: migrate blog posts (#9125)
    * init blog posts
    
    * more blog post content
    
    * more blog edits
    
    * add hero image to introducing-react-spectrum.mdx
    
    * add remaining videos
    
    * fix DragBetweenListsExample to use RAC
    
    * Revert "add remaining videos"
    
    This reverts commit 4e436bbb1d12cbbb7e54abdf435f2eb3e6189d01.
    
    * add posts to 'Blog' section
    
    * Revert "Revert "add remaining videos""
    
    This reverts commit 6d2b667f7d0b903a0a938f9dfb1e9884c9ed4dbf.
    
    * fix broken links
    
    * fix video widths
    
    * add bylines
    
    * fix layout
    
    * fix nested p tags
    
    * fix video widths
    
    * improve DnD example
    
    * fix caption fonts
    
    * fix remaining font issues

---
+++ packages/dev/s2-docs/pages/react-aria/blog/CalendarSystems.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import {Calendar, Picker, PickerItem, Provider} from '@react-spectrum/s2';
+import React from 'react';
+import {useLocale} from '@react-aria/i18n';
+import {style} from '@react-spectrum/s2/style' with {type: 'macro'};
+
+export default function CalendarSystems() {
+  let [calendar, setCalendar] = React.useState('gregory');
+  let {locale} = useLocale();
+  const calendars = [
+    {key: 'gregory', name: 'Gregorian'},
+    {key: 'japanese', name: 'Japanese'},
+    {key: 'buddhist', name: 'Buddhist'},
+    {key: 'roc', name: 'Taiwan'},
+    {key: 'persian', name: 'Persian'},
+    {key: 'indian', name: 'Indian'},
+    {key: 'islamic-umalqura', name: 'Islamic (Umm al-Qura)'},
+    {key: 'islamic-civil', name: 'Islamic Civil'},
+    {key: 'islamic-tbla', name: 'Islamic Tabular'},
+    {key: 'hebrew', name: 'Hebrew'},
+    {key: 'coptic', name: 'Coptic'},
+    {key: 'ethiopic', name: 'Ethiopic'},
+    {key: 'ethioaa', name: 'Ethiopic (Amete Alem)'}
+  ];
+
+  return (
+    <div className={style({display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 8})}>
+      <Picker label="Calendar system" items={calendars} value={calendar} onChange={setCalendar}>
+        {item => <PickerItem>{item.name}</PickerItem>}
+      </Picker>
+      <Provider locale={`${locale}-u-ca-${calendar}`}>
+        <Calendar aria-label="Date" />
+      </Provider>
+    </div>
+  )
+}
