c1691140366640890e304ac16a5250e3cba08d5f
Devon Govett <devongovett@gmail.com>
2025-11-11 11:17:07 -0800

    Add use client to illustrations

--- packages/@react-spectrum/s2/spectrum-illustrations/gradient/generic2/3D.tsx
+++ packages/@react-spectrum/s2/spectrum-illustrations/gradient/generic2/3D.tsx
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
+'use client';
 import _3D_L from 'illustration:./S2_fill_3D_generic2_160.svg';
 import _3D_M from 'illustration:./S2_fill_3D_generic2_96.svg';
 import _3D_S from 'illustration:./S2_fill_3D_generic2_48.svg';
--- packages/@react-spectrum/s2/spectrum-illustrations/gradient/generic2/AIGenerate.tsx
+++ packages/@react-spectrum/s2/spectrum-illustrations/gradient/generic2/AIGenerate.tsx
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
+'use client';
 import AIGenerate_L from 'illustration:./S2_fill_AIGenerate_generic2_160.svg';
 import AIGenerate_M from 'illustration:./S2_fill_AIGenerate_generic2_96.svg';
 import AIGenerate_S from 'illustration:./S2_fill_AIGenerate_generic2_48.svg';
