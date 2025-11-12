5aded3194675902031eed7b6e2b97d241ac727e0
mehdibha <hello@mehdibha.com>
2025-11-12 11:23:50 +0100

    Add isReadonly and isRequired to renderProps

--- packages/react-aria-components/src/ColorField.tsx
+++ packages/react-aria-components/src/ColorField.tsx
@@ -45,6 +45,16 @@ export interface ColorFieldRenderProps {
    */
   isInvalid: boolean,
   /**
+   * Whether the color field is read only.
+   * @selector [data-readonly]
+   */
+   isReadOnly: boolean,
+   /**
+    * Whether the color field is required.
+    * @selector [data-required]
+    */
+   isRequired: boolean,
+  /**
    * The color channel that this field edits, or "hex" if no `channel` prop is set.
    * @selector [data-channel="hex | hue | saturation | ..."]
    */
@@ -192,7 +202,9 @@ function useChildren(
       state,
       channel: props.channel || 'hex',
       isDisabled: props.isDisabled || false,
-      isInvalid: validation.isInvalid || false
+      isInvalid: validation.isInvalid || false,
+      isReadOnly: props.isReadOnly || false,
+      isRequired: props.isRequired || false
     },
     defaultClassName: 'react-aria-ColorField'
   });
