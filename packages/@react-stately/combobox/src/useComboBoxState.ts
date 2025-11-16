d742b49872e755ede313a46640f36dceafd00947
Will <wdavies973@gmail.com>
2025-11-15 22:14:56 -0600

    Fix formatting of if statement in useComboBoxState
    Fix lint issue

--- packages/@react-stately/combobox/src/useComboBoxState.ts
+++ packages/@react-stately/combobox/src/useComboBoxState.ts
@@ -237,7 +237,7 @@ export function useComboBoxState<T extends object>(props: ComboBoxStateOptions<T
     // Clear focused key when input value changes and display filtered collection again.
     if (inputValue !== lastValue) {
       const firstKey = filteredCollection.getFirstKey();
-      if(firstKey) selectionManager.setFocusedKey(firstKey);
+      if (firstKey) selectionManager.setFocusedKey(firstKey);
       setShowAllItems(false);
 
       // Set selectedKey to null when the user clears the input.
