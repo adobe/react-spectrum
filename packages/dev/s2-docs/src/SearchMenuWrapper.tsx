5bfb320f63d625e40da8c580525540964b655fbe
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2025-11-17 17:19:54 -0800

    docs: side nav follow up (#9195)
    * docs: side nav follow up
    
    * fix font for mobile case
    
    * update cursor
    
    * fix links, make cursor pointer

--- packages/dev/s2-docs/src/SearchMenuWrapper.tsx
+++ packages/dev/s2-docs/src/SearchMenuWrapper.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import '../tailwind/home.global.css';
-import {DialogTrigger, Modal, ModalOverlay, Pressable} from 'react-aria-components';
+import {Button, DialogTrigger, Modal, ModalOverlay} from 'react-aria-components';
 import {flushSync} from 'react-dom';
 import {preloadSearchMenu} from './SearchMenuTrigger';
-import React, {useState} from 'react';
+import React, {lazy, useState} from 'react';
+import {Modal as S2Modal} from '../../../@react-spectrum/s2/src/Modal';
 import {SearchMenu} from './SearchMenu';
 import {style} from '@react-spectrum/s2/style' with { type: 'macro' };
+
+const MobileSearchMenu = lazy(() => import('./SearchMenu').then(({MobileSearchMenu}) => ({default: MobileSearchMenu})));
 
 let underlayStyle = style({
   position: 'absolute',
@@ -81,20 +84,34 @@ export default function SearchMenuWrapper({pages, currentPage}) {
   };
 
   return (
-    <DialogTrigger isOpen={searchOpen} onOpenChange={() => {openSearchMenu();}} >
-      <Pressable><span role="button" className="no-underline bg-white/60 border border-black/10 bg-clip-padding text-base md:text-lg font-bold text-slate-800 px-8 py-3 rounded-full backdrop-saturate-150 backdrop-brightness-125 transition hover:bg-white/60 focus-ring  dark:outline-white outline-offset-2 active:scale-95">Explore Components</span></Pressable>
-      <ModalOverlay
-        style={{zIndex: 21}}
-        isDismissable
-        className={underlayStyle}>
-        <Modal className={modalStyle}>
-          <SearchMenu
-            pages={pages}
-            currentPage={currentPage}
-            onClose={closeSearchMenu}
-            initialSearchValue="" />
-        </Modal>
-      </ModalOverlay>
-    </DialogTrigger>
+    <>
+      <div className={style({display: {default: 'none', lg: 'flex'}})}>
+        <DialogTrigger isOpen={searchOpen} onOpenChange={() => {openSearchMenu();}} >
+          <Button className="font-spectrum no-underline bg-white/60 border border-black/10 bg-clip-padding text-base md:text-lg font-bold text-slate-800 px-8 py-3 rounded-full backdrop-saturate-150 backdrop-brightness-125 transition hover:bg-white/60 focus-ring  dark:outline-white outline-offset-2 active:scale-95 cursor-pointer">Explore Components</Button>
+          <ModalOverlay
+            style={{zIndex: 21}}
+            isDismissable
+            className={underlayStyle}>
+            <Modal className={modalStyle}>
+              <SearchMenu
+                pages={pages}
+                currentPage={currentPage}
+                onClose={closeSearchMenu}
+                initialSearchValue="" />
+            </Modal>
+          </ModalOverlay>
+        </DialogTrigger>
+      </div>
+      <div className={style({display: {default: 'flex', lg: 'none'}})}>
+        <DialogTrigger>
+          <Button className="font-spectrum no-underline bg-white/60 border border-black/10 bg-clip-padding text-base md:text-lg font-bold text-slate-800 px-8 py-3 rounded-full backdrop-saturate-150 backdrop-brightness-125 transition hover:bg-white/60 focus-ring  dark:outline-white outline-offset-2 active:scale-95 cursor-pointer">Explore Components</Button>
+          <S2Modal size="fullscreenTakeover">
+            <MobileSearchMenu pages={pages} currentPage={currentPage} />
+          </S2Modal>
+        </DialogTrigger>
+      </div>
+    </>
   );
 }
+
+
