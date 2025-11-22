18463259ba2b9395a8873d083b68253007574d61
Devon Govett <devongovett@gmail.com>
2025-11-21 17:29:47 -0800

    hover

--- packages/dev/s2-docs/pages/s2/home/app/Sidebar.tsx
+++ packages/dev/s2-docs/pages/s2/home/app/Sidebar.tsx
@@ -84,27 +84,37 @@ export function Sidebar({page, onPageChange}: any) {
         </SideNavItem>
       </SideNav>
       <div className={style({flexGrow: 1})} />
-      <ActionButton
-        isQuiet
-        aria-label="Toggle sidebar"
-        styles={style({alignSelf: 'start'})}
-        onPress={(e) => {
-          if (state == null) {
-            let container = e.target.closest('[data-container]') as HTMLElement;
-            setState(container?.offsetWidth > 1024 ? 'collapsed' : 'expanded')
-          } else {
-            setState(state === 'expanded' ? 'collapsed' : 'expanded');
-          }
-        }}>
-        {/* @ts-ignore */}
-        <PanelIcon state={state} />
-      </ActionButton>
+      <PanelToggleButton state={state} setState={setState} />
     </div>
   )
 }
 
+function PanelToggleButton({state, setState}: any) {
+  let [isHovered, setHovered] = useState(false);
+  return (
+    <ActionButton
+      isQuiet
+      aria-label="Toggle sidebar"
+      styles={style({alignSelf: 'start'})}
+      // @ts-ignore - should we expose this?
+      onHoverChange={setHovered}
+      onPress={(e) => {
+        if (state == null) {
+          let container = e.target.closest('[data-container]') as HTMLElement;
+          setState(container?.offsetWidth > 1024 ? 'collapsed' : 'expanded')
+        } else {
+          setState(state === 'expanded' ? 'collapsed' : 'expanded');
+        }
+        setHovered(false);
+      }}>
+      {/* @ts-ignore */}
+      <PanelIcon state={state} isHovered={isHovered} />
+    </ActionButton>
+  );
+}
+
 const PanelIcon = createIcon(props => {
-  let {state, ...otherProps} = props as any;
+  let {state, isHovered, ...otherProps} = props as any;
   return (
     <svg viewBox="0 0 20 20" fill="var(--iconPrimary)" {...otherProps}>
       <path d="M15.75 18H4.25C3.00977 18 2 16.9907 2 15.75V4.25C2 3.00928 3.00977 2 4.25 2H15.75C16.9902 2 18 3.00928 18 4.25V15.75C18 16.9907 16.9902 18 15.75 18ZM4.25 3.5C3.83691 3.5 3.5 3.83643 3.5 4.25V15.75C3.5 16.1636 3.83691 16.5 4.25 16.5H15.75C16.1631 16.5 16.5 16.1636 16.5 15.75V4.25C16.5 3.83643 16.1631 3.5 15.75 3.5H4.25Z" fill="var(--iconPrimary)" />
@@ -122,9 +132,17 @@ const PanelIcon = createIcon(props => {
             state: {
               expanded: '[5px]',
               collapsed: '[1.5px]'
+            },
+            isHovered: {
+              default: '[5px]',
+              [LG]: '[1.5px]',
+              state: {
+                expanded: '[1.5px]',
+                collapsed: '[5px]'
+              }
             }
           }
-        })({state})} />
+        })({state, isHovered})} />
     </svg>
   );
 });
