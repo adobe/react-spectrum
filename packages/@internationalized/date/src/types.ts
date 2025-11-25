e194d4c42b10c8314511811ecda04a0deb34c466
soufiane.boutahlil@outlook.com <soufiane.boutahlil@outlook.com>
2025-11-25 12:15:35 +0100

    fix: Clarify role methods

--- packages/@internationalized/date/src/types.ts
+++ packages/@internationalized/date/src/types.ts
@@ -57,9 +57,9 @@ export interface Calendar {
   getDaysInMonth(date: AnyCalendarDate): number,
   /** Returns the number of months in the year of the given date. */
   getMonthsInYear(date: AnyCalendarDate): number,
-  /** Returns the maximum month. */
+  /** Returns the maximum month across all years. */
   getMaxMonths(): number,
-  /** Returns the maximum day. */
+  /** Returns the maximum day across all months. */
   getMaxDays(): number,
   /** Returns the number of years in the era of the given date. */
   getYearsInEra(date: AnyCalendarDate): number,
