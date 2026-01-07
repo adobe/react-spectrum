eee020ded3b156374f5b9afe864521a368b90f4b
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2026-01-07 16:25:20 -0500

    add gridlist section to tailwind starter

--- starters/tailwind/stories/GridList.stories.tsx
+++ starters/tailwind/stories/GridList.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta } from '@storybook/react';
-import { GridList, GridListItem } from '../src/GridList';
+import { GridList, GridListHeader, GridListItem} from '../src/GridList';
+import { GridListSection } from 'react-aria-components';
 import React from 'react';
 
 const meta: Meta<typeof GridList> = {
@@ -31,3 +32,28 @@ DisabledItems.args = {
   ...Example.args,
   disabledKeys: ['mint']
 };
+
+export const Sections = (args: any) => (
+  <GridList aria-label="Food" {...args}>
+    <GridListSection>
+      <GridListHeader>Fruits</GridListHeader>
+      <GridListItem id="Apple">Apple</GridListItem>
+      <GridListItem id="Grape">Grape</GridListItem>
+      <GridListItem id="Peach">Peach</GridListItem>
+      <GridListItem id="Melon">Melon</GridListItem>
+    </GridListSection>
+    <GridListSection>
+      <GridListHeader>Vegetables</GridListHeader>
+      <GridListItem id="brocoli">Broccoli</GridListItem>
+      <GridListItem id="peas">Peas</GridListItem>
+      <GridListItem id="brussels-sprouts">Brussels Sprouts</GridListItem>
+      <GridListItem id="zucchini">Zucchini</GridListItem>
+    </GridListSection>
+  </GridList>
+);
+
+Sections.args = {
+  onAction: null,
+  selectionMode: 'multiple'
+};
+
