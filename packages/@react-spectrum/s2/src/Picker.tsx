e756304e53903e34c3b4619127b3b95df54c8cf5
Reid Barber <reid@reidbarber.com>
2026-01-07 14:50:09 -0600

    whitespace

--- packages/@react-spectrum/s2/src/Picker.tsx
+++ packages/@react-spectrum/s2/src/Picker.tsx
@@ -98,7 +98,7 @@ export interface PickerStyleProps {
 
 type SelectionMode = 'single' | 'multiple';
 export interface PickerProps<T extends object, M extends SelectionMode = 'single'> extends
-  Omit<AriaSelectProps<T, M>, 'children' | 'style' | 'className' | 'allowsEmptyCollection' | 'isTriggerUpWhenOpen'  | keyof GlobalDOMAttributes>,
+  Omit<AriaSelectProps<T, M>, 'children' | 'style' | 'className' | 'allowsEmptyCollection' | 'isTriggerUpWhenOpen' | keyof GlobalDOMAttributes>,
   PickerStyleProps,
   StyleProps,
   SpectrumLabelableProps,
