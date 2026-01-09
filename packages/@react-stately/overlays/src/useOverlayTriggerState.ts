06d4e391cb8b2acb6d6466be949a96a278e6beca
quisi.do <github@quisi.do>
2026-01-08 18:25:12 -0800

    add support for `unbound-method` to `OverlayTriggerState`

--- packages/@react-stately/overlays/src/useOverlayTriggerState.ts
+++ packages/@react-stately/overlays/src/useOverlayTriggerState.ts
@@ -18,13 +18,13 @@ export interface OverlayTriggerState {
   /** Whether the overlay is currently open. */
   readonly isOpen: boolean,
   /** Sets whether the overlay is open. */
-  setOpen(isOpen: boolean): void,
+  setOpen: (isOpen: boolean) => void,
   /** Opens the overlay. */
-  open(): void,
+  open: () => void,
   /** Closes the overlay. */
-  close(): void,
+  close: () => void,
   /** Toggles the overlay's visibility. */
-  toggle(): void
+  toggle: () => void
 }
 
 /**
