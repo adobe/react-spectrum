449fb774bdcb188c8037cdd9276cb224c0ad907d
Devon Govett <devongovett@gmail.com>
2026-01-08 20:55:29 -0500

    Form

--- packages/react-aria-components/src/Form.tsx
+++ packages/react-aria-components/src/Form.tsx
@@ -12,9 +12,8 @@
 
 import {ContextValue, DOMProps, useContextProps} from './utils';
 import {FormValidationContext} from 'react-stately';
-import {GlobalDOMAttributes} from '@react-types/shared';
+import {GlobalDOMAttributes, FormProps as SharedFormProps} from '@react-types/shared';
 import React, {createContext, ForwardedRef, forwardRef} from 'react';
-import {FormProps as SharedFormProps} from '@react-types/form';
 
 export interface FormProps extends SharedFormProps, DOMProps, GlobalDOMAttributes<HTMLFormElement> {
   /**
