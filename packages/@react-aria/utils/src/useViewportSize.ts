1f85ed860de550275931565dc8addc8aa0feb20d
XiaoYan Li <lxy.lixiaoyan@gmail.com>
2026-01-30 07:52:42 +0800

    fix(RAC): use ICB size as the fallback value for visual viewport (#9384)
    * fix(RAC): use ICB size as the fallback value for visual viewport
    
    * fix tests after merge
    
    ---------
    
    Co-authored-by: Robert Snow <snowystinger@gmail.com>

--- packages/@react-aria/utils/src/useViewportSize.ts
+++ packages/@react-aria/utils/src/useViewportSize.ts
@@ -57,7 +57,7 @@ export function useViewportSize(): ViewportSize {
         // Wait one frame to see if a new element gets focused.
         frame = requestAnimationFrame(() => {
           if (!document.activeElement || !willOpenKeyboard(document.activeElement)) {
-            updateSize({width: window.innerWidth, height: window.innerHeight});
+            updateSize({width: document.documentElement.clientWidth, height: document.documentElement.clientHeight});
           }
         });
       }
@@ -90,7 +90,7 @@ export function useViewportSize(): ViewportSize {
 function getViewportSize(): ViewportSize {
   return {
     // Multiply by the visualViewport scale to get the "natural" size, unaffected by pinch zooming.
-    width: visualViewport ? visualViewport.width * visualViewport.scale : window.innerWidth,
-    height: visualViewport ? visualViewport.height * visualViewport.scale : window.innerHeight
+    width: visualViewport ? visualViewport.width * visualViewport.scale : document.documentElement.clientWidth,
+    height: visualViewport ? visualViewport.height * visualViewport.scale : document.documentElement.clientHeight
   };
 }
