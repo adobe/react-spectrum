c9539eb2e6fb42af0f5d9f78bc7c9a3d6d4b2489
Robert Snow <snowystinger@gmail.com>
2026-02-11 13:18:44 +1100

    clarify some code

--- packages/dev/storybook-builder-parcel/gen-preview-modern.js
+++ packages/dev/storybook-builder-parcel/gen-preview-modern.js
@@ -79,33 +79,33 @@ module.exports.generatePreviewModern = async function generatePreviewModern(
   return code;
 };
 
-function processPreviewAnnotation(path) {
+function processPreviewAnnotation(annotationPath) {
   // If entry is an object, take the first, which is the
   // bare (non-absolute) specifier.
   // This is so that webpack can use an absolute path, and
   // continue supporting super-addons in pnp/pnpm without
   // requiring them to re-export their sub-addons as we do
   // in addon-essentials.
-  if (typeof path === "object") {
-    return path.bare;
+  if (typeof annotationPath === "object") {
+    return annotationPath.bare;
   }
   // resolve relative paths into absolute paths, but don't resolve "bare" imports
-  if (path?.startsWith("./") || path?.startsWith("../")) {
-    return /*slash*/ path.resolve(path);
+  if (annotationPath?.startsWith("./") || annotationPath?.startsWith("../")) {
+    return /*slash*/ annotationPath.resolve(annotationPath);
   }
   // This should not occur, since we use `.filter(Boolean)` prior to
   // calling this function, but this makes typescript happy
-  if (!path) {
+  if (!annotationPath) {
     throw new Error("Could not determine path for previewAnnotation");
   }
 
   // For addon dependencies that use require.resolve(), we need to convert to a bare path
   // so that vite will process it as a dependency (cjs -> esm, etc).
-  if (path.includes("node_modules")) {
-    return stripAbsNodeModulesPath(path);
+  if (annotationPath.includes("node_modules")) {
+    return stripAbsNodeModulesPath(annotationPath);
   }
 
-  return /*slash*/ path;
+  return /*slash*/ annotationPath;
 }
 
 /**
