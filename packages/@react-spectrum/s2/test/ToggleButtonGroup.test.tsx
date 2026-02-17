6d420cf37561fdf2f9973b3f783ba574515702aa
Reid Barber <reid@reidbarber.com>
2026-02-17 14:13:02 -0600

    restore unaffected tests

--- packages/@react-spectrum/s2/test/ToggleButtonGroup.test.tsx
+++ packages/@react-spectrum/s2/test/ToggleButtonGroup.test.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Adobe. All rights reserved.
+ * Copyright 2024 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,23 +10,24 @@
  * governing permissions and limitations under the License.
  */
 
-import {fireEvent, render} from '@react-spectrum/test-utils-internal';
-import {TextArea} from '../src';
+import {render} from '@react-spectrum/test-utils-internal';
+import {Text, ToggleButton, ToggleButtonGroup} from '../src';
 
-describe('TextField', () => {
-  it('should focus textarea when tapping invalid icon', async () => {
-    let {getByRole} = render(
-      <TextArea label="Description" isInvalid  />
+describe('ToggleButtonGroup', () => {
+
+  it('can disable all buttons from the group', async () => {
+    let {getAllByRole} = render(
+      <ToggleButtonGroup isDisabled>
+        <ToggleButton id={1}><Text slot="label">Bold</Text></ToggleButton>
+        <ToggleButton id={2}><Text slot="label">Italic</Text></ToggleButton>
+        <ToggleButton id={3}><Text slot="label">Underline</Text></ToggleButton>
+      </ToggleButtonGroup>
     );
 
-    let textarea = getByRole('textbox');
-    // svg doesn't have a role so grab it via queryselector
-    let icon = getByRole('presentation').querySelector('svg')!;
-    expect(icon).toBeInTheDocument();
-    // user event with touch doesn't cause touchEnd to trigger so using fireEvent
-    fireEvent.touchStart(icon);
-    fireEvent.touchEnd(icon);
 
-    expect(document.activeElement).toBe(textarea);
+    let buttons = getAllByRole('radio');
+    expect(buttons[0]).toBeDisabled();
+    expect(buttons[1]).toBeDisabled();
+    expect(buttons[2]).toBeDisabled();
   });
 });
