7118e902a643f2b652ba1dbe6db83e0ae1b05da7
David Ferguson <davidferguson@users.noreply.github.com>
2026-03-03 16:22:56 +0000

    example of rslib issues in React Spectrum

---
+++ examples/s2-rslib-semiworking/rslib.config.ts
@@ -0,0 +1,21 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: ['./src/**'],
+    },
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm',
+    },
+  ],
+  output: {
+    target: 'web',
+  },
+  plugins: [pluginReact()],
+});
