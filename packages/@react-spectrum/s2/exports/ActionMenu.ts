1a62d9b466cc82a740ebc3b6fcc7b534ae9d54a4
Devon Govett <devongovett@gmail.com>
2026-03-28 12:18:54 -0700

    fix ActionMenu

--- packages/@react-spectrum/s2/exports/ActionMenu.ts
+++ packages/@react-spectrum/s2/exports/ActionMenu.ts
@@ -1,11 +1,13 @@
 export {ActionMenu, ActionMenuContext} from '../src/ActionMenu';
 export type {ActionMenuProps} from '../src/ActionMenu';
 
-export {MenuItem, MenuTrigger, Menu, MenuSection, SubmenuTrigger, UnavailableMenuItemTrigger} from '../src/Menu';
+export {MenuItem, MenuTrigger, MenuSection, SubmenuTrigger, UnavailableMenuItemTrigger} from '../src/Menu';
 export {Collection} from 'react-aria/Collection';
-export type {MenuTriggerProps, MenuProps, MenuItemProps, MenuSectionProps, SubmenuTriggerProps, UnavailableMenuItemTriggerProps} from '../src/Menu';
+export type {MenuTriggerProps, MenuItemProps, MenuSectionProps, SubmenuTriggerProps, UnavailableMenuItemTriggerProps} from '../src/Menu';
 
 export {ContextualHelpPopover} from '../src/ContextualHelp';
 export type {ContextualHelpPopoverProps} from '../src/ContextualHelp';
 
 export {Text, Keyboard, Header, Heading} from '../src/Content';
+
+export type {Key} from '@react-types/shared';
