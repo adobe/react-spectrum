1917eae92f47cf36cdc43179bdc6f2d2a73fbe99
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2026-04-22 17:41:05 -0700

    update docs

--- packages/dev/s2-docs/src/LabeledValueTypes.ts
+++ packages/dev/s2-docs/src/LabeledValueTypes.ts
@@ -5,31 +5,31 @@ import {ReactElement} from 'react';
 // The doc generator cannot handle the generic conditional types, so this is a flattened version.
 export interface LabeledValueProps extends LabeledValueBaseProps, LabeledValueStyleProps {
   /** The value to display. */
-  value: string | string[] | number | RangeValue<number> | DateTime | RangeValue<DateTime> | ReactElement,
+	value: string | string[] | number | RangeValue<number> | DateTime | RangeValue<DateTime> | ReactElement,
   /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
   formatOptions?: Intl.NumberFormatOptions | Intl.DateTimeFormatOptions | Intl.ListFormatOptions
 }
 
 // The doc generator cannot handle the generic conditional types, so this is a flattened version.
 export interface LabeledValueNumberProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-    /** The value to display. */
-    value: number | RangeValue<number>,
-    /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-    formatOptions?: Intl.NumberFormatOptions
+/** The value to display. */
+value: number | RangeValue<number>,
+/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+formatOptions?: Intl.NumberFormatOptions
 }
 
 // The doc generator cannot handle the generic conditional types, so this is a flattened version.
 export interface LabeledValueDateTimeProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-    /** The value to display. */
-    value: DateTime | RangeValue<DateTime>,
-    /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-    formatOptions?: Intl.DateTimeFormatOptions
+	/** The value to display. */
+	value: DateTime | RangeValue<DateTime>,
+	/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+	formatOptions?: Intl.DateTimeFormatOptions
 }
 
 // The doc generator cannot handle the generic conditional types, so this is a flattened version.
 export interface LabeledValueListProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-    /** The value to display. */
-    value: string[],
-    /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-    formatOptions?: Intl.ListFormatOptions
+	/** The value to display. */
+	value: string[],
+	/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+	formatOptions?: Intl.ListFormatOptions
 }
