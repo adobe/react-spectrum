ac4047576854d48ec53c7836427d5a4be9cac712
Rob Snow <rsnow@adobe.com>
2026-04-24 15:58:25 +1000

    fix(sb10): restore story globs dropped during main.mjs conversion

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -6,6 +6,9 @@ export default {
   stories: [
     '../packages/@{react-aria,react-stately,spectrum-icons}/*/stories/*.stories.{js,jsx,ts,tsx}',
     '../packages/@react-spectrum/!(s2)/stories/*.stories.{js,jsx,ts,tsx}',
+    '../packages/@adobe/react-spectrum/stories/*/*.stories.{js,jsx,ts,tsx}',
+    '../packages/react-aria/stories/*/*.stories.{js,jsx,ts,tsx}',
+    '../packages/react-stately/stories/*/*.stories.{js,jsx,ts,tsx}',
     '../packages/react-aria-components/stories/*.stories.{js,jsx,ts,tsx}'
   ],
   addons: [
