191f5cebb19f35043865a8b1a0fbdf90fcbc8e51
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2026-05-04 11:28:17 -0700

    fix indent

--- packages/dev/s2-docs/src/LabeledValueTypes.ts
+++ packages/dev/s2-docs/src/LabeledValueTypes.ts
@@ -5,26 +5,26 @@ import {ReactElement} from 'react';
 // The doc generator cannot handle the generic conditional types, so this is a flattened version.
 export interface LabeledValueProps extends LabeledValueBaseProps, LabeledValueStyleProps {
   /** The value to display. */
-	value: string | string[] | number | RangeValue<number> | DateTime | RangeValue<DateTime> | ReactElement,
+  value: string | string[] | number | RangeValue<number> | DateTime | RangeValue<DateTime> | ReactElement,
   /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
   formatOptions?: Intl.NumberFormatOptions | Intl.DateTimeFormatOptions | Intl.ListFormatOptions
 }
 
 export interface LabeledValueNumberProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-	/** The value to display. */
-	value: number | RangeValue<number>,
-	/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-	formatOptions?: Intl.NumberFormatOptions
+  /** The value to display. */
+  value: number | RangeValue<number>,
+  /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+  formatOptions?: Intl.NumberFormatOptions
 }
 export interface LabeledValueDateTimeProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-	/** The value to display. */
-	value: DateTime | RangeValue<DateTime>,
-	/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-	formatOptions?: Intl.DateTimeFormatOptions
+  /** The value to display. */
+  value: DateTime | RangeValue<DateTime>,
+  /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+  formatOptions?: Intl.DateTimeFormatOptions
 }
 export interface LabeledValueListProps extends LabeledValueBaseProps, LabeledValueStyleProps {
-	/** The value to display. */
-	value: string[],
-	/** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
-	formatOptions?: Intl.ListFormatOptions
+  /** The value to display. */
+  value: string[],
+  /** Formatting options for the value. The available options depend on the type passed to the `value` prop. */
+  formatOptions?: Intl.ListFormatOptions
 }
