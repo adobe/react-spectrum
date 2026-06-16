2f7e24df12ceabc1e3b0b5c762a632f9bc9ecb75
Rob Snow <rsnow@adobe.com>
2026-06-16 13:25:16 +1000

    test fix of browser targets

--- packages/dev/parcel-transformer-css-env/CSSEnvTransformer.js
+++ packages/dev/parcel-transformer-css-env/CSSEnvTransformer.js
@@ -19,7 +19,7 @@ module.exports = new Transformer({
     asset.setEnvironment({
       context: 'browser',
       engines: {
-        browsers: 'baseline widely available'
+        browsers: 'chrome >= 79, firefox >= 85, safari >= 13'
       },
       shouldOptimize: asset.env.shouldOptimize,
       outputFormat: asset.env.outputFormat,
