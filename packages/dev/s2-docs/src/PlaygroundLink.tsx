185efc0ccb0fca8a5b1d42e6f861bbad337f0ce5
Rob Snow <rsnow@adobe.com>
2026-06-23 12:58:42 +1000

    fix ts

--- packages/dev/s2-docs/src/PlaygroundLink.tsx
+++ packages/dev/s2-docs/src/PlaygroundLink.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import {baseColor, focusRing, style} from '@react-spectrum/s2/style' with {type: 'macro'};
 import {createStackBlitz} from './StackBlitz';
 import type {DownloadFiles} from './CodeBlock';
-import {Link} from '@react-spectrum/s2';
+import {Link, LinkRenderProps} from 'react-aria-components/Link';
 
 const PLAYGROUND_EXAMPLE = `import {Button} from './Button';
 
@@ -15,16 +16,29 @@ interface PlaygroundLinkProps {
   files: DownloadFiles;
 }
 
+const link = style<LinkRenderProps>({
+  ...focusRing(),
+  borderRadius: 'sm',
+  font: 'ui',
+  color: {
+    default: baseColor('neutral'),
+    forcedColors: 'LinkText'
+  },
+  transition: 'default',
+  textDecoration: 'underline',
+  outlineColor: {
+    default: 'focus-ring',
+    forcedColors: 'Highlight'
+  },
+  cursor: 'pointer',
+  width: 'fit',
+  disableTapHighlight: true
+});
+
 export function PlaygroundLink({files}: PlaygroundLinkProps) {
   return (
     <Link
-      isStandalone
-      variant="secondary"
-      href="/#"
-      UNSAFE_style={{width: 'fit-content'}}
-      onClick={e => {
-        e.preventDefault();
-      }}
+      className={link}
       onPress={() => {
         createStackBlitz(
           {...files.files, 'Example.tsx': {contents: PLAYGROUND_EXAMPLE}},
