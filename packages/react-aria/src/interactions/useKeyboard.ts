4cb5437e830a3c70aea7476d681538b28a27ea2f
Rob Snow <rsnow@adobe.com>
2026-06-25 11:19:31 +1000

    clarify docs

--- packages/react-aria/src/interactions/useKeyboard.ts
+++ packages/react-aria/src/interactions/useKeyboard.ts
@@ -25,9 +25,9 @@ export interface KeyboardProps extends KeyboardEvents {
   isDisabled?: boolean;
   /** Keyboard shortcuts to handle. */
   shortcuts?: KeyboardShortcutBindings;
-  /** Whether to allow repeating keys. */
+  /** Whether to allow repeating keys. Only affects shortcuts. */
   allowRepeats?: boolean;
-  /** Whether to allow composing keys. */
+  /** Whether to allow composing keys. Only affects shortcuts. */
   allowComposing?: boolean;
 }
 
