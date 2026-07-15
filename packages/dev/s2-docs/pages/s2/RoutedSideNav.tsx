1073f01149474b080c506a27eff020146a3bb322
Rob Snow <rsnow@adobe.com>
2026-07-15 15:15:59 +1000

    fix docs ts

--- packages/dev/s2-docs/pages/s2/RoutedSideNav.tsx
+++ packages/dev/s2-docs/pages/s2/RoutedSideNav.tsx
@@ -3,11 +3,11 @@ import {RouterProvider} from 'react-aria-components';
 import React, {ReactNode, useState} from 'react';
 
 export function RoutedSideNav(props: {
-  children: ({selectedRoute}: {selectedRoute: string | undefined}) => ReactNode;
-  defaultSelectedRoute?: string;
+  children: ({selectedRoute}: {selectedRoute: string}) => ReactNode;
+  defaultSelectedRoute: string;
 }) {
   let {children} = props;
-  let [selectedRoute, setSelectedRoute] = useState<string | undefined>(props.defaultSelectedRoute);
+  let [selectedRoute, setSelectedRoute] = useState<string>(props.defaultSelectedRoute);
 
   let updateSelection = (href: string) => {
     setSelectedRoute(href);
