3e6ac9ce2a645ba37cf068eca560fda3ca6af739
Reid Barber <reid@reidbarber.com>
2026-07-19 12:43:56 -0500

    fix Button example

--- starters/hooks/src/Button.tsx
+++ starters/hooks/src/Button.tsx
@@ -7,6 +7,7 @@ import {useRef} from 'react';
 import './Button.css';
 
 interface ButtonProps extends AriaButtonProps {
+  slot?: string;
   /**
    * The visual style of the button (Vanilla CSS implementation specific).
    *
