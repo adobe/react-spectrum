1030e8a040d8a37cbf5680640a8de404e019c584
Devon Govett <devongovett@gmail.com>
2026-07-27 17:22:09 -0700

    chore: Export PixelLoader and add brandColor prop to PromptField (#10373)
    * export PixelLoader
    
    * Add brandColor prop to PromptField
    
    * export type

--- packages/@react-spectrum/ai/exports/index.ts
+++ packages/@react-spectrum/ai/exports/index.ts
@@ -34,6 +34,7 @@ export {
 } from '../src/Chat';
 export {TokenFieldValue} from 'react-aria-components/TokenField';
 export {UserMessage} from '../src/UserMessage';
+export {PixelLoader} from '../src/loader/react';
 
 export type {AttachmentProps, AttachmentListProps} from '../src/AttachmentList';
 export type {
@@ -67,3 +68,4 @@ export type {
 } from '../src/Chat';
 export type {TokenFieldValueOptions} from 'react-aria-components/TokenField';
 export type {UserMessageProps} from '../src/UserMessage';
+export type {PixelLoaderProps} from '../src/loader/react';
