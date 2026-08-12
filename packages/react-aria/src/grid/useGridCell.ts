801acd29be8e65c24a73f8e34ae5ade5ff4768d8
jsmitrah <muthukumar.j@mitrahsoft.com>
2026-08-12 11:28:11 +0530

    Reverted the useGrid.test.js file.

--- packages/react-aria/src/grid/useGridCell.ts
+++ packages/react-aria/src/grid/useGridCell.ts
@@ -123,7 +123,7 @@ export function useGridCell<T, C extends GridCollection<T>>(
         }
 
         let ownerDocument = getOwnerDocument(ref.current);
-        let shouldRestoreToLastFocused = !activeElement || activeElement === ownerDocument.body;
+        let shouldRestoreToLastFocused = !activeElement || activeElement === ownerDocument.body || activeElement === ref.current;
         if (shouldRestoreToLastFocused) {
           let lastChild = lastFocusedChild.current;
           if (
