dfb6942d91500678d2ac4a4d3b8c9f6c8b011fb1
Daniel Lu <dl1644@gmail.com>
2026-08-20 10:51:40 -0700

    chore: fix missing translations script

--- scripts/missingTranslations.js
+++ scripts/missingTranslations.js
@@ -1,21 +1,36 @@
-const glob = require('glob');
+const {globSync} = require('glob');
 const fs = require('fs');
 
-for (let dir of glob.sync('packages/**/intl')) {
-  let en = JSON.parse(fs.readFileSync(`${dir}/en-US.json`, 'utf8'));
+for (let dir of globSync('packages/**/intl')) {
+  let enPath = `${dir}/en-US.json`;
+  if (!fs.existsSync(enPath)) {
+    continue;
+  }
+  let en = JSON.parse(fs.readFileSync(enPath, 'utf8'));
 
-  for (let file of glob.sync('*.json', {cwd: dir})) {
+  for (let file of globSync('*.json', {cwd: dir})) {
+    if (file === 'en-US.json') {
+      continue;
+    }
     let lang = JSON.parse(fs.readFileSync(`${dir}/${file}`, 'utf8'));
+    let missing = [];
     let modified = false;
     for (let key in en) {
       if (!lang[key]) {
+        missing.push(key);
         lang[key] = en[key];
         modified = true;
       }
     }
 
     if (modified) {
+      console.log(`\n${dir}/${file} — ${missing.length} missing key(s):`);
+      for (let key of missing) {
+        console.log(`  - ${key}`);
+      }
       fs.writeFileSync(`${dir}/${file}`, JSON.stringify(lang, false, 2) + '\n');
     }
   }
 }
+
+console.log('\nDone.');
