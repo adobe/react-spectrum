e9ad30502a77cb5d528ceebeba82e50ed67b6cfd
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2026-09-03 11:12:56 -0700

    pls

--- packages/react-aria-components/src/GridList.tsx
+++ packages/react-aria-components/src/GridList.tsx
@@ -826,9 +826,8 @@ export interface GridListLoadMoreItemProps
   /**
    * The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the
    * element.
-   * 
    * @default 'react-aria-GridListLoadingIndicator'
-   * 
+   *
    */
   className?: string;
   /**
