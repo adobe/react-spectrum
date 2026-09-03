6ac460833c8caf79200c0f6ccaa25ee15f2154f8
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2026-09-03 11:12:01 -0700

    bringing default classname back

--- packages/react-aria-components/src/GridList.tsx
+++ packages/react-aria-components/src/GridList.tsx
@@ -826,6 +826,9 @@ export interface GridListLoadMoreItemProps
   /**
    * The CSS [className](https://developer.mozilla.org/en-US/docs/Web/API/Element/className) for the
    * element.
+   * 
+   *  @default 'react-aria-GridListLoadingIndicator'
+   * 
    */
   className?: string;
   /**
