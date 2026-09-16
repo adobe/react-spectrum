f10b654ab2dd553f998d02df61160ecb21ca6b8b
Daniel Lu <dl1644@gmail.com>
2026-09-16 15:54:29 -0700

    update test

--- packages/react-aria/test/overlays/ariaHideOutside.test.js
+++ packages/react-aria/test/overlays/ariaHideOutside.test.js
@@ -51,7 +51,9 @@ describe('ariaHideOutside', function () {
 
   it('should use the target document as the default root', function () {
     let iframe = document.createElement('iframe');
+    let iframeSibling = document.createElement('div');
     document.body.appendChild(iframe);
+    document.body.appendChild(iframeSibling);
     let iframeDocument = iframe.contentWindow.document;
     iframeDocument.body.innerHTML = '<div id="outside"></div><div id="target"></div>';
     let target = iframeDocument.getElementById('target');
@@ -61,11 +63,14 @@ describe('ariaHideOutside', function () {
 
     expect(outside).toHaveAttribute('aria-hidden', 'true');
     expect(target).not.toHaveAttribute('aria-hidden');
+    expect(iframe).not.toHaveAttribute('aria-hidden');
+    expect(iframeSibling).not.toHaveAttribute('aria-hidden');
     expect(document.body).not.toHaveAttribute('aria-hidden');
 
     revert();
     expect(outside).not.toHaveAttribute('aria-hidden');
     iframe.remove();
+    iframeSibling.remove();
   });
 
   it('should hide everything except multiple elements', function () {
